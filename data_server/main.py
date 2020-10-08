123a507311b9e6f8548d161ec3e1771e4a724eb3
mayaspivak <mspivak@google.com>
2020-10-08 15:32:46 -0700

    Add basic service skeleton for the data serving service. (#41)
    The service simply prints out "Running data server." on get requests, but will be used to start setting up github workflows for CI/CD.

---
+++ data_server/main.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_program_name():
+    return 'Running data server.'
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
