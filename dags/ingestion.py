ae1e81e52cd867db624a9955ab394755816b85d4
Lorenzo Boyice <lboyice@google.com>
2020-10-15 10:22:07 -0600

    Initial Airflow dag setup

---
+++ dags/ingestion.py
@@ -0,0 +1,28 @@
+"""Data ingestion DAG"""
+import requests
+from airflow.models import Variable
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'start_date': days_ago(0),
+}
+
+data_ingestion_dag = DAG(
+    'data_ingestion_dag',
+    default_args=default_args,
+    description='The data ingestion pipeline.')
+    
+
+def ingest_data_to_gcs(url: str, data: object):
+    resp = requests.post(url, data)
+    if resp.status_code != 200:
+        raise Exception('Failed response code: {}'.format(resp.status_code))
+    
+ingest_to_gcs_data_task = PythonOperator(
+    task_id='ingest_to_gcs',
+    python_callable=ingest_data_to_gcs,
+    op_kwargs={'url': Variable.get("INGEST_TO_GCS_SERVICE_ENDPOINT"), 'data':{'hello':'world'}},
+    dag=data_ingestion_dag,
+)
