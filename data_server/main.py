a9ed1900e0c70869c8fdece09498bf275bde7c0f
jenniebrown <jenniebrown@google.com>
2020-10-28 20:14:24 -0400

    Data serving (#91)
    * Add /metadata method to data server and add unit tests
    
    * Move test requirements to requirements/
    
    * Update Docker file
    
    * Update mypy config

--- data_server/main.py
+++ data_server/main.py
@@ -1,11 +1,38 @@
+import logging
 import os
-from flask import Flask
+
+from flask import Flask, Response
+from werkzeug.datastructures import Headers
+
+from data_server.dataset_cache import DatasetCache
+
 app = Flask(__name__)
+cache = DatasetCache()
 
 
 @app.route('/', methods=['GET'])
 def get_program_name():
     return 'Running data server.'
+
+
+@app.route('/metadata', methods=['GET'])
+def get_metadata():
+    """Downloads and returns metadata about available download files."""
+    try:
+        metadata = cache.getDataset(os.environ.get('GCS_BUCKET'),
+                                    os.environ.get('METADATA_FILENAME'))
+    except Exception as err:
+        logging.error(err)
+        return 'Internal server error: {}'.format(err), 500
+
+    def generate_response(data: bytes):
+        for row in data.splitlines():
+            yield row + b'\n'
+    headers = Headers()
+    headers.add('Content-Disposition', 'attachment',
+                filename=os.environ.get('METADATA_FILENAME'))
+    return Response(generate_response(metadata), mimetype='application/json',
+                    headers=headers)
 
 
 if __name__ == "__main__":
