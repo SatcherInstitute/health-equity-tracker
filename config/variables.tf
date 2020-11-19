b524d2db200419a9361bb9c6438feae3a0a8d3b6
Maya Spivak <mspivak@google.com>
2020-11-19 13:27:43 -0800

    Remove spaces at end of line

--- config/variables.tf
+++ config/variables.tf
@@ -4,9 +4,9 @@ variable "project_id" {
   type        = string
 }
 
-variable "gcp_credentials" {	
-  description = "Credentials for calling GCP services"	
-  type        = string	
+variable "gcp_credentials" {
+  description = "Credentials for calling GCP services"
+  type        = string
 }
 
 variable "compute_region" {
