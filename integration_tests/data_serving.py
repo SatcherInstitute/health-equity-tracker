f803cf6a3516376623e5eeb86b6989e504a4373e
jenniebrown <jennie.brown147@gmail.com>
2020-12-01 11:31:03 -0500

    Add integration tests and test deploy action

--- integration_tests/data_serving.py
+++ integration_tests/data_serving.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 
@@ -5,6 +6,7 @@ import requests
 def runTests():
     # Get the url of the service.
     service_url = os.environ.get('SERVICE_URL')
+    logging.error('SERVICE_URL=%s', service_url)
 
     # Get an auth token for making the request
     metadata_server_token_url = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
@@ -19,7 +21,15 @@ def runTests():
     # should be split out into their own functions.
     request_headers = {'Authorization': f'bearer {jwt}'}
     resp = requests.get(service_url, headers=request_headers)
-    assert 'Running data server.' in resp.content
+    if not resp.ok:
+        logging.error('Request failed with code %s', resp.status_code)
+        exit(1)
+    if 'Running data server.' not in resp.content:
+        logging.error('Unexpected response. Expected: "Running data server."'
+                      'Received: %s', resp.content)
+        exit(1)
+    print('Test completed with no errors')
+
 
 
 if '__name__' == 'main':
