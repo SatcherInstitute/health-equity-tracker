05c0f9b0a9788fff2a93f4175b9939d949e0af39
jenniebrown <jennie.brown147@gmail.com>
2020-12-03 20:37:19 -0500

    install dependencies

--- integration_tests/data_serving.py
+++ integration_tests/data_serving.py
@@ -6,30 +6,18 @@ import requests
 def runTests():
     # Get the url of the service.
     service_url = os.environ.get('SERVICE_URL')
-    logging.error('SERVICE_URL=%s', service_url)
+    print('SERVICE_URL={}'.format(service_url))
 
-    # Get an auth token for making the request
-    metadata_server_token_url = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_request_url = metadata_server_token_url + service_url
-    token_request_headers = {'Metadata-Flavor': 'Google'}
-
-    token_response = requests.get(token_request_url,
-                                  headers=token_request_headers)
-    jwt = token_response.content.decode("utf-8")
-
-    # Make the request. This is currently the only test. In the future, tests
-    # should be split out into their own functions.
-    request_headers = {'Authorization': f'bearer {jwt}'}
-    resp = requests.get(service_url, headers=request_headers)
+    resp = requests.get(service_url)
     if not resp.ok:
-        logging.error('Request failed with code %s', resp.status_code)
+        print('Request failed with code {}'.format(resp.status_code))
         exit(1)
-    if 'Running data server.' not in resp.content:
-        logging.error('Unexpected response. Expected: "Running data server."'
-                      'Received: %s', resp.content)
+    if b'Running data server.' not in resp.content:
+        print('Unexpected response. Expected: "Running data server."'
+                      'Received: {}'.format(resp.content))
         exit(1)
     print('Test completed with no errors')
 
 
-if '__name__' == 'main':
+if __name__ == '__main__':
     runTests()
