351a37cd144f46f59fa7e9f9a91bdd546c8881ef
Aaron <aaronsn@google.com>
2020-12-30 13:34:58 -0500

    Add Cloud Run service that serves the frontend, and add terraform config to deploy it (#174)
    Also adds basic authentication

--- config/serviceaccounts.tf
+++ config/serviceaccounts.tf
@@ -145,3 +145,13 @@ resource "google_project_iam_member" "aggregator_runner_binding" {
   role    = google_project_iam_custom_role.aggregator_runner_role.id
   member  = format("serviceAccount:%s", google_service_account.aggregator_runner_identity.email)
 }
+
+# Service account whose identity is used when running the frontend service.
+# The frontend service does not currently need a custom role because it doesn't
+# require any permissions. If this changes, we will add a new
+# google_project_iam_custom_role similar to the other services.
+resource "google_service_account" "frontend_runner_identity" {
+  # The account id that is used to generate the service account email. Must be 6-30 characters long and
+  # match the regex [a-z]([-a-z0-9]*[a-z0-9]).
+  account_id = var.frontend_runner_identity_id
+}
