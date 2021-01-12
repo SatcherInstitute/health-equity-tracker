f0c2ae79b91f756d3d8801c67c2a6c3bf10b679d
Aaron <aaronsn@google.com>
2021-01-11 21:20:39 -0500

    Generalize frontend usage of environment variables

--- frontend/src/utils/globals.ts
+++ frontend/src/utils/globals.ts
@@ -1,8 +1,14 @@
-import DataFetcher from "../data/DataFetcher";
+import {
+  ApiDataFetcher,
+  DataFetcher,
+  FakeDataFetcher,
+} from "../data/DataFetcher";
+import { createEnvironment, Environment } from "./Environment";
 import Logger from "./Logger";
 
 interface Globals {
   initialized: boolean;
+  environment: Environment;
   logger: Logger;
   dataFetcher: DataFetcher;
 }
@@ -17,10 +23,47 @@ function assertInitialized() {
   }
 }
 
-export function initGlobals(logger: Logger, dataFetcher: DataFetcher) {
+export function initGlobals(
+  environment: Environment,
+  logger: Logger,
+  dataFetcher: DataFetcher
+) {
+  if (globals.initialized) {
+    throw new Error("Cannot initialize globals multiple times");
+  }
+
+  globals.environment = environment;
   globals.logger = logger;
   globals.dataFetcher = dataFetcher;
   globals.initialized = true;
+}
+
+/**
+ * Automatically initializes globals based on environment variables. This may
+ * not work for all unit tests, for example if you want to directly test one of
+ * the globals. In such cases, use initGlobals instead.
+ */
+export function autoInitGlobals() {
+  const environment = createEnvironment();
+  const logger = new Logger(
+    environment.getEnableServerLogging(),
+    environment.getEnableConsoleLogging()
+  );
+  // Unit tests shouldn't do any real data fetches. All other deploy contexts
+  // rely on real data fetches to function properly.
+  const dataFetcher =
+    environment.deployContext === "test"
+      ? new FakeDataFetcher()
+      : new ApiDataFetcher(
+          environment.getBaseApiUrl(),
+          environment.deployContext
+        );
+  initGlobals(environment, logger, dataFetcher);
+}
+
+export function getEnvironment(): Environment {
+  assertInitialized();
+  return globals.environment;
 }
 
 export function getLogger(): Logger {
