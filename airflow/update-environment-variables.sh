6055691f06a71587592b155ebf479ea081b54ddf
Seth Van Grinsven <kalieki@google.com>
2021-01-25 17:00:51 +0000

    Renamed the file and split out the two scripts

--- airflow/update-environment-variables.sh
+++ airflow/update-environment-variables.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo Input ingestion service url?
 read ingestionUrl
 
@@ -13,8 +12,3 @@ echo Input aggregator service url?
 read aggegatorServiceUrl
 
 gcloud composer environments update data-ingestion-environment --update-env-variables=AIRFLOW_VAR_INGEST_TO_GCS_SERVICE_ENDPOINT=${ingestionUrl} --update-env-variables=AIRFLOW_VAR_GCS_TO_BQ_SERVICE_ENDPOINT=${gcsToBqServiceUrl} --update-env-variables=AIRFLOW_VAR_EXPORTER_SERVICE_ENDPOINT=${exporterServiceUrl} --update-env-variables=AIRFLOW_VAR_AGGREGATOR_SERVICE_ENDPOINT=${aggegatorServiceUrl} --update-env-variables=AIRFLOW_VAR_GCS_LANDING_BUCKET=msm-test-landing-bucket --update-env-variables=AIRFLOW_VAR_GCS_MANUAL_UPLOADS_BUCKET=msm-test-manual-data-bucket --location=us-central1
-
-for filename in dags/*; do
-  echo "Uploading Dag: ${filename}"
-    gcloud composer environments storage dags import --environment data-ingestion-environment --source $filename --location=us-central1
-done
