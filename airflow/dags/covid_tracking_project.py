b46c31beb4681ab1e60d4603f2ad82de2a67db8f
jenniebrown <jennie.brown147@gmail.com>
2021-01-26 11:12:44 -0500

    fix mypy.ini for ignoring airflow libraries

--- airflow/dags/covid_tracking_project.py
+++ airflow/dags/covid_tracking_project.py
@@ -1,7 +1,6 @@
-# Ignore the Airflow module, it is installed in both dev and prod
-from airflow import DAG  # type: ignore
+from airflow import DAG
 from airflow.models import Variable
-from airflow.utils.dates import days_ago  # type: ignore
+from airflow.utils.dates import days_ago
 
 import util
 
