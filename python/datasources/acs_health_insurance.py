3f01943c49cc5168c20e5cf835760b28f9c79bf8
Seth Van Grinsven <Savggamer@gmail.com>
2021-02-16 15:00:02 -0800

    Health Insurance to Dataframe

--- python/datasources/acs_health_insurance.py
+++ python/datasources/acs_health_insurance.py
@@ -1,3 +1,5 @@
+import pandas as pd 
+
 import requests
 import sys
 import json 
@@ -27,7 +29,8 @@ class Sex:
     FEMALE="Female"
 
 def meta(sex, min_age, max_age, hi_status = HealthInsurancePopulation.TOTAL):
-    return {"sex": sex, "age": {"min": min_age, "max": max_age}, "population": hi_status}
+    age = f'{min_age}+' if max_age is None else f'{min_age}-{max_age}'
+    return {"sex": sex, "age": age , "population": hi_status}
 
 HEALTH_INSURANCE_BY_RACE_GROUP_PREFIXES = [
     {"C27001A":{"race": Race.WHITE_ALONE}},
@@ -42,9 +45,9 @@ HEALTH_INSURANCE_BY_RACE_GROUP_PREFIXES = [
 ]
 
 HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES= {
-    "_002E":meta(None,None, 19),
-    "_003E":meta(None,None, 19,HealthInsurancePopulation.WITH),
-    "_004E":meta(None,None, 19,HealthInsurancePopulation.WITHOUT),
+    "_002E":meta(None,0, 19),
+    "_003E":meta(None,0, 19,HealthInsurancePopulation.WITH),
+    "_004E":meta(None,0, 19,HealthInsurancePopulation.WITHOUT),
     "_005E":meta(None,19, 64),
     "_006E":meta(None,19, 64,HealthInsurancePopulation.WITH),
     "_007E":meta(None,19, 64,HealthInsurancePopulation.WITHOUT),
@@ -57,9 +60,9 @@ HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX = {"B27001":{}}
 
 
 HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES = {
-    "_003E":meta(Sex.MALE,None, 6),
-    "_004E":meta(Sex.MALE,None, 6,HealthInsurancePopulation.WITH),
-    "_005E":meta(Sex.MALE,None, 6,HealthInsurancePopulation.WITHOUT),
+    "_003E":meta(Sex.MALE,0, 6),
+    "_004E":meta(Sex.MALE,0, 6,HealthInsurancePopulation.WITH),
+    "_005E":meta(Sex.MALE,0, 6,HealthInsurancePopulation.WITHOUT),
     "_006E":meta(Sex.MALE,6, 18),
     "_007E":meta(Sex.MALE,6, 18,HealthInsurancePopulation.WITH),
     "_008E":meta(Sex.MALE,6, 18,HealthInsurancePopulation.WITHOUT),
@@ -87,9 +90,9 @@ HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES = {
 }
 
 HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES = {
-    "_031E":meta(Sex.FEMALE,None, 6),
-    "_032E":meta(Sex.FEMALE,None, 6,HealthInsurancePopulation.WITH),
-    "_033E":meta(Sex.FEMALE,None, 6,HealthInsurancePopulation.WITHOUT),
+    "_031E":meta(Sex.FEMALE,0, 6),
+    "_032E":meta(Sex.FEMALE,0, 6,HealthInsurancePopulation.WITH),
+    "_033E":meta(Sex.FEMALE,0, 6,HealthInsurancePopulation.WITHOUT),
     "_034E":meta(Sex.FEMALE,6, 18),
     "_035E":meta(Sex.FEMALE,6, 18,HealthInsurancePopulation.WITH),
     "_036E":meta(Sex.FEMALE,6, 18,HealthInsurancePopulation.WITHOUT),
@@ -126,45 +129,58 @@ def format_params(prefixes, suffixes):
 def log_response(resp):
     print("-"*50)
     print(f'Get Request ({resp.status_code}): {resp.url}')
-    print("Content")
-    print(resp.content)
     print("-"*50)
 
-def getMetadata(key, prefixList, suffixList):
-    parts = key.split("_")
-    suffixKey = "_" + parts[1]
-    prefixKey = parts[0]
-    metadata = {}
-    suffixMetadata = suffixList.get(suffixKey).copy()
-    prefixMetadata = prefixList.get(prefixKey).copy()
-    print(f'Merging metadata: {prefixKey}:{prefixMetadata} with "{suffixKey}:{suffixMetadata}')
-    metadata.update(prefixMetadata)
-    metadata.update(suffixMetadata)
-    print(f'Result metadata: {metadata}')
-    return metadata
 
-
-    
-
-
-
-class AcsHealthInsuranceStateIngestor:
+class AcsHealhInsuranceIngestor:
 
     def __init__(self, base):
         self.baseUrl = base
-        self.stateDataBySex = {}
-        self.stateDataByRace = {}
+        self.data = {}
+
+        self.metadata = {}
+        self.buildMetadataList()
 
         self.getStateFipsMap()
+        self.getCountyFipsMap()
         self.getHealthInsuranceDataBySex()
         self.getHealthInsuranceDataByRace()
+        self.splitDataFrames()
 
     def write_local_files_debug(self):
-        with open('health_insurance_by_sex_state.json', 'w') as f:
-            print(json.dumps(self.stateDataBySex, indent = 4), file=f)
+        with open('total_health_insurance.json', 'w') as f:
+            print(json.dumps({str(k):v for k, v in self.data.items()}, indent = 4), file=f)
 
-        with open('health_insurance_by_race_state.json', 'w') as f:
-            print(json.dumps(self.stateDataByRace, indent = 4), file=f)
+        self.stateRaceFrame.to_csv('table_health_insurance_by_race_state.csv')
+        self.stateSexFrame.to_csv('table_health_insurance_by_sex_state.csv')
+        self.countyRaceFrame.to_csv('table_health_insurance_by_race_county.csv')
+        self.countySexFrame.to_csv('table_health_insurance_by_sex_county.csv')
+
+    def buildMetadataList(self):
+        for racePrefix in HEALTH_INSURANCE_BY_RACE_GROUP_PREFIXES:
+            for prefix in racePrefix:
+                for suffix in HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES:
+                    key = prefix+suffix
+                    print(f'Building merged metadata for {key}: {racePrefix[prefix]} + {HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES[suffix]}')
+                    prefixMeta = racePrefix[prefix].copy()
+                    prefixMeta.update(HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES[suffix])
+                    self.metadata[key] = prefixMeta
+
+        for sex_prefix in HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX:
+            for male_suffix in HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES:
+                key = sex_prefix + male_suffix
+                print(f'Building merged metadata for {key}: {HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX[sex_prefix]} + {HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES[male_suffix]}')
+                meta = HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX[sex_prefix].copy();
+                meta.update(HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES[male_suffix])
+                self.metadata[key] = meta
+
+            for female_suffix in HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES:
+                key = sex_prefix + female_suffix
+                print(f'Building merged metadata for {key}: {HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX[sex_prefix]} + {HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES[female_suffix]}')
+                meta = HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX[sex_prefix].copy();
+                meta.update(HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES[female_suffix])
+                self.metadata[key] = meta
+    
 
     def getStateFipsMap(self):
         params = {'for':'state',"get":"NAME"}
@@ -176,70 +192,124 @@ class AcsHealthInsuranceStateIngestor:
             fipCode = row[1]
             stateName = row[0]
             stateFips[fipCode] = stateName
-            self.stateDataByRace[fipCode] = {}
-            self.stateDataBySex[fipCode] = {}
 
         self.stateFips = stateFips
 
+    def getCountyFipsMap(self):
+        params = {'for':'county',"get":"NAME"}
+        resp = requests.get(self.baseUrl, params=params)
+        log_response(resp)
+        jResp = resp.json()
+        countyFips = {}
+        for row in jResp[1::]:
+            countyFip = row[2]
+            stateFip = row[1]
+            countyName = row[0]
+            countyFips[(stateFip, countyFip)] = countyName
+
+        self.countyFips = countyFips
     
-    def getAcsDataFromVariables(self, params):
-        params = {'for':'state',"get": params}
+    def getAcsDataFromVariables(self, params, isCounty=False):
+        params = {'for':'county' if isCounty else 'state',"get": params}
         resp = requests.get(self.baseUrl, params=params)
         log_response(resp)
         return resp.json()
 
     def getHealthInsuranceDataByRace(self):
         for prefix in HEALTH_INSURANCE_BY_RACE_GROUP_PREFIXES:
-            data = self.getAcsDataFromVariables(format_params(prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES))
-            self.accumulateAcsData(data, prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES, self.stateDataByRace)
+            stateData = self.getAcsDataFromVariables(format_params(prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES))
+            self.accumulateAcsData(stateData, prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES)
+            countySexData = self.getAcsDataFromVariables(format_params(prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES), True)
+            self.accumulateAcsData(countySexData, prefix, HEALTH_INSURANCE_BY_RACE_GROUP_SUFFIXES)
  
 
     def getHealthInsuranceDataBySex(self):
-        maleData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES))
-        femaleData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES))
+        maleStateData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES))
+        femaleStateData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES))
 
-        self.accumulateAcsData(maleData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES, self.stateDataBySex)
-        self.accumulateAcsData(femaleData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES, self.stateDataBySex)
+        self.accumulateAcsData(maleStateData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES)
+        self.accumulateAcsData(femaleStateData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES)
 
-    def accumulateAcsData(self, data, prefixes, suffixes, target):
+        maleCountyData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES), True)
+        femaleCountyData = self.getAcsDataFromVariables(format_params(HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES), True)
+
+        self.accumulateAcsData(maleCountyData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_MALE_SUFFIXES)
+        self.accumulateAcsData(femaleCountyData, HEALTH_INSURANCE_BY_SEX_GROUPS_PREFIX, HEALTH_INSURANCE_BY_SEX_FEMALE_SUFFIXES)
+
+    def accumulateAcsData(self, data, prefixes, suffixes):
         keyRow = data[0]
         for row in data[1::]:
             colIndex = 0
             data = {}
             for key in keyRow:
-                if(key != 'state'):
+                if(key != 'state' and key != 'county'):
                     data[key] = {}
-            stateFip = ''
+            stateFip = None
+            countyFip = None
             for col in row:
                 key = keyRow[colIndex]
                 if(key == 'state'):
                     stateFip = col; 
+                elif key == 'county':
+                    countyFip = col
                 else:
-                    data[key]['key'] = key
-                    data[key]['value'] = col
-                    data[key]['metadata'] = getMetadata(key, prefixes, suffixes)
+                    data[key] = {'value': col, 'meta': self.metadata[key]}
+
                 colIndex += 1
-            target[stateFip].update(data)
+            print("-"*50)
+            print(f'Analyzing Data [State: {stateFip} County: {countyFip}]')
+            for var in data:
+                # print(f'Key: {var}, Data: {data[var]}')
+                metadata = data[var]['meta']
+                value = data[var]['value']
+                row = self.upsertRow(stateFip, countyFip, metadata.get("age"), metadata.get("sex"), metadata.get("race"))
+                row[metadata['population']] = value
+            
 
-    
+    def upsertRow(self, stateFip, countyFip, age, sex, race):
+        if self.data.get((stateFip, countyFip, age, sex, race)) is None:
+            self.data[(stateFip, countyFip, age, sex, race)] = {HealthInsurancePopulation.TOTAL : -1, HealthInsurancePopulation.WITH: -1, HealthInsurancePopulation.WITHOUT: -1}
+        return self.data[(stateFip, countyFip, age, sex, race)]
+     
+    def splitDataFrames(self):
+        stateSexData = []
+        stateRaceData = []
+        countyRaceData = []
+        countySexData = []
 
-class AcsCountInsuranceCountyIngestor:
+        for data in self.data:
+            stateFip = data[0]
+            countyFip = data[1]
+            age = data[2]
+            sex = data[3]
+            race = data[4]
+            population = self.data[data]
+            whi = population[HealthInsurancePopulation.WITH]
+            wohi = population[HealthInsurancePopulation.WITHOUT]
+            total = population[HealthInsurancePopulation.TOTAL]
+            if countyFip is None:
+                #State-Sex
+                if race is None:
+                    stateSexData.append([stateFip, self.stateFips[stateFip], age, sex, whi, wohi, total])
+                #State-Race
+                else: 
+                    stateRaceData.append([stateFip, self.stateFips[stateFip], age, race, whi, wohi, total])
+            else:
 
-    def __init__(self, baseUrl):
-        self.baseUrl = baseUrl
+                #County-Sex
+                if race is None:
+                    countySexData.append([stateFip, self.stateFips[stateFip], countyFip, self.countyFips[(stateFip, countyFip)], age, sex, whi, wohi, total])
 
-    def write_local_files_debug(self):
-        pass
+                #County-Race
+                else:
+                    countyRaceData.append([stateFip, self.stateFips[stateFip], countyFip, self.countyFips[(stateFip, countyFip)], age, race, whi, wohi, total])
+
+        self.stateSexFrame = pd.DataFrame(stateSexData, columns = ['state_fips', 'state_name', 'age', 'sex', 'with_health_insurance', 'without_health_insurance', 'total'])
+        self.stateRaceFrame = pd.DataFrame(stateRaceData, columns = ['state_fips', 'state_name', 'age', 'race', 'with_health_insurance', 'without_health_insurance', 'total'])
+        self.countySexFrame = pd.DataFrame(countySexData, columns = ['state_fips', 'state_name', 'county_fips', 'county_names','age', 'sex', 'with_health_insurance', 'without_health_insurance', 'total'])
+        self.countyRaceFrame = pd.DataFrame(countyRaceData, columns = ['state_fips', 'state_name', 'county_fips', 'county_names','age', 'race', 'with_health_insurance', 'without_health_insurance', 'total'])
 
 
-#class ACSHealthInsurance(DataSource):
-
-    # @staticmethod 
-    # def get_table_name():
-    #     # Writes multiple tables, so this is not applicable.
-    #     pass
-
-    # @staticmethod
     # def get_id():
     #     """Returns the data source's unique id. """
     #     return 'ACS_HEALTH_INSURANCE'
@@ -257,10 +327,7 @@ class AcsCountInsuranceCountyIngestor:
 
     # def _create_ingesters(self):
     #     return [
-    #         AcsHealthInsuranceStateIngestor(False, BASE_ACS_URL),
-    #         AcsCountInsuranceCountyIngestor(True, BASE_ACS_URL)
+    #         AcsHealhInsuranceIngestor(BASE_ACS_URL),
     #     ]
 
-
-AcsHealthInsuranceStateIngestor(BASE_ACS_URL).write_local_files_debug()
-#AcsCountInsuranceCountyIngestor(BASE_ACS_URL).write_local_files_debug()
+AcsHealhInsuranceIngestor(BASE_ACS_URL).write_local_files_debug()
