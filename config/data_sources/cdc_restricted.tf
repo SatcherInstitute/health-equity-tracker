7c697c1d726554ec1b862a7248bec3bc0c13c771
Vansh Kumar <vanshkumar@google.com>
2021-03-10 14:54:58 -0800

    add cdc_restricted dataset to TF, update script for new hosp_yn value in latest data

---
+++ config/data_sources/cdc_restricted.tf
@@ -0,0 +1,15 @@
+# Resources and routines for CDC restricted data ingestion.
+
+# Create a BigQuery dataset for CDC restricted data.
+resource "google_bigquery_dataset" "bq_cdc_restricted" {
+  dataset_id = "cdc_restricted_data"
+  location   = "US"
+}
+
+#resource "google_bigquery_routine" "bq_agg_acs_population" {
+#  dataset_id = google_bigquery_dataset.bq_cdc_restricted.dataset_id
+#  routine_id = "AGG_acs_population"
+#  routine_type = "PROCEDURE"
+#  language = "SQL"
+#  definition_body = file("${path.module}/AGG_acs_population.sql")
+#}
