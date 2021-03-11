e82a36a849bf7bc561a3bcbc7d6e68e99b9197d6
Elizabeth <efregoso@google.com>
2021-03-11 13:28:19 -0800

    Redesign About Us > Contact Us page according to new mocks (#427)
    * Redesign About Us > Our Team Tab according to new mocks
    
    * Redesign About Us > The Project tab according to new mocks
    
    * Redesign About Us > Contact Us page according to the new mocks

--- frontend/src/pages/AboutUs/ContactUsTab.tsx
+++ frontend/src/pages/AboutUs/ContactUsTab.tsx
@@ -1,48 +1,61 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import styles from "./AboutUsPage.module.scss";
+import Typography from '@material-ui/core/Typography';
 
 /* TODO - Align with mocks, Clean up CSS */
 function ContactUsTab() {
   return (
-    <>
-      <Grid item className={styles.GreyGridItem}>
-        <div
-          style={{
-            width: "600px",
-            margin: "auto",
-            fontSize: "40px",
-            textAlign: "center",
-          }}
-        >
-          Thank you for interest in the Health Equity Tracker
-        </div>
-      </Grid>
-      <Grid
-        container
-        justify="space-around"
-        className={styles.Grid}
-        style={{ textAlign: "left" }}
-      >
-        <p>
-          <b>For general requests, contact:</b> info@healthequitytracker.org
-        </p>
-        <p>
-          <b>All media and interview requests</b>, please contact Mahia Valle,
-          at Morehouse School of Medicine: mvalle@msm.edu
-        </p>
-        <p>
-          <b>Mailing Address:</b>
-          <br />
-          Satcher Health Leadership Institute
-          <br />
-          720 Westview Drive, SW
-          <br />
-          Atlanta, GA 30310
-        </p>
-        <p>(404) 752-8654</p>
-      </Grid>
-    </>
+      <>
+        <Grid container className={styles.Grid}>
+          <Grid container className={styles.GridOutlinedImgRow}
+                direction="row"
+                justify="center"
+                alignItems="center">
+            <Grid item xs={4} className={styles.GridVerticallyAlignedItem}>
+              <Typography className={styles.ContactUsHeaderText}>
+                Let's move
+                <br/>
+                equity <b>forward</b>
+              </Typography>
+            </Grid>
+            <Grid item xs={8} className={styles.GridAlignRightItem}>
+              <img src="img/pexels-mentatdgt-1206059 2.png"
+                   className={styles.ImgContactUsHeader}
+                   alt="A woman laying with her two children"/>
+            </Grid>
+          </Grid>
+
+          <Grid container className={styles.GridOutlinedRow}
+                direction="column"
+                justify="center"
+                alignItems="center">
+            <Grid item xs={8}>
+              <Typography className={styles.ContactUsSubheaderText}>
+                Thank you for your interest in the Health Equity Tracker
+              </Typography>
+            </Grid>
+            <Grid item xs={5} className={styles.GridCenteredTextItem}>
+              <p>
+                <b>For general requests:</b> please contact the Satcher Health
+                Leadership Institute at shli@msm.edu
+              </p>
+              <p>
+                <b>Mailing Address:</b>
+                <br/>
+                Satcher Health Leadership Institute
+                <br/>
+                720 Westview Drive, SW
+                <br/>
+                Atlanta, GA 30310
+                <br/>
+                <br/>
+                (404) 752-8654
+              </p>
+            </Grid>
+          </Grid>
+        </Grid>
+      </>
   );
 }
 
