345f68746008d489c5c7e8a3e68baf5eba1a9b2d
Vansh Kumar <vanshkumar@google.com>
2021-03-12 10:56:54 -0800

    revert staging change part 2

--- config/data_sources/AGG_cdc_restricted.sql
+++ config/data_sources/AGG_cdc_restricted.sql
@@ -22,7 +22,7 @@ SELECT
     x.cases, x.hosp_y, x.hosp_n, x.hosp_unknown, x.death_y, x.death_n, x.death_unknown,
     y.population
 FROM cdc_restricted_race_state AS x
-LEFT JOIN `acs_population.by_race_state_std_staging` AS y ON
+LEFT JOIN `acs_population.by_race_state_std` AS y ON
   x.state_fips = y.state_fips AND
   x.race_and_ethnicity = y.race_and_ethnicity
 ;
@@ -88,13 +88,13 @@ WITH cdc_restricted_race_county AS (
     FROM `cdc_restricted_data.cdc_restricted_by_race_county` AS a
     LEFT JOIN `bigquery-public-data.census_utility.fips_codes_states` AS b
         ON a.state_name = b.state_postal_abbreviation
-    LEFT JOIN `acs_population.by_race_county_std_staging` as c
+    LEFT JOIN `acs_population.by_race_county_std` as c
         ON a.county_fips = c.county_fips
 )
 SELECT
     x.*, y.population
 FROM cdc_restricted_race_county AS x
-LEFT JOIN `acs_population.by_race_county_std_staging` AS y ON
+LEFT JOIN `acs_population.by_race_county_std` AS y ON
     x.county_fips = y.county_fips AND
     x.state_fips = y.state_fips AND
     x.race_and_ethnicity = y.race_and_ethnicity
