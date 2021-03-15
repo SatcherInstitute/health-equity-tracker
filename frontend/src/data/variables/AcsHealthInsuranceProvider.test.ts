924b1c95277507a3c77c3bb21691f61af17728c4
Seth Van Grinsven <Savggamer@gmail.com>
2021-03-15 08:21:08 -0700

    Fixed PR comments :)

--- frontend/src/data/variables/AcsHealthInsuranceProvider.test.ts
+++ frontend/src/data/variables/AcsHealthInsuranceProvider.test.ts
@@ -56,8 +56,8 @@ function stateRow(
   };
 }
 
-const evaluateHealthInsurancWithAndWithoutTotal = createWithAndWithoutTotalEvaluator(
-  /*metricIds=*/ ["with_health_insurance", "health_insurance_per_100k"],
+const evaluateHealthInsuranceWithAndWithoutTotal = createWithAndWithoutTotalEvaluator(
+  /*metricIds=*/ ["health_insurance_count", "health_insurance_per_100k"],
   dataFetcher,
   new AcsHealthInsuranceProvider()
 );
@@ -70,19 +70,20 @@ describe("AcsHealthInsuranceProvider", () => {
   });
 
   test("State and Race Breakdown", async () => {
-    // Create raw rows with copd_count, copd_no, diabetes_count & diabetes_no
+    // Create raw rows with health insurance coverage
     const rawData = [
       stateRow(AL, "race", ASIAN_NH, "100", "900", "1000"),
       stateRow(NC, "race", ASIAN_NH, "100", "900", "1000"),
       stateRow(NC, "race", WHITE_NH, "250", "250", "500"),
     ];
 
-    // Create final rows with diabetes_count & diabetes_per_100k
+    // Create final rows with health insurance count
+    // and health insurance per 100k
     const NC_ASIAN_FINAL = finalRow(NC, RACE, ASIAN_NH, 100, 10000);
     const NC_WHITE_FINAL = finalRow(NC, RACE, WHITE_NH, 250, 50000);
     const NC_TOTAL_FINAL = finalRow(NC, RACE, TOTAL, 350, 23333);
 
-    await evaluateHealthInsurancWithAndWithoutTotal(
+    await evaluateHealthInsuranceWithAndWithoutTotal(
       "acs_health_insurance-health_insurance_by_race_state",
       rawData,
       Breakdowns.forFips(new Fips("37")),
@@ -93,19 +94,20 @@ describe("AcsHealthInsuranceProvider", () => {
   });
 
   test("National and Race Breakdown", async () => {
-    // Create raw rows with copd_count, copd_no, diabetes_count & diabetes_no
+    // Create raw rows with health insurance coverage
     const rawData = [
       stateRow(AL, "race", ASIAN_NH, "100", "900", "1000"),
       stateRow(NC, "race", ASIAN_NH, "100", "900", "1000"),
       stateRow(NC, "race", WHITE_NH, "250", "250", "500"),
     ];
 
-    // Create final rows with diabetes_count & diabetes_per_100k
+    // Create final rows with health insurance count
+    // and health insurance per 100k
     const NC_ASIAN_FINAL = finalRow(USA, RACE, ASIAN_NH, 200, 10000);
     const NC_WHITE_FINAL = finalRow(USA, RACE, WHITE_NH, 250, 50000);
     const NC_TOTAL_FINAL = finalRow(USA, RACE, TOTAL, 450, 18000);
 
-    await evaluateHealthInsurancWithAndWithoutTotal(
+    await evaluateHealthInsuranceWithAndWithoutTotal(
       "acs_health_insurance-health_insurance_by_race_state",
       rawData,
       Breakdowns.forFips(new Fips(USA.code)),
