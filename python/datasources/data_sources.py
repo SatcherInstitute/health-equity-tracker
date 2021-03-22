32cc1ed3ad8d368b21f44f0d771621852c56593f
Vansh Kumar <vanshkumar@google.com>
2021-03-22 16:51:12 -0700

    fix typo

--- python/datasources/data_sources.py
+++ python/datasources/data_sources.py
@@ -11,7 +11,7 @@ from datasources.primary_care_access import PrimaryCareAccess
 from datasources.state_names import StateNames
 from datasources.urgent_care_facilities import UrgentCareFacilities
 from datasources.acs_health_insurance import ACSHealthInsurance
-from datasource.acs_household_income import ACSIncomeDatasource
+from datasources.acs_household_income import ACSIncomeDatasource
 
 # Map of data source ID to the class that implements the ingestion methods for
 # that data source.
