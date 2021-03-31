14212655be0a688d17431a8595c8331aef9f52e8
Vansh Kumar <vanshkumar@google.com>
2021-03-30 20:36:15 -0700

    fix name of health ins dataset

--- config/data_sources/acs_health_insurance.tf
+++ config/data_sources/acs_health_insurance.tf
@@ -1,5 +1,5 @@
 # Create a BigQuery dataset for acs health insurance information
-resource "google_bigquery_dataset" "health_insurance_dataset" {
-  dataset_id = "health_insurance_dataset"
+resource "google_bigquery_dataset" "acs_health_insurance" {
+  dataset_id = "acs_health_insurance"
   location   = "US"
 }
