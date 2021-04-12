5265caabb81c92799df50faf7b89d5292f8ca7b7
Elizabeth Fregoso <efregoso@google.com>
2021-04-12 15:54:49 -0700

    Move "What is Health Equity" page to pole position in nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,10 +44,10 @@ const MOBILE_BREAKPOINT = 600;
 
 const PAGE_URL_TO_NAMES: Record<string, string> = {
   "/": "Homepage",
-  [ABOUT_US_PAGE_LINK]: "About Us",
-  [DATA_CATALOG_PAGE_LINK]: "Downloads & Methodology",
-  [EXPLORE_DATA_PAGE_LINK]: "Explore the Data",
   [WHAT_IS_HEALTH_EQUITY_PAGE_LINK]: "What is Health Equity?",
+  [EXPLORE_DATA_PAGE_LINK]: "Explore the Data",
+  [DATA_CATALOG_PAGE_LINK]: "Downloads & Methodology",
+  [ABOUT_US_PAGE_LINK]: "About Us",
 };
 
 autoInitGlobals();
@@ -94,9 +94,9 @@ function AppToolbar() {
         </LinkWithStickyParams>
       </Typography>
       {[
+        WHAT_IS_HEALTH_EQUITY_PAGE_LINK,
         EXPLORE_DATA_PAGE_LINK,
         DATA_CATALOG_PAGE_LINK,
-        WHAT_IS_HEALTH_EQUITY_PAGE_LINK,
         ABOUT_US_PAGE_LINK,
       ].map((pageUrl, i) => (
         <LinkWithStickyParams to={pageUrl} class={styles.NavLink}>
