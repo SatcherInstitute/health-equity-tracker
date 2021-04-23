3c5f52a81590e5262f4eca75147e09ba2d76e778
Krista <kristak@google.com>
2021-04-22 17:13:33 -0700

    Moving small multiples button, updating map filter text (#646)
    * updates
    
    * Merge remote-tracking branch 'upstream/master' into issue640
    
    * no caps
    
    * update alert
    
    * update copy

--- frontend/src/cards/MapCard.tsx
+++ frontend/src/cards/MapCard.tsx
@@ -24,7 +24,10 @@ import {
   UNKNOWN_RACE,
   ALL,
 } from "../data/utils/Constants";
-import { BREAKDOWN_VAR_DISPLAY_NAMES } from "../data/query/Breakdowns";
+import {
+  BREAKDOWN_VAR_DISPLAY_NAMES,
+  BREAKDOWN_VAR_DISPLAY_NAMES_LOWER_CASE,
+} from "../data/query/Breakdowns";
 
 const POSSIBLE_BREAKDOWNS: BreakdownVar[] = [
   "race_and_ethnicity",
@@ -195,15 +198,6 @@ function MapCardWithKey(props: MapCardProps) {
                         }}
                       />
                     </Grid>
-                    <Grid item>
-                      <Button
-                        onClick={() => setSmallMultiplesDialogOpen(true)}
-                        color="primary"
-                      >
-                        Show full breakdown by{" "}
-                        {BREAKDOWN_VAR_DISPLAY_NAMES[activeBreakdownVar]}
-                      </Button>
-                    </Grid>
                   </Grid>
                 </CardContent>
               </>
@@ -232,8 +226,19 @@ function MapCardWithKey(props: MapCardProps) {
               props.metricConfig && (
                 <CardContent>
                   <Alert severity="info">
-                    Note that legend changes between races. To see races with
-                    common legend, use show all breakdowns button.
+                    <Button
+                      onClick={() => setSmallMultiplesDialogOpen(true)}
+                      color="primary"
+                      className={styles.SmallMarginButton}
+                    >
+                      Click to view a breakdown of all{" "}
+                      {
+                        BREAKDOWN_VAR_DISPLAY_NAMES_LOWER_CASE[
+                          activeBreakdownVar
+                        ]
+                      }{" "}
+                      groups
+                    </Button>
                   </Alert>
                 </CardContent>
               )}
