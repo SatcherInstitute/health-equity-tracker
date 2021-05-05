7cc35ce7ff8fc27a44b714ced7d48cc03746c81c
adams314 <80131692+adams314@users.noreply.github.com>
2021-05-05 15:14:15 -0400

    Update EquityTab.tsx

--- frontend/src/pages/WhatIsHealthEquity/EquityTab.tsx
+++ frontend/src/pages/WhatIsHealthEquity/EquityTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./WhatIsHealthEquityPage.module.scss";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
