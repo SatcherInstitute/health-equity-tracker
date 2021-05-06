cb4c796217e006935517d97798ae8549ee0deb1c
Seth Van Grinsven <Savggamer@gmail.com>
2021-05-06 16:38:08 -0700

    age_sort

--- frontend/src/charts/TableChart.tsx
+++ frontend/src/charts/TableChart.tsx
@@ -52,6 +52,28 @@ export function TableChart(props: TableChartProps) {
     },
   ].concat(columns);
 
+  if (breakdownVar === "age") {
+    data.sort((l, r) => {
+      let lAge = l["age"];
+      let rAge = r["age"]; //Rage hehe
+
+      if (lAge === "All" && rAge === "All") return 0;
+      else if (lAge === "All") return -1;
+      else if (rAge === "All") return 1;
+
+      let leftUnbounded = lAge.indexOf("+") !== -1;
+      let rightUnbounded = lAge.indexOf("+") !== -1;
+
+      if (leftUnbounded && rightUnbounded) return 0;
+      else if (leftUnbounded) return 1;
+      else if (rightUnbounded) return -1;
+
+      let lMin = lAge.split("-")[0];
+      let rMin = rAge.split("-")[1];
+      return Number(lMin) - Number(rMin);
+    });
+  }
+
   // Changes deps array to columns on save, which triggers reload loop
   // eslint-disable-next-line
   const memoCols = useMemo<Column<any>[]>(() => columns, [metrics]);
