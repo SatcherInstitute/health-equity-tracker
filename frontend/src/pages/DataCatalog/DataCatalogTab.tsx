3c5b25b937c525b4605554766be90ecbba1ae791
Seth Van Grinsven <Savggamer@gmail.com>
2021-05-09 16:58:23 -0700

    Removed unused effectg=

--- frontend/src/pages/DataCatalog/DataCatalogTab.tsx
+++ frontend/src/pages/DataCatalog/DataCatalogTab.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import DatasetExplorer from "./dataset_explorer/DatasetExplorer";
