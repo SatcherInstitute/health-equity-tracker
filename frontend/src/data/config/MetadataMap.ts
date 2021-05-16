91aaa41a8caecc125e706e62ea37239727754fdd
Vansh Kumar <vanshkumar@google.com>
2021-05-15 19:56:24 -0700

    Update CTP description to include 'tests'

--- frontend/src/data/config/MetadataMap.ts
+++ frontend/src/data/config/MetadataMap.ts
@@ -74,7 +74,7 @@ const dataSourceMetadataList: DataSourceMetadata[] = [
     demographic_granularity: "Race/ethnicity",
     update_frequency: "Final update was March 7 2021",
     description:
-      "The numbers of confirmed Covid deaths, cases, and hospitalizations at the state level.",
+      "The numbers of confirmed Covid deaths, cases, hospitalizations, and tests at the state level.",
     dataset_ids: [
       "covid_tracking_project-covid_tracking_project_cases",
       "covid_tracking_project-covid_tracking_project_deaths",
