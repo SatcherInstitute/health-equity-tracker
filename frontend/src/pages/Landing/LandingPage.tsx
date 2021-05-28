a09098d7f7be1c37c0b7b3605130c503dccbe23b
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-05-28 15:55:57 -0400

    fixes typo on landing page

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -226,7 +226,7 @@ function LandingPage() {
                 variant="body1"
               >
                 <p>
-                  We’re living through a historical moment. COVID-19 has taken a
+                  We’re living through a historic moment. COVID-19 has taken a
                   toll on everyone. But the pandemic is hitting the most
                   marginalized, vulnerable communities the hardest.
                 </p>
