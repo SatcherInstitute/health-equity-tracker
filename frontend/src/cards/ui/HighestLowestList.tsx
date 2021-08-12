dba6a1282adeaecb63b760d562306664978df04f
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-08-12 13:50:24 -0600

    no unused vars

--- frontend/src/cards/ui/HighestLowestList.tsx
+++ frontend/src/cards/ui/HighestLowestList.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./HighestLowestList.module.scss";
 import AnimateHeight from "react-animate-height";
 import { Grid } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
 import ArrowDropUp from "@material-ui/icons/ArrowDropUp";
 import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
 import { IconButton } from "@material-ui/core";
