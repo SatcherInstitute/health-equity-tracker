1f4bb5f42adccd7bc0a4658354a2d3e07860565c
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-09-10 17:35:45 -0400

    remove merged pop data file

--- config/data_sources/merged_population_data.tf
+++
@@ -1,7 +0,0 @@
-# Resources and routines for a merged population table
-
-# Create a BigQuery dataset for ACS population data.
-resource "google_bigquery_dataset" "merged_population_data" {
-  dataset_id = "merged_population_data"
-  location   = "US"
-}
