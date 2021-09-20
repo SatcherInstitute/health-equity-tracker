9f11379ae5a055c62e31a1676feec0ee42043873
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-09-20 11:08:20 -0400

    get state vaccination population comparison metrics from kff info

--- python/ingestion/github_util.py
+++ python/ingestion/github_util.py
@@ -10,10 +10,20 @@ import pandas
 
 
 def decode_json_from_url_into_df(url):
-    """Loads a file from the github api into a dataframe
+    """Loads a json file from the github api into a dataframe
 
     url: url to a base64 encoded github file"""
     r = requests.get(url)
     jsn = json.loads(r.text)
     decoded = base64.b64decode(jsn['content'])
     return pandas.read_csv(BytesIO(decoded))
+
+
+def decode_excel_from_url_into_df(url):
+    """Loads an excel file from the github api into a dataframe
+
+    url: url to a base64 encoded github file"""
+    r = requests.get(url)
+    jsn = json.loads(r.text)
+    decoded = base64.b64decode(jsn['content'])
+    return pandas.read_excel(BytesIO(decoded))
