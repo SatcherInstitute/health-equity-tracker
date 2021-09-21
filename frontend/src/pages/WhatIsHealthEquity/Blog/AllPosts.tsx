70f2cb44037a2a333bcc788ad4e50a821df7eb3b
Ben Hammond <benjamin.hammond@gmail.com>
2021-09-21 15:50:01 -0600

    adds left gutter with initial fetching of categories

--- frontend/src/pages/WhatIsHealthEquity/Blog/AllPosts.tsx
+++ frontend/src/pages/WhatIsHealthEquity/Blog/AllPosts.tsx
@@ -6,66 +6,65 @@ import parse from "html-react-parser";
 import { BLOG_TAB_LINK } from "../../../utils/urlutils";
 import { Helmet } from "react-helmet";
 import AppbarLogo from "../../../assets/AppbarLogo.png";
+import BlogCategories from "../../ui/BlogCategories";
 
 function AllPosts({ articles }: { articles: any[] }) {
   return (
-    <Grid
-      container
-      className={styles.NewsAndStoriesRow}
-      direction="row"
-      justify="center"
-    >
+    <Grid container className={styles.Grid}>
       <Helmet>
         <title>Blog - Health Equity Tracker</title>
       </Helmet>
-      <Grid item>
-        <Typography className={styles.NewsAndStoriesHeaderText} variant="h1">
-          News and stories
-        </Typography>
-        <span className={styles.NewsAndStoriesSubheaderText}>
-          Read the latest news, posts, and stories related to health equity
-        </span>
-      </Grid>
-      <Grid
-        container
-        direction="row"
-        justify="space-between"
-        alignItems="flex-start"
-      >
-        {articles.map((post: any) => {
-          console.log(post, "cant find img here?");
-          return (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={4}
-              className={styles.NewsAndStoriesItem}
-              key={post.id}
-            >
-              <img
-                className={styles.AllPostsBigImg}
-                src={
-                  post._embedded["wp:featuredmedia"]
-                    ? post._embedded["wp:featuredmedia"][0].source_url
-                    : AppbarLogo
-                }
-                alt=""
-              />
-              <Link
-                to={`${BLOG_TAB_LINK}/${post.slug}`}
-                className={styles.NewsAndStoriesTitleLink}
-              >
-                <Typography
-                  className={styles.NewsAndStoriesTitleText}
-                  variant="h3"
+      <Grid container className={styles.AllArticlesSection}>
+        <Grid item xs={12} sm={12} md={3}>
+          <Typography
+            id="main"
+            tabIndex={-1}
+            className={styles.AllArticlesHeaderText}
+            variant="h2"
+          >
+            Recent Posts
+          </Typography>
+          <BlogCategories />
+        </Grid>
+        <Grid item xs={12} sm={12} md={9}>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="flex-start"
+          >
+            {articles.map((post: any) => {
+              return (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  className={styles.AllArticlesItem}
+                  key={post.id}
                 >
-                  {parse(post.title.rendered)}
-                </Typography>
-              </Link>
-            </Grid>
-          );
-        })}
+                  <img
+                    className={styles.AllPostsBigImg}
+                    src={
+                      post._embedded["wp:featuredmedia"]
+                        ? post._embedded["wp:featuredmedia"][0].source_url
+                        : AppbarLogo
+                    }
+                    alt=""
+                  />
+                  <Link
+                    to={`${BLOG_TAB_LINK}/${post.slug}`}
+                    className={styles.AllArticlesTitleLink}
+                  >
+                    <h3 className={styles.AllArticlesTitleText}>
+                      {parse(post.title.rendered)}
+                    </h3>
+                  </Link>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
