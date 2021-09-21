9c347e6b8fd0c5eb57660984307d65bfacde075b
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-09-21 10:07:32 -0400

    adds missing population note for vaccines

--- frontend/src/reports/ReportProvider.tsx
+++ frontend/src/reports/ReportProvider.tsx
@@ -202,6 +202,16 @@ function ReportProvider(props: { madLib: MadLib; setMadLib: Function }) {
           from the 2010 census, so we use those numbers when calculating the per
           100k COVID-19 rates nationally and for all territory level rates.
         </p>
+        <p>
+          State reported population categories do not always coincide with the
+          categories reported by the census. So, for state reported vaccine
+          numbers we rely on the Kaiser Family Foundation population
+          tabulations, which only include population numbers for <b>Black</b>,{" "}
+          <b>White</b>, <b>Asian</b>, and <b>Hispanic</b>. Percent of vacinated
+          metrics for <b>Native Hawaiian and Pacific Islander</b>,{" "}
+          <b>American Indian and Alaska Native</b>, and <b>Some Other Race</b>{" "}
+          are still shown but without a population compairson metric.
+        </p>
         <div className={styles.MissingDataContactUs}>
           <p>
             Do you have information on health outcomes at the state and local
