a464ef8492404f0d518b6367a485762cbf184cef
Ben Hammond <benjamin.hammond@gmail.com>
2021-10-05 08:10:39 -0600

    fix when alerts appear

--- frontend/src/cards/MapCard.tsx
+++ frontend/src/cards/MapCard.tsx
@@ -273,8 +273,8 @@ function MapCardWithKey(props: MapCardProps) {
                   </CardContent>
                 </>
               )}
-
-            {mapQueryResponse.dataIsMissing() && (
+            {(mapQueryResponse.dataIsMissing() ||
+              dataForActiveBreakdownFilter.length === 0) && (
               <CardContent>
                 <MissingDataAlert
                   dataName={metricConfig.fullCardTitleName}
@@ -285,8 +285,10 @@ function MapCardWithKey(props: MapCardProps) {
                 />
               </CardContent>
             )}
+
             {!mapQueryResponse.dataIsMissing() &&
-              dataForActiveBreakdownFilter.length === 0 && (
+              dataForActiveBreakdownFilter.length === 0 &&
+              activeBreakdownFilter !== "All" && (
                 <CardContent>
                   <Alert severity="warning">
                     No data available for filter: <b>{activeBreakdownFilter}</b>
