7ab7803cba4e7b0f8cec5d2e247770e872aad3f1
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-10-15 10:56:19 -0400

    remove unused things

--- python/tests/test_dataset_utils.py
+++ python/tests/test_dataset_utils.py
@@ -1,40 +1,7 @@
 import json
-import pytest
 
 from pandas.testing import assert_frame_equal
 from ingestion import gcs_to_bq_util, dataset_utils  # pylint: disable=no-name-in-module
-
-_fake_race_data = [
-    ['state_fips', 'state_name', 'race', 'population'],
-    ['01', 'Alabama', 'Asian alone', '66'],
-    ['01', 'Alabama', 'Some other race alone', '70'],
-    ['01', 'Alabama', 'Two or more races', '92'],
-    ['01', 'Alabama', 'TOTAL', '228'],
-    ['02', 'Alaska', 'Asian alone', '45'],
-    ['02', 'Alaska', 'Some other race alone', '11'],
-    ['02', 'Alaska', 'Two or more races', '60'],
-    ['02', 'Alaska', 'TOTAL', '116'],
-    ['04', 'Arizona', 'Asian alone', '23'],
-    ['04', 'Arizona', 'Some other race alone', '46'],
-    ['04', 'Arizona', 'Two or more races', '26'],
-    ['04', 'Arizona', 'TOTAL', '95'],
-]
-
-_expected_pct_share_data = [
-    ['state_fips', 'state_name', 'race', 'population', 'pct_share'],
-    ['01', 'Alabama', 'Asian alone', '66', '28.95'],
-    ['01', 'Alabama', 'Some other race alone', '70', '30.7'],
-    ['01', 'Alabama', 'Two or more races', '92', '40.35'],
-    ['01', 'Alabama', 'TOTAL', '228', '100'],
-    ['02', 'Alaska', 'Asian alone', '45', '38.79'],
-    ['02', 'Alaska', 'Some other race alone', '11', '9.48'],
-    ['02', 'Alaska', 'Two or more races', '60', '51.72'],
-    ['02', 'Alaska', 'TOTAL', '116', '100'],
-    ['04', 'Arizona', 'Asian alone', '23', '24.21'],
-    ['04', 'Arizona', 'Some other race alone', '46', '48.42'],
-    ['04', 'Arizona', 'Two or more races', '26', '27.37'],
-    ['04', 'Arizona', 'TOTAL', '95', '100'],
-]
 
 _fake_race_data_without_totals = [
     ['state_fips', 'state_name', 'race', 'population'],
