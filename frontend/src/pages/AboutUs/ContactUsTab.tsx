95c8a3c9468133fad1d293ee1cea0f0a6e330c9a
Ben Hammond <benjamin.hammond@gmail.com>
2021-10-21 16:44:26 -0600

    adds mention of feedback form on contact us. ideally should auto pop-up but that may be tricky

--- frontend/src/pages/AboutUs/ContactUsTab.tsx
+++ frontend/src/pages/AboutUs/ContactUsTab.tsx
@@ -100,6 +100,13 @@ function ContactUsTab() {
               Atlanta, <span aria-label="Georgia">GA</span>{" "}
               <span aria-label="Zip Code 3 0 3 1 0">30310</span>
             </p>
+
+            <p className={styles.ContactUsP}>
+              <b>To Submit Feedback:</b>
+              <br />
+              Please use our anonymous form, found by scrolling to the bottom
+              right corner of this page.
+            </p>
           </Grid>
         </Grid>
       </Grid>
