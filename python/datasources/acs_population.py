9e7736ff0a747890c299a08ab0607692e38e8b14
Josh Zarrabi <joshnzarrabi@gmail.com>
2021-10-20 21:49:56 -0400

    Adds tests to ACS python code (#1223)
    Moves all acs population code to python, adds unit tests, and an integration test to run by hand
    
    Done in service of #1202
    Resolves #969

--- python/datasources/acs_population.py
+++ python/datasources/acs_population.py
@@ -1,4 +1,5 @@
-import pandas
+import pandas as pd
+
 from ingestion.standardized_columns import (HISPANIC_COL, RACE_COL,
                                             STATE_FIPS_COL, COUNTY_FIPS_COL,
                                             STATE_NAME_COL, COUNTY_NAME_COL,
@@ -7,12 +8,11 @@ from ingestion.standardized_columns import (HISPANIC_COL, RACE_COL,
                                             RACE_INCLUDES_HISPANIC_COL,
                                             TOTAL_VALUE,
                                             add_race_columns_from_category_id)
-from ingestion import url_file_to_gcs, gcs_to_bq_util
+from ingestion import url_file_to_gcs, gcs_to_bq_util, census
 from datasources.data_source import DataSource
-from ingestion.census import (get_census_params, fetch_acs_metadata,
-                              parse_acs_metadata, fetch_acs_group,
+from ingestion.census import (get_census_params, parse_acs_metadata,
                               get_vars_for_group, standardize_frame)
-
+from ingestion.dataset_utils import add_sum_of_rows
 
 # TODO pass this in from message data.
 BASE_ACS_URL = "https://api.census.gov/data/2019/acs/acs5"
@@ -79,41 +79,40 @@ RACE_STRING_TO_CATEGORY_ID_EXCLUDE_HISP = {
 }
 
 
-# TODO move this to a utility file and add tests.
-def add_sum_of_rows(df, breakdown_col, value_col, new_row_breakdown_val,
-                    breakdown_vals_to_sum=None):
-    """Returns a new DataFrame by appending rows by summing the values of other
-       rows. Automatically groups by all other columns, so this won't work if
-       there are extraneous columns.
+def get_decade_age_bucket(age_range):
+    if age_range in {'0-4', '5-9'}:
+        return '0-9'
+    elif age_range in {'10-14', '15-17', '18-19'}:
+        return '10-19'
+    elif age_range in {'20-20', '21-21', '22-24', '25-29'}:
+        return '20-29'
+    elif age_range in {'30-34', '35-39'}:
+        return '30-39'
+    elif age_range in {'40-44', '45-49'}:
+        return '40-49'
+    elif age_range in {'50-54', '55-59'}:
+        return '50-59'
+    elif age_range in {'60-61', '62-64', '65-66', '67-69'}:
+        return '60-69'
+    elif age_range in {'70-74', '75-79'}:
+        return '70-79'
+    elif age_range in {'80-84', '85+'}:
+        return '80+'
+    elif age_range == 'Total':
+        return 'Total'
+    else:
+        return 'Unknown'
 
-       For example, calling
-           `add_sum_of_rows(df, 'race', 'population', 'Total')`
-       will group by all columns except for 'race' and 'population, and for each
-       group add a row with race='Total' and population=the sum of population
-       for all races in that group.
 
-       df: The DataFrame to calculate new rows from.
-       breakdown_col: The name of the breakdown column that a new value is being
-                      summed over.
-       value_col: The name of the column whose values should be summed.
-       new_row_breakdown_val: The value to use for the breakdown column.
-       breakdown_vals_to_sum: The list of breakdown values to sum across. If not
-                              provided, defaults to summing across all values.
-       """
-    filtered_df = df
-    if breakdown_vals_to_sum is not None:
-        filtered_df = df.loc[df[breakdown_col].isin(breakdown_vals_to_sum)]
-
-    group_by_cols = list(df.columns)
-    group_by_cols.remove(breakdown_col)
-    group_by_cols.remove(value_col)
-
-    sums = filtered_df.groupby(group_by_cols).sum().reset_index()
-    sums[breakdown_col] = new_row_breakdown_val
-
-    result = pandas.concat([df, sums])
-    result = result.reset_index(drop=True)
-    return result
+def get_uhc_age_bucket(age_range):
+    if age_range in {'18-19', '20-24', '20-20', '21-21', '22-24', '25-29', '30-34', '35-44', '35-39', '40-44'}:
+        return '18-44'
+    elif age_range in {'45-54', '45-49', '50-54', '55-64', '55-59', '60-61', '62-64'}:
+        return '45-64'
+    elif age_range in {'65-74', '65-66', '67-69', '70-74', '75-84', '75-79', '80-84', '85+'}:
+        return '65+'
+    elif age_range == 'Total':
+        return 'Total'
 
 
 def rename_age_bracket(bracket):
@@ -176,7 +175,7 @@ class ACSPopulationIngester():
 
     def upload_to_gcs(self, gcs_bucket):
         """Uploads population data from census to GCS bucket."""
-        metadata = fetch_acs_metadata(self.base_acs_url)
+        metadata = census.fetch_acs_metadata(self.base_acs_url)
         var_map = parse_acs_metadata(metadata, list(GROUPS.keys()))
 
         concepts = list(SEX_BY_AGE_CONCEPTS_TO_RACE.keys())
@@ -200,7 +199,7 @@ class ACSPopulationIngester():
         dataset: The BigQuery dataset to write to
         gcs_bucket: The name of the gcs bucket to read the data from"""
         # TODO change this to have it read metadata from GCS bucket
-        metadata = fetch_acs_metadata(self.base_acs_url)
+        metadata = census.fetch_acs_metadata(self.base_acs_url)
         var_map = parse_acs_metadata(metadata, list(GROUPS.keys()))
 
         race_and_hispanic_frame = gcs_to_bq_util.load_values_as_dataframe(
@@ -228,53 +227,41 @@ class ACSPopulationIngester():
                 var_map, sex_by_age_frames)
         }
 
+        frames['by_sex_age_%s' % self.get_geo_name()] = self.get_by_sex_age(
+                frames[self.get_table_name_by_sex_age_race()], get_decade_age_bucket)
+
+        by_sex_age_uhc = None
+        if not self.county_level:
+            by_sex_age_uhc = self.get_by_sex_age(frames[self.get_table_name_by_sex_age_race()], get_uhc_age_bucket)
+
+        frames['by_age_%s' % self.get_geo_name()] = self.get_by_age(
+            frames['by_sex_age_%s' % self.get_geo_name()],
+            by_sex_age_uhc)
+
+        frames['by_sex_%s' % self.get_geo_name()] = self.get_by_sex(
+                frames[self.get_table_name_by_sex_age_race()])
+
         for table_name, df in frames.items():
             # All breakdown columns are strings
             column_types = {c: 'STRING' for c in df.columns}
             column_types[POPULATION_COL] = 'INT64'
-            column_types[RACE_INCLUDES_HISPANIC_COL] = 'BOOL'
+            if RACE_INCLUDES_HISPANIC_COL in df.columns:
+                column_types[RACE_INCLUDES_HISPANIC_COL] = 'BOOL'
+
             gcs_to_bq_util.add_dataframe_to_bq(
                 df, dataset, table_name, column_types=column_types)
 
-    def write_local_files_debug(self):
-        """Downloads and writes the tables to the local file system as csv and
-           json files. This is only for debugging/convenience, and should not
-           be used in production."""
-        metadata = fetch_acs_metadata(self.base_acs_url)
-        var_map = parse_acs_metadata(metadata, list(GROUPS.keys()))
-
-        by_hisp_and_race_json = fetch_acs_group(
-            self.base_acs_url, HISPANIC_BY_RACE_CONCEPT, var_map, 2,
-            self.county_level)
-        sex_by_age_frames = {}
-        for concept in SEX_BY_AGE_CONCEPTS_TO_RACE:
-            json_string = fetch_acs_group(
-                self.base_acs_url, concept, var_map, 2, self.county_level)
-            frame = gcs_to_bq_util.values_json_to_dataframe(json_string)
-            sex_by_age_frames[concept] = update_col_types(frame)
-
-        race_and_hispanic_frame = gcs_to_bq_util.values_json_to_dataframe(
-            by_hisp_and_race_json)
-        race_and_hispanic_frame = update_col_types(race_and_hispanic_frame)
-        race_and_hispanic_frame = standardize_frame(
-            race_and_hispanic_frame,
-            get_vars_for_group(HISPANIC_BY_RACE_CONCEPT, var_map, 2),
-            [HISPANIC_COL, RACE_COL],
-            self.county_level,
-            POPULATION_COL)
-
-        frames = {
-            self.get_table_name_by_race(): self.get_all_races_frame(
-                race_and_hispanic_frame),
-            self.get_table_name_by_sex_age_race(): self.get_sex_by_age_and_race(
-                var_map, sex_by_age_frames)
-        }
-        for key, df in frames.items():
-            df.to_csv("table_" + key + ".csv", index=False)
-            df.to_json("table_" + key + ".json", orient="records")
-
     def get_table_geo_suffix(self):
         return "_county" if self.county_level else "_state"
+
+    def get_geo_name(self):
+        return 'county' if self.county_level else 'state'
+
+    def get_fips_col(self):
+        return COUNTY_FIPS_COL if self.county_level else STATE_FIPS_COL
+
+    def get_geo_name_col(self):
+        return COUNTY_NAME_COL if self.county_level else STATE_NAME_COL
 
     def get_table_name_by_race(self):
         return "by_race" + self.get_table_geo_suffix() + "_std"
@@ -297,7 +284,7 @@ class ACSPopulationIngester():
     def sort_race_frame(self, df):
         sort_cols = self.base_sort_by_cols.copy()
         sort_cols.append(RACE_CATEGORY_ID_COL)
-        return df.sort_values(sort_cols)
+        return df.sort_values(sort_cols).reset_index(drop=True)
 
     def sort_sex_age_race_frame(self, df):
         sort_cols = self.base_sort_by_cols.copy()
@@ -305,7 +292,7 @@ class ACSPopulationIngester():
         # However, it doesn't matter how these are sorted in the backend, this
         # is just for convenience when looking at the data in BigQuery.
         sort_cols.extend([RACE_CATEGORY_ID_COL, SEX_COL, AGE_COL])
-        return df.sort_values(sort_cols)
+        return df.sort_values(sort_cols).reset_index(drop=True)
 
     def standardize_race_exclude_hispanic(self, df):
         """Standardized format using mutually exclusive groups by excluding
@@ -352,7 +339,7 @@ class ACSPopulationIngester():
             lambda r: RACE_STRING_TO_CATEGORY_ID_INCLUDE_HISP[r[RACE_COL]],
             axis=1)
 
-        return pandas.concat([by_hispanic, by_race])
+        return pd.concat([by_hispanic, by_race])
 
     def get_all_races_frame(self, race_and_hispanic_frame):
         """Includes all race categories, both including and not including
@@ -366,7 +353,7 @@ class ACSPopulationIngester():
         # group, so remove from one before concatenating.
         standardized_race = standardized_race[
             standardized_race[RACE_CATEGORY_ID_COL] != Race.HISP.value]
-        all_races = pandas.concat([all_races, standardized_race])
+        all_races = pd.concat([all_races, standardized_race])
 
         # Drop extra columns before adding derived rows so they don't interfere
         # with grouping.
@@ -405,7 +392,7 @@ class ACSPopulationIngester():
 
             sex_by_age[RACE_CATEGORY_ID_COL] = race
             frames.append(sex_by_age)
-        result = pandas.concat(frames)
+        result = pd.concat(frames)
         result[AGE_COL] = result[AGE_COL].apply(rename_age_bracket)
 
         result = add_sum_of_rows(result, AGE_COL, POPULATION_COL, TOTAL_VALUE)
@@ -413,6 +400,66 @@ class ACSPopulationIngester():
 
         add_race_columns_from_category_id(result)
         return self.sort_sex_age_race_frame(result)
+
+    def get_by_sex_age(self, by_sex_age_race_frame, age_aggregator_func):
+        by_sex_age = by_sex_age_race_frame.loc[by_sex_age_race_frame[RACE_CATEGORY_ID_COL] == Race.TOTAL.value]
+
+        cols = [
+            STATE_FIPS_COL,
+            self.get_fips_col(),
+            self.get_geo_name_col(),
+            SEX_COL,
+            AGE_COL,
+            POPULATION_COL,
+        ]
+
+        by_sex_age = by_sex_age[cols] if self.county_level else by_sex_age[cols[1:]]
+        by_sex_age[AGE_COL] = by_sex_age[AGE_COL].apply(age_aggregator_func)
+
+        groupby_cols = cols[:-1] if self.county_level else cols[1: -1]
+        by_sex_age = by_sex_age.groupby(groupby_cols)[POPULATION_COL].sum().reset_index()
+
+        return by_sex_age
+
+    def get_by_age(self, by_sex_age, by_sex_age_uhc=None):
+        by_age = by_sex_age.loc[by_sex_age[SEX_COL] == TOTAL_VALUE]
+
+        cols = [
+            STATE_FIPS_COL,
+            self.get_fips_col(),
+            self.get_geo_name_col(),
+            AGE_COL,
+            POPULATION_COL,
+        ]
+
+        by_age = by_age[cols] if self.county_level else by_age[cols[1:]]
+
+        if not self.county_level:
+            by_age_uhc = by_sex_age_uhc.loc[by_sex_age_uhc[SEX_COL] == TOTAL_VALUE]
+            by_age_uhc = by_age_uhc[cols[1:]]
+
+            by_age = pd.concat([by_age, by_age_uhc]).drop_duplicates().reset_index(drop=True)
+
+        by_age = by_age.sort_values(by=cols[1:-1]).reset_index(drop=True)
+        return by_age
+
+    def get_by_sex(self, by_sex_age_race_frame):
+        by_sex = by_sex_age_race_frame.loc[
+                (by_sex_age_race_frame[RACE_CATEGORY_ID_COL] == Race.TOTAL.value) &
+                (by_sex_age_race_frame[AGE_COL] == TOTAL_VALUE)]
+
+        cols = [
+            STATE_FIPS_COL,
+            self.get_fips_col(),
+            self.get_geo_name_col(),
+            SEX_COL,
+            POPULATION_COL,
+        ]
+
+        by_sex = by_sex[cols] if self.county_level else by_sex[cols[1:]]
+
+        by_sex = by_sex.sort_values(by=cols[1:-1]).reset_index(drop=True)
+        return by_sex
 
 
 class ACSPopulation(DataSource):
