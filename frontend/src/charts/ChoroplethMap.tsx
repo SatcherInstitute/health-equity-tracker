538c4bd80f02354c495f204b2562edf2c0bd3ce1
Ben Hammond <benjamin.hammond@gmail.com>
2021-10-22 16:01:04 -0600

    reverts hover label fix which was moved to #1248. best practice to silo our updates/changes into individual PRs

--- frontend/src/charts/ChoroplethMap.tsx
+++ frontend/src/charts/ChoroplethMap.tsx
@@ -145,19 +145,10 @@ export function ChoroplethMap(props: ChoroplethMapProps) {
       ? "Sample size too small"
       : NO_DATA_MESSAGE;
 
-    /* PROPERLY LABEL THE HOVERED GEO REGION IF TERRITORY */
-    const countyOrEquivalent =
-      props.fips.isTerritory() || props.fips.getParentFips().isTerritory()
-        ? "County Equivalent"
-        : "County";
-    const stateOrTerritory = props.overrideShapeWithCircle
-      ? "Territory"
-      : "State";
-    const geographyName = props.showCounties
-      ? countyOrEquivalent
-      : stateOrTerritory;
+    const geographyName = props.showCounties ? "County" : "State";
     const tooltipDatum = `format(datum.${props.metric.metricId}, ',')`;
-    // TODO: would be nice to use addMetricDisplayColumn for the tooltips here so that data formatting is consistent.
+    // TODO: would be nice to use addMetricDisplayColumn for the tooltips here
+    // so that data formatting is consistent.
     const tooltipLabel =
       props.isUnknownsMap && props.metric.unknownsVegaLabel
         ? props.metric.unknownsVegaLabel
