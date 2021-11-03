79f8bce346d83e8a718892363d17853ee369e4ee
Ben Hammond <benjamin.hammond@gmail.com>
2021-11-03 13:57:05 -0600

    adds missing details to download options

--- frontend/src/data/config/FakeDatasetMetadata.ts
+++ frontend/src/data/config/FakeDatasetMetadata.ts
@@ -135,19 +135,19 @@ const datasetMetadataList: DatasetMetadata[] = [
   {
     id: "cdc_vaccination_national-age",
     name:
-      "CDC COVID-19 Vaccination Demographics in the United States, National",
+      "CDC COVID-19 Vaccination Demographics in the United States by age, National",
     update_time: "October 2021",
   },
   {
     id: "cdc_vaccination_national-sex",
     name:
-      "CDC COVID-19 Vaccination Demographics in the United States, National",
+      "CDC COVID-19 Vaccination Demographics in the United States by sex, National",
     update_time: "October 2021",
   },
   {
     id: "cdc_vaccination_national-race_and_ethnicity",
     name:
-      "CDC COVID-19 Vaccination Demographics in the United States, National",
+      "CDC COVID-19 Vaccination Demographics in the United States by race, National",
     update_time: "October 2021",
   },
   {
