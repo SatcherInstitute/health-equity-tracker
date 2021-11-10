a1fd9395c9f0575e9332ba40c56635b400f4d3ad
Ben Hammond <benjamin.hammond@gmail.com>
2021-11-10 09:36:46 -0700

    import feedback box

---
+++ frontend/src/pages/ui/FeedbackBox.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+// import "feeder-react-feedback/dist/feeder-react-feedback.css"; // 3rd-party default styles
+import "./FeedbackOverrides.css"; // overrides to above styles
+// https://github.com/rishipr/feeder-react-feedback
+// @ts-ignore
+import Feedback from "feeder-react-feedback"; // import Feedback component
+
+import { useBottomScrollListener } from "react-bottom-scroll-listener";
+
+const FEEDBACK_ID = "6171cc2965b82c00045239dc"; // view collected feedback at feeder.sh/dashboard
+const BOTTOM_SCROLL_OPTIONS = {
+  offset: 500,
+};
+
+export default function FeedbackBox(props: { alwaysShow?: boolean }) {
+  const [showFeedback, setShowFeedback] = useState(props.alwaysShow || false);
+
+  useBottomScrollListener(() => setShowFeedback(true), BOTTOM_SCROLL_OPTIONS);
+
+  return showFeedback ? (
+    <Feedback
+      projectId={FEEDBACK_ID}
+      email={false}
+      feedbackTypes={["1", "2", "3", "4", "5", "6"]}
+      hoverBorderColor={"#0b5240"}
+      postSubmitButtonMsg="Thank you for helping us advance health equity"
+      primaryColor={"#0b5240"}
+      textColor={"#FFFFFF"}
+    />
+  ) : (
+    <></>
+  );
+}
