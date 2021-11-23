33b2403bf708c877c4673be208357877d304391c
Ben Hammond <benjamin.hammond@gmail.com>
2021-11-23 14:20:16 -0700

    problem on data page ... testing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,13 +4,10 @@ import "./styles/index.scss";
 import App from "./App";
 import "typeface-hind";
 import "typeface-montserrat";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
