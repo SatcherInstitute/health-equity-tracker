5f9e74663753b2d66cc84566ac104800d6f0d658
Ben Hammond <benjamin.hammond@gmail.com>
2021-11-29 10:49:43 -0700

    move email signup url to a lookup, this will allow automated testing for 404 later

---
+++ frontend/src/utils/externalUrls.ts
@@ -0,0 +1,7 @@
+type linkName = "newsletterSignup";
+type linkUrl = "https://satcherinstitute.us11.list-manage.com/subscribe?u=6a52e908d61b03e0bbbd4e790&id=3ec1ba23cd&";
+
+export const externalUrls: Record<linkName, linkUrl> = {
+  newsletterSignup:
+    "https://satcherinstitute.us11.list-manage.com/subscribe?u=6a52e908d61b03e0bbbd4e790&id=3ec1ba23cd&",
+};
