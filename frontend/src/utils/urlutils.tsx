e84403f879cef6e163aff235380df08bb5100e97
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-01 16:56:16 -0700

    remove html parse package ; rm logs

--- frontend/src/utils/urlutils.tsx
+++ frontend/src/utils/urlutils.tsx
@@ -292,7 +292,6 @@ window.onpopstate = () => {
 Dumps a string of HTML into a div (unless second argument of asSpan = true is sent in)
 */
 export function getHtml(item: any, asSpan?: boolean) {
-  console.log(item, asSpan);
   return asSpan ? (
     <span dangerouslySetInnerHTML={{ __html: item || "" }}></span>
   ) : (
