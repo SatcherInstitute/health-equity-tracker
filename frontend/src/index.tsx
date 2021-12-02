6475c6d063fb4d3cc3b65e2ec11f86d1ea14c587
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-02 10:35:54 -0700

    adjust cache to session so reset on new tab/window

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import "typeface-hind";
 import "typeface-montserrat";
 import { QueryClient, QueryClientProvider } from "react-query";
-// import { ReactQueryDevtools } from 'react-query/devtools'
 import { persistQueryClient } from "react-query/persistQueryClient-experimental";
 import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
 
@@ -20,19 +19,18 @@ const queryClient = new QueryClient({
 /* 
 VERY IMPORTANT: This utility is currently in an experimental stage. This means that breaking changes will happen in minor AND patch releases. Use at your own risk. If you choose to rely on this in production in an experimental stage, please lock your version to a patch-level version to avoid unexpected breakages.
  */
-const localStoragePersistor = createWebStoragePersistor({
-  storage: window.localStorage,
+const sessionStoragePersistor = createWebStoragePersistor({
+  storage: window.sessionStorage,
 });
 persistQueryClient({
   queryClient,
-  persistor: localStoragePersistor,
+  persistor: sessionStoragePersistor,
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
