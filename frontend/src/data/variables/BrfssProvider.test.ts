79797a8ad05c1dd791fd6b603c85df8ab778f569
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-15 14:42:53 -0700

    state tests working

--- frontend/src/data/variables/BrfssProvider.test.ts
+++ frontend/src/data/variables/BrfssProvider.test.ts
@@ -57,12 +57,6 @@ export async function evaluateWithAndWithoutAll(
 
   const consumedDatasetIds = ["uhc_data-race_and_ethnicity", acsDatasetId];
 
-  console.log("RESPONSE INCL ALL 2", responseIncludingAll.data[2]);
-  console.log(
-    "new MetricQ from FINAL 2",
-    new MetricQueryResponse(rowsIncludingAll, consumedDatasetIds).data[2]
-  );
-
   expect(responseIncludingAll).toEqual(
     new MetricQueryResponse(rowsIncludingAll, consumedDatasetIds)
   );
@@ -74,6 +68,7 @@ export async function evaluateWithAndWithoutAll(
       baseBreakdown.addBreakdown(breakdownVar, excludeAll())
     )
   );
+
   expect(responseExcludingAll).toEqual(
     new MetricQueryResponse(rowsExcludingAll, consumedDatasetIds)
   );
@@ -170,152 +165,152 @@ describe("BrfssProvider", () => {
     dataFetcher.setFakeMetadataLoaded(DatasetMetadataMap);
   });
 
-  // test("State and Race Breakdown", async () => {
-  //   const [AL_ASIAN_ROW, AL_ACS_ASIAN_ROW] = stateRow(
-  //     /*fips=*/ AL,
-  //     /*breakdownName=*/ RACE,
-  //     /*breakdownValue=*/ ASIAN_NH,
-  //     /*copd_pct=*/ 10,
-  //     /*diabetes_pct=*/ 10,
-  //     /* depression_pct */ 10,
-  //     /* excessive_drinking_pct */ 10,
-  //     /* frequent_mental_distress_pct */ 10,
-  //     /* illicit_opioid_use_pct */ 10,
-  //     /* non_medical_drug_use_pct */ 10,
-  //     /* suicide_per_100k */ 10,
-  //     /*population=*/ 1000
-  //   );
+  test("State and Race Breakdown", async () => {
+    const [AL_ASIAN_ROW, AL_ACS_ASIAN_ROW] = stateRow(
+      /*fips=*/ AL,
+      /*breakdownName=*/ RACE,
+      /*breakdownValue=*/ ASIAN_NH,
+      /*copd_pct=*/ 10,
+      /*diabetes_pct=*/ 10,
+      /* depression_pct */ 10,
+      /* excessive_drinking_pct */ 10,
+      /* frequent_mental_distress_pct */ 10,
+      /* illicit_opioid_use_pct */ 10,
+      /* non_medical_drug_use_pct */ 10,
+      /* suicide_per_100k */ 10,
+      /*population=*/ 1000
+    );
 
-  //   const [NC_ASIAN_ROW, NC_ACS_ASIAN_ROW] = stateRow(
-  //     /*fips=*/ NC,
-  //     /*breakdownName=*/ RACE,
-  //     /*breakdownValue=*/ ASIAN_NH,
-  //     /*copd_pct=*/ 20,
-  //     /*diabetes_pct=*/ 20,
-  //     /* depression_pct */ 20,
-  //     /* excessive_drinking_pct */ 20,
-  //     /* frequent_mental_distress_pct */ 20,
-  //     /* illicit_opioid_use_pct */ 20,
-  //     /* non_medical_drug_use_pct */ 20,
-  //     /* suicide_per_100k */ 20,
-  //     /*population=*/ 1000
-  //   );
+    const [NC_ASIAN_ROW, NC_ACS_ASIAN_ROW] = stateRow(
+      /*fips=*/ NC,
+      /*breakdownName=*/ RACE,
+      /*breakdownValue=*/ ASIAN_NH,
+      /*copd_pct=*/ 15,
+      /*diabetes_pct=*/ 15,
+      /* depression_pct */ 15,
+      /* excessive_drinking_pct */ 15,
+      /* frequent_mental_distress_pct */ 15,
+      /* illicit_opioid_use_pct */ 15,
+      /* non_medical_drug_use_pct */ 15,
+      /* suicide_per_100k */ 15,
+      /*population=*/ 1_000
+    );
 
-  //   const [NC_WHITE_ROW, NC_ACS_WHITE_ROW] = stateRow(
-  //     /*fips=*/ NC,
-  //     /*breakdownName=*/ RACE,
-  //     /*breakdownValue=*/ WHITE_NH,
-  //     /*copd_pct=*/ 50,
-  //     /*diabetes_pct=*/ 50,
-  //     /* depression_pct */ 50,
-  //     /* excessive_drinking_pct */ 50,
-  //     /* frequent_mental_distress_pct */ 50,
-  //     /* illicit_opioid_use_pct */ 50,
-  //     /* non_medical_drug_use_pct */ 50,
-  //     /* suicide_per_100k */ 50,
-  //     /*population=*/ 1000
-  //   );
+    const [NC_WHITE_ROW, NC_ACS_WHITE_ROW] = stateRow(
+      /*fips=*/ NC,
+      /*breakdownName=*/ RACE,
+      /*breakdownValue=*/ WHITE_NH,
+      /*copd_pct=*/ 25,
+      /*diabetes_pct=*/ 25,
+      /* depression_pct */ 25,
+      /* excessive_drinking_pct */ 25,
+      /* frequent_mental_distress_pct */ 25,
+      /* illicit_opioid_use_pct */ 25,
+      /* non_medical_drug_use_pct */ 25,
+      /* suicide_per_100k */ 25,
+      /*population=*/ 1_000
+    );
 
-  //   const [NC_ALL_ROW, NC_ACS_ALL_ROW] = stateRow(
-  //     /*fips=*/ NC,
-  //     /*breakdownName=*/ RACE,
-  //     /*breakdownValue=*/ ALL,
-  //     /*copd_pct=*/ 50,
-  //     /*diabetes_pct=*/ 50,
-  //     /* depression_pct */ 50,
-  //     /* excessive_drinking_pct */ 50,
-  //     /* frequent_mental_distress_pct */ 50,
-  //     /* illicit_opioid_use_pct */ 50,
-  //     /* non_medical_drug_use_pct */ 50,
-  //     /* suicide_per_100k */ 50,
-  //     /*population=*/ 5000
-  //   );
+    const [NC_ALL_ROW, NC_ACS_ALL_ROW] = stateRow(
+      /*fips=*/ NC,
+      /*breakdownName=*/ RACE,
+      /*breakdownValue=*/ ALL,
+      /*copd_pct=*/ 20,
+      /*diabetes_pct=*/ 20,
+      /* depression_pct */ 20,
+      /* excessive_drinking_pct */ 20,
+      /* frequent_mental_distress_pct */ 20,
+      /* illicit_opioid_use_pct */ 20,
+      /* non_medical_drug_use_pct */ 20,
+      /* suicide_per_100k */ 20,
+      /*population=*/ 5_000
+    );
 
-  //   const rawData = [AL_ASIAN_ROW, NC_ASIAN_ROW, NC_WHITE_ROW, NC_ALL_ROW];
+    const rawData = [AL_ASIAN_ROW, NC_ASIAN_ROW, NC_WHITE_ROW, NC_ALL_ROW];
 
-  //   const rawAcsData = [
-  //     AL_ACS_ASIAN_ROW,
-  //     NC_ACS_ASIAN_ROW,
-  //     NC_ACS_WHITE_ROW,
-  //     NC_ACS_ALL_ROW,
-  //   ];
+    const rawAcsData = [
+      AL_ACS_ASIAN_ROW,
+      NC_ACS_ASIAN_ROW,
+      NC_ACS_WHITE_ROW,
+      NC_ACS_ALL_ROW,
+    ];
 
-  //   // Create final rows
-  //   const NC_ASIAN_FINAL = finalRow(
-  //     /*fips*/ NC,
-  //     /*breakdownName*/ RACE,
-  //     /*breakdownValue*/ ASIAN_NH,
-  //     /*copd_per_100k*/ 20000,
-  //     /*diabetes_per_100k*/ 20000,
-  //     /* depression_per_100k */ 20000,
-  //     /* excessive_drinking_per_100k */ 20000,
-  //     /* frequent_mental_distress_per_100k */ 20000,
-  //     /* illicit_opioid_use_per_100k */ 20000,
-  //     /* non_medical_drug_use_per_100k */ 20000,
-  //     /* suicide_per_100k */ 20000,
-  //     /*copd_pct_share*/ 1,
-  //     /*diabetes_pct_share*/ 1,
-  //     /* depression_pct_share */ 1,
-  //     /* excessive_drinking_pct_share */ 1,
-  //     /* frequent_mental_distress_pct_share */ 1,
-  //     /* illicit_opioid_use_pct_share */ 1,
-  //     /* non_medical_drug_use_pct_share */ 1,
-  //     /* suicide_pct_share */ 1
-  //   );
-  //   const NC_WHITE_FINAL = finalRow(
-  //     NC,
-  //     RACE,
-  //     WHITE_NH,
-  //     /*copd_per_100k*/ 50000,
-  //     /*diabetes_per_100k*/ 50000,
-  //     /* depression_per_100k */ 50000,
-  //     /* excessive_drinking_per_100k */ 50000,
-  //     /* frequent_mental_distress_per_100k */ 50000,
-  //     /* illicit_opioid_use_per_100k */ 50000,
-  //     /* non_medical_drug_use_per_100k */ 50000,
-  //     /* suicide_per_100k */ 50000,
-  //     /*copd_pct_share*/ 1,
-  //     /*diabetes_pct_share*/ 1,
-  //     /* depression_pct_share */ 1,
-  //     /* excessive_drinking_pct_share */ 1,
-  //     /* frequent_mental_distress_pct_share */ 1,
-  //     /* illicit_opioid_use_pct_share */ 1,
-  //     /* non_medical_drug_use_pct_share */ 1,
-  //     /* suicide_pct_share */ 1
-  //   );
-  //   const NC_ALL_FINAL = finalRow(
-  //     NC,
-  //     RACE,
-  //     ALL,
-  //     /*copd_per_100k*/ 1,
-  //     /*diabetes_per_100k*/ 1,
-  //     /* depression_per_100k */ 1,
-  //     /* excessive_drinking_per_100k */ 1,
-  //     /* frequent_mental_distress_per_100k */ 1,
-  //     /* illicit_opioid_use_per_100k */ 1,
-  //     /* non_medical_drug_use_per_100k */ 1,
-  //     /* suicide_per_100k */ 1,
-  //     /*copd_pct_share*/ 1,
-  //     /*diabetes_pct_share*/ 1,
-  //     /* depression_pct_share */ 1,
-  //     /* excessive_drinking_pct_share */ 1,
-  //     /* frequent_mental_distress_pct_share */ 1,
-  //     /* illicit_opioid_use_pct_share */ 1,
-  //     /* non_medical_drug_use_pct_share */ 1,
-  //     /* suicide_pct_share */ 1
-  //   );
+    // Create final rows
+    const NC_ASIAN_FINAL = finalRow(
+      /*fips*/ NC,
+      /*breakdownName*/ RACE,
+      /*breakdownValue*/ ASIAN_NH,
+      /*copd_per_100k*/ 15_000,
+      /*diabetes_per_100k*/ 15_000,
+      /* depression_per_100k */ 15_000,
+      /* excessive_drinking_per_100k */ 15_000,
+      /* frequent_mental_distress_per_100k */ 15_000,
+      /* illicit_opioid_use_per_100k */ 15_000,
+      /* non_medical_drug_use_per_100k */ 15_000,
+      /* suicide_per_100k */ 15,
+      /*copd_pct_share*/ 15,
+      /*diabetes_pct_share*/ 15,
+      /* depression_pct_share */ 15,
+      /* excessive_drinking_pct_share */ 15,
+      /* frequent_mental_distress_pct_share */ 15,
+      /* illicit_opioid_use_pct_share */ 15,
+      /* non_medical_drug_use_pct_share */ 15,
+      /* suicide_pct_share */ 15
+    );
+    const NC_WHITE_FINAL = finalRow(
+      NC,
+      RACE,
+      WHITE_NH,
+      /*copd_per_100k*/ 25_000,
+      /*diabetes_per_100k*/ 25_000,
+      /* depression_per_100k */ 25_000,
+      /* excessive_drinking_per_100k */ 25_000,
+      /* frequent_mental_distress_per_100k */ 25_000,
+      /* illicit_opioid_use_per_100k */ 25_000,
+      /* non_medical_drug_use_per_100k */ 25_000,
+      /* suicide_per_100k */ 25,
+      /*copd_pct_share*/ 25,
+      /*diabetes_pct_share*/ 25,
+      /* depression_pct_share */ 25,
+      /* excessive_drinking_pct_share */ 25,
+      /* frequent_mental_distress_pct_share */ 25,
+      /* illicit_opioid_use_pct_share */ 25,
+      /* non_medical_drug_use_pct_share */ 25,
+      /* suicide_pct_share */ 25
+    );
+    const NC_ALL_FINAL = finalRow(
+      NC,
+      RACE,
+      ALL,
+      /*copd_per_100k*/ 20_000,
+      /*diabetes_per_100k*/ 20_000,
+      /* depression_per_100k */ 20_000,
+      /* excessive_drinking_per_100k */ 20_000,
+      /* frequent_mental_distress_per_100k */ 20_000,
+      /* illicit_opioid_use_per_100k */ 20_000,
+      /* non_medical_drug_use_per_100k */ 20_000,
+      /* suicide_per_100k */ 20,
+      /*copd_pct_share*/ 100,
+      /*diabetes_pct_share*/ 100,
+      /* depression_pct_share */ 100,
+      /* excessive_drinking_pct_share */ 100,
+      /* frequent_mental_distress_pct_share */ 100,
+      /* illicit_opioid_use_pct_share */ 100,
+      /* non_medical_drug_use_pct_share */ 100,
+      /* suicide_pct_share */ 100
+    );
 
-  //   await evaluateWithAndWithoutAll(
-  //     "uhc_data-race_and_ethnicity",
-  //     rawData,
-  //     "acs_population-by_race_state_std",
-  //     rawAcsData,
-  //     Breakdowns.forFips(new Fips("37")),
-  //     RACE,
-  //     [NC_ASIAN_FINAL, NC_WHITE_FINAL],
-  //     [NC_ALL_FINAL, NC_ASIAN_FINAL, NC_WHITE_FINAL]
-  //   );
-  // });
+    await evaluateWithAndWithoutAll(
+      "uhc_data-race_and_ethnicity",
+      rawData,
+      "acs_population-by_race_state_std",
+      rawAcsData,
+      Breakdowns.forFips(new Fips("37")),
+      RACE,
+      [NC_ASIAN_FINAL, NC_WHITE_FINAL],
+      [NC_ALL_FINAL, NC_ASIAN_FINAL, NC_WHITE_FINAL]
+    );
+  });
 
   test("National and Race Breakdown", async () => {
     const [USA_ASIAN_ROW, USA_ACS_ASIAN_ROW] = stateRow(
