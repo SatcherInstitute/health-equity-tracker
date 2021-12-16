0b4b9558c3eac865f3a72abbb5c5d280cce492bc
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-16 16:42:45 -0700

    adds test for each URL = 200 code

--- frontend/src/utils/externalUrls.test.ts
+++ frontend/src/utils/externalUrls.test.ts
@@ -1,4 +1,12 @@
+/**
+ * @jest-environment node
+ */
+
 import { LinkName, urlMap } from "./externalUrls";
+import axios from "axios";
+
+export const SUCCESS_CODE = 200;
+export const TWO_MINUTES = 120_000; // it can take a long time to check every external URL
 
 describe("ExternalUrls", () => {
   test("Links use HTTPS", () => {
@@ -8,6 +16,7 @@ describe("ExternalUrls", () => {
       expect(testUrl).toMatch(new RegExp(`^${requiredPrefix}?`));
     }
   });
+
   test("No Duplicate Links", () => {
     // All Urls
     const testUrlsArray = Object.values(urlMap);
@@ -16,4 +25,29 @@ describe("ExternalUrls", () => {
     // Converting to set shouldn't change the total number of URLs
     expect(testUrlsArray.length).toEqual(testUrlsSet.size);
   });
+
+  test(
+    "Links return SUCCESS status code",
+    async () => {
+      async function getStatus(url: string) {
+        try {
+          const response = await axios.get(url);
+          return response.status;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      for (const urlName in urlMap) {
+        const testUrl = urlMap[urlName as LinkName];
+
+        // skip LinkedIn
+        if (testUrl.includes("linkedin.com")) continue;
+
+        const urlStatus = await getStatus(testUrl);
+        expect(urlStatus).toEqual(SUCCESS_CODE);
+      }
+    },
+    TWO_MINUTES
+  );
 });
