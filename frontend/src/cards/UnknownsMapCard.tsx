0cd75ba4d0612e4e3e9550ed4369a41a17afe0eb
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-20 23:35:18 -0700

    geos

--- frontend/src/cards/UnknownsMapCard.tsx
+++ frontend/src/cards/UnknownsMapCard.tsx
@@ -128,7 +128,7 @@ function UnknownsMapCardWithKey(props: UnknownsMapCardProps) {
                   : unknownEthnicities[index];
               });
 
-        // remove the undefined values from suppressed states
+        // remove the undefined values from suppressed geos
         const unknowns = unknownsUnfiltered.filter(
           (unknown) => unknown[metricConfig.metricId] !== undefined
         );
