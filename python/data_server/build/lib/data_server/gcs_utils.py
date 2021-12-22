728d11e980748c57790a388bee8bed30cf940c87
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-22 00:17:33 -0700

    rm build

--- python/data_server/build/lib/data_server/gcs_utils.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import storage
-
-
-def download_blob_as_bytes(gcs_bucket: str, filename: str) -> bytes:
-    client = storage.Client()
-    bucket = client.get_bucket(gcs_bucket)
-    blob = bucket.blob(filename)
-    return blob.download_as_bytes()
