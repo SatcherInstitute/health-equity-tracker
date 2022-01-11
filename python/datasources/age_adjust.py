cd1386ebf6224339ab5d7e87ca654cf009553f58
Josh Zarrabi <joshnzarrabi@gmail.com>
2022-01-11 14:11:04 -0800

    more changes

--- python/datasources/age_adjust.py
+++ python/datasources/age_adjust.py
@@ -3,11 +3,10 @@ from google.cloud import bigquery
 import ingestion.standardized_columns as std_col
 import pandas as pd
 
-# plan for age adjustment
-# 1. do a race_and_age type situation
-# 2. age adjust based off of that
-# 3. profit
 REFERENCE_POPULATION = std_col.Race.WHITE_NH.value
+
+AGE_ADJUST_RACES = {std_col.Race.WHITE_NH.value, std_col.Race.BLACK_NH.value, std_col.Race.HISP.value,
+                    std_col.Race.AIAN_NH.value, std_col.Race.NHPI_NH.value, std_col.Race.ASIAN_NH.value}
 
 # def get_population_df():
 #     bqclient = bigquery.Client()
@@ -19,6 +18,16 @@ REFERENCE_POPULATION = std_col.Race.WHITE_NH.value
 #     return bqclient.query(query_string).result().to_dataframe()
 
 
+def get_race_age_covid_df():
+    bqclient = bigquery.Client()
+
+    query_string = """
+SELECT *
+FROM `jzarrabi-het-infra-test-f4.cdc_restricted_data.by_race_age_state`
+"""
+    return bqclient.query(query_string).result().to_dataframe()
+
+
 def per_100k(rate):
     return round(rate * 1000 * 100, 2)
 
@@ -28,10 +37,10 @@ def get_expected_deaths(race_and_age_df, population_df):
     def get_expected_death_rate(row):
         true_death_rate = float(row['death_y']) / float(row['population'])
 
-        ref_pop_size = df.loc[
-                (df[std_col.RACE_CATEGORY_ID_COL] == REFERENCE_POPULATION) &
-                (df[std_col.AGE_COL] == row[std_col.AGE_COL]) &
-                (df[std_col.STATE_FIPS_COL] == row[std_col.STATE_FIPS_COL])
+        ref_pop_size = population_df.loc[
+                (population_df[std_col.RACE_CATEGORY_ID_COL] == REFERENCE_POPULATION) &
+                (population_df[std_col.AGE_COL] == row[std_col.AGE_COL]) &
+                (population_df[std_col.STATE_FIPS_COL] == row[std_col.STATE_FIPS_COL])
             ][std_col.POPULATION_COL].values[0]
 
         return round(true_death_rate * ref_pop_size, 2)
@@ -40,7 +49,11 @@ def get_expected_deaths(race_and_age_df, population_df):
     on_cols.extend(std_col.RACE_COLUMNS)
 
     df = pd.merge(race_and_age_df, population_df, how='left', on=on_cols)
-    df = df.loc[df[std_col.AGE_COL] != "UNKNOWN"]
+
+    states_with_pop = set(population_df[std_col.STATE_FIPS_COL].drop_duplicates().to_list())
+    df = df.loc[df[std_col.AGE_COL] != "UNKNOWN"].reset_index(drop=True)
+    df = df.loc[df[std_col.RACE_CATEGORY_ID_COL].isin(AGE_ADJUST_RACES)].reset_index(drop=True)
+    df = df.loc[df[std_col.STATE_FIPS_COL].isin(states_with_pop)].reset_index(drop=True)
 
     df['expected_deaths'] = df.apply(get_expected_death_rate, axis=1)
 
@@ -71,9 +84,12 @@ def age_adjust(df, population_df):
 
     return df[needed_cols]
 
-    # fips_codes = race_and_age_df['state_fips'].drop_duplicates().to_list()
 
-    # for fips_code in fips_codes:
-    #     geo_area = race_and_age_df.loc[race_and_age_df[GEO_TO_FIPS_COL[geo]] == fips_code]
-    #     races = geo_area['race_category_id'].drop_duplicates().to_list()
-    # return race_and_age_df
+def age_adjust_states():
+    pop_data = pd.read_csv('all-pop.csv', dtype={'state_fips': str})
+    covid_data = get_race_age_covid_df()
+
+    df = get_expected_deaths(covid_data, pop_data)
+    df = age_adjust(df, pop_data)
+
+    df.to_json('age-adjusted-all.json', orient='records')
