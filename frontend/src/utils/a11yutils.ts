c27ce1905b2ef1f206e4daa037061f051eff2dbb
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-10 23:02:56 -0700

    rm old code

--- frontend/src/utils/a11yutils.ts
+++ frontend/src/utils/a11yutils.ts
@@ -1,13 +1,15 @@
 /*
 Temporarily creates a hidden HTML element with role="alert"
-causing screen readers to announce an important update
+causing screen readers to announce message as an important update
 */
 export function srSpeak(message: string) {
   const temp = document.createElement("div");
+
   // unique ID
   const id = "speak-" + Date.now();
   temp.setAttribute("id", id);
   temp.setAttribute("role", "alert");
+
   // hide from browsers
   temp.classList.add("srOnly");
   document.body.appendChild(temp);
