22ee212ced4c55870283f0a9b500d6a3a53013d2
Ben Hammond <benjamin.hammond@gmail.com>
2022-02-15 14:10:51 -0700

    new age adj card

---
+++ frontend/src/cards/AgeAdjustedTableCard.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import { AgeAdjustedTableChart } from "../charts/AgeAdjustedTableChart";
+import CardWrapper from "./CardWrapper";
+import { MetricQuery } from "../data/query/MetricQuery";
+import { Fips } from "../data/utils/Fips";
+import {
+  Breakdowns,
+  BreakdownVar,
+  BREAKDOWN_VAR_DISPLAY_NAMES,
+} from "../data/query/Breakdowns";
+import { CardContent } from "@material-ui/core";
+import {
+  MetricConfig,
+  MetricId,
+  VariableConfig,
+  getPer100kAndPctShareMetrics,
+  VAXX,
+} from "../data/config/MetricConfig";
+import { exclude } from "../data/query/BreakdownFilter";
+import {
+  NON_HISPANIC,
+  RACE,
+  UNKNOWN,
+  UNKNOWN_RACE,
+  UNKNOWN_ETHNICITY,
+  ALL,
+  BROAD_AGE_BUCKETS,
+  DECADE_PLUS_5_AGE_BUCKETS,
+} from "../data/utils/Constants";
+import { Row } from "../data/utils/DatasetTypes";
+import MissingDataAlert from "./ui/MissingDataAlert";
+import Alert from "@material-ui/lab/Alert";
+import Divider from "@material-ui/core/Divider";
+import {
+  UHC_BROAD_AGE_DETERMINANTS,
+  UHC_DECADE_PLUS_5_AGE_DETERMINANTS,
+} from "../data/variables/BrfssProvider";
+import { urlMap } from "../utils/externalUrls";
+import styles from "./Card.module.scss";
+
+/* minimize layout shift */
+const PRELOAD_HEIGHT = 298;
+
+export interface AgeAdjustedTableCardProps {
+  fips: Fips;
+  breakdownVar: BreakdownVar;
+  variableConfig: VariableConfig;
+}
+
+export function AgeAdjustedTableCard(props: AgeAdjustedTableCardProps) {
+  const metrics = getPer100kAndPctShareMetrics(props.variableConfig);
+  const current100k = props.variableConfig.metrics.per100k.metricId;
+
+  // choose demographic groups to exclude from the table
+  let exclusionList = [ALL];
+  props.breakdownVar === "race_and_ethnicity" &&
+    exclusionList.push(NON_HISPANIC);
+  UHC_BROAD_AGE_DETERMINANTS.includes(current100k) &&
+    exclusionList.push(...DECADE_PLUS_5_AGE_BUCKETS);
+  UHC_DECADE_PLUS_5_AGE_DETERMINANTS.includes(current100k) &&
+    exclusionList.push(...BROAD_AGE_BUCKETS);
+
+  const breakdowns = Breakdowns.forFips(props.fips).addBreakdown(
+    props.breakdownVar,
+    exclude(...exclusionList)
+  );
+
+  let metricConfigs: Record<string, MetricConfig> = {};
+  metrics.forEach((metricConfig) => {
+    // We prefer known breakdown metric if available.
+    if (metricConfig.knownBreakdownComparisonMetric) {
+      metricConfigs[metricConfig.knownBreakdownComparisonMetric.metricId] =
+        metricConfig.knownBreakdownComparisonMetric;
+    } else {
+      metricConfigs[metricConfig.metricId] = metricConfig;
+    }
+
+    if (metricConfig.populationComparisonMetric) {
+      metricConfigs[metricConfig.populationComparisonMetric.metricId] =
+        metricConfig.populationComparisonMetric;
+    }
+
+    if (metricConfig.secondaryPopulationComparisonMetric) {
+      metricConfigs[metricConfig.secondaryPopulationComparisonMetric.metricId] =
+        metricConfig.secondaryPopulationComparisonMetric;
+    }
+  });
+  const metricIds = Object.keys(metricConfigs) as MetricId[];
+  const query = new MetricQuery(metricIds as MetricId[], breakdowns);
+
+  const displayingCovidData = metrics
+    .map((config) => config.metricId)
+    .some((metricId) => metricId.includes("covid"));
+
+  return (
+    <CardWrapper
+      minHeight={PRELOAD_HEIGHT}
+      queries={[query]}
+      title={
+        <>{`Age Adjusted Ratio of ${
+          props.variableConfig.variableFullDisplayName
+        } By ${
+          BREAKDOWN_VAR_DISPLAY_NAMES[props.breakdownVar]
+        } In ${props.fips.getFullDisplayName()}`}</>
+      }
+    >
+      {([queryResponse]) => {
+        let dataWithoutUnknowns = queryResponse.data.filter(
+          (row: Row) =>
+            row[props.breakdownVar] !== UNKNOWN &&
+            row[props.breakdownVar] !== UNKNOWN_RACE &&
+            row[props.breakdownVar] !== UNKNOWN_ETHNICITY
+        );
+
+        return (
+          <>
+            {queryResponse.shouldShowMissingDataMessage(
+              metricIds as MetricId[]
+            ) && (
+              <CardContent>
+                <MissingDataAlert
+                  dataName={props.variableConfig.variableFullDisplayName + " "}
+                  breakdownString={
+                    BREAKDOWN_VAR_DISPLAY_NAMES[props.breakdownVar]
+                  }
+                  noDemographicInfo={
+                    props.variableConfig.variableId === VAXX &&
+                    props.fips.isCounty()
+                  }
+                  fips={props.fips}
+                />
+              </CardContent>
+            )}
+            {!queryResponse.dataIsMissing() &&
+              displayingCovidData &&
+              props.breakdownVar === RACE && (
+                <>
+                  <CardContent>
+                    <Alert severity="warning" role="note">
+                      Share of COVID-19 cases reported for American Indian,
+                      Alaska Native, Native Hawaiian and Pacific Islander are
+                      underrepresented at the national level and in many states
+                      because these racial categories are often not recorded.
+                      The Urban Indian Health Institute publishes{" "}
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={urlMap.uihiBestPractice}
+                      >
+                        guidelines for American Indian and Alaska Native Data
+                        Collection
+                      </a>
+                      .
+                    </Alert>
+                  </CardContent>
+                  <Divider />
+                </>
+              )}
+
+            {!queryResponse.dataIsMissing() && (
+              <div className={styles.TableChart}>
+                <AgeAdjustedTableChart
+                  data={dataWithoutUnknowns}
+                  breakdownVar={props.breakdownVar}
+                  metrics={Object.values(metricConfigs)}
+                />
+              </div>
+            )}
+          </>
+        );
+      }}
+    </CardWrapper>
+  );
+}
---
+++ frontend/src/charts/AgeAdjustedTableChart.tsx
@@ -0,0 +1,209 @@
+import React, { useMemo } from "react";
+import {
+  Column,
+  HeaderGroup,
+  Row,
+  usePagination,
+  useSortBy,
+  useTable,
+} from "react-table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import Paper from "@material-ui/core/Paper";
+import {
+  MetricConfig,
+  MetricId,
+  formatFieldValue,
+} from "../data/config/MetricConfig";
+import {
+  BREAKDOWN_VAR_DISPLAY_NAMES,
+  BreakdownVar,
+} from "../data/query/Breakdowns";
+import { Tooltip, useMediaQuery } from "@material-ui/core";
+import WarningRoundedIcon from "@material-ui/icons/WarningRounded";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import styles from "./Chart.module.scss";
+
+export const MAX_NUM_ROWS_WITHOUT_PAGINATION = 20;
+
+const cellStyle = {
+  width: "200px",
+};
+
+export interface AgeAdjustedTableChartProps {
+  data: Readonly<Record<string, any>>[];
+  breakdownVar: BreakdownVar;
+  metrics: MetricConfig[];
+}
+
+export function AgeAdjustedTableChart(props: AgeAdjustedTableChartProps) {
+  const wrap100kUnit = useMediaQuery("(max-width:500px)");
+
+  const { data, metrics, breakdownVar } = props;
+  let columns = metrics.map((metricConfig) => {
+    return {
+      Header: metricConfig.fullCardTitleName,
+      Cell: (a: any) =>
+        formatFieldValue(
+          /* metricType: MetricType, */ metricConfig.type,
+          /*   value: any, */ a.value,
+          /*   omitPctSymbol: boolean = false */ true
+        ),
+      accessor: metricConfig.metricId,
+    };
+  });
+  columns = [
+    {
+      Header: BREAKDOWN_VAR_DISPLAY_NAMES[breakdownVar],
+      Cell: (cell: any) => cell.value,
+      accessor: breakdownVar as MetricId,
+    },
+  ].concat(columns);
+
+  // Changes deps array to columns on save, which triggers reload loop
+  // eslint-disable-next-line
+  const memoCols = useMemo<Column<any>[]>(() => columns, [metrics]);
+  const memoData = useMemo(() => data, [data]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    gotoPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns: memoCols,
+      data: memoData,
+      initialState: {
+        pageSize: MAX_NUM_ROWS_WITHOUT_PAGINATION,
+        sortBy: [
+          {
+            id: breakdownVar,
+            desc: false,
+          },
+        ],
+      },
+    },
+    useSortBy,
+    usePagination
+  );
+
+  /** Component for the table's header row **/
+  function TableHeaderRow({ group }: { group: HeaderGroup<any> }) {
+    return (
+      <TableRow {...group.getHeaderGroupProps()}>
+        {group.headers.map((col, index) => (
+          <TableCell key={col.id} style={cellStyle}>
+            {col.render("Header")}
+          </TableCell>
+        ))}
+      </TableRow>
+    );
+  }
+
+  /** Component for the table's data rows **/
+  function TableDataRow({ row }: { row: Row<any> }) {
+    prepareRow(row);
+    return (
+      <TableRow {...row.getRowProps()}>
+        {row.cells.map((cell, index) =>
+          cell.value == null ? (
+            <TableCell {...cell.getCellProps()} style={cellStyle}>
+              <Tooltip title="No data available">
+                <WarningRoundedIcon />
+              </Tooltip>
+              <span className={styles.ScreenreaderTitleHeader}>
+                No Data Available
+              </span>
+            </TableCell>
+          ) : (
+            <TableCell {...cell.getCellProps()}>
+              {cell.render("Cell")}
+              <Units
+                column={index}
+                metric={props.metrics}
+                wrap100kUnit={wrap100kUnit}
+              />
+            </TableCell>
+          )
+        )}
+      </TableRow>
+    );
+  }
+
+  return (
+    <>
+      {props.data.length <= 0 || props.metrics.length <= 0 ? (
+        <h1>No Data provided</h1>
+      ) : (
+        <TableContainer component={Paper} style={{ maxHeight: "100%" }}>
+          <Table stickyHeader {...getTableProps()}>
+            <TableHead>
+              {headerGroups.map((group, index) => (
+                <TableHeaderRow group={group} key={index} />
+              ))}
+            </TableHead>
+            <TableBody {...getTableBodyProps()}>
+              {page.map((row: Row<any>, index) => (
+                <TableDataRow row={row} key={index} />
+              ))}
+            </TableBody>
+            {/* If the number of rows is less than the smallest page size, we can hide pagination */}
+            {props.data.length > MAX_NUM_ROWS_WITHOUT_PAGINATION && (
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    count={memoData.length}
+                    rowsPerPage={pageSize}
+                    page={pageIndex}
+                    onPageChange={(event, newPage) => {
+                      gotoPage(newPage);
+                    }}
+                    onChangeRowsPerPage={(event) => {
+                      setPageSize(Number(event.target.value));
+                    }}
+                    rowsPerPageOptions={[
+                      MAX_NUM_ROWS_WITHOUT_PAGINATION,
+                      MAX_NUM_ROWS_WITHOUT_PAGINATION * 2,
+                      MAX_NUM_ROWS_WITHOUT_PAGINATION * 5,
+                    ]} // If changed, update pagination condition above
+                  />
+                </TableRow>
+              </TableFooter>
+            )}
+          </Table>
+        </TableContainer>
+      )}
+    </>
+  );
+}
+
+interface UnitsProps {
+  column: number;
+  metric: MetricConfig[];
+  wrap100kUnit: boolean;
+}
+function Units(props: UnitsProps) {
+  if (!props.column) return null;
+
+  const unit =
+    props.column === 1
+      ? "per 100k"
+      : props.metric[props.column - 1].shortVegaLabel;
+
+  // inline vs block
+  return props.wrap100kUnit && props.column === 1 ? (
+    <p className={styles.Unit}>{unit}</p>
+  ) : (
+    <span className={styles.Unit}>{unit}</span>
+  );
+}
