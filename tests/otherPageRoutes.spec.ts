dff0c23e91a0668f1cad7e726cbe2db8c4135c93
Ben Hammond <benjamin.hammond@gmail.com>
2022-03-29 18:09:43 -0600

    parralel tests for speed. click EXPLORE btn on landing page test works

--- tests/otherPageRoutes.spec.ts
+++ tests/otherPageRoutes.spec.ts
@@ -1,46 +1,39 @@
 import { test, expect } from '@playwright/test';
 
-const HET = `http://localhost:3000/`
-const EXPLOREDATA = `exploredata/`
-const WIHE = `whatishealthequity/`
-const FAQS = `faqs/`
-const NEWS = `news/`
-const RESOURCES = `resources/`
-const DATA = `datacatalog/`
-const ABOUTUS = `aboutus/`
-const METHODOLOGY = `methodology/`
-const OURTEAM = `ourteam/`
-const CONTACT = `contact/`
-const TERMS = `termsofuse/`
-
-test('Home Page Loads', async ({ page }) => {
-    await page.goto(HET);
-    const mainHeading = page.locator('#main');
-    await expect(mainHeading).toContainText(['Advancing', 'Health', 'Equity']);
-});
-
+const HET = `http://localhost:3000`
+const EXPLOREDATA = `exploredata`
+const WIHE = `whatishealthequity`
+const FAQS = `faqs`
+const NEWS = `news`
+const RESOURCES = `resources`
+const DATA = `datacatalog`
+const ABOUTUS = `aboutus`
+const METHODOLOGY = `methodology`
+const OURTEAM = `ourteam`
+const CONTACT = `contact`
+const TERMS = `termsofuse`
 
 test('WIHE Page Loads', async ({ page }) => {
-    await page.goto(HET + WIHE);
+    await page.goto(`${HET}/${WIHE}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['What is Health Equity?']);
 });
 
 
 test('FAQ Tab Loads', async ({ page }) => {
-    await page.goto(HET + FAQS);
+    await page.goto(`${HET}/${FAQS}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Data']);
 });
 
 test('News Tab Loads', async ({ page }) => {
-    await page.goto(HET + NEWS);
+    await page.goto(`${HET}/${NEWS}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['News and Stories']);
 });
 
 test('Resources Tab Loads', async ({ page }) => {
-    await page.goto(HET + RESOURCES);
+    await page.goto(`${HET}/${RESOURCES}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Resources']);
 });
@@ -50,20 +43,20 @@ test('Resources Tab Loads', async ({ page }) => {
 
 
 test('Tracker Page Loads', async ({ page }) => {
-    await page.goto(HET + EXPLOREDATA);
+    await page.goto(`${HET}/${EXPLOREDATA}`);
     const madLib = page.locator('#onboarding-start-your-search');
     await expect(madLib).toContainText(['Investigate']);
 });
 
 test('Data Catalog Page Loads', async ({ page }) => {
-    await page.goto(HET + DATA);
+    await page.goto(`${HET}/${DATA}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['View and download Health Equity Tracker data sources']);
 });
 
 
 test('Methodology Tab Loads', async ({ page }) => {
-    await page.goto(HET + METHODOLOGY);
+    await page.goto(`${HET}/${METHODOLOGY}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Recommended Citation (APA) for the Health Equity Tracker:']);
 });
@@ -72,26 +65,26 @@ test('Methodology Tab Loads', async ({ page }) => {
 
 
 test('About Us Page / Project Tab Loads', async ({ page }) => {
-    await page.goto(HET + ABOUTUS);
+    await page.goto(`${HET}/${ABOUTUS}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`We're focused on equitable data.`]);
 });
 
 test('Our Team Tab Loads', async ({ page }) => {
-    await page.goto(HET + OURTEAM);
+    await page.goto(`${HET}/${OURTEAM}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`We're working towards a better tomorrow.`]);
 });
 
 test('Contact Tab Loads', async ({ page }) => {
-    await page.goto(HET + CONTACT);
+    await page.goto(`${HET}/${CONTACT}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toContainText([`Let's`, `move`, `equity`, `forward`]);
 });
 
 
 test('Terms of Use Page Loads', async ({ page }) => {
-    await page.goto(HET + TERMS);
+    await page.goto(`${HET}/${TERMS}`);
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`Terms of Use`]);
 });
