96b21fcc12377468a14769cd7bd78fc83ec9823b
Ben Hammond <benjamin.hammond@gmail.com>
2022-03-30 17:37:10 -0600

    tweak

--- frontend/src/cards/ui/CardLink.tsx
+++ frontend/src/cards/ui/CardLink.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import LinkIcon from "@material-ui/icons/Link";
-import { Grid } from "@material-ui/core";
+import { Button, Grid, Snackbar } from "@material-ui/core";
 import styles from "../Card.module.scss";
 import { CardId } from "../../utils/urlutils";
+import { Alert } from "@material-ui/lab";
 
 interface CardLinkProps {
   cardId: CardId;
 }
 
 export function CardLink(props: CardLinkProps) {
-  function handleClick() {
+  const [open, setOpen] = React.useState(false);
+
+  function handleClick(e: any) {
     const linkWithCardHash = window.location.href + props.cardId;
     navigator.clipboard.writeText(linkWithCardHash).then(
       () => {
+        setOpen(true);
+
         /* clipboard successfully set */
         console.log(linkWithCardHash);
       },
@@ -23,14 +28,20 @@ export function CardLink(props: CardLinkProps) {
     );
   }
 
+  const handleClose = (event: any) => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <a
-        href={window.location.href}
-        className={styles.CardLinkButton}
+      <Button
+        // className={styles.CardLinkButton}
+        color="primary"
         onClick={(e) => {
-          e.preventDefault();
-          handleClick();
+          // e.preventDefault();
+          // e.stopPropagation();
+          // e.nativeEvent.stopImmediatePropagation();
+          handleClick(e);
         }}
       >
         <Grid container alignItems="center">
@@ -41,7 +52,22 @@ export function CardLink(props: CardLinkProps) {
             Get Link
           </Grid>
         </Grid>
-      </a>
+      </Button>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        className={styles.SnackBar}
+      >
+        <Alert severity="success" onClose={handleClose}>
+          Direct link to {props.cardId} card copied to clipboard
+        </Alert>
+      </Snackbar>
     </>
   );
 }
