20c1da5f719521ee72776d49c71fb55b24e06d4c
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-10 22:59:24 -0600

    adds playwright flows; passing e2e tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -44,6 +44,9 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // launchOptions: {
+    //   slowMo: 1000
+    // }
   },
 
   /* Configure projects for major browsers */
