e7d191f393445de39ebe0a6ffa29f42a9530bebe
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-13 10:37:19 -0600

    fixing units on table

--- frontend/src/charts/TableChart.tsx
+++ frontend/src/charts/TableChart.tsx
@@ -204,6 +204,24 @@ export function TableChart(props: TableChartProps) {
   );
 }
 
+// interface UnitsProps {
+//   column: number;
+//   metric: MetricConfig[];
+//   wrap100kUnit: boolean;
+// }
+// function Units(props: UnitsProps) {
+//   if (!props.column) return null;
+
+//   const unit =
+//     props.metric[props.column - 1].type === "per100k" ? "per 100k" : "";
+
+//   // inline vs block
+//   return props.wrap100kUnit && props.column === 1 ? (
+//     <p className={styles.Unit}>{unit}</p>
+//   ) : (
+//     <span className={styles.Unit}>{unit}</span>
+//   );
+// }
 interface UnitsProps {
   column: number;
   metric: MetricConfig[];
@@ -213,7 +231,7 @@ function Units(props: UnitsProps) {
   if (!props.column) return null;
 
   const unit =
-    props.metric[props.column - 1].type === "per100k" ? "per 100k" : "";
+    props.column === 1 ? "per 100k" : props.metric[props.column - 1].shortLabel;
 
   // inline vs block
   return props.wrap100kUnit && props.column === 1 ? (
