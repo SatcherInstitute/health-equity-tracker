30d2277081fb9a93a80ce0b3ba74cd7b57fae78e
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-13 17:05:55 -0600

    adds test

--- frontend/src/data/config/MetricConfig.test.ts
+++ frontend/src/data/config/MetricConfig.test.ts
@@ -1,9 +1,16 @@
-import { isPctType, MetricType } from "./MetricConfig";
+import { formatFieldValue, isPctType, MetricType } from "./MetricConfig";
 
 describe("Test Metric Config Functions", () => {
   test("Test Detection of Percent Type", () => {
     expect(isPctType("pct")).toBe(true);
     expect(isPctType("per100k")).toBe(false);
     expect(isPctType("something broken" as MetricType)).toBe(false);
+  });
+
+  test("Test Formatting of Field Values", () => {
+    expect(formatFieldValue("pct", 33)).toBe("33.0%");
+    expect(formatFieldValue("pct", 33, true)).toBe("33.0");
+    expect(formatFieldValue("pct_share", 3, false)).toBe("3.0%");
+    expect(formatFieldValue("per100k", 30_000, false)).toBe("30,000");
   });
 });
