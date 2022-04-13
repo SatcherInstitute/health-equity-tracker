c983c0871c7598d796ba6b0a34b745c51bd854cf
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-13 10:20:33 -0600

    wording

--- frontend/src/cards/DisparityBarChartCard.tsx
+++ frontend/src/cards/DisparityBarChartCard.tsx
@@ -173,8 +173,8 @@ function DisparityBarChartCardWithKey(props: DisparityBarChartCardProps) {
                 these racial categories are not mutually exclusive. Individuals
                 who identify with multiple specific races (e.g. both "White" and
                 "Black") are represented multiple times in the visualization:
-                across each corresponding category, and in the "Two or More /
-                Unrepresented Race" category.
+                across each corresponding category, and also as "Two or more
+                races & Unrepresented race".
               </Alert>
             )}
           </>
