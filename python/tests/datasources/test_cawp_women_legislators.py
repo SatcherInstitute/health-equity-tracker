a76066e5ba0f42882bc630d459e62ed79ff77392
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-22 10:06:14 -0600

    cleanup

--- python/tests/datasources/test_cawp_women_legislators.py
+++ python/tests/datasources/test_cawp_women_legislators.py
@@ -1,15 +1,10 @@
 import json
 from unittest import mock
 import os
-
 import pandas as pd
 from pandas._testing import assert_frame_equal
-
+import ingestion.standardized_columns as std_col
 from datasources.cawp import (CAWPData,
-                              CAWP_TOTALS_URL,
-                              CAWP_LINE_ITEMS_FILE,
-                              PROPUB_US_HOUSE_FILE,
-                              PROPUB_US_SENATE_FILE,
                               get_standard_code_from_cawp_phrase,
                               get_pct,
                               count_matching_rows,
@@ -18,10 +13,6 @@ from datasources.cawp import (CAWPData,
                               STATE,
                               POSITION_COL, RACE_COL)
 
-import ingestion.standardized_columns as std_col
-
-
-# test utility functions
 
 def test_get_standard_code_from_cawp_phrase():
     assert get_standard_code_from_cawp_phrase("American Samoa - AS") == "AS"
@@ -58,55 +49,9 @@ def test_count_matching_rows():
         df_test, "Florida", NATIONAL, "All") == 1
     assert count_matching_rows(
         df_test, "United States", STATE, "All") == 2
-    # include "Multiracial Alone" +  multiple races e.g. "White, Black"
     assert count_matching_rows(
         df_test, "United States", NATIONAL, "Multiracial Alone") == 2
 
-
-# Map production URLs to mock CSVs
-mock_file_map = {
-    # state leg TOTALS by state (FULL FILE) mocking WEB CALL
-    CAWP_TOTALS_URL: {
-        "filename": 'cawp_test_input_totals.csv',  # FULL CSV FILE
-        "data_types": {
-            "State": str,
-            "State Rank": object,
-            "Senate": str,
-            "Total Women/Total Senate": str,
-            "House": str,
-            "Total Women/Total House": str,
-            "Total Women/Total Legislators": str,
-            "%Women Overall": str
-        }
-    },
-    # for LINE LEVEL mocking /data FOLDER
-    CAWP_LINE_ITEMS_FILE: {
-        "filename": 'cawp_test_input_line_items.csv',  # FULL CSV FILE
-        "data_types": {
-            "id": str,
-            "year": str,
-            "first_name": str,
-            "middle_name": str,
-            "last_name": str,
-            "party": str,
-            "level": str,
-            "position": str,
-            "state": str,
-            "district": str,
-            "race_ethnicity": str
-        }
-    },
-    # for US SENATE mocking /data FOLDER
-    PROPUB_US_SENATE_FILE: {
-        "filename": 'test_input_propublica-us-senate.json',  # FULL FILE
-        "data_types": {}
-    },
-    # for US HOUSE mocking /data FOLDER
-    PROPUB_US_HOUSE_FILE: {
-        "filename": 'test_input_propublica-us-house.json',  # FULL FILE
-        "data_types": {}
-    }
-}
 
 # Current working directory.
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -118,41 +63,52 @@ GOLDEN_DATA = {
 }
 
 
-def _get_test_csv_as_df(*args):
+def _get_test_line_items_csv_as_df(*args):
 
-    # read in correct CSV (mocking the network call to the CAWP api or /data)
-    filename_arg_index = 0
-    if len(args) > 1:
-        filename_arg_index = 1
+    [_folder, filename] = args
+    test_input_data_types = {
+        "id": str,
+        "year": str,
+        "first_name": str,
+        "middle_name": str,
+        "last_name": str,
+        "party": str,
+        "level": str,
+        "position": str,
+        "state": str,
+        "district": str,
+        "race_ethnicity": str
+    }
 
-    test_input_csv = mock_file_map[args[filename_arg_index]]["filename"]
-    test_input_dtype = mock_file_map[args[filename_arg_index]]["data_types"]
-    return pd.read_csv(os.path.join(TEST_DIR, test_input_csv),
-                       dtype=test_input_dtype)
+    test_input_filename = f'test_input_{filename}'
+    return pd.read_csv(os.path.join(TEST_DIR, test_input_filename),
+                       dtype=test_input_data_types)
 
 
-def _get_test_json_as_df(*args):
-    # read in correct json (mocking the call to /data or API)
-    filename_arg_index = 0
-    if len(args) > 1:
-        filename_arg_index = 1
+def _get_test_totals_csv_as_df(*args):
 
-    test_input_json = mock_file_map[args[filename_arg_index]]["filename"]
-    test_input_dtype = mock_file_map[args[filename_arg_index]]["data_types"]
-
-    return pd.read_json(os.path.join(TEST_DIR, test_input_json), dtype=test_input_dtype)
+    filename = "test_input_cawp_totals.csv"
+    test_input_data_types = {
+        "State": str,
+        "State Rank": object,
+        "Senate": str,
+        "Total Women/Total Senate": str,
+        "House": str,
+        "Total Women/Total House": str,
+        "Total Women/Total Legislators": str,
+        "%Women Overall": str
+    }
+    return pd.read_csv(os.path.join(TEST_DIR, filename),
+                       dtype=test_input_data_types)
 
 
 def _get_test_json_as_df_based_on_key_list(*args):
 
     [_mock_data_folder, mock_data_filename, mock_data_keys_list] = args
-
     test_json_filename = os.path.join(
         TEST_DIR, f'test_input_{mock_data_filename}')
-
     with open(test_json_filename) as data_file:
         data = json.load(data_file)
-
     df = pd.json_normalize(data, mock_data_keys_list)
     return df
 
@@ -161,7 +117,6 @@ def _get_test_pop_data_as_df(*args):
     [mock_pop_dir, mock_pop_filename, mock_pop_dtype] = args
     mock_pop_df = pd.read_json(os.path.join(
         TEST_DIR, mock_pop_dir, f'{mock_pop_filename}.json'), dtype=mock_pop_dtype)
-    print(mock_pop_df)
     return mock_pop_df
 
 
@@ -180,18 +135,15 @@ def _get_test_state_names(*args, **kwargs):
              side_effect=_get_test_json_as_df_based_on_key_list)
 @ mock.patch('ingestion.gcs_to_bq_util.load_df_from_bigquery',
              side_effect=_get_test_pop_data_as_df)
-@ mock.patch('ingestion.gcs_to_bq_util.load_json_as_df_from_data_dir',
-             side_effect=_get_test_json_as_df)
 @ mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
-             side_effect=_get_test_csv_as_df)
+             side_effect=_get_test_line_items_csv_as_df)
 @ mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_web',
-             side_effect=_get_test_csv_as_df)
+             side_effect=_get_test_totals_csv_as_df)
 @ mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq',
              return_value=None)
 def testWriteToBq(mock_bq: mock.MagicMock,
                   mock_web_csv: mock.MagicMock,
                   mock_data_dir_csv: mock.MagicMock,
-                  mock_data_dir_json: mock.MagicMock,
                   mock_pop_data: mock.MagicMock,
                   mock_data_dir_based_on_key_list_data: mock.MagicMock,
                   mock_bq_state_names: mock.MagicMock):
@@ -208,7 +160,6 @@ def testWriteToBq(mock_bq: mock.MagicMock,
     mock_bq.assert_called_once
     mock_web_csv.assert_called_once
     mock_data_dir_csv.assert_called_once
-    mock_data_dir_json.assert_called_once
     mock_pop_data.assert_called_once
     mock_data_dir_based_on_key_list_data.assert_called_once
     mock_bq_state_names.assert_called_once
@@ -237,26 +188,6 @@ def testWriteToBq(mock_bq: mock.MagicMock,
 
     mock_df_state = mock_bq.call_args_list[0].args[0]
     mock_df_national = mock_bq.call_args_list[1].args[0]
-
-    # save STATE results to file
-    mock_df_state.to_json(
-        "cawp-run-results-state.json", orient="records")
-
-    # save NATIONAL results to file
-    mock_df_national.to_json(
-        "cawp-run-results-national.json", orient="records")
-
-    # print("mock state results")
-    # print(mock_df_state.to_string())
-
-    # print("expected state output file")
-    # print(expected_df_state.to_string())
-
-    # print("mock national call results")
-    # print(mock_df_national.to_string())
-
-    # print("expected national output file")
-    # print(expected_df_national.to_string())
 
     # output created in mocked load_csv_as_df_from_web() should be the same as the expected df
     assert set(mock_df_state) == set(
