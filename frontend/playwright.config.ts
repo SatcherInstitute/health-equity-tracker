97f3b808118163c6a6a086d0dddab597509b167e
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-26 15:07:38 -0600

    fix

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -25,7 +25,7 @@ const config: PlaywrightTestConfig = {
   reporter: process.env.CI ? "dot" : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    browserName: 'firefox',
+    browserName: 'chromium',
     headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
