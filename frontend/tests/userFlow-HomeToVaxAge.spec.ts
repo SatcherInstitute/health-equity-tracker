01164831512f31ce6e6e1fdb4186d473099f6d2d
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-26 12:09:28 -0600

    re-enable HET tests

--- frontend/tests/userFlow-HomeToVaxAge.spec.ts
+++ frontend/tests/userFlow-HomeToVaxAge.spec.ts
@@ -1,49 +1,49 @@
-// import { test, expect } from '@playwright/test';
-// import { EXPLORE_DATA_PAGE_LINK } from './otherInternalPageRoutes.spec';
+import { test, expect } from '@playwright/test';
+import { EXPLORE_DATA_PAGE_LINK } from './otherInternalPageRoutes.spec';
 
-// const VAX_USA_RACE = `?mls=1.covid_vaccinations-3.00`
-// const BY_AGE = `&demo=age`
+const VAX_USA_RACE = `?mls=1.covid_vaccinations-3.00`
+const BY_AGE = `&demo=age`
 
-// test('User Flow from Home to Covid Vax by Age', async ({ page }) => {
+test('User Flow from Home to Covid Vax by Age', async ({ page }) => {
 
-//     // Landing Page Loads
-//     await page.goto('/');
-//     const mainHeading = page.locator('#main');
-//     await expect(mainHeading).toContainText(['Advancing', 'Health', 'Equity']);
+    // Landing Page Loads
+    await page.goto('/');
+    const mainHeading = page.locator('#main');
+    await expect(mainHeading).toContainText(['Advancing', 'Health', 'Equity']);
 
-//     // Clicking large CTA button takes us to the tracker
-//     const exploreButton = await page.locator('a:has-text("Explore the Health Equity Tracker")')
-//     exploreButton.click();
-//     await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}`);
+    // Clicking large CTA button takes us to the tracker
+    const exploreButton = await page.locator('a:has-text("Explore the Health Equity Tracker")')
+    exploreButton.click();
+    await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}`);
 
-//     // changes madlib to VAXX properly
-//     const madLibTopic = await page.locator('button:has-text("COVID-19")')
-//     madLibTopic.click();
-//     const covidVaxOption = await page.locator('span:has-text("COVID-19 Vaccinations")')
-//     covidVaxOption.click();
-//     await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}${VAX_USA_RACE}`);
+    // changes madlib to VAXX properly
+    const madLibTopic = await page.locator('button:has-text("COVID-19")')
+    madLibTopic.click();
+    const covidVaxOption = await page.locator('span:has-text("COVID-19 Vaccinations")')
+    covidVaxOption.click();
+    await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}${VAX_USA_RACE}`);
 
-//     // Confirm no failed Vega visualizations
-//     let mainChunk = await page.locator('main')
-//     await expect(mainChunk).not.toContainText("Oops")
-
-
-//     // MAP CARD contains correct title
-//     const mapCard = await page.locator('#map')
-//     await expect(mapCard).toContainText(['COVID-19 Vaccinations']);
-
-//     // Changing to AGE demographic toggle should change URL
-//     const ageToggleButton = await page.locator('button:has-text("Age")')
-//     ageToggleButton.click();
-//     await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}${VAX_USA_RACE}${BY_AGE}`);
-
-//     // Age should be reflected in card title
-//     const barChartCard = await page.locator('#bar')
-//     await expect(barChartCard).toContainText(['COVID-19', 'Vaccinations', 'By Age']);
-
-//     // Confirm no failed Vega visualizations
-//     mainChunk = await page.locator('main')
-//     await expect(mainChunk).not.toContainText("Oops")
+    // Confirm no failed Vega visualizations
+    let mainChunk = await page.locator('main')
+    await expect(mainChunk).not.toContainText("Oops")
 
 
-// });
+    // MAP CARD contains correct title
+    const mapCard = await page.locator('#map')
+    await expect(mapCard).toContainText(['COVID-19 Vaccinations']);
+
+    // Changing to AGE demographic toggle should change URL
+    const ageToggleButton = await page.locator('button:has-text("Age")')
+    ageToggleButton.click();
+    await expect(page).toHaveURL(`${EXPLORE_DATA_PAGE_LINK}${VAX_USA_RACE}${BY_AGE}`);
+
+    // Age should be reflected in card title
+    const barChartCard = await page.locator('#bar')
+    await expect(barChartCard).toContainText(['COVID-19', 'Vaccinations', 'By Age']);
+
+    // Confirm no failed Vega visualizations
+    mainChunk = await page.locator('main')
+    await expect(mainChunk).not.toContainText("Oops")
+
+
+});
--- frontend/tests/userFlow-TrackerToCovidDeathsDenverVsCO.spec.ts
+++ frontend/tests/userFlow-TrackerToCovidDeathsDenverVsCO.spec.ts
@@ -1,68 +1,68 @@
-// import { test, expect } from '@playwright/test';
-// import { EXPLORE_DATA_PAGE_LINK } from './otherInternalPageRoutes.spec';
+import { test, expect } from '@playwright/test';
+import { EXPLORE_DATA_PAGE_LINK } from './otherInternalPageRoutes.spec';
 
-// const COVID_DEN_VS_CO = "?mls=1.covid-3.08031-5.08&mlp=comparegeos"
+const COVID_DEN_VS_CO = "?mls=1.covid-3.08031-5.08&mlp=comparegeos"
 
-// test('User Flow from Default Tracker Page to Compare Covid Deaths by race between Denver County and State of Colorado', async ({ page }) => {
+test('User Flow from Default Tracker Page to Compare Covid Deaths by race between Denver County and State of Colorado', async ({ page }) => {
 
-//     // Landing Page Loads
-//     await page.goto(EXPLORE_DATA_PAGE_LINK);
+    // Landing Page Loads
+    await page.goto(EXPLORE_DATA_PAGE_LINK);
 
-//     // change carousel to "Compare Geo mode"
-//     const advanceMadlibCarouselArrowButton = await page.locator('id=onboarding-madlib-arrow')
-//     await advanceMadlibCarouselArrowButton.click();
+    // change carousel to "Compare Geo mode"
+    const advanceMadlibCarouselArrowButton = await page.locator('id=onboarding-madlib-arrow')
+    await advanceMadlibCarouselArrowButton.click();
 
-//     const madlibBox = await page.locator('id=onboarding-start-your-search')
-//     await expect(madlibBox).toContainText('Compare rates of');
-
-
-//     // Changing first location via madlib buttons
-//     const location1MadlibButton = await page.locator('#onboarding-start-your-search button:has-text("United States")')
-//     await location1MadlibButton.click();
-
-//     await page.fill('[placeholder="County, State, Territory, or United States"]', 'denver');
-//     await page.keyboard.press('Enter');
-
-//     const populationCard = await page.locator('id=populationCard')
-//     await expect(populationCard).toContainText("Denver County, Colorado")
-
-//     // Changing first location via madlib buttons
-//     const location2MadlibButton = await page.locator('#onboarding-start-your-search button:has-text("Georgia")')
-//     await location2MadlibButton.click();
-
-//     await page.fill('[placeholder="County, State, Territory, or United States"]', 'Colorado');
-//     await page.keyboard.press('Enter');
-
-//     // Confirm correct URL
-//     await expect(page).toHaveURL(EXPLORE_DATA_PAGE_LINK + COVID_DEN_VS_CO);
-
-//     // Confirm no failed Vega visualizations
-//     let mainChunk = await page.locator('main')
-//     await expect(mainChunk).not.toContainText("Oops")
+    const madlibBox = await page.locator('id=onboarding-start-your-search')
+    await expect(madlibBox).toContainText('Compare rates of');
 
 
-//     // Change both data types to COVID deaths
-//     await page.locator(':nth-match(:text("Deaths"), 2)').waitFor();
-//     const deathsToggleOption1 = await page.locator(':nth-match(:text("Deaths"), 1)')
-//     await deathsToggleOption1.click();
-//     const deathsToggleOption2 = await page.locator(':nth-match(:text("Deaths"), 2)')
-//     await deathsToggleOption2.click()
+    // Changing first location via madlib buttons
+    const location1MadlibButton = await page.locator('#onboarding-start-your-search button:has-text("United States")')
+    await location1MadlibButton.click();
 
-//     const missingDataLink = await page.locator('a:has-text("Read more about missing and misidentified people")')
-//     await missingDataLink.click()
+    await page.fill('[placeholder="County, State, Territory, or United States"]', 'denver');
+    await page.keyboard.press('Enter');
+
+    const populationCard = await page.locator('id=populationCard')
+    await expect(populationCard).toContainText("Denver County, Colorado")
+
+    // Changing first location via madlib buttons
+    const location2MadlibButton = await page.locator('#onboarding-start-your-search button:has-text("Georgia")')
+    await location2MadlibButton.click();
+
+    await page.fill('[placeholder="County, State, Territory, or United States"]', 'Colorado');
+    await page.keyboard.press('Enter');
+
+    // Confirm correct URL
+    await expect(page).toHaveURL(EXPLORE_DATA_PAGE_LINK + COVID_DEN_VS_CO);
+
+    // Confirm no failed Vega visualizations
+    let mainChunk = await page.locator('main')
+    await expect(mainChunk).not.toContainText("Oops")
 
 
-//     // Age-adjusted cards render
-//     const ageAdjustedCard1 = await page.locator('id=age-adjusted')
-//     // await ageAdjustedCard1.scrollIntoViewIfNeeded()
-//     const ageAdjustedCard2 = await page.locator('id=age-adjusted2')
-//     await expect(ageAdjustedCard1).toContainText("We do not currently have Age-Adjusted Risk of COVID-19 Death Compared to White (Non-Hispanic) broken down by Race And Ethnicity for Denver County")
-//     await expect(ageAdjustedCard2).toContainText("Age-Adjusted Risk of COVID-19 Death Compared to White (Non-Hispanic) in Colorado")
+    // Change both data types to COVID deaths
+    await page.locator(':nth-match(:text("Deaths"), 2)').waitFor();
+    const deathsToggleOption1 = await page.locator(':nth-match(:text("Deaths"), 1)')
+    await deathsToggleOption1.click();
+    const deathsToggleOption2 = await page.locator(':nth-match(:text("Deaths"), 2)')
+    await deathsToggleOption2.click()
 
-//     // Confirm no failed Vega visualizations
-//     mainChunk = await page.locator('main')
-//     await expect(mainChunk).not.toContainText("Oops")
-
+    const missingDataLink = await page.locator('a:has-text("Read more about missing and misidentified people")')
+    await missingDataLink.click()
 
 
-// });
+    // Age-adjusted cards render
+    const ageAdjustedCard1 = await page.locator('id=age-adjusted')
+    // await ageAdjustedCard1.scrollIntoViewIfNeeded()
+    const ageAdjustedCard2 = await page.locator('id=age-adjusted2')
+    await expect(ageAdjustedCard1).toContainText("We do not currently have Age-Adjusted Risk of COVID-19 Death Compared to White (Non-Hispanic) broken down by Race And Ethnicity for Denver County")
+    await expect(ageAdjustedCard2).toContainText("Age-Adjusted Risk of COVID-19 Death Compared to White (Non-Hispanic) in Colorado")
+
+    // Confirm no failed Vega visualizations
+    mainChunk = await page.locator('main')
+    await expect(mainChunk).not.toContainText("Oops")
+
+
+
+});
