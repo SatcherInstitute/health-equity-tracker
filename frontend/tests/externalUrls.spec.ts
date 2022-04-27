c974d5cd502d88f50d46fc60bd46c982a5fd1246
Ben Hammond <benjamin.hammond@gmail.com>
2022-04-27 15:32:11 -0600

    load instead of dom content

--- frontend/tests/externalUrls.spec.ts
+++ frontend/tests/externalUrls.spec.ts
@@ -10,7 +10,7 @@ for (const url of Object.values(urlMap)) {
     if (!url) continue
     test(`External URL Test: ${url}`, async ({ page }) => {
         console.log(url);
-        const response = await page.goto(url, { waitUntil: "domcontentloaded" });
+        const response = await page.goto(url);
         if (response.status() !== 200) console.log(response.status());
         // if (response.status() === 999) return // skip linkedin blocking
         // await expect(response.ok()).toBeTruthy()
@@ -21,7 +21,7 @@ for (const url of PARTNERS.map(partner => partner.url)) {
     if (!url) continue
     test(`Team Page: Partner External URL Test: ${url}`, async ({ page }) => {
         console.log(url);
-        const response = await page.goto(url, { waitUntil: "domcontentloaded" });
+        const response = await page.goto(url);
         if (response.status() !== 200) console.log(url, response.status());
         // await expect(response.ok()).toBeTruthy()
     });
@@ -31,7 +31,7 @@ for (const url of GOOGLE_FELLOWS.filter(fellow => fellow.link).map(fellow => fel
     if (!url) continue
     test(`Team Page: Google Fellow External URL Test: ${url}`, async ({ page }) => {
         console.log(url);
-        const response = await page.goto(url, { waitUntil: "domcontentloaded" })
+        const response = await page.goto(url)
         if (response.status() !== 200) console.log(url, response.status());
         // await expect(response.ok()).toBeTruthy()
     });
@@ -41,7 +41,7 @@ for (const url of RESOURCES.filter(resource => resource.url).map(fellow => fello
     if (!url) continue
     test(`Resource Page: External URL Test: ${url}`, async ({ page }) => {
         console.log(url);
-        const response = await page.goto(url, { waitUntil: "domcontentloaded" });
+        const response = await page.goto(url);
         if (response.status() !== 200) console.log(url, response.status());
         // await expect(response.ok()).toBeTruthy()
     });
