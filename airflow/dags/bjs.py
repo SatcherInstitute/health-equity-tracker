4b0ba389bb8ee585cb9d5a4c5ad4fde0a7744fe6
Ben Hammond <benjamin.hammond@gmail.com>
2022-05-16 22:18:23 -0600

    add DAG step; pare down test pop files

--- airflow/dags/bjs.py
+++ airflow/dags/bjs.py
@@ -19,7 +19,7 @@ data_ingestion_dag = DAG(
 
 bjs_bq_payload = util.generate_bq_payload(
     _BJS_WORKFLOW_ID, _BJS_DATASET_NAME)
-bjs_pop_bq_operator = util.create_bq_ingest_operator(
+bjs_bq_operator = util.create_bq_ingest_operator(
     'bjs_to_bq', bjs_bq_payload, data_ingestion_dag)
 
 bjs_aggregator_payload = {
@@ -33,4 +33,4 @@ bjs_exporter_operator = util.create_exporter_operator(
     'bjs_exporter', bjs_exporter_payload, data_ingestion_dag)
 
 # Ingestion DAG
-bjs_pop_bq_operator >> bjs_aggregator_operator >> bjs_exporter_operator
+bjs_bq_operator >> bjs_aggregator_operator >> bjs_exporter_operator
