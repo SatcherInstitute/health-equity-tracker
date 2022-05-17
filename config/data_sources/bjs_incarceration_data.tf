a5e37abb82e41d03910c1a2a412da7fdbedf33c2
Ben Hammond <benjamin.hammond@gmail.com>
2022-05-16 21:14:56 -0600

    try different naming

--- config/data_sources/bjs_incarceration_data.tf
+++
@@ -1,7 +0,0 @@
-# Resources and routines for BJS_INCARCERATION Data ingestion.
-
-# Create a BigQuery dataset for BJS_INCARCERATION data.
-resource "google_bigquery_dataset" "bjs_incarceration" {
-  dataset_id = "bjs_incarceration_data"
-  location   = "US"
-}
