51a36e1e10b8ded9fc7a00813f738ad0fe867af1
Ben Hammond <benjamin.hammond@gmail.com>
2022-05-24 13:15:40 -0600

    flag icon

--- frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
+++ frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
@@ -4,11 +4,19 @@ import AlertTitle from "@material-ui/lab/AlertTitle";
 import styles from "./IncarceratedChildrenLongAlert.module.scss";
 import { Link } from "react-router-dom";
 import { METHODOLOGY_TAB_LINK } from "../../utils/urlutils";
+// import GavelIcon from "@material-ui/icons/Gavel";
+import FlagIcon from "@material-ui/icons/Flag";
 
 function IncarceratedChildrenLongAlert() {
   return (
     <div>
-      <Alert severity="error" className={styles.ReportAlert} role="note">
+      <Alert
+        severity="error"
+        className={styles.ReportAlert}
+        // icon={<GavelIcon />}
+        icon={<FlagIcon />}
+        role="note"
+      >
         <AlertTitle>Children in Adult Prisons</AlertTitle>
         <p>
           Although the criminal justice system makes distinctions between adults
