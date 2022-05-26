b53471169b175cc6f668d0ce3c0dbfe339f0e898
Ben Hammond <benjamin.hammond@gmail.com>
2022-05-26 13:10:04 -0600

    update text from aye

--- frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
+++ frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
@@ -20,21 +20,21 @@ function IncarceratedChildrenLongAlert() {
         <AlertTitle>Children in Adult Prisons</AlertTitle>
         <p>
           Although the criminal justice system makes distinctions between adults
-          and children, individual states have transfer laws that remove
-          children from the protective cover of these distinctions. Children in
-          adult prisons are more exposed to physical and sexual abuse, fewer
-          age-appropriate services, and worse health outcomes. Lower
-          socio-economic status and the inability to properly understand and
-          navigate the criminal justice system are some of the reasons why
-          children of color are overrepresented in adult prisons. These racial
-          disparities are an unfortunate reflection of the inequities that
-          plague the broader criminal justice system.
+          and children, individual states have laws that remove children from
+          the protective cover of these distinctions and enable the
+          incarceration of children in adult facilities. Such children are more
+          exposed to physical and sexual abuse, fewer age-appropriate services,
+          and worse health outcomes. Lower socioeconomic status and the
+          inability to properly understand and navigate the criminal justice
+          process amongst others are responsible for the overrepresentation of
+          children of color in adult facilities and are reflective of the
+          inequities that plague the entire criminal justice system.
         </p>
         <p>
           When showing reports filtered by age, we have highlighted the{" "}
-          <b>total count of these imprisoned children</b>, rather than
-          presenting a per 100k rate which would need to be calculated using an
-          ambiguous population base. Read more in{" "}
+          <b>total number of these imprisoned children</b>, rather than
+          presenting a per 100k rate calculated using an ambiguous population
+          base. Read more in{" "}
           <Link to={METHODOLOGY_TAB_LINK}>our methodology</Link>.
         </p>
       </Alert>
