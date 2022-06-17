a335e51bd2ad5df5a59b2899aefb38f76a16a82c
Ben Hammond <benjamin.hammond@gmail.com>
2022-06-17 13:28:29 -0600

    Delete bjs_data.tf

--- config/data_sources/bjs_data.tf
+++
@@ -1,7 +0,0 @@
-# Resources and routines for BJS Data ingestion.
-
-# Create a BigQuery dataset for BJS data.
-resource "google_bigquery_dataset" "bjs" {
-  dataset_id = "bjs_data"
-  location   = "US"
-}
