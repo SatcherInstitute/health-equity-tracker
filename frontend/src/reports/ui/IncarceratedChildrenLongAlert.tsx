4398d8adb2a4ba3cbee0c2ce4d460c99cf5baaf9
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-01 10:59:05 -0600

    update alert text

--- frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
+++ frontend/src/reports/ui/IncarceratedChildrenLongAlert.tsx
@@ -16,20 +16,38 @@ function IncarceratedChildrenLongAlert() {
         icon={<FlagIcon />}
         role="note"
       >
-        <AlertTitle>Jails and Prison Incarceration</AlertTitle>
+        <AlertTitle>The 3 Ws of Incarceration: Where, What and Why</AlertTitle>
 
         <p>
-          Jail is this....is this....is this....is this....is this..... prison
-          iis this....is this....is this....is this....is this..... Although the
-          criminal justice system makes distinctions between adults and
-          children, individual states have laws that remove children from the
-          protective cover of these distinctions and{" "}
+          <b>Where are Justice involved individuals confined?</b> A Prison or a
+          Jail.
+        </p>
+        <p>
+          <b>
+            What determines the type of facility where a justice involved
+            individual is confined?
+          </b>
+        </p>
+        <ul>
+          <li>
+            <b>Prison</b>: when the justice-involved individual is serving a
+            sentence of more than one year.
+          </li>
+          <li>
+            <b>Jail</b>: when the justice-involved individual is either awaiting
+            trial for certain crimes or serving a sentence of one year or less.
+          </li>
+        </ul>
+
+        <p>
+          <b>Why are there Children in Adult Prison and Jail Facilities?</b>{" "}
+          There are children in adult prisons and jails because some state laws{" "}
           <a target="_blank" rel="noreferrer" href={urlMap.prisonPolicy}>
-            enable the incarceration of children in adult institutions
-          </a>
-          . Such children are more exposed to physical and sexual abuse, fewer
-          age-appropriate services, and worse health outcomes. When reporting on
-          incarceration, we highlight the{" "}
+            enable children to be prosecuted as adults
+          </a>{" "}
+          in certain circumstances. Such children are more exposed to fewer
+          age-appropriate services, physical and sexual abuse, and worse health
+          outcomes. When reporting on incarceration, we highlight the{" "}
           <b>total number of confined children</b> in adult facilities. Read
           more in <Link to={METHODOLOGY_TAB_LINK}>our methodology</Link>.
         </p>
