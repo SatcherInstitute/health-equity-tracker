3d20702c63afbc37113ffd43d83627c221a8a905
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2022-07-06 16:53:42 -0400

    added filtering for svi

--- python/datasources/cdc_svi_county.py
+++ python/datasources/cdc_svi_county.py
@@ -2,8 +2,8 @@ import ingestion.standardized_columns as std_col
 
 from datasources.data_source import DataSource
 from ingestion import gcs_to_bq_util
+from numpy import NaN
 
-from ingestion.dataset_utils import merge_fips_codes
 
 # need to add merge_utils file, temporarily removed to pass test
 
@@ -12,10 +12,11 @@ BASE_CDC_URL = 'https://data.cdc.gov/resource/8xkx-amqh.csv'
 FILE_SIZE_LIMIT = 5000
 
 columns_to_standard = {
-            "FIPS": std_col.COUNTY_FIPS_COL,
-            "LOCATION": std_col.COUNTY_NAME_COL,
-            "RPL_THEMES": std_col.SVI,
+    "FIPS": std_col.COUNTY_FIPS_COL,
+    "LOCATION": std_col.COUNTY_NAME_COL,
+    "RPL_THEMES": std_col.SVI,
 }
+
 
 class CDCSviCounty(DataSource):
 
@@ -35,7 +36,8 @@ class CDCSviCounty(DataSource):
         # params = {"$limit": FILE_SIZE_LIMIT}
         # df = gcs_to_bq_util.load_csv_as_df_from_web(
         #     BASE_CDC_URL, dtype={COUNTY_FIPS_COL: str}, params=params)
-        df = gcs_to_bq_util.load_csv_as_df_from_data_dir('cdc_svi_county', "cdc_svi_county_totals.csv")
+        df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
+            'cdc_svi_county', "cdc_svi_county_totals.csv")
 
         # print(df.to_string())
 
@@ -57,7 +59,13 @@ class CDCSviCounty(DataSource):
             return x.split(",")[0]
 
         def round_svi(x):
-            return round(x,2)
+            if x > 1:
+                raise ValueError(
+                    'The provided SVI is not an expected number between 0.0-1.0')
+            if x == -999.0:
+                return NaN
+            if x >= 0 or x <= 1:
+                return round(x, 2)
 
         df["county_name"] = df["county_name"].apply(update_county_name)
         df["svi"] = df["svi"].apply(round_svi)
@@ -65,9 +73,8 @@ class CDCSviCounty(DataSource):
         df[std_col.AGE_COL] = std_col.ALL_VALUE
         cols_to_keep = [*columns_to_standard.values(), std_col.AGE_COL]
         df = df[cols_to_keep]
-        
+
         print("\n")
         print(df.to_string())
-         
 
         return df
