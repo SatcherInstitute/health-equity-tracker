3706824b19358efa6021d98b20c9463a23b312da
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-07 13:51:53 -0600

    settings

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -27,10 +27,13 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
-    // headless: false,
-    headless: true,
+    launchOptions: {
+      slowMo: 1000,
+    },
+    headless: false,
+    // headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 10_000,
     baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
