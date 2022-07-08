5d14f685886d6965a7d073cbbe73bd2638f36829
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-07 20:47:12 -0600

    revert playwright changes; should work

--- frontend/playwright-tests/externalUrls.spec.ts
+++ frontend/playwright-tests/externalUrls.spec.ts
@@ -12,7 +12,7 @@ for (const url of Object.values(urlMap)) {
     if (!url || knownFlakyUrls.includes(url)) continue
     test(`${url}`, async ({ page }) => {
         const response = await page.goto(url, { waitUntil: "domcontentloaded" });
-        if (response?.status() !== 200) console.log(url, response?.status());
+        if (response.status() !== 200) console.log(url, response.status());
     });
 }
 
@@ -20,7 +20,7 @@ for (const url of PARTNERS.map(partner => partner.url)) {
     if (!url || knownFlakyUrls.includes(url)) continue
     test(`Team Page - Partner: ${url}`, async ({ page }) => {
         const response = await page.goto(url, { waitUntil: "domcontentloaded" });
-        if (response?.status() !== 200) console.log(url, response?.status());
+        if (response.status() !== 200) console.log(url, response.status());
     });
 }
 
@@ -28,7 +28,7 @@ for (const url of GOOGLE_FELLOWS.filter(fellow => fellow.link).map(fellow => fel
     if (!url || knownFlakyUrls.includes(url)) continue
     test(`Team Page - Google Fellow: ${url}`, async ({ page }) => {
         const response = await page.goto(url, { waitUntil: "domcontentloaded" })
-        if (response?.status() !== 200) console.log(url, response?.status());
+        if (response.status() !== 200) console.log(url, response.status());
     });
 }
 
@@ -37,6 +37,6 @@ for (const url of RESOURCES.filter(resource => resource.url).map(fellow => fello
     test(`Resource Page: ${url}`, async ({ page }) => {
         const response = await page.goto(url);
         // console.log(url, response)
-        if (response?.status() !== 200) console.log(url, response?.status());
+        if (response.status() !== 200) console.log(url, response.status());
     });
 }
