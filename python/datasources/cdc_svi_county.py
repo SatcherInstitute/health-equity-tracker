287fa92fb8f5bf7434088c399314896ae8b019c4
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2022-07-08 10:42:30 -0400

    updated svi tests

--- python/datasources/cdc_svi_county.py
+++ python/datasources/cdc_svi_county.py
@@ -2,11 +2,9 @@ import ingestion.standardized_columns as std_col
 
 from datasources.data_source import DataSource
 from ingestion import gcs_to_bq_util
-from numpy import NaN
+import numpy as np
 
-
-# need to add merge_utils file, temporarily removed to pass test
-
+from ingestion.merge_utils import (merge_county_names)
 
 BASE_CDC_URL = 'https://data.cdc.gov/resource/8xkx-amqh.csv'
 FILE_SIZE_LIMIT = 5000
@@ -16,6 +14,27 @@ columns_to_standard = {
     "LOCATION": std_col.COUNTY_NAME_COL,
     "RPL_THEMES": std_col.SVI,
 }
+
+
+def format_svi(value):
+    """
+    Takes the RPL_THEMES column and formats it to match an expected number between 0.0 - 1.0,
+    or null. If the RPL_THEMES column that is greater than 1.0, this function raises an 
+    assertion error. The columns that have a value within the expected range, are then rounded
+    to two decimal places. The value is then outputted on the svi column of the dataframe. 
+
+    Parameters:
+        svi: number
+
+    Returns:
+        df: return svi wih two decimal places, nan, or an assertion error. 
+    """
+    if value == -999.0:
+        return np.nan
+    if value >= 0 and value <= 1:
+        return round(value, 2)
+    raise ValueError(
+        f'The provided SVI: {value} is not an expected number between 0.0-1.0')
 
 
 class CDCSviCounty(DataSource):
@@ -36,39 +55,26 @@ class CDCSviCounty(DataSource):
         # params = {"$limit": FILE_SIZE_LIMIT}
         # df = gcs_to_bq_util.load_csv_as_df_from_web(
         #     BASE_CDC_URL, dtype={COUNTY_FIPS_COL: str}, params=params)
-        df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
-            'cdc_svi_county', "cdc_svi_county_totals.csv")
+        for geo in ['county']:
+            df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
+                'cdc_svi_county', "cdc_svi_county_totals.csv")
 
-        # print(df.to_string())
+            df = self.generate_for_bq(df, geo)
 
-        df = self.generate_for_bq(df)
+            column_types = {c: 'STRING' for c in df.columns}
+            column_types[std_col.SVI] = 'FLOAT'
 
-        column_types = {c: 'STRING' for c in df.columns}
-        column_types[std_col.SVI] = 'FLOAT'
+            gcs_to_bq_util.add_df_to_bq(
+                df, dataset, "age", column_types=column_types)
 
-        # print(df.to_string())
-
-        gcs_to_bq_util.add_df_to_bq(
-            df, dataset, "age", column_types=column_types)
-
-    def generate_for_bq(self, df):
+    def generate_for_bq(self, df, geo):
 
         df = df.rename(columns=columns_to_standard)
 
-        def update_county_name(x: str):
-            return x.split(",")[0]
+        if geo == 'county':
+            df = merge_county_names(df)
 
-        def round_svi(x):
-            if x > 1:
-                raise ValueError(
-                    'The provided SVI is not an expected number between 0.0-1.0')
-            if x == -999.0:
-                return NaN
-            if x >= 0 or x <= 1:
-                return round(x, 2)
-
-        df["county_name"] = df["county_name"].apply(update_county_name)
-        df["svi"] = df["svi"].apply(round_svi)
+        df["svi"] = df["svi"].apply(format_svi)
 
         df[std_col.AGE_COL] = std_col.ALL_VALUE
         cols_to_keep = [*columns_to_standard.values(), std_col.AGE_COL]
