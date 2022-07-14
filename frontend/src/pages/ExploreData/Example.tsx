a51a389b3fc9f71c5093a20132b53706e5994a95
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-14 13:12:46 -0600

    add example elemnt

---
+++ frontend/src/pages/ExploreData/Example.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { FixedSizeList as List } from "react-window";
+import { Fips, FIPS_MAP } from "../../data/utils/Fips";
+import { PhraseSegment } from "../../utils/MadLibs";
+
+function getOptionsFromPhraseSegement(phraseSegment: PhraseSegment): Fips[] {
+  console.log("test");
+
+  return Object.keys(phraseSegment)
+    .sort((a: string, b: string) => {
+      if (a.length === b.length) {
+        return a.localeCompare(b);
+      }
+      return b.length > a.length ? -1 : 1;
+    })
+    .map((fipsCode) => new Fips(fipsCode));
+}
+
+const items = getOptionsFromPhraseSegement(FIPS_MAP);
+
+const Row = ({ index, style }: { index: number; style: any }) => {
+  const placeFips = items[index];
+
+  return placeFips ? (
+    <div style={style}>{placeFips.getDisplayName()}</div>
+  ) : (
+    <></>
+  );
+};
+
+export const Example = () => (
+  <List height={150} itemCount={items.length} itemSize={35} width={300}>
+    {Row}
+  </List>
+);
