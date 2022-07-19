ccead959535755b41b8be69153f6c2d9b09b1a69
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-19 09:20:53 -0600

    missing work

--- frontend/src/reports/ui/LifelineAlert.tsx
+++ frontend/src/reports/ui/LifelineAlert.tsx
@@ -19,11 +19,12 @@ function LifelineAlert() {
  */}
         <AlertTitle>988 Suicide & Crisis Lifeline</AlertTitle>
         <p>
-          24/7, free and confidential support, prevention and crisis resources,
-          and professional best practices, call <a href="tel:988">9-8-8</a> or
-          visit <a href={urlMap.lifeline}>988lifeline.org</a>. If you or a loved
-          one is experiencing an emergency, call 911 or go to your nearest
-          emergency room.
+          For 24/7, free and confidential support, prevention and crisis
+          resources, and professional best practices, call{" "}
+          <a href="tel:988">9-8-8</a> or visit{" "}
+          <a href={urlMap.lifeline}>988lifeline.org</a>. If you or a loved one
+          is experiencing an emergency, call 911 or go to your nearest emergency
+          room.
         </p>
       </Alert>
     </div>
