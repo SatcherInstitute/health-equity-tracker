47447e3763357481264886e10a32367e613f46cb
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-20 15:04:51 -0600

    timing test

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -66,7 +66,7 @@ function LandingPage() {
               paragraph={true}
               component="h3"
             >
-              One Year of Advancing Health Equity
+              One Year of Advancing Health Equity - timing test
             </Typography>
             <Typography
               className={styles.HeaderSubtext}
