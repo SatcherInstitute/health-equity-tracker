8b0fd668f55c466b0500f9d252d2379e4502d1d8
Eric Warren <eric.m.warren1@gmail.com>
2022-07-21 15:42:15 -0400

    Added React Router Hash Link

--- frontend/src/cards/ui/SviAlert.tsx
+++ frontend/src/cards/ui/SviAlert.tsx
@@ -4,6 +4,8 @@ import { MetricQueryResponse } from "../../data/query/MetricQuery";
 import { Fips } from "../../data/utils/Fips";
 import { urlMap } from "../../utils/externalUrls";
 import styles from "./SviAlert.module.scss";
+import { HashLink as Link } from "react-router-hash-link";
+import { METHODOLOGY_TAB_LINK } from "../../utils/internalRoutes";
 
 interface SviAlertProps {
   svi: number;
@@ -47,9 +49,9 @@ function SviAlert(props: SviAlertProps) {
         <Alert severity="info" className={styles.Alert}>
           This county has a social vulnerability index of <b>{props.svi}</b>;
           which indicates a{" "}
-          <a href={urlMap.cdcSvi} className={color}>
+          <Link to={`${METHODOLOGY_TAB_LINK}#svi`} className={color}>
             <b>{rating} level of vulnerability.</b>
-          </a>
+          </Link>
         </Alert>
       )}
     </>
