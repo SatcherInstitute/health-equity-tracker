ec6802341c67a8364fb6da1acb7576fbb3012dd7
Ben Hammond <benjamin.hammond@gmail.com>
2022-07-26 16:32:22 -0600

    add placeholder cards for 2N

---
+++ frontend/src/cards/RateTrendsChartCard.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { CardContent } from "@material-ui/core";
+import { Fips } from "../data/utils/Fips";
+import {
+  Breakdowns,
+  BreakdownVar,
+  BREAKDOWN_VAR_DISPLAY_NAMES,
+} from "../data/query/Breakdowns";
+import { MetricQuery } from "../data/query/MetricQuery";
+import {
+  // isPctType,
+  MetricId,
+  VariableConfig,
+} from "../data/config/MetricConfig";
+import CardWrapper from "./CardWrapper";
+import { exclude } from "../data/query/BreakdownFilter";
+import { NON_HISPANIC } from "../data/utils/Constants";
+import MissingDataAlert from "./ui/MissingDataAlert";
+
+/* minimize layout shift */
+const PRELOAD_HEIGHT = 668;
+
+export interface RateTrendsChartCardProps {
+  key?: string;
+  breakdownVar: BreakdownVar;
+  variableConfig: VariableConfig;
+  fips: Fips;
+}
+
+// Intentionally removed key wrapper found in other cards as 2N prefers card not re-render
+// and instead D3 will handle updates to the data
+export function RateTrendsChartCard(props: RateTrendsChartCardProps) {
+  const metricConfig = props.variableConfig.metrics["per100k"];
+
+  const metricIdsToFetch: MetricId[] = [];
+  metricIdsToFetch.push(metricConfig.metricId);
+
+  const breakdowns = Breakdowns.forFips(props.fips).addBreakdown(
+    props.breakdownVar,
+    exclude(NON_HISPANIC)
+  );
+
+  const query = new MetricQuery(metricIdsToFetch, breakdowns);
+
+  function getTitleText() {
+    return `Trends in ${metricConfig.fullCardTitleName} By ${
+      BREAKDOWN_VAR_DISPLAY_NAMES[props.breakdownVar]
+    } In ${props.fips.getSentenceDisplayName()}`;
+  }
+  function CardTitle() {
+    return <>{getTitleText()}</>;
+  }
+
+  return (
+    <CardWrapper
+      queries={[query]}
+      title={<CardTitle />}
+      minHeight={PRELOAD_HEIGHT}
+    >
+      {([queryResponse]) => {
+        const data = queryResponse.getValidRowsForField(metricConfig.metricId);
+        console.log(data);
+        return (
+          <CardContent>
+            {queryResponse.shouldShowMissingDataMessage([
+              metricConfig.metricId,
+            ]) ? (
+              <>
+                <MissingDataAlert
+                  dataName={metricConfig.fullCardTitleName}
+                  breakdownString={
+                    BREAKDOWN_VAR_DISPLAY_NAMES[props.breakdownVar]
+                  }
+                  fips={props.fips}
+                />
+              </>
+            ) : (
+              <>
+                {/* 2N INCIDENCE RATE TRENDS VIZ COMPONENT HERE */}
+                <img src="/img.webp" alt="rick roll flow chart" height="300" />
+              </>
+            )}
+          </CardContent>
+        );
+      }}
+    </CardWrapper>
+  );
+}
