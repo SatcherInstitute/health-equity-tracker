94f07d4d30c342014b4db65e21bccc9255baba55
miasza <mia@two-n.com>
2022-07-29 16:26:16 -0400

    add initial tooltip container

---
+++ frontend/src/charts/trendsChart/tooltip.tsx
@@ -0,0 +1,21 @@
+/* External Imports */
+import React from "react";
+
+/* Local Imports */
+
+/* Styles */
+import styles from "./Trends.module.scss";
+
+/* Components */
+
+/* Constants */
+import { TrendsData, ColorScale } from "./types";
+/* Helpers */
+
+/* Define type interface */
+export interface TooltipProps {}
+
+/* Render component */
+export function Tooltip({}: TooltipProps) {
+  return <div>tooltip</div>;
+}
