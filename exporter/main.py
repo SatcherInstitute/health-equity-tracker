6dada7df15777fc0c0f3b3d1ac48c04eed5cc466
Ben Hammond <benjamin.hammond@gmail.com>
2022-08-26 16:15:16 -0600

    rm logs

--- exporter/main.py
+++ exporter/main.py
@@ -13,13 +13,9 @@ def export_dataset_tables():
     """Exports the tables in the given dataset to GCS.
 
        Request form must include the dataset name."""
-
-    print("*&*&*&*&* start of export_dataset_tables()")
-
     data = request.get_json()
 
     if data.get('dataset_name') is None:
-        print("NO DATASET NAME XXXXX")
         return ('Request must include dataset name.', 400)
 
     dataset_name = data['dataset_name']
@@ -34,15 +30,10 @@ def export_dataset_tables():
     # If there are no tables in the dataset, return an error so the pipeline will alert
     # and a human can look into any potential issues.
     if not tables:
-        print("no tables XXXXXXX")
         return ('Dataset has no tables.', 500)
 
-    print("right before table loop")
-
     for table in tables:
-
         # also split up county-level tables by state and export those individually
-        print("-----> table id", table.table_id)
         export_split_county_tables(bq_client, table, export_bucket)
 
         # export the full table
@@ -69,7 +60,6 @@ def export_dataset_tables():
 
 def export_table(bq_client, table_ref, dest_uri, dest_fmt):
     """ Run the extract job to export the given table to the given destination and wait for completion"""
-    print("made it inside export table")
     job_config = bigquery.ExtractJobConfig(destination_format=dest_fmt)
     extract_job = bq_client.extract_table(
         table_ref, dest_uri, location='US', job_config=job_config)
@@ -79,7 +69,6 @@ def export_table(bq_client, table_ref, dest_uri, dest_fmt):
 
 def export_split_county_tables(bq_client, table, export_bucket):
     """ Split county-level table by parent state FIPS, and export as individual blobs to the given destination and wait for completion"""
-    print("made it inside export_split_county_tables() ")
     table_name = "{}.{}.{}".format(
         table.project, table.dataset_id, table.table_id)
     if "county" not in table_name:
@@ -92,18 +81,12 @@ def export_split_county_tables(bq_client, table, export_bucket):
                 SELECT *
                 FROM {table_name}
                 WHERE county_fips LIKE '{fips}___'
-                LIMIT 2
                 """
 
-            print("from", table_name, "to", state_file_name)
+            print("extracting", table_name, "to", state_file_name)
 
             query_job = bq_client.query(query)
-
             rows_df = query_job.to_dataframe()
-
-            print(rows_df)
-
-            print("***")
             storage_client = storage.Client()  # Storage API request
 
             bucket = storage_client.get_bucket(export_bucket)
@@ -125,59 +108,59 @@ if __name__ == "__main__":
 
 STATE_LEVEL_FIPS_TO_NAME_MAP = {
     "01": "Alabama",
-    # "02": "Alaska",
-    # "04": "Arizona",
-    # "05": "Arkansas",
-    # "06": "California",
-    # "08": "Colorado",
-    # "09": "Connecticut",
-    # "10": "Delaware",
-    # "11": "District of Columbia",
-    # "12": "Florida",
-    # "13": "Georgia",
-    # "15": "Hawaii",
-    # "16": "Idaho",
-    # "17": "Illinois",
-    # "18": "Indiana",
-    # "19": "Iowa",
-    # "20": "Kansas",
-    # "21": "Kentucky",
-    # "22": "Louisiana",
-    # "23": "Maine",
-    # "24": "Maryland",
-    # "25": "Massachusetts",
-    # "26": "Michigan",
-    # "27": "Minnesota",
-    # "28": "Mississippi",
-    # "29": "Missouri",
-    # "30": "Montana",
-    # "31": "Nebraska",
-    # "32": "Nevada",
-    # "33": "New Hampshire",
-    # "34": "New Jersey",
-    # "35": "New Mexico",
-    # "36": "New York",
-    # "37": "North Carolina",
-    # "38": "North Dakota",
-    # "39": "Ohio",
-    # "40": "Oklahoma",
-    # "41": "Oregon",
-    # "42": "Pennsylvania",
-    # "44": "Rhode Island",
-    # "45": "South Carolina",
-    # "46": "South Dakota",
-    # "47": "Tennessee",
-    # "48": "Texas",
-    # "49": "Utah",
-    # "50": "Vermont",
-    # "51": "Virginia",
-    # "53": "Washington",
-    # "54": "West Virginia",
-    # "55": "Wisconsin",
-    # "56": "Wyoming",
-    # "60": "American Samoa",
-    # "66": "Guam",
-    # "69": "Northern Mariana Islands",
-    # "72": "Puerto Rico",
+    "02": "Alaska",
+    "04": "Arizona",
+    "05": "Arkansas",
+    "06": "California",
+    "08": "Colorado",
+    "09": "Connecticut",
+    "10": "Delaware",
+    "11": "District of Columbia",
+    "12": "Florida",
+    "13": "Georgia",
+    "15": "Hawaii",
+    "16": "Idaho",
+    "17": "Illinois",
+    "18": "Indiana",
+    "19": "Iowa",
+    "20": "Kansas",
+    "21": "Kentucky",
+    "22": "Louisiana",
+    "23": "Maine",
+    "24": "Maryland",
+    "25": "Massachusetts",
+    "26": "Michigan",
+    "27": "Minnesota",
+    "28": "Mississippi",
+    "29": "Missouri",
+    "30": "Montana",
+    "31": "Nebraska",
+    "32": "Nevada",
+    "33": "New Hampshire",
+    "34": "New Jersey",
+    "35": "New Mexico",
+    "36": "New York",
+    "37": "North Carolina",
+    "38": "North Dakota",
+    "39": "Ohio",
+    "40": "Oklahoma",
+    "41": "Oregon",
+    "42": "Pennsylvania",
+    "44": "Rhode Island",
+    "45": "South Carolina",
+    "46": "South Dakota",
+    "47": "Tennessee",
+    "48": "Texas",
+    "49": "Utah",
+    "50": "Vermont",
+    "51": "Virginia",
+    "53": "Washington",
+    "54": "West Virginia",
+    "55": "Wisconsin",
+    "56": "Wyoming",
+    "60": "American Samoa",
+    "66": "Guam",
+    "69": "Northern Mariana Islands",
+    "72": "Puerto Rico",
     "78": "U.S. Virgin Islands"
 }
