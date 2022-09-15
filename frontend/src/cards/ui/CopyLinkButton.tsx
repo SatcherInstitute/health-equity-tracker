80a393c186b48072d225aed1694dba49f4477c2d
Ben Hammond <benjamin.hammond@gmail.com>
2022-09-15 17:08:22 -0600

    adds copy link function with snackbar alert

---
+++ frontend/src/cards/ui/CopyLinkButton.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import LinkIcon from "@material-ui/icons/Link";
+import styles from "../Card.module.scss";
+import { IconButton, Snackbar } from "@material-ui/core";
+import { ScrollableHashId } from "../../utils/hooks/useStepObserver";
+import { useLocation } from "react-router-dom";
+import { Alert } from "@material-ui/lab";
+
+interface CopyLinkButtonProps {
+  scrollToHash: ScrollableHashId;
+}
+
+export default function CopyLinkButton(props: CopyLinkButtonProps) {
+  const location = useLocation();
+
+  const [open, setOpen] = useState(false);
+
+  const urlWithoutHash = window.location.href.split("#")[0];
+  const cardHashLink = `${urlWithoutHash}#${props.scrollToHash}`;
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(cardHashLink);
+    setOpen(true);
+    location.hash = `${props.scrollToHash}`;
+  };
+
+  return (
+    <IconButton
+      className={styles.CopyLinkButton}
+      aria-label="copy card link"
+      onClick={handleClick}
+    >
+      <LinkIcon />
+      <Snackbar
+        open={open}
+        autoHideDuration={4000}
+        onClose={() => setOpen(false)}
+      >
+        <Alert>
+          <p>Card link copied to clipboard.</p>
+          <p>{cardHashLink}</p>
+          <a href={cardHashLink} target="_blank" rel="noreferrer">
+            Open in new tab
+          </a>
+        </Alert>
+      </Snackbar>
+    </IconButton>
+  );
+}
