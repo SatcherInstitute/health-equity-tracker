ba3d58cdca50504e6ec721aca55d39facbd99e90
Ben Hammond <benjamin.hammond@gmail.com>
2022-09-16 09:56:51 -0600

    update update

--- frontend/src/data/config/DatasetMetadata.ts
+++ frontend/src/data/config/DatasetMetadata.ts
@@ -300,12 +300,12 @@ export const datasetMetadataList: DatasetMetadata[] = [
   {
     id: "cawp_data-race_and_ethnicity_national",
     name: "National representation of women by race/ethnicity in the US Congress and across county and territory legislatures",
-    update_time: "2022",
+    update_time: "September 2022",
   },
   {
     id: "cawp_data-race_and_ethnicity_state",
     name: "Representation of women by race/ethnicity from each state and territory to the US Congress and their respective state legislature",
-    update_time: "2022",
+    update_time: "September 2022",
   },
   {
     id: "propublica_congress",
