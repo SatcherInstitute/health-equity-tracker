b7f9f39aead597453c163508358e93c76ebe22b1
Ben Hammond <benjamin.hammond@gmail.com>
2022-10-16 14:14:42 -0600

    fix territories

--- python/datasources/cawp_time.py
+++ python/datasources/cawp_time.py
@@ -11,6 +11,25 @@ US_CONGRESS_HISTORICAL_URL = "https://theunitedstates.io/congress-legislators/le
 CAWP_LINE_ITEMS_FILE = "cawp-by_race_and_ethnicity_time_series.csv"
 
 
+def get_postal_from_cawp_phrase(cawp_place_phrase: str):
+    """ Accepts a CAWP place phrase found in the LINE ITEM table
+    `{STATE_COL_LINE NAME} - {CODE}` with the standard 2 letter code
+     """
+
+    # swap out non-standard 2 letter codes
+    cawp_place_phrase = {"American Samoa - AM":
+                         "American Samoa - AS",
+                         "Northern Mariana Islands - MI":
+                         "Northern Mariana Islands - MP"}.get(
+                             cawp_place_phrase, cawp_place_phrase)
+
+    # only keep 2 letter code portion
+    place_terms_list = cawp_place_phrase.split(" - ")
+    place_code = place_terms_list[1]
+
+    return place_code
+
+
 class CAWPTimeData(DataSource):
 
     @ staticmethod
@@ -31,7 +50,7 @@ class CAWPTimeData(DataSource):
         for geo_level in [STATE_LEVEL]:
 
             # restrict index years to this list
-            time_periods = ["2021", "2022"]
+            time_periods = ["2018", "2019", "2020", "2021", "2022"]
 
             # for BQ
             table_name = f'race_and_ethnicity_{geo_level}'
@@ -52,32 +71,41 @@ class CAWPTimeData(DataSource):
             raw_current_congress_json = gcs_to_bq_util.fetch_json_from_web(
                 US_CONGRESS_CURRENT_URL)
 
-            raw_terms_json = list(
-                map((lambda x: x["terms"]), raw_historical_congress_json)) + list(
-                map((lambda x: x["terms"]), raw_current_congress_json))
+            raw_legislators_json = [
+                *raw_historical_congress_json,
+                *raw_current_congress_json
+            ]
 
             us_congress_totals_list_of_dict = []
 
             # iterate through each legislator
-            for legislator_term_list in raw_terms_json:
+            for item in raw_legislators_json:
+
                 # and each term they served
-                for term in legislator_term_list:
-                    years = list(
+                for term in item["terms"]:
+
+                    term_years = list(
                         range(int(term["start"][:4]), int(term["end"][:4])+1))
                     # and each year of each term
-                    for year in years:
+                    for year in term_years:
+
                         year = str(year)
-                        if year in time_periods:
-                            # add entry of service for each state's count
-                            us_congress_totals_list_of_dict.append({
-                                std_col.STATE_POSTAL_COL: term["state"],
-                                std_col.TIME_PERIOD_COL: year
-                            })
-                            # and to the national count
-                            # us_congress_totals_list_of_dict.append({
-                            #     std_col.STATE_POSTAL_COL: US_ABBR,
-                            #     std_col.TIME_PERIOD_COL: year
-                            # })
+
+                        entry = {
+                            "id": item["id"]["govtrack"],
+                            std_col.STATE_POSTAL_COL: term["state"],
+                            std_col.TIME_PERIOD_COL: year
+                        }
+
+                        if year in time_periods and entry not in us_congress_totals_list_of_dict:
+                            # add entry of service for id/year/state. this should avoid double counting and match CAWP which only has one entry per legislator per year
+                            us_congress_totals_list_of_dict.append(entry)
+
+                        # and to the national count
+                        # us_congress_totals_list_of_dict.append({
+                        #     std_col.STATE_POSTAL_COL: US_ABBR,
+                        #     std_col.TIME_PERIOD_COL: year
+                        # })
 
                         # convert to df
             us_congress_total_count_df = pd.DataFrame.from_dict(
@@ -100,6 +128,8 @@ class CAWPTimeData(DataSource):
             merge_cols = [std_col.TIME_PERIOD_COL, std_col.STATE_FIPS_COL]
             df = pd.merge(df, us_congress_total_count_df, on=merge_cols)
 
+            ###
+
             # load in CAWP counts of women by race by year by state
             line_items_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
                 'cawp', CAWP_LINE_ITEMS_FILE)
@@ -110,7 +140,7 @@ class CAWPTimeData(DataSource):
 
             # standardize CAWP state names as postal
             line_items_df[std_col.STATE_POSTAL_COL] = line_items_df["state"].apply(
-                lambda x: x[-2:])
+                get_postal_from_cawp_phrase)
 
             # merge in FIPS codes
             line_items_df = merge_utils.merge_state_fips_codes(
