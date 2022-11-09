a2bd747c5cfc37ef859a628222978dedec5ed258
Ben Hammond <benjamin.hammond@gmail.com>
2022-11-08 17:00:04 -0700

    fix race word:

--- python/datasources/census_pop_estimates_sc.py
+++ python/datasources/census_pop_estimates_sc.py
@@ -69,9 +69,11 @@ class CensusPopEstimatesSC(DataSource):
                 column_types = {c: 'STRING' for c in breakdown_df.columns}
                 if s.RACE_INCLUDES_HISPANIC_COL in df.columns:
                     column_types[s.RACE_INCLUDES_HISPANIC_COL] = 'BOOL'
-                    breakdown = s.RACE_OR_HISPANIC_COL
+
+                demo = breakdown if breakdown != s.RACE_CATEGORY_ID_COL else s.RACE_OR_HISPANIC_COL
+
                 gcs_to_bq_util.add_df_to_bq(
-                    breakdown_df, dataset, f'by_{breakdown}_age_{geo}', column_types=column_types)
+                    breakdown_df, dataset, f'by_{demo}_age_{geo}', column_types=column_types)
 
 
 def generate_pop_data_18plus(df, breakdown, geo):
