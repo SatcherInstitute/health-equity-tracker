6a41cc5440ac6cc3219b5227ec8f5b1b7af62b01
Ben Hammond <benjamin.hammond@gmail.com>
2022-11-29 08:46:54 -0700

    split dags

--- airflow/dags/cawp.py
+++ airflow/dags/cawp.py
@@ -31,38 +31,8 @@ cawp_exporter_payload_race = {
 cawp_exporter_operator_race = util.create_exporter_operator(
     'cawp_exporter_race', cawp_exporter_payload_race, data_ingestion_dag)
 
-# NEW FLOW - TIME SERIES for US CONGRESS
-
-_CAWP_TIME_WORKFLOW_ID = 'CAWP_TIME_DATA'
-_CAWP_TIME_DATASET_NAME = 'cawp_time_data'
-
-default_args = {
-    'start_date': days_ago(0),
-}
-
-data_ingestion_dag = DAG(
-    'cawp_time_ingestion_dag',
-    default_args=default_args,
-    schedule_interval=None,
-    description='Ingestion configuration for CAWP_TIME')
-
-cawp_time_bq_payload = util.generate_bq_payload(
-    _CAWP_TIME_WORKFLOW_ID, _CAWP_TIME_DATASET_NAME)
-cawp_time_pop_bq_operator = util.create_bq_ingest_operator(
-    'cawp_time_to_bq', cawp_time_bq_payload, data_ingestion_dag)
-
-cawp_time_exporter_payload_race = {
-    'dataset_name': _CAWP_TIME_DATASET_NAME,
-    'demographic': "race_and_ethnicity"
-}
-cawp_time_exporter_operator_race = util.create_exporter_operator(
-    'cawp_time_exporter_race', cawp_time_exporter_payload_race, data_ingestion_dag)
-
-
 # Ingestion DAG
 (
     cawp_pop_bq_operator >>
-    cawp_time_pop_bq_operator >>
-    cawp_exporter_operator_race >>
-    cawp_time_exporter_operator_race
+    cawp_exporter_operator_race
 )
