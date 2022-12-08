ab5e5b12ba415cda43df380ac9674c468f44ebeb
Ben Hammond <benjamin.hammond@gmail.com>
2022-12-08 11:05:33 -0700

    fix tests

--- frontend/src/data/utils/Fips.test.ts
+++ frontend/src/data/utils/Fips.test.ts
@@ -1,7 +1,26 @@
 import { Fips } from "./Fips";
 
 describe("Test getDisplayName()", () => {
-  test("US", async () => {
+  test("US/STATE with no addon", async () => {
     expect(new Fips("00").getDisplayName()).toEqual("United States");
+  });
+  test("County without addon", async () => {
+    expect(new Fips("02016").getDisplayName()).toEqual(
+      "Aleutians West Census Area"
+    );
+  });
+  test("County with Parish addon", async () => {
+    expect(new Fips("22001").getDisplayName()).toEqual("Acadia Parish");
+  });
+  test("County with county addon", async () => {
+    expect(new Fips("08031").getDisplayName()).toEqual("Denver County");
+  });
+});
+
+describe("Test getSentenceDisplayName()", () => {
+  test("The US", async () => {
+    expect(new Fips("00").getSentenceDisplayName()).toEqual(
+      " the United States"
+    );
   });
 });
