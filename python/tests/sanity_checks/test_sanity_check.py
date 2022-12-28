4260a420c6fb0f032469f244eced4f9e8ef3d127
Eric Warren <eric.m.warren1@gmail.com>
2022-12-28 14:31:04 -0500

    testing mock path

--- python/tests/sanity_checks/test_sanity_check.py
+++ python/tests/sanity_checks/test_sanity_check.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import json
+from unittest import TestCase, mock
 from sanity_checks.sanity_check import check_pct_values
 
 # Current working directory
@@ -15,37 +16,66 @@ CDC_RESTRICTED = {
     'sex_state': os.path.join(TEST_DIR, 'cdc_restricted_sex_state.json')
 }
 
+test_dtype = {
+    'county_fips': str,
+    'state_fips': str,
+}
 
-def testGenerateCountyDatasetAge():
-    with open(CDC_RESTRICTED['age_county'], 'r') as data_file:
-        data = json.loads(data_file.read())
-    df = pd.json_normalize(data)
-    result = check_pct_values(df)
+
+def get_age_county_as_json():
+    with open(CDC_RESTRICTED['age_county']) as f:
+        return json.load(f)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.load_df_from_bigquery',
+            return_value=get_age_county_as_json())
+def testGenerateTest(mock_bq: mock.MagicMock):
+    expected_df = pd.read_json(CDC_RESTRICTED['sex_county'], dtype=test_dtype)
+    result = check_pct_values(expected_df)
     return result
 
 
-def testGenerateCountyDatasetSexTime():
-    df = pd.read_json(CDC_RESTRICTED['sex_county_time'], dtype={
-                      'county_fips': str, 'state_fips': str})
-    result = check_pct_values(df)
-    return result
-
-
-def testGenerateCountyDatasetSex():
-    df = pd.read_json(CDC_RESTRICTED['sex_county'], dtype={
-                      'county_fips': str, 'state_fips': str})
-    result = check_pct_values(df)
-    return result
-
-
-def testGenerateNationalDatasetSex():
-    df = pd.read_json(CDC_RESTRICTED['sex_national'], dtype={
-                      'state_fips': str})
-    result = check_pct_values(df)
-    return result
-
-
-def testGenerateStateDatasetSex():
-    df = pd.read_json(CDC_RESTRICTED['sex_state'], dtype={'state_fips': str})
-    result = check_pct_values(df)
-    return result
+# def testGenerateCountyDatasetAge(mock_bq: mock.MagicMock):
+#     expected_df = pd.read_json(CDC_RESTRICTED, dtype={
+#         'state_fips': str,
+#         'covid_cases_share': float,
+#         'covid_hosp_share': float,
+#         'covid_deaths_share': float,
+#     })
+#     print(expected_df)
+#     pass
+# class SanityCheck(TestCase):
+#     "Test the county by age dataset"
+#     def testGenerateCountyDatasetAge(self):
+#         with open(CDC_RESTRICTED['age_county'], 'r') as data_file:
+#             data = json.loads(data_file.read())
+#         df = pd.json_normalize(data)
+#         result = check_pct_values(df)
+#         return result
+#     pass
+# def testGenerateCountyDatasetAge():
+#     with open(CDC_RESTRICTED['age_county'], 'r') as data_file:
+#         data = json.loads(data_file.read())
+#     df = pd.json_normalize(data)
+#     result = check_pct_values(df)
+#     print(get_age_county_as_json())
+#     return result
+# def testGenerateCountyDatasetSexTime():
+#     df = pd.read_json(CDC_RESTRICTED['sex_county_time'], dtype={
+#                       'county_fips': str, 'state_fips': str})
+#     result = check_pct_values(df)
+#     return result
+# def testGenerateCountyDatasetSex():
+#     df = pd.read_json(CDC_RESTRICTED['sex_county'], dtype={
+#                       'county_fips': str, 'state_fips': str})
+#     result = check_pct_values(df)
+#     return result
+# def testGenerateNationalDatasetSex():
+#     df = pd.read_json(CDC_RESTRICTED['sex_national'], dtype={
+#                       'state_fips': str})
+#     result = check_pct_values(df)
+#     return result
+# def testGenerateStateDatasetSex():
+#     df = pd.read_json(CDC_RESTRICTED['sex_state'], dtype={'state_fips': str})
+#     result = check_pct_values(df)
+#     return result
