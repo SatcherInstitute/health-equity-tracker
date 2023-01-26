da1fb5629b4b99952508cab9c28b050b931c2b95
Eric Warren <eric.m.warren1@gmail.com>
2023-01-26 17:02:02 -0500

    added test data

--- python/datasources/cdc_hiv_diagnoses.py
+++ python/datasources/cdc_hiv_diagnoses.py
@@ -1,6 +1,71 @@
 from datasources.data_source import DataSource
-from ingestion import gcs_to_bq_util
+from ingestion import gcs_to_bq_util, merge_utils
 import ingestion.standardized_columns as std_col
+import ingestion.constants as constants
+from ingestion.standardized_columns import Race
+import pandas as pd
+
+cols_to_std = {
+    'Geography': std_col.COUNTY_NAME_COL,
+    'FIPS': std_col.COUNTY_FIPS_COL,
+    'Year': std_col.TIME_PERIOD_COL,
+    'Age Group': std_col.AGE_COL,
+    'Cases': std_col.HIV_CASES,
+    'Rate per 100000': std_col.HIV_PER_100K,
+    'Population': std_col.POPULATION_COL
+}
+
+RACE_GROUPS_TO_STANDARD = {
+    'White': Race.WHITE_NH.value,
+    'Black': Race.BLACK_NH,
+    'Hispanic': Race.HISP,
+    'American Indian/Alaska Native': Race.AIAN_NH,
+    'Asian': Race.ASIAN_NH,
+    'Native Hawaiian/Other Pacific Islander': Race.NHPI_NH,
+    'Two or more races': Race.MULTI_NH,
+    'Other': Race.OTHER_STANDARD_NH,
+    'Unknown': Race.UNKNOWN,
+    # 'Unknown' + 'Did not report' -> "Unknown"
+    'Total': Race.ALL
+}
+
+
+def generate_raw_breakdown(demo, geo_level, df):
+
+    # map
+    group_dict = {
+        std_col.RACE_OR_HISPANIC_COL: RACE_GROUPS_TO_STANDARD,
+    }
+
+    source_dfs = []
+
+    for group in group_dict[demo].keys():
+        # load in table that is needed
+        # add table to source_dfs
+        source_dfs.append()
+
+    source_df = pd.concat(source_dfs, axis=0)
+
+    if demo == std_col.AGE_COL and geo_level == 'county':
+        needed_cols = [std_col.COUNTY_FIPS_COL, std_col.COUNTY_NAME_COL, std_col.TIME_PERIOD_COL,
+                       std_col.AGE_COL, std_col.HIV_CASES, std_col.HIV_PER_100K, std_col.POPULATION_COL]
+        df = df.rename(columns=cols_to_std)
+        df = df.astype({'county_fips': 'str'})
+        df[std_col.COUNTY_FIPS_COL] = df[std_col.COUNTY_FIPS_COL].apply(
+            lambda x: x.zfill(5))
+        df = merge_utils.merge_county_names(df)
+        df = df[needed_cols].sort_values(
+            [std_col.COUNTY_NAME_COL, std_col.AGE_COL]).reset_index(drop=True)
+
+        # add the alls
+
+        print("/n")
+        print(df)
+
+    if demo == std_col.AGE_COL and geo_level == 'state':
+        pass
+
+    pass
 
 
 class CDCHIVDiagnosesData(DataSource):
@@ -18,15 +83,28 @@ class CDCHIVDiagnosesData(DataSource):
             'upload_to_gcs should not be called for CDCHIVDiagnosesData'
         )
 
+    def generate_breakdown_df(self, breakdown, geo_level):
+        source_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
+            'cdc_hiv_diagnoses', '_ages_13-24_county_2019.csv', dtype={'FIPS': str})
+        return source_df
+
     def write_to_bq(self, dataset, gcs_bucket, **attrs):
-        """
-        Main function for this data source that fetches external data and runs
-        needed cleaning and standardization, and then writes needed tables to
-        BigQuery
 
-        """
+        for geo_level in [constants.NATIONAL_LEVEL, constants.STATE_LEVEL, constants.COUNTY_LEVEL]:
+            for demo in [std_col.SEX_COL, std_col.AGE_COL, std_col.RACE_OR_HISPANIC_COL]:
+                table_name = f'{demo}_{geo_level}'
 
-        all_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
-            'cdc_hiv_diagnoses', 'state', '_alls_state_2019.csv')
+                df = self.generate_breakdown_df(demo, geo_level)
+                # df.to_csv(f'{demo}_{geo_level}_output.csv', index=False)
+                float_cols = [std_col.POPULATION_PCT_COL,
+                              std_col.POPULATION_COL,
+                              std_col.HIV_CASES,
+                              std_col.HIV_PER_100K,
+                              std_col.HIV_PCT_SHARE
+                              ]
 
-        gcs_to_bq_util.add_df_to_bq(all_df, dataset, 'sex_state')
+                column_types = gcs_to_bq_util.get_bq_column_types(
+                    df, float_cols=float_cols)
+
+                gcs_to_bq_util.add_df_to_bq(
+                    df, dataset, table_name, column_types=column_types)
--- python/tests/datasources/test_cdc_hiv_diagnoses.py
+++ python/tests/datasources/test_cdc_hiv_diagnoses.py
@@ -4,24 +4,71 @@ import pandas as pd
 import os
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-TEST_DIR = os.path.join(THIS_DIR, os.pardir, "data", "cdc_hiv_diagnoses")
+TEST_DIR = os.path.join(THIS_DIR, os.pardir, "data", "cdc_hiv_diagnoses",)
 
 
-def get_hiv_as_df():
-    return pd.read_csv(os.path.join(TEST_DIR, 'test_sex_male_state_2019.csv'))
+# AGE
+
+def get_ages_13_24_county():
+    return pd.read_csv(os.path.join(TEST_DIR, '_ages_13-24_county_2019.csv'))
 
 
-@mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', return_value=get_hiv_as_df())
-@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
-def testWriteToBq(mock_bq: mock.MagicMock, mock_csv: mock.MagicMock):
+def get_ages_25_34_county():
+    return pd.read_csv(os.path.join(TEST_DIR, '_ages_25-34_county_2019.csv'))
 
-    kwargs_for_bq = {'filename': 'test_file.csv',
-                     'metadata_table_id': 'test_metadata', 'table_name': 'output_table'}
 
-    cdcHivDiagnosesData = CDCHIVDiagnosesData()
-    cdcHivDiagnosesData.write_to_bq('dataset', 'gcs_bucket', **kwargs_for_bq)
-    assert mock_bq.call_count == 1
-    assert mock_csv.call_count == 1
+def get_ages_35_44_county():
+    return pd.read_csv(os.path.join(TEST_DIR, '_ages_35-44_county_2019.csv'))
 
-    print("/n")
-    print(mock_bq.call_args_list[0].args[0])
+
+def get_ages_45_54_county():
+    return pd.read_csv(os.path.join(TEST_DIR, '_ages_45-54_county_2019.csv'))
+
+
+def get_ages_over_54_county():
+    return pd.read_csv(os.path.join(TEST_DIR, '_ages_55+_county_2019.csv'))
+
+
+def _load_csv_as_df_from_data_dir(*args, **kwargs):
+    print('mocking load from data dir')
+    for arg in args:
+        print("-")
+        print(arg)
+
+    for kwarg in kwargs:
+        print("--")
+        print(kwarg)
+
+    dataset, filename = args
+    return pd.read_csv(os.path.join(TEST_DIR, filename))
+
+
+@mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq',
+            return_value=None)
+def testRunner(
+    mock_bq: mock.MagicMock,
+    mock_data_dir: mock.MagicMock,
+):
+    datasource = CDCHIVDiagnosesData()
+    datasource.write_to_bq('dataset', 'gcs_bucket')
+
+    # def testGenerateBreakdownAgeCounty():
+
+    #     df_ages_13_24 = get_ages_13_24_county()
+    #     df_ages_25_34 = get_ages_25_34_county()
+    #     df_ages_35_44 = get_ages_35_44_county()
+    #     df_ages_45_54 = get_ages_45_54_county()
+    #     df_ages_over_54 = get_ages_over_54_county()
+
+    #     table_list = [df_ages_13_24, df_ages_25_34,
+    #                   df_ages_35_44, df_ages_45_54, df_ages_over_54]
+
+    #     datasource = CDCHIVDiagnosesData()
+    #     df = datasource.generate_breakdown_df('age', 'county', table_list)
+
+    #     # update assert statements here
+    #     pass
+
+    # def testGenerateBreakdownAgeState():
+    #     pass
