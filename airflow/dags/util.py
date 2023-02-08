dcd59389b7ea5db1aec06d60c8d5179dd6e92398
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-08 13:19:34 -0700

    allow year instead of url param

--- airflow/dags/util.py
+++ airflow/dags/util.py
@@ -48,6 +48,7 @@ def generate_gcs_payload(workflow_id: str, filename: str = None,
 def generate_bq_payload(workflow_id: str, dataset: str, filename: str = None,
                         gcs_bucket: str = None, url: str = None,
                         demographic: str = None,
+                        year: str = None,
                         geo_level: str = None) -> dict:
     """Creates the payload object required for the BQ ingestion operator.
 
@@ -64,7 +65,9 @@ def generate_bq_payload(workflow_id: str, dataset: str, filename: str = None,
     demographic: The demographic group to generate the bq pipeline for.
                  Either `race`/`race_and_ethnicity`, `sex` or `age`.
     geo_level: string for the specific geographic level `national`,
-        `state`, or `county` """
+        `state`, or `county`
+    year: string 4 digit year that determines which year should be processed
+        """
     message = get_required_attrs(workflow_id, gcs_bucket=gcs_bucket)
     message['dataset'] = dataset
     if filename is not None:
