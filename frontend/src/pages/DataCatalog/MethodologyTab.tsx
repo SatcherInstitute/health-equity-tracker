0abe0de5e5b067f71d653dc9558c43b1dc658aa2
Eric Warren <eric.m.warren1@gmail.com>
2023-02-10 15:53:21 -0500

    fixing linting issue

--- frontend/src/pages/DataCatalog/MethodologyTab.tsx
+++ frontend/src/pages/DataCatalog/MethodologyTab.tsx
@@ -488,10 +488,10 @@ function MethodologyTab() {
                 <p>
                   Health departments report to the CDC all confirmed HIV
                   infections diagnosed in their jurisdiction and clinical
-                  information about each infection. The CDC's NCHHSTP AtlasPlus
-                  database contains HIV surveillance data reported to the CDC.
-                  All HIV data come from health departments in all 50 states,
-                  the District of Columbia, and U.S. territories. The HIV data
+                  information about each infection. The CDC's AtlasPlus database
+                  contains HIV surveillance data reported to the CDC. All HIV
+                  data come from health departments in all 50 states, the
+                  District of Columbia, and U.S. territories. The HIV data
                   includes adolescents and adults aged 13 and older.
                 </p>
 
