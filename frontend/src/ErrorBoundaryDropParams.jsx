8a4ceaccf50678576ca91cda1dd4bd80c4fa8451
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-16 08:14:28 -0700

    rm comment

--- frontend/src/ErrorBoundaryDropParams.jsx
+++ frontend/src/ErrorBoundaryDropParams.jsx
@@ -12,11 +12,6 @@ export default class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, info) {
-    // Example "componentStack":
-    //   in ComponentThatThrows (created by App)
-    //   in ErrorBoundary (created by App)
-    //   in div (created by App)
-    //   in App
     console.log(error, info.componentStack);
   }
 
