3478526001715b40889c30f31061e7616e83b291
Eric Warren <eric.m.warren1@gmail.com>
2023-02-17 14:10:30 -0500

    time refactor

--- python/datasources/cdc_hiv.py
+++ python/datasources/cdc_hiv.py
@@ -1,85 +1,75 @@
-from datasources.data_source import DataSource
-from ingestion import gcs_to_bq_util, merge_utils, standardized_columns as std_col, constants
-from ingestion.dataset_utils import generate_pct_share_col_without_unknowns
-import pandas as pd
 import numpy as np
+import pandas as pd
+from datasources.data_source import DataSource
+from ingestion import gcs_to_bq_util, standardized_columns as std_col
+from ingestion.merge_utils import merge_county_names
+from ingestion.dataset_utils import (generate_pct_share_col_without_unknowns,
+                                     generate_pct_rel_inequity_col)
+from ingestion.constants import (COUNTY_LEVEL,
+                                 STATE_LEVEL,
+                                 NATIONAL_LEVEL,
+                                 US_FIPS)
 
-AGE_GROUPS = {
-    '13-24': '13_24',
-    '25-34': '25_34',
-    '35-44': '35_44',
-    '45-54': '45_54',
-    '55+': '55+',
+HIV_DIR = 'hiv_time'
+
+# nested dict that contains values swaps, per column name
+HIV_TERMS_STANDARD_BY_COL = {
+    std_col.RACE_OR_HISPANIC_COL: {
+        'All races/ethnicities': std_col.Race.ALL,
+        'American Indian/Alaska Native': std_col.Race.AIAN_NH.value,
+        'Asian': std_col.Race.ASIAN_NH.value,
+        'Black/African American': std_col.Race.BLACK_NH.value,
+        'Hispanic/Latino': std_col.Race.HISP.value,
+        'Multiracial': std_col.Race.MULTI_NH.value,
+        'Native Hawaiian/Other Pacific Islander': std_col.Race.NHPI_NH.value,
+        'White': std_col.Race.WHITE_NH.value},
+    std_col.AGE_COL: {'Ages 13 years and older': std_col.ALL_VALUE},
+    std_col.SEX_COL: {'Both sexes': std_col.ALL_VALUE},
+    std_col.HIV_DIAGNOSES: {'Data suppressed': np.nan, 'Data not available': np.nan},
+    std_col.HIV_DIAGNOSES_PER_100K: {'Data suppressed': np.nan, 'Data not available': np.nan},
+    std_col.TIME_PERIOD_COL: {'2020 (COVID-19 Pandemic)': '2020'}
 }
 
 RACE_GROUPS_TO_STANDARD = {
-    'White': std_col.Race.WHITE_NH.value,
-    'Black/African American': std_col.Race.BLACK_NH.value,
-    'Hispanic/Latino': std_col.Race.HISP.value,
-    'American Indian/Alaska Native': std_col.Race.AIAN_NH.value,
-    'Asian': std_col.Race.ASIAN_NH.value,
-    'Native Hawaiian/Other Pacific Islander': std_col.Race.NHPI_NH.value,
-    'Multiracial': std_col.Race.MULTI_NH.value,
-}
+    std_col.RACE_OR_HISPANIC_COL: {
+        'All races/ethnicities': std_col.Race.ALL,
+        'American Indian/Alaska Native': std_col.Race.AIAN_NH.value,
+        'Asian': std_col.Race.ASIAN_NH.value,
+        'Black/African American': std_col.Race.BLACK_NH.value,
+        'Hispanic/Latino': std_col.Race.HISP.value,
+        'Multiracial': std_col.Race.MULTI_NH.value,
+        'Native Hawaiian/Other Pacific Islander': std_col.Race.NHPI_NH.value,
+        'White': std_col.Race.WHITE_NH.value}}
 
-SEX_GROUPS = {
-    "male": constants.Sex.MALE,
-    "female": constants.Sex.FEMALE
-}
+ALL_GROUPS_TO_STANDARD = {
+    std_col.AGE_COL: {'Ages 13 years and older': std_col.ALL_VALUE},
+    std_col.SEX_COL: {'Both sexes': std_col.ALL_VALUE}}
 
-GROUP_DICT = {
-    std_col.AGE_COL: AGE_GROUPS,
-    std_col.RACE_OR_HISPANIC_COL: RACE_GROUPS_TO_STANDARD,
-    std_col.SEX_COL: SEX_GROUPS,
-}
+MISSING_DATA_TO_STANDARD = {
+    std_col.HIV_DIAGNOSES: {'Data suppressed': np.nan, 'Data not available': np.nan},
+    std_col.HIV_DIAGNOSES_PER_100K: {'Data suppressed': np.nan, 'Data not available': np.nan}}
 
-demo_dict = {
-    'American Indian/Alaska Native': 'aian',
-    'Asian': 'asian',
-    'Black/African American': 'black',
-    'Hispanic/Latino': 'hisp',
-    'Multiracial': 'multi',
-    'Native Hawaiian/Other Pacific Islander': 'nhpi',
-    'White': 'white',
-}
+TIME_PERIOD_TO_STANDARD = {
+    std_col.TIME_PERIOD_COL: {'2020 (COVID-19 Pandemic)': '2020'}}
 
-pct_share_dict = {
+PCT_SHARE_DICT = {
     std_col.HIV_DIAGNOSES: std_col.HIV_DIAGNOSES_PCT_SHARE,
-    std_col.POPULATION_COL: std_col.HIV_POPULATION_PCT
-}
+    std_col.POPULATION_COL: std_col.HIV_POPULATION_PCT}
 
 
 def generate_alls_df(geo_level: str):
     """
     Fetches the combined dataframe of each demographic for county & state levels
-
     geo_level: string of `state` or `county`
-
     returns a formatted dataframe with total values for specified geo_level
     """
-    alls_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
-        'cdc_hiv', f'totals_{geo_level}_2019.csv',
-        subdirectory=geo_level, dtype={'FIPS': str}, skiprows=9, thousands=',')
-
-    alls_df[['Sex', 'Age Group', 'Race/Ethnicity']] = 'All'
-
-    if geo_level == constants.NATIONAL_LEVEL:
-        alls_df['FIPS'] = constants.US_FIPS
-        alls_df['Geography'] = constants.US_NAME
-        return alls_df
+    alls_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(HIV_DIR,
+                                                          f'hiv-{geo_level}-all.csv',
+                                                          skiprows=8,
+                                                          thousands=',',
+                                                          dtype={'FIPS': str, 'Year': str})
 
     return alls_df
-
-
-def generate_nat_breakdown_df(breakdown: str):
-    nat_from_state_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
-        'cdc_hiv', f'{breakdown}_national_2019.csv',
-        subdirectory='national', skiprows=9, thousands=",")
-
-    nat_from_state_df['FIPS'] = constants.US_FIPS
-    nat_from_state_df['Geography'] = constants.US_NAME
-
-    return nat_from_state_df
 
 
 class CDCHIVData(DataSource):
@@ -94,144 +84,150 @@ class CDCHIVData(DataSource):
 
     def upload_to_gcs(self, gcs_bucket, **attrs):
         raise NotImplementedError(
-            'upload_to_gcs should not be called for CDCHIVData'
-        )
+            'upload_to_gcs should not be called for CDCHIVData')
 
     def write_to_bq(self, dataset, gcs_bucket, **attrs):
-        for geo_level in [constants.COUNTY_LEVEL, constants.STATE_LEVEL, constants.NATIONAL_LEVEL]:
+
+        for geo_level in [COUNTY_LEVEL, STATE_LEVEL, NATIONAL_LEVEL]:
+            alls_df = generate_alls_df(geo_level)
             for breakdown in [std_col.AGE_COL, std_col.RACE_OR_HISPANIC_COL, std_col.SEX_COL]:
                 table_name = f'{breakdown}_{geo_level}'
+                df = self.generate_breakdown_df(breakdown, geo_level, alls_df)
+                df.to_csv(f'{table_name}_output.csv', index=False)
+<<<<<<< HEAD
+=======
+                
+>>>>>>> bd18ebf6 (completed refactor for time)
 
+<<<<<<< HEAD
+                float_cols = [std_col.HIV_DIAGNOSES,
+                              std_col.HIV_DIAGNOSES_PCT_INEQUITY,
+                              std_col.HIV_DIAGNOSES_PCT_SHARE,
+=======
                 df = self.generate_breakdown_df(breakdown, geo_level)
 
                 float_cols = [std_col.HIV_POPULATION_PCT,
                               std_col.HIV_DIAGNOSES,
+>>>>>>> 5688d4aa (hiv_population_pct rename)
                               std_col.HIV_DIAGNOSES_PER_100K,
-                              std_col.HIV_DIAGNOSES_PCT_SHARE
-                              ]
+                              std_col.HIV_POPULATION_PCT]
+                column_types = gcs_to_bq_util.get_bq_column_types(df,
+                                                                  float_cols=float_cols)
+                gcs_to_bq_util.add_df_to_bq(df,
+                                            dataset,
+                                            table_name,
+                                            column_types=column_types)
 
-                column_types = gcs_to_bq_util.get_bq_column_types(
-                    df, float_cols=float_cols)
+    def generate_breakdown_df(self, breakdown: str, geo_level: str, alls_df: pd.DataFrame):
+        GEO_COL = std_col.COUNTY_NAME_COL if geo_level == COUNTY_LEVEL else std_col.STATE_NAME_COL
+        FIPS = std_col.COUNTY_FIPS_COL if geo_level == COUNTY_LEVEL else std_col.STATE_FIPS_COL
 
-                gcs_to_bq_util.add_df_to_bq(
-                    df, dataset, table_name, column_types=column_types)
-
-    def generate_breakdown_df(self, breakdown, geo_level):
-        COL_NAME = std_col.COUNTY_NAME_COL if geo_level == constants.COUNTY_LEVEL else std_col.STATE_NAME_COL
-        FIPS = std_col.COUNTY_FIPS_COL if geo_level == constants.COUNTY_LEVEL else std_col.STATE_FIPS_COL
-
-        cols_std = {
-            'Geography': COL_NAME,
-            'FIPS': FIPS,
+        columns_to_standard = {
             'Age Group': std_col.AGE_COL,
-            'Sex': std_col.SEX_COL,
-            'Race/Ethnicity': std_col.RACE_OR_HISPANIC_COL,
-            'Year': std_col.TIME_PERIOD_COL,
             'Cases': std_col.HIV_DIAGNOSES,
-            'Rate per 100000': std_col.HIV_DIAGNOSES_PER_100K,
+            'FIPS': FIPS,
+            'Geography': GEO_COL,
             'Population': std_col.POPULATION_COL,
-        }
+            'Race/Ethnicity': std_col.RACE_OR_HISPANIC_COL,
+            'Rate per 100000': std_col.HIV_DIAGNOSES_PER_100K,
+            'Sex': std_col.SEX_COL,
+            'Year': std_col.TIME_PERIOD_COL}
 
-        cols = [
-            COL_NAME,
+        columns_to_keep = [
+            GEO_COL,
             FIPS,
             std_col.TIME_PERIOD_COL,
             breakdown,
             std_col.HIV_DIAGNOSES,
             std_col.HIV_DIAGNOSES_PER_100K,
             std_col.HIV_DIAGNOSES_PCT_SHARE,
+<<<<<<< HEAD
+<<<<<<< HEAD
+            std_col.HIV_POPULATION_PCT,
+            std_col.HIV_DIAGNOSES_PCT_INEQUITY]
+=======
             std_col.HIV_POPULATION_PCT]
+>>>>>>> 5688d4aa (hiv_population_pct rename)
+=======
+            std_col.HIV_POPULATION_PCT,
+            std_col.HIV_DIAGNOSES_PCT_INEQUITY]
+>>>>>>> bd18ebf6 (completed refactor for time)
 
-        source_dfs = []
-        missing_data = ['Data suppressed', 'Data not available']
+        breakdown_group_df = gcs_to_bq_util.load_csv_as_df_from_data_dir('hiv_time',
+                                                                         f'hiv-{geo_level}-{breakdown}.csv',
+                                                                         skiprows=8,
+                                                                         thousands=',',
+                                                                         dtype={'FIPS': str, 'Year': str})
+        # append the alls DF
+        combined_group_df = pd.concat([breakdown_group_df, alls_df], axis=0)
 
-        # county fips needs 5 digits, state digits use 2
-        format_num = 5 if geo_level == constants.COUNTY_LEVEL else 2
-        # flag for creating only county & state level dataframes
-        geo = constants.COUNTY_LEVEL if geo_level == constants.COUNTY_LEVEL else constants.STATE_LEVEL
-
-        for group in GROUP_DICT[breakdown].keys():
-            # skiprows skips unreadable rows on df/ thousands convert popuplation numbers to floats
-            filename = f'{breakdown}_{demo_dict.get(group, group)}_{geo}_2019.csv'
-            source_group_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
-                'cdc_hiv', filename, subdirectory=geo,
-                skiprows=9, thousands=',', dtype={'FIPS': str})
-
-            # adds leading zeros to fips
-            source_group_df['FIPS'] = source_group_df['FIPS'].str.zfill(
-                format_num)
-            source_dfs.append(source_group_df)
-
-        if geo_level != constants.NATIONAL_LEVEL:
-            alls_df = generate_alls_df(geo_level)
-            source_dfs.append(alls_df)
-
-        combined_group_df = pd.concat(source_dfs, axis=0)
-        combined_group_df = combined_group_df.rename(columns=cols_std)
+        combined_group_df = combined_group_df.rename(
+            columns=columns_to_standard)
         combined_group_df = combined_group_df.sort_values(
             [FIPS, breakdown]).reset_index(drop=True)
 
-        if geo_level == constants.COUNTY_LEVEL:
-            combined_group_df = merge_utils.merge_county_names(
-                combined_group_df)
+        # replace unformatted HIV data with standardized df
+        combined_group_df = combined_group_df.replace(
+            HIV_TERMS_STANDARD_BY_COL)
+
+        # replace string number with whole number
+        combined_group_df[std_col.HIV_DIAGNOSES] = combined_group_df[std_col.HIV_DIAGNOSES].replace(',',
+                                                                                                    '',
+                                                                                                    regex=True)
+
+        if geo_level == COUNTY_LEVEL:
+            combined_group_df = merge_county_names(combined_group_df)
             combined_group_df[std_col.STATE_FIPS_COL] = combined_group_df[std_col.COUNTY_FIPS_COL].str.slice(
                 0, 2)
 
-        if geo_level == constants.NATIONAL_LEVEL:
-            alls_df = generate_alls_df(constants.NATIONAL_LEVEL)
-            nat_breakdown_df = generate_nat_breakdown_df(breakdown)
-
-            nat_df = pd.concat([nat_breakdown_df, alls_df])
-            nat_df = nat_df.rename(columns=cols_std)
-
-            # remove territories (not included in national numbers)
-            combined_group_df = combined_group_df[-combined_group_df[std_col.STATE_FIPS_COL].isin(
-                set(constants.TERRITORY_FIPS_LIST))]
-
-            combined_group_df[std_col.STATE_FIPS_COL] = constants.US_FIPS
-            combined_group_df[std_col.STATE_NAME_COL] = constants.US_NAME
-
-            # combine population numbers
-            combined_group_df.loc[combined_group_df[breakdown] == 'All',
-                                  std_col.POPULATION_COL] = combined_group_df[std_col.POPULATION_COL].sum()
-
-            # combine HIV cases
-            group_by_cols = [std_col.STATE_NAME_COL, std_col.STATE_FIPS_COL,
-                             std_col.TIME_PERIOD_COL, breakdown]
-            combined_group_df = combined_group_df.groupby(
-                group_by_cols).sum().reset_index()
-            combined_group_df = combined_group_df[group_by_cols + [
-                std_col.POPULATION_COL]]
-
-            # merge nat & state population dataframes
-            combined_group_df = nat_df.merge(
-                combined_group_df, on=group_by_cols, how='left')
-            combined_group_df.loc[combined_group_df[breakdown] == 'All',
-                                  std_col.POPULATION_COL] = combined_group_df[std_col.POPULATION_COL].sum()
-
-        combined_group_df.loc[combined_group_df[std_col.HIV_DIAGNOSES_PER_100K].isin(
-            missing_data), std_col.HIV_DIAGNOSES_PER_100K] = np.nan
-        combined_group_df[std_col.HIV_DIAGNOSES] = combined_group_df[std_col.HIV_DIAGNOSES].replace(
-            ',', '', regex=True)
-        combined_group_df.loc[combined_group_df[std_col.HIV_DIAGNOSES].isin(
-            missing_data), std_col.HIV_DIAGNOSES] = np.nan
-        combined_group_df[std_col.HIV_DIAGNOSES] = combined_group_df[std_col.HIV_DIAGNOSES].astype(
-            float)
-        combined_group_df[std_col.HIV_DIAGNOSES_PER_100K] = combined_group_df[std_col.HIV_DIAGNOSES_PER_100K].astype(
-            float)
-
-        combined_group_df = generate_pct_share_col_without_unknowns(
-            combined_group_df, pct_share_dict, breakdown, 'All')
+        if geo_level == NATIONAL_LEVEL:
+            combined_group_df[std_col.STATE_FIPS_COL] = US_FIPS
 
         if breakdown == std_col.RACE_OR_HISPANIC_COL:
             combined_group_df = combined_group_df.replace(
-                {std_col.RACE_OR_HISPANIC_COL: RACE_GROUPS_TO_STANDARD})
-            combined_group_df = combined_group_df.replace(['All'], 'ALL')
+                RACE_GROUPS_TO_STANDARD)
             combined_group_df = combined_group_df.rename(
                 columns={std_col.RACE_OR_HISPANIC_COL: std_col.RACE_CATEGORY_ID_COL})
             std_col.add_race_columns_from_category_id(combined_group_df)
-            cols.append(std_col.RACE_CATEGORY_ID_COL)
+            columns_to_keep.append(std_col.RACE_CATEGORY_ID_COL)
 
-        df = combined_group_df[cols]
+        combined_group_df = generate_pct_share_col_without_unknowns(combined_group_df,
+                                                                    PCT_SHARE_DICT,
+                                                                    breakdown,
+                                                                    std_col.ALL_VALUE)
+
+        combined_group_df = generate_pct_rel_inequity_col(combined_group_df,
+                                                          std_col.HIV_DIAGNOSES_PCT_SHARE,
+                                                          std_col.HIV_POPULATION_PCT,
+                                                          std_col.HIV_DIAGNOSES_PCT_INEQUITY)
+
+        df = combined_group_df[columns_to_keep]
 
         return df
+
+
+# RACE_GROUPS_TO_STANDARD = {
+#     std_col.RACE_OR_HISPANIC_COL: {
+#         'All races/ethnicities': std_col.Race.ALL,
+#         'American Indian/Alaska Native': std_col.Race.AIAN_NH.value,
+#         'Asian': std_col.Race.ASIAN_NH.value,
+#         'Black/African American': std_col.Race.BLACK_NH.value,
+#         'Hispanic/Latino': std_col.Race.HISP.value,
+#         'Multiracial': std_col.Race.MULTI_NH.value,
+#         'Native Hawaiian/Other Pacific Islander': std_col.Race.NHPI_NH.value,
+#         'White': std_col.Race.WHITE_NH.value}}
+
+# ALL_GROUPS_TO_STANDARD = {
+#     std_col.AGE_COL: {'Ages 13 years and older': std_col.ALL_VALUE},
+#     std_col.SEX_COL: {'Both sexes': std_col.ALL_VALUE}}
+
+# MISSING_DATA_TO_STANDARD = {
+#     std_col.HIV_DIAGNOSES: {'Data suppressed': np.nan, 'Data not available': np.nan},
+#     std_col.HIV_DIAGNOSES_PER_100K: {'Data suppressed': np.nan, 'Data not available': np.nan}}
+
+# TIME_PERIOD_TO_STANDARD = {
+#     std_col.TIME_PERIOD_COL: {'2020 (COVID-19 Pandemic)': '2020'}}
+
+        # combined_group_df = combined_group_df.replace(MISSING_DATA_TO_STANDARD)
+        # combined_group_df = combined_group_df.replace(ALL_GROUPS_TO_STANDARD)
+        # combined_group_df = combined_group_df.replace(TIME_PERIOD_TO_STANDARD)
--- python/tests/datasources/test_cdc_hiv.py
+++ python/tests/datasources/test_cdc_hiv.py
@@ -7,77 +7,85 @@ import os
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.join(THIS_DIR, os.pardir, 'data')
+GOLDEN_DIR = os.path.join(TEST_DIR, 'hiv_time', 'golden_data')
+
+dtype = {'state_fips': str, 'time_period': str}
+
+ALLS_DATA = {
+    'all_national': os.path.join(TEST_DIR, 'hiv_time', 'hiv-national-all.csv'),
+    'all_state': os.path.join(TEST_DIR, 'hiv_time', 'hiv-state-all.csv')}
 
 GOLDEN_DATA = {
-    'race_national': os.path.join(TEST_DIR, 'cdc_hiv/golden_data', 'race_and_ethnicity_national_output.csv'),
-    'age_national': os.path.join(TEST_DIR, 'cdc_hiv/golden_data', 'age_national_output.csv'),
-    'sex_national': os.path.join(TEST_DIR, 'cdc_hiv/golden_data', 'sex_national_output.csv'),
-    'race_state': os.path.join(TEST_DIR, 'cdc_hiv/golden_data', 'race_and_ethnicity_state_output.csv'),
-}
+    'age_national': os.path.join(GOLDEN_DIR, 'age_national_output.csv'),
+    'race_national': os.path.join(GOLDEN_DIR, 'race_and_ethnicity_national_output.csv'),
+    'race_state': os.path.join(GOLDEN_DIR, 'race_and_ethnicity_state_output.csv'),
+    'sex_national': os.path.join(GOLDEN_DIR, 'sex_national_output.csv')}
 
 
 def _load_csv_as_df_from_data_dir(*args, **kwargs):
-    subdirectory = kwargs['subdirectory']
     directory, filename = args
-    df = pd.read_csv(os.path.join(TEST_DIR, directory, subdirectory, filename),
-                     dtype={'FIPS': str}, skiprows=9, thousands=',')
+    df = pd.read_csv(os.path.join(TEST_DIR, directory, filename),
+                     dtype={'FIPS': str}, skiprows=8, thousands=',')
 
     return df
 
 
 @mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
-def testGenerateRaceNational(
-    mock_data_dir: mock.MagicMock,
-):
+def testGenerateAgeNational(mock_data_dir: mock.MagicMock):
     datasource = CDCHIVData()
-    df = datasource.generate_breakdown_df(
-        'race_and_ethnicity', 'national')
-    expected_df_race_national = pd.read_csv(
-        GOLDEN_DATA['race_national'], dtype={'state_fips': str})
+    breakdown = 'age'
+    geo_level = 'national'
 
-    assert_frame_equal(df, expected_df_race_national, check_like=True)
+    alls_df = pd.read_csv(ALLS_DATA["all_national"], skiprows=8, thousands=',')
+    df = datasource.generate_breakdown_df(breakdown, geo_level, alls_df)
+    expected_df = pd.read_csv(GOLDEN_DATA['age_national'], dtype=dtype)
+
+    assert_frame_equal(df, expected_df, check_like=True)
 
 
 @mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
-def testGenerateAgeNational(
-    mock_data_dir: mock.MagicMock,
-):
+def testGenerateRaceNational(mock_data_dir: mock.MagicMock):
     datasource = CDCHIVData()
-    df = datasource.generate_breakdown_df(
-        'age', 'national')
-    expected_df_age_national = pd.read_csv(
-        GOLDEN_DATA['age_national'], dtype={'state_fips': str})
+    breakdown = 'race_and_ethnicity'
+    geo_level = 'national'
 
-    assert_frame_equal(df, expected_df_age_national, check_like=True)
+    alls_df = pd.read_csv(ALLS_DATA["all_national"], skiprows=8, thousands=',')
+    df = datasource.generate_breakdown_df(breakdown, geo_level, alls_df)
+    expected_df = pd.read_csv(GOLDEN_DATA['race_national'], dtype=dtype)
+
+    assert_frame_equal(df, expected_df, check_like=True)
 
 
 @mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
-def testGenerateSexNational(
-    mock_data_dir: mock.MagicMock,
-):
+def testGenerateSexNational(mock_data_dir: mock.MagicMock):
     datasource = CDCHIVData()
-    df = datasource.generate_breakdown_df(
-        'sex', 'national')
+    breakdown = 'sex'
+    geo_level = 'national'
+
+    alls_df = pd.read_csv(ALLS_DATA["all_national"], skiprows=8, thousands=',')
+    df = datasource.generate_breakdown_df(breakdown, geo_level, alls_df)
     expected_df_sex_national = pd.read_csv(
-        GOLDEN_DATA['sex_national'], dtype={'state_fips': str})
+        GOLDEN_DATA['sex_national'], dtype=dtype)
 
     assert_frame_equal(df, expected_df_sex_national, check_like=True)
 
 
 @mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
-def testGenerateRaceState(
-    mock_data_dir: mock.MagicMock,
-):
+def testGenerateRaceState(mock_data_dir: mock.MagicMock):
     datasource = CDCHIVData()
-    df = datasource.generate_breakdown_df(
-        'race_and_ethnicity', 'state')
-    expected_df_race_sex = pd.read_csv(
+    breakdown = 'race_and_ethnicity'
+    geo_level = 'state'
+
+    alls_df = pd.read_csv(ALLS_DATA['all_state'], dtype={
+                          'FIPS': str}, skiprows=8, thousands=',')
+    df = datasource.generate_breakdown_df(breakdown, geo_level, alls_df)
+    expected_df = pd.read_csv(
         GOLDEN_DATA['race_state'], dtype={'state_fips': str})
 
-    assert_frame_equal(df, expected_df_race_sex, check_like=True)
+    assert_frame_equal(df, expected_df, check_like=True)
 
 
-def _generate_breakdown(*args):
+def _generate_breakdown_df(*args):
     print("mocking the breakdown calc function")
     return pd.DataFrame({
         "state_fips": ["01", "02", "03"],
@@ -101,31 +109,52 @@ def _generate_alls_df(*args):
     })
 
 
-@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq',
-            return_value=None)
-@mock.patch('datasources.cdc_hiv.CDCHIVData.generate_breakdown_df',
-            side_effect=_generate_breakdown)
-@mock.patch('datasources.cdc_hiv.generate_alls_df',
-            side_effect=_generate_alls_df)
-def testWriteToBqCalls(
-    mock_alls: mock.MagicMock,
-    mock_breakdown: mock.MagicMock,
-    mock_bq: mock.MagicMock,
-):
-    datasource = CDCHIVData()
-    datasource.write_to_bq('dataset', 'gcs_bucket')
+# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+# @mock.patch('datasources.cdc_hiv_.CDCHIVData.generate_breakdown_df', side_effect=_generate_breakdown_df)
+# @mock.patch('datasources.cdc_hiv_.generate_alls_df', side_effect=_generate_alls_df)
+# def testWriteToBqCalls(
+#     mock_alls_df: mock.MagicMock,
+#     mock_breakdown_df: mock.MagicMock,
+#     mock_bq: mock.MagicMock,
+# ):
+#     datasource = CDCHIVData()
+#     datasource.write_to_bq('dataset', 'gcs_bucket')
 
-    assert mock_bq.call_count == 9
+#     assert mock_bq.call_count == 9
 
-    expected_table_names = [
-        call[0][2] for call in mock_bq.call_args_list
-    ]
-    assert expected_table_names == ["age_county",
-                                    "race_and_ethnicity_county",
-                                    "sex_county",
-                                    "age_state",
-                                    "race_and_ethnicity_state",
-                                    "sex_state",
-                                    "age_national",
-                                    "race_and_ethnicity_national",
-                                    "sex_national"]
+#     expected_table_names = [
+#         call[0][2] for call in mock_bq.call_args_list
+#     ]
+#     assert expected_table_names == ["age_county",
+#                                     "race_and_ethnicity_county",
+#                                     "sex_county",
+#                                     "age_state",
+#                                     "race_and_ethnicity_state",
+#                                     "sex_state",
+#                                     "age_national",
+#                                     "race_and_ethnicity_national",
+#                                     "sex_national"]
+
+# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+# @mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir', side_effect=_load_csv_as_df_from_data_dir)
+# def testWriteToBqCalls(
+#     mock_data_dir_df: mock.MagicMock,
+#     mock_bq: mock.MagicMock,
+# ):
+#     datasource = CDCHIVTimeData()
+#     datasource.write_to_bq('dataset', 'gcs_bucket')
+
+#     assert mock_bq.call_count == 9
+
+#     expected_table_names = [
+#         call[0][2] for call in mock_bq.call_args_list
+#     ]
+#     assert expected_table_names == ["age_county",
+#                                     "race_and_ethnicity_county",
+#                                     "sex_county",
+#                                     "age_state",
+#                                     "race_and_ethnicity_state",
+#                                     "sex_state",
+#                                     "age_national",
+#                                     "race_and_ethnicity_national",
+#                                     "sex_national"]
