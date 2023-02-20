57a3707466b626f829b193620b21ceb06edeb9a1
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-20 14:32:02 -0700

    guh

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
 import "typeface-hind";
@@ -27,11 +27,14 @@ persistQueryClient({
   persistor: localStoragePersistor,
 });
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
