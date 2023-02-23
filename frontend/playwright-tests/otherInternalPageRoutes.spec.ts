743625edd1ff4cb3ac89901fcf520567756c5886
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-23 16:12:25 -0700

    use role locator

--- frontend/playwright-tests/otherInternalPageRoutes.spec.ts
+++ frontend/playwright-tests/otherInternalPageRoutes.spec.ts
@@ -18,12 +18,10 @@ test.describe.configure({ mode: 'parallel' });
 
 
 test('WIHE Page Loads', async ({ page }) => {
-
     await page.goto(WHAT_IS_HEALTH_EQUITY_PAGE_LINK);
-    // @ts-ignore
+    // @ts-ignore   
     await expect(page).toPassAxe()
-    const mainHeading = page.locator('#main');
-    await expect(mainHeading).toHaveText(['What is Health Equity?']);
+    await expect(page.getByRole('heading', { name: 'What is Health Equity?', exact: true })).toBeVisible();
 });
 
 
