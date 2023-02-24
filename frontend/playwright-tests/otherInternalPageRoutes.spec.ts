48cb246069211e90e380306351acc15e9e04d7f4
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-24 08:57:14 -0700

    wait

--- frontend/playwright-tests/otherInternalPageRoutes.spec.ts
+++ frontend/playwright-tests/otherInternalPageRoutes.spec.ts
@@ -18,7 +18,7 @@ test.describe.configure({ mode: 'parallel' });
 
 
 test('WIHE Page Loads', async ({ page }) => {
-    await page.goto(WHAT_IS_HEALTH_EQUITY_PAGE_LINK);
+    await page.goto(WHAT_IS_HEALTH_EQUITY_PAGE_LINK, { waitUntil: "networkidle" });
     // @ts-ignore   
     await expect(page).toPassAxe()
     await expect(page.getByRole('heading', { name: 'What is Health Equity?', exact: true })).toBeVisible();
