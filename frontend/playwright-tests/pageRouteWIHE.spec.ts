699b990e2b878ff1800575952adba19fbaec5208
Ben Hammond <benjamin.hammond@gmail.com>
2023-02-24 22:23:05 -0700

    do a11y test after load

--- frontend/playwright-tests/pageRouteWIHE.spec.ts
+++ frontend/playwright-tests/pageRouteWIHE.spec.ts
@@ -5,8 +5,8 @@ const WHAT_IS_HEALTH_EQUITY_PAGE_LINK = "/whatishealthequity";
 
 test('WIHE Page Loads', async ({ page }) => {
     await page.goto(WHAT_IS_HEALTH_EQUITY_PAGE_LINK, { waitUntil: "networkidle" });
+    await expect(page.getByRole('heading', { name: 'What is Health Equity?', exact: true })).toBeVisible();
     // @ts-ignore   
     await expect(page).toPassAxe()
-    await expect(page.getByRole('heading', { name: 'What is Health Equity?', exact: true })).toBeVisible();
 });
 
--- frontend/playwright-tests/pageRoutesInternal.spec.ts
+++ frontend/playwright-tests/pageRoutesInternal.spec.ts
@@ -16,58 +16,58 @@ test.describe.configure({ mode: 'parallel' });
 
 test('Resources Tab Loads', async ({ page }) => {
     await page.goto(RESOURCES_TAB_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Health Equity']);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 
 test('Methodology Tab Loads', async ({ page }) => {
     await page.goto(METHODOLOGY_TAB_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Recommended citation (APA) for the Health Equity Tracker:']);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 test('Age-Adjustment Tab Loads', async ({ page }) => {
     await page.goto(AGE_ADJUSTMENT_TAB_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText(['Calculating Age-Adjusted Ratios']);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 test('About Us Page / Project Tab Loads', async ({ page }) => {
     await page.goto(ABOUT_US_PAGE_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`We're focused on equitable data.`]);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 test('Our Team Tab Loads', async ({ page }) => {
     await page.goto(OURTEAM_TAB_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`We're working towards a better tomorrow.`]);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 test('Contact Tab Loads', async ({ page }) => {
     await page.goto(CONTACT_TAB_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toContainText([`Let's move`], { useInnerText: true });
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
 
 
 test('Terms of Use Page Loads', async ({ page }) => {
     await page.goto(TERMS_OF_USE_PAGE_LINK, { waitUntil: "networkidle" });
-    // @ts-ignore
-    await expect(page).toPassAxe()
     const mainHeading = page.locator('#main');
     await expect(mainHeading).toHaveText([`Terms of Use`]);
+    // @ts-ignore
+    await expect(page).toPassAxe()
 });
