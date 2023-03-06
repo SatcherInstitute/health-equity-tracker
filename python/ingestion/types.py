cc493129b7ada8220a3227c41d5a54dc4ae9a487
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-06 16:42:02 -0700

    types file

---
+++ python/ingestion/types.py
@@ -0,0 +1,13 @@
+from typing import Literal
+
+""" Use these when using cast(TYPE, variable) to explicitly tell the linter 
+that the variable is actually the expected type.
+
+Prefer using an explicit `Literal["a", "b"]` in function signitures however, as
+it seems that (at least VSCODE) can't interpret these TYPE variables as the underlying literals. """
+
+SEX_RACE_AGE_TYPE = Literal["sex", "age", "race"]
+SEX_RACE_ETH_AGE_TYPE = Literal["sex", "age", "race_and_ethnicity"]
+INCARCERATION_TYPE = Literal["jail", "prison"]
+VERA_PROPERTY_TYPE = Literal["raw", "rate",
+                             "population", "total_confined_children"]
