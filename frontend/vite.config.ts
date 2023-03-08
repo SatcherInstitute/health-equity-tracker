b0a1a7951bdc65bdca0d42f2ca77dfc710dc18f0
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-08 11:33:43 -0700

    fixup tests; make jest run watch mode on dev local server start

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
 	build: {
 		outDir: 'build',
