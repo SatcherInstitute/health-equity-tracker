693a61c7e4f39c4cefe1b54f89f6b817551907bd
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-10 14:06:46 -0700

    update dag to accept GEO and DEMO

--- airflow/dags/decia_2020_territory_population.py
+++ airflow/dags/decia_2020_territory_population.py
@@ -1,6 +1,7 @@
 # Ignore the Airflow module, it is installed in both dev and prod
 from airflow import DAG  # type: ignore
 from airflow.utils.dates import days_ago  # type: ignore
+from airflow.operators.dummy_operator import DummyOperator  # type: ignore
 
 import util
 
@@ -15,38 +16,94 @@ data_ingestion_dag = DAG(
     'decia_2020_population_data_ingestion_dag',
     default_args=default_args,
     schedule_interval=None,
-    description='Ingestion configuration for Island Areas population data')
+    description='Ingestion configuration for Island Areas 2020 population data')
 
-decia_2020_population_data_bq_payload = util.generate_bq_payload(
-    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME)
-decia_2020_population_data_bq_operator = util.create_bq_ingest_operator(
-    'decia_2020_population_data_to_bq', decia_2020_population_data_bq_payload, data_ingestion_dag)
 
-payload_race = {
+# 6 ingest operators: 3 demographics X 2 geographics
+bq_payload_race_state = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="race_and_ethnicity",
+    geographic="state")
+decia_2020_bq_ingest_race_state = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_race_state', bq_payload_race_state, data_ingestion_dag)
+
+bq_payload_age_state = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="age",
+    geographic="state")
+decia_2020_bq_ingest_age_state = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_age_state', bq_payload_age_state, data_ingestion_dag)
+
+bq_payload_sex_state = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="sex",
+    geographic="state")
+decia_2020_bq_ingest_sex_state = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_sex_state', bq_payload_sex_state, data_ingestion_dag)
+
+bq_payload_race_county = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="race_and_ethnicity",
+    geographic="county"
+)
+decia_2020_bq_ingest_race_county = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_race_county', bq_payload_race_county, data_ingestion_dag)
+
+bq_payload_age_county = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="age",
+    geographic="county"
+)
+decia_2020_bq_ingest_age_county = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_age_county', bq_payload_age_county, data_ingestion_dag)
+
+bq_payload_sex_county = util.generate_bq_payload(
+    _DECIA_2020_TERRITORY_POPULATION_WORKFLOW_ID, _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
+    demographic="sex",
+    geographic="county"
+)
+decia_2020_bq_ingest_sex_county = util.create_bq_ingest_operator(
+    'decia_2020_pop_to_bq_sex_county', bq_payload_sex_county, data_ingestion_dag)
+
+exporter_payload_race = {
     'dataset_name': _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
     'demographic': "race_and_ethnicity"
 }
 decia_2020_population_data_exporter_operator_race = util.create_exporter_operator(
-    'decia_2020_population_data_exporter_race', payload_race, data_ingestion_dag)
+    'decia_2020_population_data_exporter_race', exporter_payload_race, data_ingestion_dag)
 
-payload_age = {
+exporter_payload_age = {
     'dataset_name': _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
     'demographic': "age"
 }
 decia_2020_population_data_exporter_operator_age = util.create_exporter_operator(
-    'decia_2020_population_data_exporter_age', payload_age, data_ingestion_dag)
+    'decia_2020_population_data_exporter_age', exporter_payload_age, data_ingestion_dag)
 
 
-payload_sex = {
+exporter_payload_sex = {
     'dataset_name': _DECIA_2020_TERRITORY_POPULATION_DATASET_NAME,
     'demographic': "sex"
 }
 decia_2020_population_data_exporter_operator_sex = util.create_exporter_operator(
-    'decia_2020_population_data_exporter_sex', payload_sex, data_ingestion_dag)
+    'decia_2020_population_data_exporter_sex', exporter_payload_sex, data_ingestion_dag)
+
+connector = DummyOperator(
+    default_args=default_args,
+    dag=data_ingestion_dag,
+    task_id='connector'
+)
 
 # Ingestion DAG
 (
-    decia_2020_population_data_bq_operator >> [
+    [
+        decia_2020_bq_ingest_race_state,
+        decia_2020_bq_ingest_age_state,
+        decia_2020_bq_ingest_sex_state,
+        decia_2020_bq_ingest_race_county,
+        decia_2020_bq_ingest_age_county,
+        decia_2020_bq_ingest_sex_county,
+    ] >>
+    connector >> [
         decia_2020_population_data_exporter_operator_race,
         decia_2020_population_data_exporter_operator_age,
         decia_2020_population_data_exporter_operator_sex,
