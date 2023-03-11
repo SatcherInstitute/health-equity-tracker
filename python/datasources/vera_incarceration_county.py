355ccd60db5d6487c6b47e69066662f1a57a485f
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-11 16:05:20 -0700

    cleanup

--- python/datasources/vera_incarceration_county.py
+++ python/datasources/vera_incarceration_county.py
@@ -1,53 +1,46 @@
 import pandas as pd
 from datasources.data_source import DataSource
 from ingestion import gcs_to_bq_util, dataset_utils
-from ingestion.standardized_columns import (
-    Race,
-    RACE_OR_HISPANIC_COL,
-    SEX_COL,
-)
-from ingestion.dataset_utils import (
-    generate_pct_share_col_without_unknowns,
-    ensure_leading_zeros,
-    generate_pct_rel_inequity_col,
-    zero_out_pct_rel_inequity
-)
+from ingestion.standardized_columns import (Race,
+                                            RACE_OR_HISPANIC_COL,
+                                            SEX_COL)
+from ingestion.dataset_utils import (ensure_leading_zeros,
+                                     generate_pct_share_col_without_unknowns,
+                                     generate_pct_rel_inequity_col,
+                                     zero_out_pct_rel_inequity)
 from ingestion.merge_utils import merge_county_names
-from ingestion.constants import (Sex)
+from ingestion.constants import (Sex, COUNTY_LEVEL)
 import ingestion.standardized_columns as std_col
 from typing import Literal, cast
-from ingestion.types import (
-    INCARCERATION_TYPE, VERA_PROPERTY_TYPE, SEX_RACE_AGE_TYPE, SEX_RACE_ETH_AGE_TYPE, DEMOGRAPHIC_TYPE
-)
+from ingestion.types import (SEX_RACE_AGE_TYPE,
+                             SEX_RACE_ETH_AGE_TYPE,
+                             DEMOGRAPHIC_TYPE)
 
-JAIL = cast(INCARCERATION_TYPE, std_col.JAIL_PREFIX)
-PRISON = std_col.PRISON_PREFIX
+BASE_VERA_URL = 'https://github.com/vera-institute/incarceration_trends/blob/master/incarceration_trends.csv?raw=true'
 
-RAW = "raw"
-RATE = "rate"
-PCT_SHARE = "pct_share"
-POP = "population"
-CHILDREN = cast(VERA_PROPERTY_TYPE, std_col.CHILDREN)
+VERA_YEAR = "year"
+VERA_FIPS = "fips"
+VERA_COUNTY = "county_name"
 
 RAW_COL_MAP = {
-    JAIL: std_col.JAIL_RAW,
-    PRISON: std_col.PRISON_RAW
+    std_col.JAIL_PREFIX: std_col.JAIL_RAW,
+    std_col.PRISON_PREFIX: std_col.PRISON_RAW
 }
 
 PER_100K_COL_MAP = {
-    JAIL: std_col.JAIL_RATE,
-    PRISON: std_col.PRISON_RATE
+    std_col.JAIL_PREFIX: std_col.JAIL_RATE,
+    std_col.PRISON_PREFIX: std_col.PRISON_RATE
 }
 
 PCT_SHARE_COL_MAP = {
-    JAIL: std_col.JAIL_PCT_SHARE,
-    PRISON: std_col.PRISON_PCT_SHARE,
-    POP: std_col.POP_PCT_SHARE
+    std_col.JAIL_PREFIX: std_col.JAIL_PCT_SHARE,
+    std_col.PRISON_PREFIX: std_col.PRISON_PCT_SHARE,
+    std_col.POPULATION_COL: std_col.POP_PCT_SHARE
 }
 
 PCT_REL_INEQUITY_COL_MAP = {
-    JAIL: std_col.JAIL_PCT_INEQUITY,
-    PRISON: std_col.PRISON_PCT_INEQUITY
+    std_col.JAIL_PREFIX: std_col.JAIL_PCT_INEQUITY,
+    std_col.PRISON_PREFIX: std_col.PRISON_PCT_INEQUITY
 }
 
 # VERA ALL COLS
@@ -57,9 +50,6 @@ PRISON_RATE_ALL = "total_prison_pop_rate"
 JAIL_RATE_ALL = "total_jail_pop_rate"
 POP_ALL = "total_pop_15to64"
 
-BASE_VERA_URL = 'https://github.com/vera-institute/incarceration_trends/blob/master/incarceration_trends.csv?raw=true'
-LATEST_JAIL_YEAR = 2018
-LATEST_PRISON_YEAR = 2016
 
 RACE_POP_TO_STANDARD = {
     "aapi_pop_15to64": Race.API_NH.value,
@@ -73,7 +63,6 @@ SEX_POP_TO_STANDARD = {
     "female_pop_15to64": Sex.FEMALE,
     "male_pop_15to64": Sex.MALE
 }
-
 
 RACE_PRISON_RAW_COLS_TO_STANDARD = {
     "aapi_prison_pop": Race.API_NH.value,
@@ -130,53 +119,34 @@ SEX_JAIL_RATE_COLS_TO_STANDARD = {
 }
 
 DATA_TYPE_TO_COL_MAP = {
-    PRISON: {RAW_COL_MAP[PRISON]: PCT_SHARE_COL_MAP[PRISON]},
-    JAIL: {RAW_COL_MAP[JAIL]: PCT_SHARE_COL_MAP[JAIL]}
+    std_col.PRISON_PREFIX: {RAW_COL_MAP[std_col.PRISON_PREFIX]: PCT_SHARE_COL_MAP[std_col.PRISON_PREFIX]},
+    std_col.JAIL_PREFIX: {
+        RAW_COL_MAP[std_col.JAIL_PREFIX]: PCT_SHARE_COL_MAP[std_col.JAIL_PREFIX]}
 }
 
-# Column mappings used for melting the jail_pct_share columns into rows
-SEX_JAIL_PCT_SHARE_MELT_COLS_MAP = {
-    f'{Sex.FEMALE}_jail_pct_share': Sex.FEMALE,
-    f'{Sex.MALE}_jail_pct_share': Sex.MALE
-}
-RACE_JAIL_PCT_SHARE_MELT_COLS_MAP = {
-    f'{Race.API_NH.value}_jail_pct_share': Race.API_NH.value,
-    f'{Race.BLACK_NH.value}_jail_pct_share': Race.BLACK_NH.value,
-    f'{Race.HISP.value}_jail_pct_share': Race.HISP.value,
-    f'{Race.AIAN_NH.value}_jail_pct_share': Race.AIAN_NH.value,
-    f'{Race.WHITE_NH.value}_jail_pct_share': Race.WHITE_NH.value,
-}
-
-ALL_JAIL_PCT_SHARE = "all_jail_pct_share"
-
-JUVENILE_COLS = [
-    "female_juvenile_jail_pop",
-    "male_juvenile_jail_pop"
-]
-
+JUVENILE_COLS = ["female_juvenile_jail_pop",
+                 "male_juvenile_jail_pop"]
 JUVENILE = "0-17"
 ADULT = "18+"
 
-# NO PRISON/AGE BREAKDOWN DATA
+# NO AGE BREAKDOWN DATA
 
-DATA_COLS = [
-    *RACE_PRISON_RAW_COLS_TO_STANDARD.keys(),
-    *RACE_PRISON_RATE_COLS_TO_STANDARD.keys(),
-    *SEX_PRISON_RAW_COLS_TO_STANDARD.keys(),
-    *SEX_PRISON_RATE_COLS_TO_STANDARD.keys(),
-    *RACE_JAIL_RAW_COLS_TO_STANDARD.keys(),
-    *RACE_JAIL_RATE_COLS_TO_STANDARD.keys(),
-    *SEX_JAIL_RAW_COLS_TO_STANDARD.keys(),
-    *SEX_JAIL_RATE_COLS_TO_STANDARD.keys(),
-    PRISON_RAW_ALL,
-    JAIL_RAW_ALL,
-    PRISON_RATE_ALL,
-    JAIL_RATE_ALL
-]
+DATA_COLS = [*RACE_PRISON_RAW_COLS_TO_STANDARD.keys(),
+             *RACE_PRISON_RATE_COLS_TO_STANDARD.keys(),
+             *SEX_PRISON_RAW_COLS_TO_STANDARD.keys(),
+             *SEX_PRISON_RATE_COLS_TO_STANDARD.keys(),
+             *RACE_JAIL_RAW_COLS_TO_STANDARD.keys(),
+             *RACE_JAIL_RATE_COLS_TO_STANDARD.keys(),
+             *SEX_JAIL_RAW_COLS_TO_STANDARD.keys(),
+             *SEX_JAIL_RATE_COLS_TO_STANDARD.keys(),
+             PRISON_RAW_ALL,
+             JAIL_RAW_ALL,
+             PRISON_RATE_ALL,
+             JAIL_RATE_ALL]
 
 GEO_COLS_TO_STANDARD = {
-    "fips": std_col.COUNTY_FIPS_COL,
-    "county_name": std_col.COUNTY_NAME_COL
+    VERA_FIPS: std_col.COUNTY_FIPS_COL,
+    VERA_COUNTY: std_col.COUNTY_NAME_COL
 }
 
 POP_COLS = [
@@ -189,7 +159,7 @@ location_col_types = {col: str for col in GEO_COLS_TO_STANDARD.keys()}
 data_col_types = {col: float for col in DATA_COLS}
 pop_col_types = {col: float for col in POP_COLS}
 VERA_COL_TYPES = {
-    "year": str,
+    VERA_YEAR: str,
     **location_col_types,
     **data_col_types,  # type: ignore
     **pop_col_types  # type: ignore
@@ -216,7 +186,7 @@ class VeraIncarcerationCounty(DataSource):
         df = gcs_to_bq_util.load_csv_as_df_from_web(
             BASE_VERA_URL, dtype=VERA_COL_TYPES)
         df = df.rename(
-            columns={"fips": std_col.COUNTY_FIPS_COL, "year": std_col.TIME_PERIOD_COL})
+            columns={VERA_FIPS: std_col.COUNTY_FIPS_COL, VERA_YEAR: std_col.TIME_PERIOD_COL})
         df = ensure_leading_zeros(df, std_col.COUNTY_FIPS_COL, 5)
         df = merge_county_names(df)
 
@@ -231,11 +201,11 @@ class VeraIncarcerationCounty(DataSource):
 
         float_cols = [
             *PER_100K_COL_MAP.values(),
-            CHILDREN,
+            std_col.CHILDREN,
             *PCT_SHARE_COL_MAP.values(),
             *RAW_COL_MAP.values(),
-            POP,
-            PCT_SHARE_COL_MAP[POP],
+            std_col.POPULATION_COL,
+            PCT_SHARE_COL_MAP[std_col.POPULATION_COL],
             *PCT_REL_INEQUITY_COL_MAP.values()
         ]
         column_types = gcs_to_bq_util.get_bq_column_types(
@@ -261,34 +231,33 @@ class VeraIncarcerationCounty(DataSource):
             demo_col = demo_type
             demo_short = demo_type
 
-        """ {std_col.POPULATION_COL: count_group_cols_map,
-                std_col.POPULATION_PCT_COL: pct_share_group_cols_map} """
         # dict of desired metric col name to its
         # mapping of group-metric cols to HET groups
         melt_map = {
             std_col.RACE_OR_HISPANIC_COL: {
-                POP: {**RACE_POP_TO_STANDARD, POP_ALL: all_val},
-                RAW_COL_MAP[JAIL]: {**RACE_JAIL_RAW_COLS_TO_STANDARD, JAIL_RAW_ALL: all_val},
-                PER_100K_COL_MAP[JAIL]: {**RACE_JAIL_RATE_COLS_TO_STANDARD, JAIL_RATE_ALL: all_val},
-                RAW_COL_MAP[PRISON]: {**RACE_PRISON_RAW_COLS_TO_STANDARD, PRISON_RAW_ALL: all_val},
-                PER_100K_COL_MAP[PRISON]: {**RACE_PRISON_RATE_COLS_TO_STANDARD, PRISON_RATE_ALL: all_val},
-                CHILDREN: {CHILDREN: all_val},
+                std_col.POPULATION_COL: {**RACE_POP_TO_STANDARD, POP_ALL: all_val},
+                RAW_COL_MAP[std_col.JAIL_PREFIX]: {**RACE_JAIL_RAW_COLS_TO_STANDARD, JAIL_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.JAIL_PREFIX]: {**RACE_JAIL_RATE_COLS_TO_STANDARD, JAIL_RATE_ALL: all_val},
+                RAW_COL_MAP[std_col.PRISON_PREFIX]: {**RACE_PRISON_RAW_COLS_TO_STANDARD, PRISON_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.PRISON_PREFIX]: {
+                    **RACE_PRISON_RATE_COLS_TO_STANDARD, PRISON_RATE_ALL: all_val},
+                std_col.CHILDREN: {std_col.CHILDREN: all_val},
             },
             std_col.SEX_COL: {
-                POP: {**SEX_POP_TO_STANDARD, POP_ALL: all_val},
-                RAW_COL_MAP[JAIL]: {**SEX_JAIL_RAW_COLS_TO_STANDARD, JAIL_RAW_ALL: all_val},
-                PER_100K_COL_MAP[JAIL]: {**SEX_JAIL_RATE_COLS_TO_STANDARD, JAIL_RATE_ALL: all_val},
-                RAW_COL_MAP[PRISON]: {**SEX_PRISON_RAW_COLS_TO_STANDARD, PRISON_RAW_ALL: all_val},
-                PER_100K_COL_MAP[PRISON]: {**SEX_PRISON_RATE_COLS_TO_STANDARD, PRISON_RATE_ALL: all_val},
-                CHILDREN: {CHILDREN: all_val}
+                std_col.POPULATION_COL: {**SEX_POP_TO_STANDARD, POP_ALL: all_val},
+                RAW_COL_MAP[std_col.JAIL_PREFIX]: {**SEX_JAIL_RAW_COLS_TO_STANDARD, JAIL_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.JAIL_PREFIX]: {**SEX_JAIL_RATE_COLS_TO_STANDARD, JAIL_RATE_ALL: all_val},
+                RAW_COL_MAP[std_col.PRISON_PREFIX]: {**SEX_PRISON_RAW_COLS_TO_STANDARD, PRISON_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.PRISON_PREFIX]: {**SEX_PRISON_RATE_COLS_TO_STANDARD, PRISON_RATE_ALL: all_val},
+                std_col.CHILDREN: {std_col.CHILDREN: all_val}
             },
             std_col.AGE_COL: {
-                POP: {POP_ALL: all_val},
-                RAW_COL_MAP[JAIL]: {JAIL_RAW_ALL: all_val},
-                PER_100K_COL_MAP[JAIL]: {JAIL_RATE_ALL: all_val},
-                RAW_COL_MAP[PRISON]: {PRISON_RAW_ALL: all_val},
-                PER_100K_COL_MAP[PRISON]: {PRISON_RATE_ALL: all_val},
-                CHILDREN: {CHILDREN: all_val},
+                std_col.POPULATION_COL: {POP_ALL: all_val},
+                RAW_COL_MAP[std_col.JAIL_PREFIX]: {JAIL_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.JAIL_PREFIX]: {JAIL_RATE_ALL: all_val},
+                RAW_COL_MAP[std_col.PRISON_PREFIX]: {PRISON_RAW_ALL: all_val},
+                PER_100K_COL_MAP[std_col.PRISON_PREFIX]: {PRISON_RATE_ALL: all_val},
+                std_col.CHILDREN: {std_col.CHILDREN: all_val},
             },
         }
 
@@ -300,32 +269,34 @@ class VeraIncarcerationCounty(DataSource):
         )
 
         # round 100k values
-        for data_type in [PRISON, JAIL]:
+        for data_type in [std_col.PRISON_PREFIX, std_col.JAIL_PREFIX]:
             breakdown_df[PER_100K_COL_MAP[data_type]
                          ] = breakdown_df[PER_100K_COL_MAP[data_type]].dropna().round()
 
         breakdown_df[std_col.STATE_FIPS_COL] = breakdown_df[std_col.COUNTY_FIPS_COL].astype(
             str).str[:2]
 
-        # calculate pct_share cols for JAIL (share of summed group counts)
-        # and PRISON and POP (share of provided ALL counts)
+        # calculate pct_share cols for std_col.JAIL_PREFIX (share of summed group counts)
+        # and std_col.PRISON_PREFIX and std_col.POPULATION_COL (share of provided ALL counts)
         breakdown_df = generate_pct_share_col_without_unknowns(
             breakdown_df,
             {
-                **DATA_TYPE_TO_COL_MAP[PRISON],
-                **DATA_TYPE_TO_COL_MAP[JAIL],
-                POP: PCT_SHARE_COL_MAP[POP]
+                **DATA_TYPE_TO_COL_MAP[std_col.PRISON_PREFIX],
+                **DATA_TYPE_TO_COL_MAP[std_col.JAIL_PREFIX],
+                std_col.POPULATION_COL: PCT_SHARE_COL_MAP[std_col.POPULATION_COL]
             },
             cast(SEX_RACE_AGE_TYPE, demo_col),
             all_val)
 
         # add relative inequity cols for jail and prison
-        for data_type in [PRISON, JAIL]:
-            breakdown_df = generate_pct_rel_inequity_col(
-                breakdown_df, PCT_SHARE_COL_MAP[data_type], PCT_SHARE_COL_MAP[POP], PCT_REL_INEQUITY_COL_MAP[data_type])
+        for data_type in [std_col.PRISON_PREFIX, std_col.JAIL_PREFIX]:
+            breakdown_df = generate_pct_rel_inequity_col(breakdown_df,
+                                                         PCT_SHARE_COL_MAP[data_type],
+                                                         PCT_SHARE_COL_MAP[std_col.POPULATION_COL],
+                                                         PCT_REL_INEQUITY_COL_MAP[data_type])
             breakdown_df = zero_out_pct_rel_inequity(
                 breakdown_df,
-                "county",
+                COUNTY_LEVEL,
                 cast(SEX_RACE_AGE_TYPE, demo_short),
                 {
                     PER_100K_COL_MAP[data_type]: PCT_REL_INEQUITY_COL_MAP[data_type]
@@ -339,8 +310,8 @@ class VeraIncarcerationCounty(DataSource):
                        *PCT_SHARE_COL_MAP.values(),
                        *PCT_REL_INEQUITY_COL_MAP.values(),
                        *RAW_COL_MAP.values(),
-                       POP,
-                       CHILDREN
+                       std_col.POPULATION_COL,
+                       std_col.CHILDREN
                        ]
 
         # by_race gets extra cols
@@ -359,7 +330,7 @@ def add_confined_children_col(df):
     """ Parameters: df: pandas df containing the entire Vera csv file.
     Returns same df replacing juvenile cols with a summed, rounded `total_confined_children` col
     """
-    df[CHILDREN] = df[JUVENILE_COLS].sum(
+    df[std_col.CHILDREN] = df[JUVENILE_COLS].sum(
         axis="columns", numeric_only=True).round(0)
     df = df.drop(columns=JUVENILE_COLS)
     return df
@@ -370,7 +341,7 @@ def use_sum_of_jail_counts_as_all(df, demo_type: Literal["sex", "race_and_ethnic
     as Vera TOTAL jail counts are yearly averages, while the GROUP
     jail counts are single-day actual counts.
     This means that the sum of the groups might not equal the TOTAL.
-    Because of this, we will overwrite the given ALL JAIL ESTIMATE TOTAL with a
+    Because of this, we will overwrite the given ALL std_col.JAIL_PREFIX ESTIMATE TOTAL with a
     SUM OF ALL GROUPS ESTIMATED TOTALS for sex and race. We don't do this for age
     as we don't have any group breakdowns for age, only the given ALL values. """
     if demo_type == std_col.SEX_COL:
