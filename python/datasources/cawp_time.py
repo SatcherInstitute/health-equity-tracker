c481d2509033bec8238c9fa474ff97eefb19c982
Ben Hammond <benjamin.hammond@gmail.com>
2023-03-16 15:18:13 -0600

    lint types

--- python/datasources/cawp_time.py
+++ python/datasources/cawp_time.py
@@ -13,12 +13,8 @@ from ingestion.constants import (
     US_ABBR, US_FIPS, US_NAME,
     TERRITORY_POSTALS,
 )
-from typing import Literal, cast, List
-
-SEX_RACE_AGE_TYPE = Literal["sex", "age", "race"]
-GEO_LEVEL_TYPE = Literal["national", "state", "county"]
-
-
+from typing import cast, List
+from ingestion.types import (GEO_TYPE, SEX_RACE_AGE_TYPE)
 RACE = cast(SEX_RACE_AGE_TYPE, "race")
 
 
@@ -287,7 +283,8 @@ class CAWPTimeData(DataSource):
         df = df.drop(
             columns=[std_col.W_ALL_RACES_CONGRESS_COUNT, std_col.W_ALL_RACES_STLEG_COUNT])
 
-        df = merge_utils.merge_yearly_pop_numbers(df, RACE, geo_level)
+        df = merge_utils.merge_yearly_pop_numbers(
+            df, RACE, cast(GEO_TYPE, geo_level))
 
         df = generate_pct_rel_inequity_col(df,
                                            std_col.PCT_OF_W_CONGRESS,
@@ -295,7 +292,7 @@ class CAWPTimeData(DataSource):
                                            std_col.W_CONGRESS_PCT_INEQUITY,
                                            )
         df = zero_out_pct_rel_inequity(df,
-                                       cast(GEO_LEVEL_TYPE, geo_level),
+                                       cast(GEO_TYPE, geo_level),
                                        RACE,
                                        {std_col.PCT_OF_CONGRESS: std_col.W_CONGRESS_PCT_INEQUITY},
                                        std_col.POPULATION_PCT_COL
@@ -307,7 +304,7 @@ class CAWPTimeData(DataSource):
                                            std_col.W_STLEG_PCT_INEQUITY,
                                            )
         df = zero_out_pct_rel_inequity(df,
-                                       cast(GEO_LEVEL_TYPE, geo_level),
+                                       cast(GEO_TYPE, geo_level),
                                        RACE,
                                        {std_col.PCT_OF_STLEG: std_col.W_STLEG_PCT_INEQUITY},
                                        std_col.POPULATION_PCT_COL
