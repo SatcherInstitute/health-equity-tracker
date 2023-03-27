989b419320aa93b0051c8f546136c076abde44c2
Eric Warren <eric.m.warren1@gmail.com>
2023-03-27 12:22:31 -0400

    updating test files

--- python/tests/datasources/test_uhc_test.py
+++ python/tests/datasources/test_uhc_test.py
@@ -1,15 +1,21 @@
 from unittest import mock
 import os
+
 import pandas as pd
 from pandas._testing import assert_frame_equal
+
 from test_utils import get_state_fips_codes_as_df
 from datasources.uhc_test import UHCData
 import ingestion.standardized_columns as std_col
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
-TEST_DIR = os.path.join(THIS_DIR, os.pardir, "data")
+TEST_DIR = os.path.join(THIS_DIR, os.pardir, "data", "uhc_brfss")
 GOLDEN_DIR = os.path.join(TEST_DIR, 'golden_data')
+
+GOLDEN_DATA = {
+    'race_ethnicity_national': os.path.join(GOLDEN_DIR, 'age_national_output.csv'),
+    'age_national': os.path.join(GOLDEN_DIR, 'race_and_ethnicity_national_output.csv'),
+    'sex_state': os.path.join(GOLDEN_DIR, 'race_and_ethnicity_state_output.csv')}
 
 
 def _load_csv_as_df_from_data_dir(*args, **kwargs):
