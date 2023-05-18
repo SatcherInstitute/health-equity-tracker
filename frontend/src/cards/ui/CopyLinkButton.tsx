f3059f9117d417c2f22eb55876554f3c13843d64
Eric M Warren II <eric.m.warren1@gmail.com>
2023-05-18 11:15:15 -0400

    updated styling for CopyLinkButton

--- frontend/src/cards/ui/CopyLinkButton.tsx
+++ frontend/src/cards/ui/CopyLinkButton.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
-import LinkIcon from '@mui/icons-material/Link'
-import styles from './CopyLinkButton.module.scss'
 import { IconButton, Snackbar, Alert } from '@mui/material'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import LinkIcon from '@mui/icons-material/Link'
 import { type ScrollableHashId } from '../../utils/hooks/useStepObserver'
+import styles from './CopyLinkButton.module.scss'
 
 interface CopyLinkButtonProps {
-  scrollToHash?: ScrollableHashId
+  scrollToHash: ScrollableHashId
 }
 
 export default function CopyLinkButton(props: CopyLinkButtonProps) {
   const [open, setOpen] = useState(false)
 
   const urlWithoutHash = window.location.href.split('#')[0]
-  const cardHashLink = `${urlWithoutHash}#${props.scrollToHash ?? ''}`
+  const cardHashLink = `${urlWithoutHash}#${props.scrollToHash}`
+
+  let cardName = props.scrollToHash.replaceAll('-', ' ') ?? 'Card'
+  cardName = cardName[0].toUpperCase() + cardName.slice(1)
+
+  const title = `Copy direct link to: ${cardName}`
 
   function handleClose() {
     setOpen(false)
@@ -26,21 +33,19 @@ export default function CopyLinkButton(props: CopyLinkButtonProps) {
     asyncHandleClick().catch((error) => error)
   }
 
-  let cardName = props.scrollToHash?.replaceAll('-', ' ') ?? 'Card'
-  cardName = cardName[0].toUpperCase() + cardName.slice(1)
-
-  const title = `Copy direct link to: ${cardName}`
-
   return (
     <>
-      <IconButton
-        className={styles.CopyLinkButton}
-        aria-label={title}
-        onClick={handleClick}
-        sx={{ width: '3rem', height: '3rem' }}
-      >
-        <LinkIcon />
-      </IconButton>
+      <ListItemIcon>
+        <IconButton
+          aria-label={title}
+          className={styles.CopyLinkButton}
+          onClick={handleClick}
+          sx={{ width: '3rem', height: '3rem' }}
+        >
+          <LinkIcon />
+        </IconButton>
+      </ListItemIcon>
+      <ListItemText primary={'Copy card link'} />
       <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
         <Alert onClose={handleClose} className={styles.SnackBarAlert}>
           Direct link to <b>{cardName}</b> copied to clipboard!
