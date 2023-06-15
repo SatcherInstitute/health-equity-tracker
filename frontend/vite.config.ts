2ffcb32b75e5bccdf05c97ab459b886b522c493c
Ben Hammond <benjamin.hammond@gmail.com>
2023-06-14 20:44:44 -0600

    src map

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import { configDefaults } from 'vitest/config'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
 	build: {
+		sourcemap: true,
 		outDir: 'build',
 	},
 	server: {
@@ -18,7 +20,17 @@ export default defineConfig({
 	plugins: [
 		react({
 			include: "**/*.tsx",
-		}), viteTsconfigPaths(), svgrPlugin(),
+		}),
+		viteTsconfigPaths(),
+		svgrPlugin(),
+		sentryVitePlugin({
+			org: "https://healthequitytracker.org",
+			project: "health-equity-tracker",
+
+			// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+			// and need `project:releases` and `org:read` scopes
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+		}),
 	],
 	test: {
 		exclude: [
