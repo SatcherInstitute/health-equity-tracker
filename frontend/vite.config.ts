276c20a8008931c045be4fb88261acce3fc7d07b
Ben Hammond <benjamin.hammond@gmail.com>
2023-06-22 12:15:54 -0600

    Revert "Sentry vite2 (#2261)"
    This reverts commit 24948789f8f7630857b13de6dcd40fb4c851258e.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,56 +1,35 @@
-import { defineConfig, loadEnv } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import { configDefaults } from 'vitest/config'
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 
+// https://vitejs.dev/config/
 
-export default defineConfig(({ mode }) => {
-	// Load env file based on `mode` in the current working directory; local builds are `production`.
-	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-	const env = loadEnv(mode, process.cwd(), '')
-
-	console.log("***** loaded env:", env);
-
-	return {
-		build: {
-			outDir: 'build',
-			sourcemap: true
-		},
-		server: {
-			open: true,
-			port: 3000
-		},
-		plugins: [
-			react({
-				include: "**/*.tsx",
-			}),
-			viteTsconfigPaths(),
-			svgrPlugin(),
-			sentryVitePlugin({
-				org: env.SENTRY_ORG,
-				project: env.SENTRY_PROJECT,
-				// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-				// and need `project:releases` and `org:read` scopes
-				authToken: env.SENTRY_AUTH_TOKEN,
-			}),
+export default defineConfig({
+	build: {
+		outDir: 'build',
+	},
+	server: {
+		open: true,
+		port: 3000
+	},
+	plugins: [
+		react({
+			include: "**/*.tsx",
+		}), viteTsconfigPaths(), svgrPlugin(),
+	],
+	test: {
+		exclude: [
+			...configDefaults.exclude,
+			'playwright-tests/*'
 		],
-		test: {
-			exclude: [
-				...configDefaults.exclude,
-				'playwright-tests/*'
-			],
-			globals: true,
-			environment: 'jsdom',
-			setupFiles: './src/setupTests.ts',
-			coverage: {
-				reporter: ['text', 'html'],
-			},
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/setupTests.ts',
+		coverage: {
+			reporter: ['text', 'html'],
 		},
-	}
-})
-
-
-
-
+	},
+});
