bdad4b8421eed96c67579ea006b9f36cca7d8ef5
Ben Hammond <benjamin.hammond@gmail.com>
2023-06-22 09:21:32 -0600

    log

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,13 +7,11 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 
 export default defineConfig(({ mode }) => {
-	// Load env file based on `mode` in the current working directory.
+	// Load env file based on `mode` in the current working directory; local builds are `production`.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-
 	const env = loadEnv(mode, process.cwd(), '')
 
-
-	console.log(env);
+	console.log("***** loaded env:", env);
 
 	return {
 		build: {
