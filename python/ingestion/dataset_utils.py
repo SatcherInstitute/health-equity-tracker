c108fba7688b9995588d4b51046fa3d3aa6dd82c
Ben Hammond <benjamin.hammond@gmail.com>
2023-07-05 13:07:26 -0600

    lint

--- python/ingestion/dataset_utils.py
+++ python/ingestion/dataset_utils.py
@@ -109,7 +109,7 @@ def scaffold_fips_df(geo_level: Literal["national", "state", "county"]) -> pd.Da
 
 def generate_pct_share_col_without_unknowns(df: pd.DataFrame,
                                             raw_count_to_pct_share: dict,
-                                            breakdown_col: Literal["sex", "age", "race", "race_and_ethnicity", "black_women"],
+                                            breakdown_col: str,
                                             all_val: str) -> pd.DataFrame:
     """Returns a DataFrame with a percent share column based on the raw_count_cols
        Each row must have a corresponding 'ALL' row.
@@ -400,7 +400,7 @@ def zero_out_pct_rel_inequity(df: pd.DataFrame,
                               geo: Literal["national", "state", "county"],
                               demographic: Literal["sex", "age", "race"],
                               rate_to_inequity_col_map: dict,
-                              pop_pct_col: str = None):
+                              pop_pct_col: str = ""):
     """Sets inequitable share of targeted conditions to zero if every known
     demographic group in a particular place/time reports rates of `0` or null.
     The justification for this is that such a small number of case counts can
