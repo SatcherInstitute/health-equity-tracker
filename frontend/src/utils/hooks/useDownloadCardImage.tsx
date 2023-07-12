ea9b5601a316b069fe939249919cc5515bcbf5e8
Eric M Warren II <eric.m.warren1@gmail.com>
2023-07-12 17:02:04 -0400

    added water mark to screenshot

--- frontend/src/utils/hooks/useDownloadCardImage.tsx
+++ frontend/src/utils/hooks/useDownloadCardImage.tsx
@@ -1,36 +1,84 @@
 import { createRef } from 'react'
-import { useScreenshot, createFileName } from 'use-react-screenshot'
+import html2canvas from 'html2canvas'
+import { createFileName } from 'use-react-screenshot'
+import AppBarLogo from '../../assets/AppbarLogo.png'
 
-export function useDownloadCardImage(cardTitle: string) {
-  const screenshotTargetRef = createRef<any>()
+const WATERMARK_X = 10
+const WATERMARK_Y = 10
+const WATERMARK_WIDTH = 50
+const WATERMARK_HEIGHT = 50
 
-  const [, takeScreenShot] = useScreenshot({
-    type: 'image/png',
-    quality: 1.0,
-  })
+export function useDownloadCardImage(cardTitle: string, hiddenElements: string[] = []) {
+  const screenshotTargetRef = createRef<HTMLDivElement>();
 
-  function download(image: any, { name = cardTitle, extension = 'png' } = {}) {
-    const a = document.createElement('a')
-    a.href = image
-    a.download = createFileName(
-      extension,
-      `${name} from Health Equity Tracker ${new Date().toLocaleDateString(
-        undefined,
-        { month: 'short', year: 'numeric' }
-      )}`
-    )
-    a.click()
+  function download(canvas: HTMLCanvasElement, { name = cardTitle, extension = 'png' } = {}) {
+    // Create a new canvas to combine the screenshot and watermark
+    const combinedCanvas = document.createElement('canvas')
+    combinedCanvas.width = canvas.width
+    combinedCanvas.height = canvas.height
+    const context = combinedCanvas.getContext('2d')
+
+    // Draw the screenshot on the combined canvas
+    context?.drawImage(canvas, 0, 0)
+
+    // Load the logo image
+    const logoImage = new Image()
+    logoImage.src = AppBarLogo
+
+    // Position the logo as a watermark on the combined canvas
+    logoImage.onload = () => {
+      // Adjust the opacity of the watermark
+      context && (context.globalAlpha = 0.5)
+      context?.drawImage(logoImage, WATERMARK_X, WATERMARK_Y, WATERMARK_WIDTH, WATERMARK_HEIGHT)
+
+      // Convert the combined canvas to an image
+      const image = combinedCanvas.toDataURL('image/png')
+
+      // Create a download link for the screenshot with watermark
+      const a = document.createElement('a')
+      a.href = image
+      a.download = createFileName(
+        extension,
+        `${name} from Health Equity Tracker ${new Date().toLocaleDateString(undefined, {
+          month: 'short',
+          year: 'numeric',
+        })}`
+      )
+      a.click()
+    }
   }
 
   async function downloadTargetScreenshot() {
     try {
-      await takeScreenShot(screenshotTargetRef.current).then(download)
+      // Hide specified elements temporarily for the screenshot
+      hiddenElements.forEach((element) => {
+        const elementToHide = screenshotTargetRef.current?.querySelector(element) as HTMLElement | null
+        if (elementToHide) {
+          elementToHide.style.visibility = 'hidden'
+        }
+      })
+
+      const canvas = await html2canvas(screenshotTargetRef.current as HTMLElement, {
+        logging: true,
+        useCORS: true,
+      })
+
+      // Restore the original visibility of hidden elements
+      hiddenElements.forEach((element) => {
+        const elementToHide = screenshotTargetRef.current?.querySelector(element) as HTMLElement | null
+        if (elementToHide) {
+          elementToHide.style.visibility = 'visible'
+        }
+      })
+
+      // Download the screenshot with watermark
+      download(canvas)
+
       return true
     } catch (e) {
       return false
     }
   }
 
-  // use `as const` to have ts treat this as a Tuple with each item specifically typed, rather than an array with all types as options for all items
-  return [screenshotTargetRef, downloadTargetScreenshot] as const
+  return [screenshotTargetRef, downloadTargetScreenshot] as const;
 }
