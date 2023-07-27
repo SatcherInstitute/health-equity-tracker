1696b168fa6fc0fe35686eab1cff9bce5a203279
Ben Hammond <benjamin.hammond@gmail.com>
2023-07-27 16:35:17 -0600

    naming dag

--- airflow/dags/acs_condition.py
+++ airflow/dags/acs_condition.py
@@ -19,11 +19,12 @@ data_ingestion_dag = DAG(
     description="Ingestion configuration for ACS Condition",
 )
 
-acs_condition_gcs_task_id = "acs_condition_to_gcs"
+# acs_condition_gcs_task_id = "acs_condition_to_gcs"
+
 acs_condition_gcs_payload_2019 = util.generate_gcs_payload(
     _ACS_WORKFLOW_ID, year='2019')
-acs_condition_gcs_operator = util.create_gcs_ingest_operator(
-    acs_condition_gcs_task_id, acs_condition_gcs_payload_2019, data_ingestion_dag)
+acs_condition_gcs_operator_2019 = util.create_gcs_ingest_operator(
+    'acs_condition_to_gcs_2019', acs_condition_gcs_payload_2019, data_ingestion_dag)
 
 # acs_condition_bq_payload_2009 = util.generate_bq_payload(
 #     _ACS_WORKFLOW_ID, _ACS_DATASET_NAME, year='2009')
@@ -33,7 +34,7 @@ acs_condition_gcs_operator = util.create_gcs_ingest_operator(
 acs_condition_bq_payload_2019 = util.generate_bq_payload(
     _ACS_WORKFLOW_ID, _ACS_DATASET_NAME, year='2019')
 acs_condition_bq_operator_2019 = util.create_bq_ingest_operator(
-    "acs_condition_to_bq", acs_condition_bq_payload_2019, data_ingestion_dag)
+    "acs_condition_to_bq_2019", acs_condition_bq_payload_2019, data_ingestion_dag)
 
 acs_condition_exporter_payload_race = {
     'dataset_name': _ACS_DATASET_NAME,
@@ -62,7 +63,7 @@ acs_condition_exporter_operator_sex = util.create_exporter_operator(
 
 # Ingestion DAG
 (
-    acs_condition_gcs_operator >>
+    acs_condition_gcs_operator_2019 >>
     # acs_condition_bq_operator_2009 >>
     acs_condition_bq_operator_2019 >> [
         acs_condition_exporter_operator_race,
