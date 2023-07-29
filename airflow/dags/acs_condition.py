07c60a309aff194680c046430508053aa9f773eb
Ben Hammond <benjamin.hammond@gmail.com>
2023-07-28 21:19:32 -0600

    rm 11

--- airflow/dags/acs_condition.py
+++ airflow/dags/acs_condition.py
@@ -19,11 +19,6 @@ data_ingestion_dag = DAG(
 )
 
 # CACHE ACS SOURCE INTO TMP JSON IN BUCKETS
-acs_condition_gcs_payload_2011 = util.generate_gcs_payload(
-    _ACS_WORKFLOW_ID, year='2011')
-acs_condition_gcs_operator_2011 = util.create_gcs_ingest_operator(
-    'acs_condition_to_gcs_2011', acs_condition_gcs_payload_2011, data_ingestion_dag)
-
 acs_condition_gcs_payload_2012 = util.generate_gcs_payload(
     _ACS_WORKFLOW_ID, year='2012')
 acs_condition_gcs_operator_2012 = util.create_gcs_ingest_operator(
@@ -65,11 +60,6 @@ acs_condition_gcs_operator_2019 = util.create_gcs_ingest_operator(
     'acs_condition_to_gcs_2019', acs_condition_gcs_payload_2019, data_ingestion_dag)
 
 # PROCESS AND WRITE TO BQ
-acs_condition_bq_payload_2011 = util.generate_bq_payload(
-    _ACS_WORKFLOW_ID, _ACS_DATASET_NAME, year='2011')
-acs_condition_bq_operator_2011 = util.create_bq_ingest_operator(
-    "acs_condition_to_bq_2011", acs_condition_bq_payload_2011, data_ingestion_dag)
-
 acs_condition_bq_payload_2012 = util.generate_bq_payload(
     _ACS_WORKFLOW_ID, _ACS_DATASET_NAME, year='2012')
 acs_condition_bq_operator_2012 = util.create_bq_ingest_operator(
