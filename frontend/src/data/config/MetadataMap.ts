3272bdc092a46cdbf6a1c530580bebaa002953dd
Ben Hammond <benjamin.hammond@gmail.com>
2023-08-10 06:54:05 -0600

    Fix sources for HIV Black women (#2314)
    ## Description
    
    - the `black_women` tables were missing from DatasetMetadata and
    MetadataMap, which resulted in the black women cards not having source
    info at the bottom
    
    ## Motivation and Context
    <!--- use keywords (eg "closes #144" or "fixes #4323") -->
    
    bug part of #2313 
    
    
    BUG
    <img width="1338" alt="Screen Shot 2023-08-09 at 8 27 08 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/a5cf3ad5-403c-4c98-a3fd-dde1c712d27c">
    
    FIXED
    <img width="1338" alt="Screen Shot 2023-08-09 at 8 26 24 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/6ddf2b1e-ed87-4c54-a8c8-af09800c0bb8">
    
    
    ## Has this been tested? How?
    
    yes
    
    
    ## Types of changes
    - Bug fix
    
    ## Post-merge TODO
    I have inspected frontend changes and/or run affected data pipelines:
    - [ ] on DEV
    - [ ] on PROD
    
    ## Any target [user
    persona(s)](https://docs.google.com/document/d/1EASpK_THTE_uy_Yk0sut2GTVd3w5pKtKH7LpLtF-0co/)?
    
    ## Preview link below in Netlify comment 😎

--- frontend/src/data/config/MetadataMap.ts
+++ frontend/src/data/config/MetadataMap.ts
@@ -205,6 +205,8 @@ export const dataSourceMetadataList: DataSourceMetadata[] = [
       'cdc_hiv_data-race_and_ethnicity_state_time_series',
       'cdc_hiv_data-race_and_ethnicity_national_time_series-with_age_adjust',
       'cdc_hiv_data-race_and_ethnicity_state_time_series-with_age_adjust',
+      'cdc_hiv_data-black_women_national_time_series',
+      'cdc_hiv_data-black_women_state_time_series',
     ],
     downloadable: true,
   },
