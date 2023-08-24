027849aca34fd3c9dd8be0e3a0313b94b434c993
Ben Hammond <benjamin.hammond@gmail.com>
2023-08-23 18:03:47 -0600

    adds alls to exporter

--- exporter/main.py
+++ exporter/main.py
@@ -131,7 +131,8 @@ def has_multi_demographics(table_id: str):
     return (
         ("age" in table_id and "sex" in table_id) or
         ("age" in table_id and "race" in table_id) or
-        ("sex" in table_id and "race" in table_id)
+        ("sex" in table_id and "race" in table_id) or
+        "alls" in table_id
     )
 
 
