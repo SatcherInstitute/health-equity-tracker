a3e718f089c9e8bebd67c76181798ac7c1056e60
Ben Hammond <benjamin.hammond@gmail.com>
2023-08-23 22:26:44 -0600

    make an ALLS table not fake race table with just alls; update tests

--- airflow/dags/cdc_vaccination_county.py
+++ airflow/dags/cdc_vaccination_county.py
@@ -22,15 +22,15 @@ cdc_vaccination_county_bq_payload = util.generate_bq_payload(
 cdc_vaccination_county_bq_operator = util.create_bq_ingest_operator(
     'cdc_vaccination_county_to_bq', cdc_vaccination_county_bq_payload, data_ingestion_dag)
 
-cdc_vaccination_county_exporter_payload_race = {
+cdc_vaccination_county_exporter_payload_alls = {
     'dataset_name': _CDC_VACCINATION_COUNTY_DATASET_NAME,
-    'demographic': "race"
+    'demographic': "multi"
 }
-cdc_vaccination_county_exporter_operator_race = util.create_exporter_operator(
-    'cdc_vaccination_county_exporter_race', cdc_vaccination_county_exporter_payload_race, data_ingestion_dag)
+cdc_vaccination_county_exporter_operator_alls = util.create_exporter_operator(
+    'cdc_vaccination_county_exporter_alls', cdc_vaccination_county_exporter_payload_alls, data_ingestion_dag)
 
 # Ingestion DAG
 (
     cdc_vaccination_county_bq_operator >>
-    cdc_vaccination_county_exporter_operator_race
+    cdc_vaccination_county_exporter_operator_alls
 )
