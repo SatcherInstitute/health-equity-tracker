79bd2fe1d0f96cd7e9bff460e48d815e5bebf189
Ben Hammond <benjamin.hammond@gmail.com>
2023-08-27 13:16:51 -0600

    output

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -47,7 +47,7 @@ const config: PlaywrightTestConfig = {
   },
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'test-results/',
+  outputDir: 'playwright-report/',
 
   projects: [
     {
