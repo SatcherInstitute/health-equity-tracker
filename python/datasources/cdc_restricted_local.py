4f39bd616675f743e069420a9ed5fc9deef56e31
Ben Hammond <benjamin.hammond@gmail.com>
2023-08-29 09:57:46 -0600

    hm

--- python/datasources/cdc_restricted_local.py
+++ python/datasources/cdc_restricted_local.py
@@ -133,50 +133,56 @@ ALL_DATA_SUPPRESSION_STATES = ("MP", "MS", "WV")
 HOSP_DATA_SUPPRESSION_STATES = ("HI", "NE", "RI", "SD")
 DEATH_DATA_SUPPRESSION_STATES = ("HI", "NE", "SD", "DE")
 
+CHUNK_SIZE = 100_000
+print("CHUNKSIZE", CHUNK_SIZE)
 
+
+# print("new gpt race/eth")
 # def combine_race_eth(df):
 #     """Combines the race and ethnicity fields into the legacy race/ethnicity category.
 #        We will keep this in place until we can figure out a plan on how to display
 #        the race and ethnicity to our users in a disaggregated way."""
 
-#     def get_combined_value(row):
-#         if row[ETH_COL] == 'Hispanic/Latino':
-#             return std_col.Race.HISP.value
+#     # Create a mask for Hispanic/Latino
+#     hispanic_mask = df[ETH_COL] == 'Hispanic/Latino'
 
-#         elif row[RACE_COL] in {'NA', 'Missing', 'Unknown'} or row[ETH_COL] in {'NA', 'Missing', 'Unknown'}:
-#             return std_col.Race.UNKNOWN.value
+#     # Create masks for 'NA', 'Missing', 'Unknown'
+#     race_missing_mask = df[RACE_COL].isin({'NA', 'Missing', 'Unknown'})
+#     eth_missing_mask = df[ETH_COL].isin({'NA', 'Missing', 'Unknown'})
 
-#         else:
-#             return RACE_NAMES_MAPPING[row[RACE_COL]]
+#     # Create a mask for other cases
+#     other_mask = ~race_missing_mask & ~eth_missing_mask
 
-#     df[RACE_ETH_COL] = df.apply(get_combined_value, axis=1)
+#     # Create a new combined race/eth column Initialize with UNKNOWN
+#     df[RACE_ETH_COL] = std_col.Race.UNKNOWN.value
+#     # Overwrite specific race if given
+#     df.loc[other_mask, RACE_ETH_COL] = df.loc[other_mask, RACE_COL].map(RACE_NAMES_MAPPING)
+#     # overwrite with Hispanic if given
+#     df.loc[hispanic_mask, RACE_ETH_COL] = std_col.Race.HISP.value
+
+#     # Drop unnecessary columns
 #     df = df.drop(columns=[RACE_COL, ETH_COL])
+
 #     return df
 
-
+print("old gpt race/eth")
 def combine_race_eth(df):
     """Combines the race and ethnicity fields into the legacy race/ethnicity category.
        We will keep this in place until we can figure out a plan on how to display
        the race and ethnicity to our users in a disaggregated way."""
 
-    # Create a mask for Hispanic/Latino
-    hispanic_mask = df[ETH_COL] == 'Hispanic/Latino'
+    def get_combined_value(row):
+        if row[ETH_COL] == 'Hispanic/Latino':
+            return std_col.Race.HISP.value
 
-    # Create masks for 'NA', 'Missing', 'Unknown'
-    race_missing_mask = df[RACE_COL].isin({'NA', 'Missing', 'Unknown'})
-    eth_missing_mask = df[ETH_COL].isin({'NA', 'Missing', 'Unknown'})
+        elif row[RACE_COL] in {'NA', 'Missing', 'Unknown'} or row[ETH_COL] in {'NA', 'Missing', 'Unknown'}:
+            return std_col.Race.UNKNOWN.value
 
-    # Create a mask for other cases
-    other_mask = ~race_missing_mask & ~eth_missing_mask
+        else:
+            return RACE_NAMES_MAPPING[row[RACE_COL]]
 
-    # Create a new column with combined values
-    df[RACE_ETH_COL] = std_col.Race.UNKNOWN.value  # Initialize with UNKNOWN
-    df.loc[hispanic_mask, RACE_ETH_COL] = std_col.Race.HISP.value
-    df.loc[other_mask, RACE_ETH_COL] = df.loc[other_mask, RACE_COL].map(RACE_NAMES_MAPPING)
-
-    # Drop unnecessary columns
+    df[RACE_ETH_COL] = df.apply(get_combined_value, axis=1)
     df = df.drop(columns=[RACE_COL, ETH_COL])
-
     return df
 
 
@@ -234,8 +240,8 @@ def accumulate_data(df, geo_cols, overall_df, demog_cols, names_mapping):
     groupby_cols = groupby_cols + [CASE_DATE_COL]
     total_groupby_cols = total_groupby_cols + [CASE_DATE_COL]
 
-    df = df.groupby(groupby_cols).sum().reset_index()
-    totals = df.groupby(total_groupby_cols).sum().reset_index()
+    df = df.groupby(groupby_cols).sum(numeric_only=True).reset_index()
+    totals = df.groupby(total_groupby_cols).sum(numeric_only=True).reset_index()
 
     # Special case required due to later processing.
     if demog_cols[0] == RACE_ETH_COL:
@@ -342,7 +348,7 @@ def process_data(dir, files):
         chunked_frame = pd.read_csv(
             os.path.join(dir, f),
             dtype=str,
-            chunksize=100000,
+            chunksize=CHUNK_SIZE,
             keep_default_na=False,
             usecols=USE_COLS
         )
