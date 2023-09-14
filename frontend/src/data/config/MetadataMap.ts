90c9e855c9bfbe8d0139c06ae8050309dff3fb06
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-14 11:41:51 -0600

    unhide data card for phrma preview link

--- frontend/src/data/config/MetadataMap.ts
+++ frontend/src/data/config/MetadataMap.ts
@@ -361,7 +361,6 @@ export const dataSourceMetadataList: DataSourceMetadata[] = [
     downloadable: true,
   },
   {
-    hideFromUser: true,
     id: 'phrma',
     data_source_name: 'Medicare Administrative Data',
     data_source_pretty_site_name: 'cms.gov',
