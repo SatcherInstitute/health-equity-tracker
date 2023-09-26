23c742bfd6be09989f0f2125c9a6647587c9f11b
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-25 21:10:50 -0600

    typo

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -94,7 +94,7 @@ function LandingPage() {
                 className={styles.ExploreDataButton}
                 href={EXPLORE_DATA_PAGE_LINK}
               >
-                Explore the data
+                Explore the data eror
               </Button>
             </Box>
           </Grid>
