68f582eb87a8113047920e0c48f9c969dedc0a1e
Ben Hammond <benjamin.hammond@gmail.com>
2023-10-04 12:41:23 -0600

    tweaks

--- frontend/src/cards/MapCard.tsx
+++ frontend/src/cards/MapCard.tsx
@@ -536,7 +536,6 @@ function MapCardWithKey(props: MapCardProps) {
                     xs={12}
                     sm={mapIsWide ? 8 : 12}
                     md={mapIsWide ? 9 : 12}
-                    lg={mapIsWide ? 10 : 12}
                   >
                     <Grid item minHeight={preloadHeight * 0.3} xs={12}>
                       <ChoroplethMap
@@ -592,7 +591,6 @@ function MapCardWithKey(props: MapCardProps) {
                     xs={12}
                     sm={mapIsWide ? 4 : 12}
                     md={mapIsWide ? 3 : 12}
-                    lg={mapIsWide ? 2 : 12}
                   >
                     <Legend
                       dataTypeConfig={props.dataTypeConfig}
