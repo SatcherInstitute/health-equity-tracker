a58d9d3b0252a51e7b18a0a43789ee29b54d6928
Ben Hammond <benjamin.hammond@gmail.com>
2023-10-18 15:00:24 -0400

    Remove sentry (#2439)
    ## Description
    
    - the only errors we were seeing were strange and unrelated to our
    actual codebase. perhaps a configuration error, but in the months it's
    been in place it hasn't caught a single, actionable error
    - we can always reimplement as needed, using this PR as a guide for how
    to add it back in
    
    Example unhelpful alert
    <img width="1027" alt="Screen Shot 2023-10-18 at 12 23 23 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/5ffd697b-4e7c-4392-8653-0064a302c07a">

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import { configDefaults } from 'vitest/config'
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 
-
-export default defineConfig(({ mode }) => {
-	// Load env file based on `mode` in the current working directory; local builds are `production`.
-	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-	const env = loadEnv(mode, process.cwd(), '')
-	// we only want the source maps created from npm build to upload to Sentry when Netlify builds from the `main` branch
-	// this avoids pushing sourcemaps for local development / deploy previews, and also bypasses dealing with passing
-	// secrets into docker images for dev / prod builds
-	const sentryAuthToken = env.BRANCH === 'main' ? env.SENTRY_AUTH_TOKEN : ""
+export default defineConfig(({ }) => {
 
 	return {
 		build: {
 			outDir: 'build',
-			sourcemap: true
 		},
 		server: {
 			open: true,
@@ -30,13 +20,6 @@ export default defineConfig(({ mode }) => {
 			}),
 			viteTsconfigPaths(),
 			svgrPlugin(),
-			sentryVitePlugin({
-				org: env.SENTRY_ORG,
-				project: env.SENTRY_PROJECT,
-				// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-				// and need `project:releases` and `org:read` scopes
-				authToken: sentryAuthToken,
-			}),
 		],
 		test: {
 			exclude: [
