089bafad9a19f0a2ae9eb6115a7a5d50f4ccc218
Ben Hammond <benjamin.hammond@gmail.com>
2023-10-19 13:02:06 -0600

    nightly not //

--- frontend/playwright-tests/prod.nightly.spec.ts
+++ frontend/playwright-tests/prod.nightly.spec.ts
@@ -1,7 +1,5 @@
 import { test } from '@playwright/test';
 
-test.describe.configure({ mode: 'parallel' });
-
 test(`Production Site Loads`, async ({ page }) => {
     await page.goto("https://healthequitytracker.org", { waitUntil: "commit" });
 
