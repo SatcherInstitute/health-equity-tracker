99dbc7593099dcf84e31422381e5f7d310834e36
Ben Hammond <benjamin.hammond@gmail.com>
2023-10-19 21:33:45 -0600

    move county names over to shared mocks, county vaxx

--- python/tests/datasources/test_cdc_vaccination_county.py
+++ python/tests/datasources/test_cdc_vaccination_county.py
@@ -3,66 +3,75 @@ import os
 
 import pandas as pd  # type: ignore
 from pandas._testing import assert_frame_equal  # type: ignore
-
+from test_utils import _load_public_dataset_from_bigquery_as_df
 from datasources.cdc_vaccination_county import CDCVaccinationCounty  # type: ignore
 
 # Current working directory.
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.join(THIS_DIR, os.pardir, "data", "cdc_vaccination_county")
 
-GOLDEN_DATA = os.path.join(
-    TEST_DIR, 'cdc_vaccination_county-alls_county.csv')
+GOLDEN_DATA = os.path.join(TEST_DIR, 'cdc_vaccination_county-alls_county.csv')
 
 
 def get_total_vaccinations_as_df():
-    return pd.read_csv(os.path.join(TEST_DIR, 'cdc_vaccination_county_test.csv'),
-                       dtype={'fips': str, 'recip_county': str})
+    return pd.read_csv(
+        os.path.join(TEST_DIR, 'cdc_vaccination_county_test.csv'),
+        dtype={'fips': str, 'recip_county': str},
+    )
 
 
 def get_pop_data_as_df(*args):
-
     if args[1] == 'by_race_county':
-        return pd.read_csv(os.path.join(TEST_DIR, 'population_race_county.csv'), dtype={'county_fips': str})
+        return pd.read_csv(
+            os.path.join(TEST_DIR, 'population_race_county.csv'),
+            dtype={'county_fips': str},
+        )
 
     if args[1] == 'by_race_and_ethnicity_territory_county_level':
         return pd.read_csv(
             os.path.join(
-                TEST_DIR, 'population_by_race_and_ethnicity_territory_county_level.csv'),
-            dtype={'county_fips': str})
+                TEST_DIR, 'population_by_race_and_ethnicity_territory_county_level.csv'
+            ),
+            dtype={'county_fips': str},
+        )
 
 
-def get_county_names_as_df():
-    return pd.read_csv(os.path.join(TEST_DIR, 'county_names.csv'), dtype=str)
-
-
-@mock.patch('ingestion.gcs_to_bq_util.load_csv_as_df_from_web',
-            return_value=get_total_vaccinations_as_df())
-@mock.patch('ingestion.gcs_to_bq_util.load_df_from_bigquery',
-            side_effect=get_pop_data_as_df)
-@mock.patch('ingestion.gcs_to_bq_util.load_public_dataset_from_bigquery_as_df',
-            return_value=get_county_names_as_df())
-@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq',
-            return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_web',
+    return_value=get_total_vaccinations_as_df(),
+)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_df_from_bigquery', side_effect=get_pop_data_as_df
+)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_public_dataset_from_bigquery_as_df',
+    side_effect=_load_public_dataset_from_bigquery_as_df,
+)
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
 def testWriteToBq(
-        mock_bq: mock.MagicMock,
-        mock_pop: mock.MagicMock,
-        mock_county_names: mock.MagicMock,
-        mock_csv: mock.MagicMock):
-
+    mock_bq: mock.MagicMock,
+    mock_pop: mock.MagicMock,
+    mock_county_names: mock.MagicMock,
+    mock_csv: mock.MagicMock,
+):
     cdcVaccinationCounty = CDCVaccinationCounty()
 
-    kwargs = {'filename': 'test_file.csv',
-              'metadata_table_id': 'test_metadata',
-              'table_name': 'output_table'}
+    kwargs = {
+        'filename': 'test_file.csv',
+        'metadata_table_id': 'test_metadata',
+        'table_name': 'output_table',
+    }
 
     cdcVaccinationCounty.write_to_bq('dataset', 'gcs_bucket', **kwargs)
     assert mock_bq.call_count == 1
     assert mock_bq.call_args_list[0].args[2] == 'alls_county'
 
-    expected_df = pd.read_csv(GOLDEN_DATA, dtype={
-        'county_fips': str,
-        'vaccinated_per_100k': float,
-    })
+    expected_df = pd.read_csv(
+        GOLDEN_DATA,
+        dtype={
+            'county_fips': str,
+            'vaccinated_per_100k': float,
+        },
+    )
 
-    assert_frame_equal(
-        mock_bq.call_args_list[0].args[0], expected_df, check_like=True)
+    assert_frame_equal(mock_bq.call_args_list[0].args[0], expected_df, check_like=True)
