ce778eba071507f7812943a2715e63e2284f855a
eriwarr <eric.m.warren1@gmail.com>
2023-10-23 15:33:54 -0500

    added a category map

--- frontend/src/utils/MadLibs.ts
+++ frontend/src/utils/MadLibs.ts
@@ -33,7 +33,7 @@ export const MADLIB_MODE_MAP: Record<string, MadLibId> = {
 
 export const CategoryMap = {
   'behavioral-health': 'Behavioral Health',
-  'black-womens-health': `Black Women's Health`,
+  'black-women-health': `Black Women's Health`,
   'chronic-disease': 'Chronic Disease',
   covid: 'COVID-19',
   hiv: 'HIV',
