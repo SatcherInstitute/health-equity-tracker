5fd836c1461c050562e8019990ae5428b0f766db
Ben Hammond <benjamin.hammond@gmail.com>
2023-10-26 23:23:06 -0600

    responsive refacotr

--- frontend/src/pages/AboutUs/ContactUsTab.tsx
+++ frontend/src/pages/AboutUs/ContactUsTab.tsx
@@ -1,7 +1,7 @@
 import styles from './AboutUsPage.module.scss'
 import { Helmet } from 'react-helmet-async'
 import { urlMap } from '../../utils/externalUrls'
-import { Button, TextField, Grid } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import {
   NEWS_PAGE_LINK,
   SHARE_YOUR_STORY_TAB_LINK,
@@ -16,7 +16,7 @@ function ContactUsTab() {
       </Helmet>
       <h2 className="sr-only">Contact Us</h2>
       <div className=" flex w-full flex-col content-center items-center">
-        <div className="hidden w-full border-2 border-l-0 border-r-0 border-t-0 border-solid border-border-color md:flex">
+        <div className="border-l-0 border-r-0 border-t-0 hidden w-full border-1 border-solid border-border-color md:flex">
           <div className="grid w-full place-content-center md:w-5/12">
             <h2
               id="main"
@@ -30,88 +30,83 @@ function ContactUsTab() {
               equity <i className="font-normal">forward</i>
             </h2>
           </div>
-          <div className="w-full border-2 border-b-0 border-r-0 border-t-0 border-solid border-border-color  md:w-7/12">
+          <div className="border-b-0 border-r-0 border-t-0 w-full border-1 border-solid border-border-color  md:w-7/12">
             <img
               width="870"
               height="644"
               src="/img/stock/women-laughing-in-line.png"
-              className="m-10 h-auto w-full max-w-xl rounded-3xl"
+              className="m-10 h-auto w-9/12 max-w-xl rounded-3xl"
               alt=""
             />
           </div>
         </div>
 
-        <div className="flex w-full flex-col items-center md:w-8/12 lg:w-6/12">
+        <div className="flex w-full flex-col items-center px-12 md:w-8/12 lg:w-6/12">
           <div className="grid  w-full place-content-center">
             <h4
-              className="px-12
-              pt-24
-                            font-serif
-                            text-header
-                            font-light
-                            leading-lhSomeMoreSpace
-                            text-alt-green"
+              className="px-18
+              mb-0
+                py-6
+                font-serif
+                text-header
+                font-light
+                leading-lhSomeMoreSpace
+                text-alt-green
+                md:py-12"
             >
               Thank you for your interest in the Health Equity Tracker
             </h4>
           </div>
           <div className="w-full md:w-7/12">
-            <p className={styles.ContactUsP}>
-              <b>Join our mailing list:</b>
-            </p>
+            <h5 className="mb-2 mt-8 text-text font-bold">
+              Join our mailing list:
+            </h5>
+            <div>
+              <form
+                action={urlMap.newsletterSignup}
+                method="post"
+                target="_blank"
+                className="flex justify-center"
+              >
+                <TextField
+                  id="Enter email address to sign up" // Accessibility label
+                  name="MERGE0"
+                  variant="outlined"
+                  className={styles.EmailTextField}
+                  type="email"
+                  aria-label="Enter Email Address for Newsletter signup"
+                  placeholder="Enter email address"
+                />
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  className={styles.EmailAddressFormSubmit}
+                  aria-label="Sign Up for Newsletter in a new window"
+                >
+                  Sign up
+                </Button>
+              </form>
+            </div>
 
-            <form
-              action={urlMap.newsletterSignup}
-              method="post"
-              target="_blank"
-            >
-              <Grid container justifyContent="center" alignContent="center">
-                <Grid item>
-                  <TextField
-                    id="Enter email address to sign up" // Accessibility label
-                    name="MERGE0"
-                    variant="outlined"
-                    className={styles.EmailTextField}
-                    type="email"
-                    aria-label="Enter Email Address for Newsletter signup"
-                    placeholder="Enter email address"
-                  />
-                </Grid>
-                <Grid item>
-                  <Button
-                    type="submit"
-                    color="primary"
-                    variant="contained"
-                    className={styles.EmailAddressFormSubmit}
-                    aria-label="Sign Up for Newsletter in a new window"
-                  >
-                    Sign up
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-
-            <p className={styles.ContactUsP}>
-              <b>For general requests and media inquiries:</b>
-              <br />
+            <h5 className="mb-0 mt-8 text-text font-bold">
+              For general requests and media inquiries:
+            </h5>
+            <div>
               Please contact the{' '}
-              <a href={urlMap.shli}>
-                Satcher Health Leadership Institute
-              </a> at{' '}
+              <a href={urlMap.shli}>Satcher Health Leadership Institute</a> at{' '}
               <a href="mailto:info@healthequitytracker.org">
                 info@healthequitytracker.org
               </a>
-            </p>
+            </div>
 
-            <p className={styles.ContactUsP}>
-              <b>Phone:</b>
-              <br />
+            <h5 className="mb-0 mt-8 text-text font-bold">Phone:</h5>
+            <div>
               <a href="tel:4047528654">(404) 752-8654</a>
-            </p>
+            </div>
 
-            <p className={styles.ContactUsP}>
-              <b>Mailing address:</b>
-              <br />
+            <h5 className="mb-0 mt-8 text-text font-bold">Mailing address:</h5>
+            <div>
               Morehouse School of Medicine
               <br />
               Satcher Health Leadership Institute
@@ -119,11 +114,10 @@ function ContactUsTab() {
               720 Westview Drive SW
               <br />
               Atlanta, Georgia 30310
-            </p>
+            </div>
 
-            <p className={styles.ContactUsP}>
-              <b>Share your story:</b>
-              <br />
+            <h5 className="mb-0 mt-8 text-text font-bold">Share your story:</h5>
+            <div>
               Read our{' '}
               <Link to={SHARE_YOUR_STORY_TAB_LINK}>
                 article submission guidelines
@@ -131,7 +125,7 @@ function ContactUsTab() {
               for potential inclusion on our{' '}
               <Link to={NEWS_PAGE_LINK}>News and Stories page</Link>, and email
               the address above with any specific questions.
-            </p>
+            </div>
           </div>
         </div>
       </div>
