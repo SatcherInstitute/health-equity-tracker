383a70f8b07aaba276fc36b4144b5e08b407d36d
Ben Hammond <benjamin.hammond@gmail.com>
2023-11-02 18:46:46 -0600

    rf skip link to tailwind

---
+++ frontend/src/SkipLink.tsx
@@ -0,0 +1,10 @@
+export default function SkipLink() {
+  return (
+    <a
+      className='border-4 m-30 absolute -top-96 left-1/2 z-z-top h-auto overflow-auto rounded border-solid border-yellow bg-yellow p-1 text-center text-smallestHeader text-white focus:top-5  active:top-5 '
+      href='#main'
+    >
+      Skip to main content
+    </a>
+  )
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -142,6 +142,15 @@ export default {
       biggerHeader: '3.125rem',
       biggestHeader: '4rem',
     },
+    zIndex: {
+      "z-bottom": "-999",
+      // multimap modal blur backdrop -1
+      "z-middle": "0",
+      // MUI sticky headers assigned 2
+      "z-almost-top": "3",
+      // joyride warm welcome auto-assigned 101
+      "z-top": "999",
+    }
   },
 
   plugins: [],
