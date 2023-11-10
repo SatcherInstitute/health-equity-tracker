9c2fda807f5ee07fefb8a05956c2cd11013f3b69
Eric M Warren II <eric.m.warren1@gmail.com>
2023-11-10 13:10:25 -0600

    Tailwind RF: Landing page (#2495)
    Removes MUI grid and box components and adds tailwind classes.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,7 +18,6 @@ export default {
     maxHeight: {
       aimToGo: "255px",
       sm: '600px',
-      articleLogo: '770px',
       md: '960px',
       lg: '1280px',
       xl: '1920px',
@@ -26,6 +25,7 @@ export default {
     maxWidth: {
       aimToGo: "255px",
       sm: '600px',
+      articleLogo: '700px',
       md: '960px',
       lg: '1280px',
       xl: '1920px',
@@ -48,7 +48,7 @@ export default {
       'alert-color': 'rgb(var(--alert-color) / <alpha-value>)',
       'alt-black': 'rgb(var(--alt-black) / <alpha-value>)',
       'alt-dark': 'rgb(var(--alt-dark)  / <alpha-value>)',
-      'alt-green': 'rgb(var(--alt-green)  / <alpha-value>)',
+      'alt-green': 'rgb(var(--alt-green) / <alpha-value>)',
       'alt-grey': 'rgb(var(--alt-grey) / <alpha-value>)',
       'alt-orange': 'rgb(var(--alt-orange) / <alpha-value>)',
       'alt-red': 'rgb(var(--alt-red) / <alpha-value>)',
@@ -125,7 +125,7 @@ export default {
       'unknown-map-min': 'rgb(var(--unknown-map-min) / <alpha-value>)',
       'unknown-map-more': 'rgb(var(--unknown-map-more) / <alpha-value>)',
       'unknown-map-most': 'rgb(var(--unknown-map-most) / <alpha-value>)',
-      'white': 'rgb(var(--white) / <alpha-value>)',
+      'white': 'rgb(var(--white) / <alpha-value>) !important',
       'why-box-color': 'rgb(var(--why-box-color) / <alpha-value>)',
       'yellow': 'rgb(var(--yellow) / <alpha-value>)',
     },
@@ -157,16 +157,17 @@ export default {
       biggerHeader: '3.125rem',
       biggestHeader: '4rem',
     },
+  },
+  extend: {
     zIndex: {
       "z-bottom": 'var(--z-bottom)',
       "z-middle": 'var(--z-middle)',
       "z-almost-top": 'var(--z-almost-top)',
       "z-top": 'var(--z-top)',
-    }
+    },
   },
-
   plugins: [],
   corePlugins: {
     preflight: false,
-  },
+  }
 }
