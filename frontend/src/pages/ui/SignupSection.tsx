084c09839ec1bd02d6a3b6df3dadaedc648e463d
Ben Hammond <benjamin.hammond@gmail.com>
2023-11-14 17:19:08 -0700

    email signup component

--- frontend/src/pages/ui/SignupSection.tsx
+++ frontend/src/pages/ui/SignupSection.tsx
@@ -1,5 +1,4 @@
-import { Button, Grid, TextField } from '@mui/material'
-import { urlMap } from '../../utils/externalUrls'
+import { Grid } from '@mui/material'
 import {
   CONTACT_TAB_LINK,
   SHARE_YOUR_STORY_TAB_LINK,
@@ -7,6 +6,7 @@ import {
 import { LinkWithStickyParams } from '../../utils/urlutils'
 
 import styles from './SignupSection.module.scss'
+import HetEmailSignup from '../../styles/HetComponents/HetEmailSignup'
 
 export default function SignupSection() {
   return (
@@ -34,27 +34,7 @@ export default function SignupSection() {
         </p>
       </Grid>
       <Grid item container justifyContent='center' alignItems='center'>
-        <form action={urlMap.newsletterSignup} method='post' target='_blank'>
-          <TextField
-            id='Enter email address to sign up' // Accessibility label
-            name='MERGE0'
-            variant='outlined'
-            className={styles.NewsEmailTextField}
-            type='email'
-            role='textbox'
-            aria-label='Enter Email Address for Newsletter signup'
-            placeholder='Enter email address'
-          />
-          <Button
-            type='submit'
-            color='primary'
-            variant='contained'
-            className={styles.NewsEmailAddressFormSubmit}
-            aria-label='Sign Up for Newsletter in a new window'
-          >
-            Sign up
-          </Button>
-        </form>
+        <HetEmailSignup id='signup-section-email-signup' />
       </Grid>
     </Grid>
   )
