1d4407e4bd0d717165c671fdee40a1ff0ed9ee74
Ben Hammond <benjamin.hammond@gmail.com>
2023-11-14 17:30:39 -0700

    rm css

--- frontend/src/pages/ui/SignupSection.tsx
+++ frontend/src/pages/ui/SignupSection.tsx
@@ -1,41 +1,30 @@
-import { Grid } from '@mui/material'
 import {
   CONTACT_TAB_LINK,
   SHARE_YOUR_STORY_TAB_LINK,
 } from '../../utils/internalRoutes'
 import { LinkWithStickyParams } from '../../utils/urlutils'
 
-import styles from './SignupSection.module.scss'
 import HetEmailSignup from '../../styles/HetComponents/HetEmailSignup'
 
 export default function SignupSection() {
   return (
-    <Grid
-      container
-      direction='column'
-      justifyContent='center'
-      className={styles.NewsEmailSignup}
-    >
-      <Grid item>
-        <p>
-          Please{' '}
-          <LinkWithStickyParams to={CONTACT_TAB_LINK}>
-            contact us
-          </LinkWithStickyParams>{' '}
-          with any questions or concerns, and{' '}
-          <LinkWithStickyParams to={SHARE_YOUR_STORY_TAB_LINK}>
-            consider sharing your own story
-          </LinkWithStickyParams>
-          .
-        </p>
-        <p>
-          For more information about health equity, please sign up for our
-          Satcher Health Leadership Institute newsletter.
-        </p>
-      </Grid>
-      <Grid item container justifyContent='center' alignItems='center'>
-        <HetEmailSignup id='signup-section-email-signup' />
-      </Grid>
-    </Grid>
+    <section className='flex w-full flex-col p-20'>
+      <p>
+        Please{' '}
+        <LinkWithStickyParams to={CONTACT_TAB_LINK}>
+          contact us
+        </LinkWithStickyParams>{' '}
+        with any questions or concerns, and{' '}
+        <LinkWithStickyParams to={SHARE_YOUR_STORY_TAB_LINK}>
+          consider sharing your own story
+        </LinkWithStickyParams>
+        .
+      </p>
+      <p>
+        For more information about health equity, please sign up for our Satcher
+        Health Leadership Institute newsletter.
+      </p>
+      <HetEmailSignup id='signup-section-email-signup' />
+    </section>
   )
 }
