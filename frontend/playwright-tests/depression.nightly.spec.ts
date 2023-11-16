e90f1c97837a120b5950a1d3d51350f322937783
Ali <anix1@kent.edu>
2023-11-16 13:52:49 -0700

    adds nightly e2e testing depression

---
+++ frontend/playwright-tests/depression.nightly.spec.ts
@@ -0,0 +1,49 @@
+import { test } from '@playwright/test'
+
+test('Depression Flow', async ({ page }) => {
+  await page.goto(
+    'https://healthequitytracker.org/exploredata?mls=1.depression-3.00&group1=All'
+  )
+  await page
+    .getByText('See the states/territories with the highest and lowest rates.')
+    .click()
+  await page
+    .getByLabel(
+      'Bar Chart showing Depression in the United States, by Race and Ethnicity'
+    )
+    .getByRole('img')
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Share of total depression cases with unknown race and ethnicity in the United States',
+    })
+    .click()
+  await page
+    .getByText(
+      'No unknown values for race and ethnicity reported in this dataset at the state/t'
+    )
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total depression cases in the United States'
+    )
+    .getByRole('img')
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total depression cases in the United States'
+    )
+    .getByText('1.1% of cases', { exact: true })
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total depression cases in the United States'
+    )
+    .getByText('2.9% of cases', { exact: true })
+    .click()
+  await page
+    .getByRole('figure', {
+      name: 'Breakdown summary for depression cases in the United States',
+    })
+    .click()
+})
