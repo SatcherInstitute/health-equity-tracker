40f2bbda1951727e426116a2020ef5706fa28324
Ali Nix <anix1@kent.edu>
2023-11-21 13:18:46 -0700

    Nightly e2e drinking (#2597)
    # Description
    
    - adds codegen script 
    - adds drinking nightly e2e 
    
    #Motivation and Context 
    
    closes #2561 
    
    ## Has this been tested? How?
    
    - tests pass locally
    
    ## Screenshots (if appropriate)
    
    <img width="885" alt="Screenshot 2023-11-21 at 11 20 40 AM"
    src="https://user-images.githubusercontent.com/28677929/284689336-7557f3b1-cd51-43a4-baf3-3fdc62625cb7.png">
    
    ## Types of changes
    
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

---
+++ frontend/playwright-tests/drinking.nightly.spec.ts
@@ -0,0 +1,77 @@
+import { test } from '@playwright/test'
+
+test('Excessive Drinking Flow', async ({ page }) => {
+  await page.goto(
+    'https://healthequitytracker.org/exploredata?mls=1.excessive_drinking-3.00&group1=All'
+  )
+  await page
+    .getByLabel(
+      'Map showing Excessive drinking cases in the United States : including data from 50 states/territories'
+    )
+    .getByRole('img')
+  await page
+    .getByText('See the states/territories with the highest and lowest rates.')
+    .click()
+  await page
+    .getByLabel(
+      'Bar Chart showing Excessive drinking cases in the United States, by Race and Ethnicity'
+    )
+    .getByRole('img')
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Share of all excessive drinking cases with unknown race and ethnicity in the United States',
+    })
+    .click()
+  await page
+    .getByText(
+      'No unknown values for race and ethnicity reported in this dataset at the state/t'
+    )
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Population vs. distribution of total excessive drinking cases in the United States',
+    })
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total excessive drinking cases in the United States'
+    )
+    .getByRole('img')
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total excessive drinking cases in the United States'
+    )
+    .getByText('3.8% of all cases', { exact: true })
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total excessive drinking cases in the United States'
+    )
+    .getByText('1.5% of all cases', { exact: true })
+    .click()
+  await page
+    .getByLabel(
+      'Comparison bar chart showing Population vs. distribution of total excessive drinking cases in the United States'
+    )
+    .getByText('64.5% of all cases', { exact: true })
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Breakdown summary for excessive drinking cases in the United States',
+    })
+    .click()
+  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click()
+  await page
+    .getByRole('columnheader', {
+      name: 'Excessive drinking cases per 100k adults',
+    })
+    .click()
+  await page
+    .getByRole('columnheader', {
+      name: 'Share of all excessive drinking cases',
+    })
+    .click()
+  await page.getByRole('columnheader', { name: 'Population share' }).click()
+})
