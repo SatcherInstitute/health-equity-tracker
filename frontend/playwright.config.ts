ce6622f967530377383f75dcb93f7aab07725847
Ben Hammond <benjamin.hammond@gmail.com>
2023-11-27 21:30:38 -0700

    rm login

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -63,21 +63,12 @@ const config: PlaywrightTestConfig = {
         baseURL: 'https://healthequitytracker.org'
       }
     },
-    // TODO: either figure out how to let playwright log into dev.healthequitytracker
-    // TODO: or disable the basic auth that is in place (and ensure robots.txt block tracking)
-    {
-      name: 'E2E_STAGING_LOGIN',
-      testMatch: /.*staging.login.setup.spec.ts/,
-    },
     {
       name: 'E2E_STAGING',
       testMatch: /.*nightly.spec.ts/,
       use: {
         baseURL: 'https://dev.healthequitytracker.org',
-        // Use prepared auth state.
-        storageState: 'playwright/.auth/user.json',
       },
-      dependencies: ['E2E_STAGING_LOGIN']
     },
     {
       name: 'E2E',
