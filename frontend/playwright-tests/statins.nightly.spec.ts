25bd3ce358e13a243c581d2f11338afb48a3fdf0
JDemlow <jdemlowdev@gmail.com>
2023-12-01 17:07:46 -0700

    should complete e2e.statins.nightly.specs.ts

--- frontend/playwright-tests/statins.nightly.spec.ts
+++ frontend/playwright-tests/statins.nightly.spec.ts
@@ -4,25 +4,17 @@ test('Statin Adherence', async ({ page }) => {
   await page.goto(
     '/exploredata?mls=1.medicare_cardiovascular-3.00&group1=All&dt1=statins_adherence'
   )
-  await page.locator('#landingPageCTA').click()
-  await page.getByRole('button', { name: 'select a topic' }).click()
-  await page
-    .getByRole('button', {
-      name: 'Cardiovascular Conditions and Medication Adherence',
-    })
-    .click()
-  await page
-    .getByRole('button', {
-      name: 'Persistence of Beta Blocker Treatment after a Heart Attack',
-    })
-    .click()
-  await page.getByRole('button', { name: 'Adherence to Statins' }).click()
   await page.getByLabel('Race and Ethnicity:').click()
   await page.locator('.MuiBackdrop-root').click()
   await page
-    .getByRole('combobox', { name: 'Demographic Race/ethnicity' })
+    .locator('#rate-map')
+    .getByRole('heading', {
+      name: 'Population adherent to statins in the United States',
+    })
     .click()
-  await page.getByRole('option', { name: 'Sex' }).click()
+  await page.getByText('Compare mode').nth(2).click()
+  await page.getByText('Off').nth(1).click()
+  await page.locator('#menu- div').first().click()
   await page
     .locator('#rate-chart')
     .getByRole('heading', {
@@ -31,9 +23,15 @@ test('Statin Adherence', async ({ page }) => {
     .click()
   await page
     .getByRole('heading', {
-      name: 'Adherent beneficiary population with unknown sex in the United States',
+      name: 'Adherent beneficiary population with unknown race and ethnicity in the United States',
     })
     .click()
+  await page
+    .getByRole('heading', { name: 'Breakdown Summary in the United States' })
+    .click()
+  await page.locator('#data-table').getByLabel('Card export options').click()
+  await page.locator('.MuiBackdrop-root').first().click()
+  await page.getByText('Share this report:').click()
   await page.getByRole('heading', { name: 'Definitions:' }).click()
   await page.getByText('Adherence to statins', { exact: true }).click()
   await page
