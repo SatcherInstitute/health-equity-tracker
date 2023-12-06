4e3a98f5d77fa46fc15a51b8b4d82c1b4c87fcba
JDemlow <jdemlowdev@gmail.com>
2023-12-05 20:23:27 -0700

    Adds nightly e2e for cardiovascular diseases

---
+++ frontend/playwright-tests/cardiovasculardiseases.nightly.spec.ts
@@ -0,0 +1,54 @@
+import { test } from '@playwright/test'
+
+test('Cardiovascular Diseases', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.cardiovascular_diseases-3.00&group1=All')
+  await page.getByText('Race and Ethnicity:').click()
+  await page.locator('.MuiBackdrop-root').click()
+  await page
+    .locator('#rate-map')
+    .getByRole('heading', {
+      name: 'Cardiovascular diseases in the United States',
+    })
+    .click()
+  await page
+    .getByRole('combobox', { name: 'Demographic Race/ethnicity' })
+    .click()
+  await page.locator('#menu- div').first().click()
+  await page.getByText('Compare mode').nth(2).click()
+  await page
+    .locator('#rate-chart')
+    .getByRole('heading', {
+      name: 'Cardiovascular diseases in the United States',
+    })
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Share of all cases of cardiovascular diseases with unknown race and ethnicity in the United States',
+    })
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Population vs. distribution of total cases of cardiovascular diseases in the United States',
+    })
+    .click()
+  await page
+    .getByRole('heading', {
+      name: 'Breakdown summary for cases of cardiovascular diseases in the United States',
+    })
+    .click()
+  await page
+    .getByRole('columnheader', {
+      name: 'Cases of cardiovascular diseases per 100k adults',
+    })
+    .click()
+  await page.getByText('Share this report:').click()
+  await page
+    .getByText('Cases of cardiovascular diseases', { exact: true })
+    .click()
+  await page.getByRole('heading', { name: 'What data are missing?' }).click()
+  await page
+    .getByText(
+      'Do you have information that belongs on the Health Equity Tracker? We would love'
+    )
+    .click()
+})
