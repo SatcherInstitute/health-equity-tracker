bf4f96420c96a6fd3d02e2580fb0cf0c3beee75e
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-06 14:04:15 -0700

    tweak e2e

--- frontend/playwright-tests/prison.jail.nightly.spec.ts
+++ frontend/playwright-tests/prison.jail.nightly.spec.ts
@@ -1,35 +1,29 @@
 import { test } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Prison by Race', async ({ page }) => {
   await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All');
-
+  await page.getByLabel('Launch multiple maps view with side-by-side maps of each race and ethnicity group').click();
+  await page.getByLabel('close multiple maps modal').click();
+  await page.getByLabel('open the topic info modal').click();
+  await page.getByRole('heading', { name: 'People in prison' }).click();
+  await page.getByLabel('close topic info modal').click();
+  await page.locator('#rate-chart').getByText('3,232 children confined in adult facilities in the United States. Learn more.').click();
+  await page.locator('#unknown-demographic-map').getByRole('note').click();
+  await page.getByRole('heading', { name: 'Breakdown summary for people in prison in the United States' }).click();
+  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click();
   await page.getByRole('button', { name: 'Prison', exact: true }).click();
   await page.getByRole('button', { name: 'Jail' }).click();
-  await page.getByLabel('open the topic info modal').click();
+})
+test('Jail by Age', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All&dt1=jail');
+  await page.getByRole('combobox', { name: 'Demographic Race/ethnicity' }).click();
+  await page.getByRole('option', { name: 'Age' }).click();
+  await page.getByText('Age:').click();
+  await page.getByRole('button', { name: '18+' }).click();
+  await page.getByRole('heading', { name: 'Ages 18+' }).click();
+  await page.getByLabel('Click for more info on people in jail').click();
+  await page.getByRole('heading', { name: 'People in jail' }).click();
   await page.getByLabel('close topic info modal').click();
-  await page.locator('#rate-map').getByRole('heading', { name: 'Jail incarceration in the United States' }).click();
-  await page.getByText('See the states/territories with the highest and lowest rates.').click();
-  await page.getByRole('heading', { name: '5 Highest:' }).click();
-  await page.getByRole('heading', { name: 'National overall:' }).click();
-  await page.getByText('Jail incarceration', { exact: true }).click();
-  await page.getByRole('heading', { name: 'Graph unavailable: Rates of jail incarceration over time in the United States' }).click();
-  await page.locator('#madlib-box').getByRole('button', { name: 'United States' }).click();
-  await page.locator('#mui-53').fill('den');
-  await page.getByRole('option', { name: 'Denver County, Colorado' }).click();
-  await page.getByRole('heading', { name: 'Rates of jail incarceration over time in Denver County, Colorado' }).click();
-  await page.getByText('Expand rates over time table').click();
-  await page.getByRole('columnheader', { name: 'Time period (1970 - 2018)' }).click();
-  await page.getByRole('columnheader', { name: 'Black or African American (NH) jail per 100k' }).click();
-  await page.locator('#rate-chart').getByRole('note').click();
-  await page.getByText('No unknown values for race and ethnicity reported in this dataset.').click();
-  await page.getByText('This graph visualizes the disproportionate share of People in jail as experience').click();
-  await page.getByRole('heading', { name: 'Population vs. distribution of total people in jail in Denver County, Colorado' }).click();
-  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click();
+  await page.getByRole('columnheader', { name: 'Age' }).click();
   await page.getByRole('columnheader', { name: 'People in jail per 100k' }).click();
-  await page.getByRole('columnheader', { name: 'Percent share of total jail population' }).click();
-  await page.getByRole('columnheader', { name: 'Total population share' }).click();
-  await page.getByText('Individuals of any age, including children, under the jurisdiction of an adult p').click();
-  await page.getByRole('button', { name: 'Jail', exact: true }).click();
-  await page.getByRole('button', { name: 'Prison' }).click();
-  await page.getByLabel('open the topic info modal').click();
 });
