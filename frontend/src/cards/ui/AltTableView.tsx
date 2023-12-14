1c9d25a3678bd8938231283b38f6c7c6b3dacf63
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-14 10:15:23 -0700

    Refactors Highest Lowest and Units to Tailwind (#2652)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - closes #2514 
    - extracts `<ExtremeList>` and refactors to tailwind
    - makes `<HetUnitLabel>` for anywhere on the site we need things like
    "100k" in a smaller, thinner font style
    - starts #2508 
    
    ## Has this been tested? How?
    
    manually at various breakpoints and open/closed states
    
    ## Screenshots (if appropriate)
    
    <img width="912" alt="Screenshot 2023-12-13 at 6 19 09 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/653b6b46-9ce9-4962-8890-85d5d114c6b2">
    <img width="912" alt="Screenshot 2023-12-13 at 6 19 56 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/e6324886-fa3d-41fb-a09e-ec90aee7a859">
    <img width="476" alt="Screenshot 2023-12-13 at 6 47 53 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/1da1336d-07f4-448d-aefc-e0101152c9c5">
    <img width="607" alt="Screenshot 2023-12-13 at 6 48 05 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/89e106e5-a8c5-4e44-8592-5dd7e9e22e90">
    <img width="1251" alt="Screenshot 2023-12-13 at 6 48 21 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/ff3c4f25-6af0-48e3-8a48-7289c6254532">
    <img width="458" alt="Screenshot 2023-12-13 at 7 03 12 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/195481df-6022-4388-a064-940eb7c6d549">
    
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

--- frontend/src/cards/ui/AltTableView.tsx
+++ frontend/src/cards/ui/AltTableView.tsx
@@ -10,7 +10,7 @@ import {
 } from '@mui/material'
 import WarningRoundedIcon from '@mui/icons-material/WarningRounded'
 import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material'
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import AnimateHeight from 'react-animate-height'
 import { type MetricConfig } from '../../data/config/MetricConfig'
 import { type DemographicType } from '../../data/query/Breakdowns'
@@ -75,7 +75,7 @@ export default function AltTableView(props: AltTableViewProps) {
           : ALT_TABLE_VIEW_1_PARAM_KEY
       }
     >
-      <div className={styles.CollapseButton}>
+      <div className='float-right'>
         <IconButton
           aria-label={`${
             !props.expanded ? 'Expand' : 'Collapse'
