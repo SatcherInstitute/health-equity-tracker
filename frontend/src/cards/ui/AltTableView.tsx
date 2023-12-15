14e880b600f141510681c6d4367608f24b93715b
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-14 21:56:04 -0700

    rv alt table sort direction

--- frontend/src/cards/ui/AltTableView.tsx
+++ frontend/src/cards/ui/AltTableView.tsx
@@ -58,8 +58,8 @@ export default function AltTableView(props: AltTableViewProps) {
     props.hasUnknowns
   )
 
-  const firstTimePeriod: string = accessibleData[0][TIME_PERIOD_LABEL]
-  const lastTimePeriod: string =
+  const latestTimePeriod: string = accessibleData[0][TIME_PERIOD_LABEL]
+  const earliestTimePeriod: string =
     accessibleData[accessibleData.length - 1][TIME_PERIOD_LABEL]
 
   return (
@@ -150,7 +150,7 @@ export default function AltTableView(props: AltTableViewProps) {
                           !isUnknownPctCol &&
                           ` ${dataColumnLabel}`}
                         {isTimeCol &&
-                          ` (${lastTimePeriod} - ${firstTimePeriod})`}
+                          ` (${earliestTimePeriod} - ${latestTimePeriod})`}
                       </TableCell>
                     )
                   })}
