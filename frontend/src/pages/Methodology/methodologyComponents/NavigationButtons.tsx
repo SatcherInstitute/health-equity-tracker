3718554077ee3136f24c7fc54c98ef846442c312
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-27 10:28:41 -0700

    adds new pagination button

--- frontend/src/pages/Methodology/methodologyComponents/NavigationButtons.tsx
+++ frontend/src/pages/Methodology/methodologyComponents/NavigationButtons.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
-import { Button } from '@mui/material'
 import { routeConfigs } from '../methodologyContent/routeConfigs'
-import { ArrowBack, ArrowForward } from '@mui/icons-material'
+import HetPaginationButton from '../../../styles/HetComponents/HetPaginationButton'
 
-export const NavigationButtons: React.FC = () => {
+export default function NavigationButtons() {
   const history = useHistory()
   const location = useLocation()
 
@@ -27,69 +25,31 @@ export const NavigationButtons: React.FC = () => {
     }
   }
 
+  /* When a previous or next step isn't available, render empty div to keep flex alignment working */
   return (
-    <div
-      // className={styles.NavigationButtonsDiv}
-      className='lg:flex-grow-1 lg:flex-basis-0" mx-0 mb-0 mt-4 flex w-full flex-col justify-between lg:mt-8 lg:flex-shrink-0 lg:flex-row lg:self-stretch '
-    >
+    <div className='lg:flex-grow-1 lg:flex-basis-0 mx-0 mb-0 mt-4 flex w-full flex-col justify-between lg:mt-8 lg:flex-shrink-0 lg:flex-row lg:self-stretch '>
       {prevRoute ? (
-        <Button
-          disabled={currentIndex === 0}
+        <HetPaginationButton
+          direction='previous'
+          label={prevRoute.label}
           onClick={goPrevious}
-          // className={styles.Previous}
-          className='mx-auto mb-0 mt-6 flex w-full flex-col justify-center rounded-2xl bg-dark-green p-1 font-sansTitle text-exploreButton font-medium leading-lhSomeMoreSpace tracking-wide text-alt-black lg:w-80'
-        >
-          <span className='flex items-center self-stretch p-2 font-sansText leading-lhLoose text-alt-black lg:leading-lhSomeMoreSpace'>
-            {prevRoute ? (
-              <span>
-                <ArrowBack /> Previous
-              </span>
-            ) : null}
-          </span>
-          <span className='flex-grow-1 flex-basis-0 flex flex-shrink-0 flex-col justify-center gap-2 self-stretch p-2'>
-            {/* <span className={styles.Previous}> */}
-            <span className='items-start justify-start text-left'>
-              {prevRoute ? prevRoute.label : null}
-            </span>
-          </span>
-        </Button>
+        />
       ) : (
-        <Button
-          style={{ visibility: 'hidden' }}
-          disabled={currentIndex === 0}
-          onClick={goPrevious}
-          className='items-start justify-start text-left'
-        >
-          <span className='flex items-center self-stretch p-2 font-sansText leading-lhSomeMoreSpace text-alt-black  '>
-            {prevRoute ? (
-              <span>
-                <ArrowBack /> Previous
-              </span>
-            ) : null}
-          </span>
-        </Button>
+        <div></div>
       )}
 
       {nextRoute ? (
-        <Button
-          disabled={currentIndex === routeConfigs.length - 1}
+        <HetPaginationButton
+          direction='next'
+          label={nextRoute.label}
           onClick={goNext}
-          className='items-end justify-end text-right'
-        >
-          <span className='flex items-center self-stretch p-2 font-sansText leading-lhSomeMoreSpace text-alt-black  '>
-            {nextRoute ? (
-              <span>
-                Up Next <ArrowForward />
-              </span>
-            ) : null}
-          </span>
-          <span className='flex flex-col justify-center gap-2 p-2 '>
-            <span>{nextRoute ? nextRoute.label : null}</span>
-          </span>
-        </Button>
-      ) : null}
+          disabled={currentIndex === routeConfigs.length - 1}
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
 
-export default NavigationButtons
+/*  */
---
+++ frontend/src/styles/HetComponents/HetPaginationButton.tsx
@@ -0,0 +1,50 @@
+import { ArrowBack, ArrowForward } from '@mui/icons-material'
+import { Button } from '@mui/material'
+
+export type HetPaginationDirection = 'previous' | 'next'
+
+interface HetPaginationButtonsProps {
+  direction: HetPaginationDirection
+  label: string
+  onClick: () => void
+  disabled?: boolean
+}
+
+export default function HetPaginationButton(props: HetPaginationButtonsProps) {
+  const isPrevious = props.direction === 'previous'
+  return (
+    <Button
+      onClick={props.onClick}
+      className='m-5 flex w-full flex-col justify-center rounded-3xl bg-methodology-green font-sansTitle  font-medium leading-lhSomeMoreSpace tracking-wide text-alt-black lg:w-80'
+    >
+      {/* ARROW AND DIRECTION WORD */}
+      <span
+        className={`mt-5 flex items-center self-stretch font-sansText leading-lhLoose text-alt-black lg:leading-lhSomeMoreSpace ${
+          isPrevious ? 'ml-5 justify-start' : 'mr-5 justify-end'
+        }`}
+      >
+        {isPrevious ? (
+          <span>
+            <ArrowBack /> Previous
+          </span>
+        ) : (
+          <span>
+            Up Next <ArrowForward />
+          </span>
+        )}
+      </span>
+      {/* LABEL FOR LINKED PAGE */}
+      <span className='flex-grow-1 flex-basis-0 mb-5 flex flex-shrink-0 flex-col justify-center gap-2 self-stretch p-2 text-exploreButton'>
+        <span
+          className={
+            isPrevious
+              ? 'ml-5 items-start justify-start text-left'
+              : 'mr-5 items-end justify-end text-right'
+          }
+        >
+          {props.label}
+        </span>
+      </span>
+    </Button>
+  )
+}
