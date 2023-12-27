d9100fd2949abc50de9bf38db3cb09a9db5d8008
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-27 09:02:16 -0700

    strong

--- frontend/src/styles/HetComponents/HetTerm.tsx
+++ frontend/src/styles/HetComponents/HetTerm.tsx
@@ -2,5 +2,7 @@ interface HetTermProps {
   children?: JSX.Element | string | string[]
 }
 export default function HetTerm(props: HetTermProps) {
-  return <mark className='font-sansTitle font-semibold'>{props.children}</mark>
+  return (
+    <strong className='font-sansTitle font-semibold'>{props.children}</strong>
+  )
 }
