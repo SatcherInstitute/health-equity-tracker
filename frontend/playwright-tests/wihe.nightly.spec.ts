d5615f8c1bb82c874d8fe32a8ba94543b1747f84
Ben Hammond <benjamin.hammond@gmail.com>
2023-12-28 09:24:47 -0700

    update e2e

--- frontend/playwright-tests/wihe.nightly.spec.ts
+++ frontend/playwright-tests/wihe.nightly.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('WIHE Page Loads', async ({ page }) => {
     await page.goto('/whatishealthequity', { waitUntil: "commit" });
-    await expect(page.getByRole('heading', { name: 'What is Health Equity?', exact: true })).toBeVisible();
+    await page.getByRole('heading', { name: 'What is Health Equity?', exact: true }).click();
     // @ts-ignore
     await expect(page).toPassAxe()
 });
