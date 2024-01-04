SatcherInstitute/health-equity-tracker

commit 728f0a71691c2b1f57cb6e31096244cde79502f4

Improves E2E Scripts (#2697)
# Description and Motivation
<!--- bulleted, high level items. use keywords (eg "closes #144" or
"fixes #4323") -->

Goal here was to simplify the way we can interact with our E2E
Playwright tests, allowing us to run the suite or single test files FROM
various locations like local development or GitHub Actions CI, AGAINST
various deployments, such as a localhost:3000 server, a dynamic Netlify
deploy preview link, the staging site (dev) or the production website.

Note: this new configuration also sets us up for the next step in #2698 

## Improved Scripts

- adjusts setup to use command line args rather than playwright projects
for testing against different environments (e.g. testing against prod,
staging/dev, localhost, etc)
- new way to run tests in development against localhost is simply `npm
run e2e`
- new way to run a single test like above is `npm run e2e
statins.nightly.spec.ts` (NOTE: you don't need to add the path or
anything, just simply the file name)
- you can still trigger the tests from your local machine against the
prod site or the dev site using `npm run e2e-prod` and `npm run e2e-dev`
respectively if you want (same idea just add the file name directly
after to run a single test file)

## README

- outlines usage of new scripts
- updates and cleans up description of various playwright testing
options


closes #2694 

## Has this been tested? How?

- `npm run e2e` locally runs the full suite against localhost
- `npm run e2e statins.nightly.spec.ts` runs the single test file
against localhost
- `npm run e2e-prod` and `npm run e2e-prod statins.nightly.spec.ts` work
as expected against the prod url
- [x] the deploy preview tests triggered by this PR's updates pass
- [ ] manually triggering the staging and prod tests pass (this can only
be tested once this PR is merged to `main`)

## Screenshots (if appropriate)

frontend/playwright.config.ts
@@ -56,29 +56,8 @@ const config: PlaywrightTestConfig = {
       testMatch: /.*externalUrls.spec.ts/,
     },
     {
-      name: 'E2E_PROD',
-      testMatch: /.*nightly.spec.ts/,
-      use: {
-        baseURL: 'https://healthequitytracker.org',
-      }
-    },
-    {
-      name: 'E2E_STAGING',
-      testMatch: /.*nightly.spec.ts/,
-      use: {
-        baseURL: 'https://dev.healthequitytracker.org',
-      }
-    },
-    {
-      name: 'E2E_DEPLOY_PREVIEW',
-      testMatch: /.*nightly.spec.ts/,
-      use: {
-        channel: 'chrome'
-      }
-    },
-    {
 
-      name: 'E2E_LOCAL',
+      name: 'E2E_NIGHTLY',
       testMatch: /.*nightly.spec.ts/,
     },
   ],

.github/workflows/e2eStaging.yml
@@ -31,7 +31,7 @@ jobs:
         run: npx playwright install chromium --with-deps
 
       - name: Runs playwright E2E_STAGING
-        run: npx playwright test --project=E2E_STAGING
+        run: npm run e2e-staging
 
       - uses: actions/upload-artifact@v3
         if: always()

.github/workflows/frontend.yml
@@ -86,7 +86,7 @@ jobs:
       - name: Install playwright deps
         run: npx playwright install --with-deps chromium
       - name: run E2E_DEPLOY_PREVIEW
-        run: npx playwright test --project=E2E_DEPLOY_PREVIEW
+        run: npx playwright test --project=E2E_NIGHTLY --workers 6
         # base url based on the GITHUB_PR_NUMBER + NETLIFY_SITE_NAME
         env:
           PLAYWRIGHT_BASE_URL: 'https://deploy-preview-${{env.GITHUB_PR_NUMBER}}--${{env.NETLIFY_SITE_NAME}}.netlify.app'

frontend/.env.deploy_preview
@@ -5,4 +5,3 @@ VITE_BASE_API_URL=https://dev.healthequitytracker.org
 VITE_FORCE_STATIC=geographies.json
 VITE_SHOW_PHRMA_MENTAL_HEALTH=1
 # PLAYWRIGHT_BASE_URL needs to be set dynamically via the command line, with the associated Netlify preview link
-

frontend/.env.staging
@@ -3,5 +3,3 @@ VITE_DEPLOY_CONTEXT=staging
 # relative to the frontend domain.
 
 VITE_FORCE_STATIC=geographies.json
-PLAYWRIGHT_BASE_URL=https://dev.healthequitytracker.org
-

commit 47315d48d26b26a37a0023df27e1612bde257be2

Fix Windows issue exporting env vars in package json  (#2701)
# Description and Motivation
<!--- bulleted, high level items. use keywords (eg "closes #144" or
"fixes #4323") -->

- installs `cross-env` package to fix issue with Unix vs. Windows
setting of env variables within package json script caused by #2697
- specifically uses the `cross-env` command before the second `npx`
portion, this was also probably causing issues when i was declaring the
variable at the front of the line, rather than directly before the
relevant npx command
- uses `E2E_BASE_URL` variable name to not conflict with Playwright's
own `PLAYWRIGHT_TEST_BASE_URL`, this might have been causing some issues
- adjusts playwright timeouts and fixes a flaky test


## Has this been tested? How?

- [x] works on mac
- [ ] works on windows

## Screenshots (if appropriate)


<img width="1283" alt="Screenshot 2024-01-03 at 4 25 21 PM"
src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/8b394021-21c8-4b0f-b176-fecba69becec">


## Types of changes

(leave all that apply)

- Bug fix
- Refactor / chore

## New frontend preview link is below in the Netlify comment 😎

frontend/playwright.config.ts
@@ -1,9 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
+
 // For AXE a11y
 import { expect } from '@playwright/test'
 import matchers from 'expect-axe-playwright'
 expect.extend(matchers)
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -17,10 +19,10 @@ const config: PlaywrightTestConfig = {
   },
   testDir: './playwright-tests',
   /* Maximum time one test can run for, default was 30s. */
-  timeout: 90 * 1000,
-  /* Maximum time one "expect"" can run for, default was 5 seconds and was too quick */
+  timeout: 120 * 1000,
+  /* Maximum time one "expect" can run for, default was 5 seconds and was too quick */
   expect: {
-    timeout: 30 * 1000
+    timeout: 60 * 1000
   },
   /* run all tests, even those within a shared file, in parallel  */
   fullyParallel: true,
@@ -30,13 +32,14 @@ const config: PlaywrightTestConfig = {
     ['html']
   ],
 
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
     headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    baseURL: process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000',
+    baseURL: process.env.E2E_BASE_URL ?? 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     screenshot: 'only-on-failure',

frontend/playwright-tests/guided-tour.nightly.spec.ts
@@ -8,7 +8,7 @@ test.describe('Guided tour of COVID-19 from Tracker Default Helper Box', () => {
 
         // Landing Page Loads
         await page.goto('/exploredata', { waitUntil: "commit" });
-        await expect(page.getByRole('heading', { name: 'Select a topic above...', exact: true })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Select a topic above' })).toBeVisible();
 
         // Clicking small link under the video starts the joyride guided tour
         const tourLink = page.locator('a:has-text("guided tour of a COVID-19 report.")')