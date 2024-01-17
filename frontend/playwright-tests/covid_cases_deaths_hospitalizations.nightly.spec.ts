13b1a695d2844a6b43abba7f14f5bf8657ed11b4
JDemlow <jdemlowdev@gmail.com>
2024-01-16 17:00:12 -0700

    splits tests into three separate test blocks

--- frontend/playwright-tests/covid_cases_deaths_hospitalizations.nightly.spec.ts
+++ frontend/playwright-tests/covid_cases_deaths_hospitalizations.nightly.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test'
 
-test('Covid Cases, Deaths, Hospitalizations', async ({ page }) => {
-  await page.goto('/exploredata?mls=1.covid-3.00&group1=All')
+test('Covid Cases', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.covid-3.00&group1=All&dt1=covid_cases')
   await page.getByText('Race and Ethnicity:').click()
   await page.locator('.MuiBackdrop-root').click()
   await page
@@ -18,75 +18,10 @@ test('Covid Cases, Deaths, Hospitalizations', async ({ page }) => {
     .click()
   await page.getByText('rates over time', { exact: true }).click()
   await page.getByLabel('Collapse data table view of').click()
-  await page
-    .locator('#rate-chart')
-    .getByRole('heading', { name: 'Rates of COVID-19 cases since' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Share of total COVID-19 cases' })
-    .click()
-  await page
-    .getByRole('button', { name: 'Inequities over time', exact: true })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Relative inequity for COVID-' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Population vs. distribution' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Breakdown summary for COVID-' })
-    .click()
-  await page.getByText('Share this report:').click()
-  await page.getByText('COVID-19 cases', { exact: true }).click()
-  await page.getByText('Do you have information that').click()
-  await page.goto(
-    'http://localhost:3000/exploredata?mls=1.covid-3.00&group1=All'
-  )
-  await page.getByRole('button', { name: 'Cases', exact: true }).click()
-  await page.getByRole('button', { name: 'Deaths' }).click()
-  await page.getByText('Race and Ethnicity:').click()
-  await page.locator('.MuiBackdrop-root').click()
-  await page.getByLabel('open the topic info modal').click()
-  await page.getByLabel('close topic info modal').click()
-  await page
-    .locator('#rate-map')
-    .getByRole('heading', { name: 'Rates of COVID-19 deaths' })
-    .click()
-  await page.getByText('Demographic', { exact: true }).nth(2).click()
-  await page.getByText('Off').nth(1).click()
-  await page.locator('#menu- div').first().click()
-  await page
-    .getByRole('heading', { name: 'Monthly COVID-19 deaths per' })
-    .click()
-  await page.getByText('rates over time', { exact: true }).click()
-  await page.getByLabel('Collapse data table view of').click()
-  await page
-    .locator('#rate-chart')
-    .getByRole('heading', { name: 'Rates of COVID-19 deaths' })
-    .click()
-  await page.getByRole('heading', { name: 'Share of total COVID-19' }).click()
-  await page
-    .getByRole('button', { name: 'Inequities over time', exact: true })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Relative inequity for COVID-' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Population vs. distribution' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Breakdown summary for COVID-' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Age-adjusted COVID-19 deaths' })
-    .click()
-  await page.getByText('Share this report:').click()
-  await page.getByText('COVID-19 deaths', { exact: true }).click()
-  await page.getByText('Do you have information that').click()
-  await page.goto(
-    'http://localhost:3000/exploredata?mls=1.covid-3.00&group1=All&dt1=covid_deaths'
-  )
+})
+
+test('Covid Deaths', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.covid-3.00&group1=All&dt1=covid_deaths')
   await page.getByRole('button', { name: 'Deaths', exact: true }).click()
   await page.getByRole('button', { name: 'Hospitalizations' }).click()
   await page.getByText('Race and Ethnicity:').click()
@@ -98,17 +33,15 @@ test('Covid Cases, Deaths, Hospitalizations', async ({ page }) => {
     .getByRole('heading', { name: 'Rates of COVID-19' })
     .click()
   await page.getByRole('heading', { name: 'Share of total COVID-19' }).click()
+})
+
+test('Covid Hospitalizations', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.covid-3.00&dt1=covid_hospitalizations')
   await page
-    .getByRole('button', { name: 'Inequities over time', exact: true })
+    .locator('#rate-map')
+    .getByRole('heading', {
+      name: 'Rates of COVID-19 hospitalizations since Jan 2020 in the United States',
+    })
     .click()
-  await page
-    .getByRole('heading', { name: 'Relative inequity for COVID-' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Population vs. distribution' })
-    .click()
-  await page
-    .getByRole('heading', { name: 'Breakdown summary for COVID-' })
-    .click()
-  await page.getByRole('heading', { name: 'Age-adjusted COVID-19' }).click()
+  await page.getByText('Expand rates over time table').click()
 })
