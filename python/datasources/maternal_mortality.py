baa422c1bd9ea57c77220c7d3ddc53ddb24d6488
Ben Hammond <benjamin.hammond@gmail.com>
2024-01-22 16:04:14 -0700

    tweak

--- python/datasources/maternal_mortality.py
+++ python/datasources/maternal_mortality.py
@@ -2,6 +2,10 @@ from ingestion import gcs_to_bq_util
 from datasources.data_source import DataSource
 
 
+# ZIP FILE CONTAINING STATE-LEVEL CSV FOR /data
+# https://ghdx.healthdata.org/record/ihme-data/united-states-maternal-mortality-by-state-race-ethnicity-1999-2019
+
+
 class MaternalMortalityData(DataSource):
     @staticmethod
     def get_id():
@@ -23,8 +27,7 @@ class MaternalMortalityData(DataSource):
             'IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV',
         )
 
-        print(df.columns)
-        print(df.head())
+        print(df)
 
         # col_types = gcs_to_bq_util.get_bq_column_types(breakdown_df, float_cols)
 
