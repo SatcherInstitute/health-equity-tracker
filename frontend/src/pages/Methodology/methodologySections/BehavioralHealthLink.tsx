e629ee0f9f6e48f0cd610f6064483edddc6fb5fa
Ben Hammond <benjamin.hammond@gmail.com>
2024-01-30 12:17:22 -0700

    rv

--- frontend/src/pages/Methodology/methodologySections/BehavioralHealthLink.tsx
+++ frontend/src/pages/Methodology/methodologySections/BehavioralHealthLink.tsx
@@ -79,7 +79,7 @@ export default function BehavioralHealthLink() {
           <a href={'urlMap.censusVoting'}>U.S. Census</a>.{' '}
         </p>
         <HetNotice
-          className='my-10'
+          className='my-12'
           title="A note about the CDC's Behavioral Risk Factor Surveillance System
             (BRFSS) survey"
         >
--- frontend/src/pages/Methodology/methodologySections/ChronicDiseaseLink.tsx
+++ frontend/src/pages/Methodology/methodologySections/ChronicDiseaseLink.tsx
@@ -71,7 +71,7 @@ const ChronicDiseaseLink = () => {
           <a href={'urlMap.censusVoting'}>U.S. Census</a>.{' '}
         </p>
         <HetNotice
-          className='my-10'
+          className='my-12'
           title="A note about the CDC's Behavioral Risk Factor Surveillance System
             (BRFSS) survey"
         >
