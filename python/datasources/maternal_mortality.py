7a050b599919da24069c06e505b999faa59f8d01
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-05 12:09:20 -0700

    map races

--- python/datasources/maternal_mortality.py
+++ python/datasources/maternal_mortality.py
@@ -1,6 +1,15 @@
 from ingestion import gcs_to_bq_util
 from datasources.data_source import DataSource
+import ingestion.standardized_columns as std_col
 
+RACE_GROUPS_TO_STANDARD = {
+    'Non-Hispanic American Indian and Alaska Native': std_col.Race.AIAN_NH.value,
+    'Non-Hispanic Asian, Native Hawaiian, or Other Pacific Islander': std_col.Race.API_NH.value,
+    'Non-Hispanic Black': std_col.Race.BLACK_NH.value,
+    'Non-Hispanic White': std_col.Race.WHITE_NH.value,
+    'Hispanic and any race': std_col.Race.HISP.value,
+    'All racial and ethnic groups': std_col.Race.ALL.value,
+}
 
 # ZIP FILE CONTAINING STATE-LEVEL CSV FOR /data
 # https://ghdx.healthdata.org/record/ihme-data/united-states-maternal-mortality-by-state-race-ethnicity-1999-2019
@@ -27,10 +36,18 @@ class MaternalMortalityData(DataSource):
             'IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV',
         )
 
-        df = df.rename(columns={'val': 'maternal_mortality_per_100k'})
+        df = df.rename(
+            columns={
+                'val': 'maternal_mortality_per_100k',
+                'race_group': std_col.RACE_CATEGORY_ID_COL,
+            }
+        )
 
         # round to whole numbers
         df['maternal_mortality_per_100k'] = df['maternal_mortality_per_100k'].round(0)
+
+        df = df.replace(RACE_GROUPS_TO_STANDARD)
+        std_col.add_race_columns_from_category_id(df)
 
         print("\n")
         print(df)
