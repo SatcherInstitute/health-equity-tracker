13ad8ad82f838efef44fd3e09cfee5cca84b1048
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-12 16:44:32 -0700

    updates axe a11y testing

--- frontend/playwright-tests/faqs.ci.spec.ts
+++ frontend/playwright-tests/faqs.ci.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 test('FAQ Tab Loads', async ({ page }) => {
     await page.goto('/faqs', { waitUntil: "commit" });
     await expect(page.getByRole('heading', { name: 'Data', exact: true })).toBeVisible();
-    // @ts-ignore
-    await expect(page).toPassAxe()
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+
 });
