07dc0eff7bcc8129d4bf3127110fed9a0b9d3825
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-13 16:31:52 -0700

    Updates e2e accessibility testing (#2904)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - closes #2868 
    - replaces 3rd party package with official playwright package from axe
    - updates all uses
    - skips checks of youtube embedded iframes as they contain a11y issues
    - simplifies playwright config
    
    ## Has this been tested? How?
    
    new checker caught issues the previous tool did not. those were fixed or
    skipped
    
    ## Types of changes
    
    (leave all that apply)
    
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

--- frontend/playwright-tests/wihe.ci.spec.ts
+++ frontend/playwright-tests/wihe.ci.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 test('WIHE Page Loads', async ({ page }) => {
     await page.goto('/whatishealthequity', { waitUntil: "commit" });
     await page.getByRole('heading', { name: 'What is Health Equity?', exact: true }).click();
-    // @ts-ignore
-    await expect(page).toPassAxe()
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .exclude('iframe') // YouTube embed is not fully accessible
+        .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+
+
+
 });
 
