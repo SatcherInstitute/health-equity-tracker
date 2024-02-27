6c50e2693e207bd65b57bc025772464404db1843
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-27 08:44:54 -0700

    Local Pipeline: National COVID Vaccinations (#2936)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    Related to #2911 
    
    By extending the existing `write_to_bq` method, we can keep our existing
    tests in place, and allow the BQ flow to continue uninterupted, but now
    optionally redirect the written table to local frontend instead of BQ.
    NOTE: Eventually we'll likely want to rename this method across the
    entire codebase including the `data_server` to something like
    `run_pipeline()`
    
    ## Pipeline
    
    - removes deprecated table name methods from the parent DataSource class
    - adds a `local_pipeline_utils.py` file to hold new utility functions
    that will help us run our pipelines locally, including reading real data
    directly from the `/data` dir, and writing json files to
    frontend/public/tmp folder (to allow local loading)
    - adds a new python file that instantiates the class (and can easily
    extend to iterate over all data sources, and the calls the write to bq
    with the "write to local frontend" flag enabled. NOTE: this is still
    making real calls out to BQ for population data and FIPS data, etc. We
    should consider re-structuring this, so that here and also for our
    tests, instead of having to mock, our data sources could just read from
    the data directory like `data/acs_population/` or `data/state_ids` or
    similar
    
    ## `CDCVaccinationNational`
    
    - adjusts `CDCVaccinationNational.write_to_bq()` to accept an optional
    boolean keyword arg
    - adjusts `CDCVaccinationNational.write_to_bq()` so if that flag is
    passed, it uses the util from above to write to frontend instead of
    writing to bigquery
    
    
    ## Has this been tested? How?
    
    - working locally, it writes out json files as expected to the
    `/frontend/public/tmp` folder
    - by adding the file names into the .env file and restarting the
    localhost server it automatically serves those tables instead of reading
    from the backend
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

---
+++ python/ingestion/local_pipeline_utils.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+DATA_DIR = os.path.abspath('data')
+FRONTEND_TMP_DIR = os.path.abspath('frontend/public/tmp')
+
+
+def load_csv_as_df_from_data_dir(
+    directory, filename, subdirectory='', dtype=None, skiprows=None, na_values=None, thousands=None, usecols=None
+) -> pd.DataFrame:
+    file_path = os.path.join(DATA_DIR, directory, subdirectory, filename)
+
+    return pd.read_csv(
+        file_path, dtype=dtype, skiprows=skiprows, na_values=na_values, usecols=usecols, thousands=thousands
+    )
+
+
+def write_df_as_json_to_frontend_tmp(df: pd.DataFrame, filename: str):
+    file_path = os.path.join(FRONTEND_TMP_DIR, filename)
+    df.to_json(f'{file_path}.json', orient='records', date_format='iso', date_unit='s')
