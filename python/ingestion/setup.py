8dc4a0dea93e89f4e5980b967acd0ff27f0c9057
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-27 20:11:20 -0700

    install csvs?

--- python/ingestion/setup.py
+++ python/ingestion/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
-setup(name='ingestion',
-      package_dir={'ingestion': ''},
-      packages=['ingestion'],
-      )
+setup(
+    name='ingestion',
+    package_dir={'ingestion': ''},
+    packages=['ingestion'],
+    package_data={'ingestion': ['state_fips_codes.csv', 'county_fips_codes.csv']},
+)
