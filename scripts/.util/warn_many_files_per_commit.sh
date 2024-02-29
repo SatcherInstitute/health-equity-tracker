0c6770ee714eecff99f945083bc83411df2a3ebd
Ben Hammond <benjamin.hammond@gmail.com>
2024-02-29 08:52:56 -0700

    add warning with override instructions

--- scripts/.util/warn_many_files_per_commit.sh
+++ scripts/.util/warn_many_files_per_commit.sh
@@ -7,8 +7,8 @@ files=$(git diff --cached --name-only)
 file_count=$(echo "$files" | wc -l)
 
 # If the file count exceeds 100, issue a warning
-if [ "$file_count" -gt 1 ]; then
-  echo "Warning: More than 100 files are being committed. Are you sure?"
+if [ "$file_count" -gt 100 ]; then
+  echo "Warning: More than 100 files are being committed. If you are sure, run your commit command again starting with SKIP=check-file-count"
   exit 1  # Return non-zero to indicate failure
 fi
 
