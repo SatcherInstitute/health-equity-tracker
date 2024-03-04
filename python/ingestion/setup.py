9fe9ed4a2448d50e4a53bf6347d9501b816cedce
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-03 20:26:26 -0700

    install ALL csvs in merge data incl FIPS and ACS

--- python/ingestion/setup.py
+++ python/ingestion/setup.py
@@ -5,5 +5,5 @@ setup(
     package_dir={'ingestion': ''},
     packages=['ingestion'],
     include_package_data=True,
-    package_data={'ingestion': ['merge_data/state_level_fips.csv', 'merge_data/county_level_fips.csv']},
+    package_data={'ingestion': ['merge_data/*.csv']},
 )
