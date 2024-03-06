49bb67233e4bd1772614ba78ff0099dd0fc4955a
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-06 14:37:23 -0700

    Updates new methodology with Dr. Purcells edits (#2959)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - closes #2951 
    - lots of formatting
    - refactor mobile and desktop menus to iterate over strongly typed
    routeConfigs to ensure consistent rendering between devices and easier
    to keep up to date as routes change
    - moves "Limitations and Missing Data" to its own top level menu item
    - adds Dr. Purcell's limitations content
    
    
    ## Has this been tested? How?
    
    - [x] manual testing on mobile and desktop views
    - [x] tests passing
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    (leave all that apply)
    
    - Bug fix
    - New content or feature
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎
    
    
    Co-authored-by: Donrie Purcell <dpurcell@msm.edu>

--- frontend/src/pages/Methodology/methodologySections/MethodologyHomeLink.tsx
+++ frontend/src/pages/Methodology/methodologySections/MethodologyHomeLink.tsx
@@ -1,42 +1,29 @@
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet-async';
 
 const MethodologyHomeLink = () => {
-  return (
-    <section>
-      <article>
-        <Helmet>
-          <title>Methodology - Health Equity Tracker</title>
-        </Helmet>
-        <h2 className='sr-only'>Methodology</h2>
+	return (
+		<section>
+			<article>
+				<Helmet>
+					<title>Methodology - Health Equity Tracker</title>
+				</Helmet>
+				<h2 className='sr-only'>Methodology</h2>
 
-        <div>
-          <p>
-            We are committed to principles of{' '}
-            <em>Transparency & Accountability,</em> <em>Community First</em>,
-            and <em>Open Access</em>, engaging closely with diverse communities
-            to shape the overall health narrative and drive actionable policies.
-            As we continue to expand our data sources and refine our analyses,
-            our goal remains to inform and empower policymakers, while
-            highlighting disparities and continually measuring progress towards
-            health equity.
-          </p>
-          <p>
-            We need to hear from you. Our team is quickly developing bigger and
-            bolder ideas – to expand the tracker not just in terms of breadth of
-            health topics covered, but more importantly, in depth – to increase
-            the Health Equity Tracker’s utility to reach decision makers at
-            every level and especially those involved in advancing policy so
-            Health Equity has a seat at the table. We believe data can be
-            transformative and that data quality and access is arguably one of
-            the most critical determinants of health. We invite you to tell us
-            what you are working on to advance Health Equity, and how the we
-            might help. In the spirit of collaboration, I hope you will join us
-            in saying: <strong>Health Equity for All!</strong>
-          </p>
-        </div>
-      </article>
-    </section>
-  )
-}
+				<div>
+					<p>
+						We are committed to principles of{' '}
+						<em>Transparency & Accountability,</em> <em>Community First</em>,
+						and <em>Open Access</em>, engaging closely with diverse communities
+						to shape the overall health narrative and drive actionable policies.
+						As we continue to expand our data sources and refine our analyses,
+						our goal remains to inform and empower policymakers, while
+						highlighting disparities and continually measuring progress towards
+						health equity.
+					</p>
+				</div>
+			</article>
+		</section>
+	);
+};
 
-export default MethodologyHomeLink
+export default MethodologyHomeLink;
--- frontend/src/utils/internalRoutes.ts
+++ frontend/src/utils/internalRoutes.ts
@@ -1,82 +1,83 @@
 // PAGE URLS
-export const HET_URL = 'https://healthequitytracker.org'
-export const EXPLORE_DATA_PAGE_LINK = '/exploredata'
-export const DATA_CATALOG_PAGE_LINK = '/datacatalog'
-export const NEWS_PAGE_LINK = '/news'
-export const ABOUT_US_PAGE_LINK = '/aboutus'
-export const WHAT_IS_HEALTH_EQUITY_PAGE_LINK = '/whatishealthequity'
-export const TERMS_OF_USE_PAGE_LINK = '/termsofuse'
-export const FAQ_TAB_LINK = '/faqs'
-export const CONTACT_TAB_LINK = '/contact'
-export const OURTEAM_TAB_LINK = '/ourteam'
-export const SHARE_YOUR_STORY_TAB_LINK = '/shareyourstory'
+export const HET_URL = 'https://healthequitytracker.org';
+export const EXPLORE_DATA_PAGE_LINK = '/exploredata';
+export const DATA_CATALOG_PAGE_LINK = '/datacatalog';
+export const NEWS_PAGE_LINK = '/news';
+export const ABOUT_US_PAGE_LINK = '/aboutus';
+export const WHAT_IS_HEALTH_EQUITY_PAGE_LINK = '/whatishealthequity';
+export const TERMS_OF_USE_PAGE_LINK = '/termsofuse';
+export const FAQ_TAB_LINK = '/faqs';
+export const CONTACT_TAB_LINK = '/contact';
+export const OURTEAM_TAB_LINK = '/ourteam';
+export const SHARE_YOUR_STORY_TAB_LINK = '/shareyourstory';
 
 // OLD METHODOLOGY SECTIONS
-export const OLD_AGE_ADJUSTMENT_LINK = '/ageadjustment'
-export const OLD_METHODOLOGY_PAGE_LINK = '/methodology'
+export const OLD_AGE_ADJUSTMENT_LINK = '/ageadjustment';
+export const OLD_METHODOLOGY_PAGE_LINK = '/methodology';
 
 // NEW METHODOLOGY SECTIONS
-export const NEW_METHODOLOGY_PAGE_LINK = '/new-methodology'
+export const NEW_METHODOLOGY_PAGE_LINK = '/new-methodology';
 
 export const NEW_AGE_ADJUSTMENT_LINK =
-  NEW_METHODOLOGY_PAGE_LINK + '/age-adjustment'
-export const SOURCES_LINK = NEW_METHODOLOGY_PAGE_LINK + '/data-sources'
+	NEW_METHODOLOGY_PAGE_LINK + '/age-adjustment';
+export const SOURCES_LINK = NEW_METHODOLOGY_PAGE_LINK + '/data-sources';
 export const TOPIC_CATEGORIES_LINK =
-  NEW_METHODOLOGY_PAGE_LINK + '/topic-categories'
+	NEW_METHODOLOGY_PAGE_LINK + '/topic-categories';
+export const LIMITATIONS_LINK = NEW_METHODOLOGY_PAGE_LINK + '/limitations';
 export const BEHAVIORAL_HEALTH_LINK =
-  TOPIC_CATEGORIES_LINK + '/behavioral-health'
-export const CHRONIC_DISEASE_LINK = TOPIC_CATEGORIES_LINK + '/chronic-disease'
-export const COVID_19_LINK = TOPIC_CATEGORIES_LINK + '/covid'
-export const GLOSSARY_LINK = NEW_METHODOLOGY_PAGE_LINK + '/glossary'
-export const HIV_LINK = TOPIC_CATEGORIES_LINK + '/hiv'
-export const PDOH_LINK = TOPIC_CATEGORIES_LINK + '/pdoh'
-export const SDOH_LINK = TOPIC_CATEGORIES_LINK + '/sdoh'
+	TOPIC_CATEGORIES_LINK + '/behavioral-health';
+export const CHRONIC_DISEASE_LINK = TOPIC_CATEGORIES_LINK + '/chronic-disease';
+export const COVID_19_LINK = TOPIC_CATEGORIES_LINK + '/covid';
+export const GLOSSARY_LINK = NEW_METHODOLOGY_PAGE_LINK + '/glossary';
+export const HIV_LINK = TOPIC_CATEGORIES_LINK + '/hiv';
+export const PDOH_LINK = TOPIC_CATEGORIES_LINK + '/pdoh';
+export const SDOH_LINK = TOPIC_CATEGORIES_LINK + '/sdoh';
 export const MEDICATION_UTILIZATION_LINK =
-  TOPIC_CATEGORIES_LINK + '/medication-utilization'
+	TOPIC_CATEGORIES_LINK + '/medication-utilization';
 export const DATA_METHOD_DEFINITIONS_LINK =
-  NEW_METHODOLOGY_PAGE_LINK + '/definitions'
-export const METRICS_LINK = DATA_METHOD_DEFINITIONS_LINK + '/metrics'
+	NEW_METHODOLOGY_PAGE_LINK + '/definitions';
+export const METRICS_LINK = DATA_METHOD_DEFINITIONS_LINK + '/metrics';
 export const TOPIC_DEFINITIONS_LINK =
-  DATA_METHOD_DEFINITIONS_LINK + '/topic-definitions'
+	DATA_METHOD_DEFINITIONS_LINK + '/topic-definitions';
 export const RACES_AND_ETHNICITIES_LINK =
-  DATA_METHOD_DEFINITIONS_LINK + '/races-and-ethnicities'
+	DATA_METHOD_DEFINITIONS_LINK + '/races-and-ethnicities';
 export const RECOMMENDED_CITATION_LINK =
-  NEW_METHODOLOGY_PAGE_LINK + '/recommended-citation'
+	NEW_METHODOLOGY_PAGE_LINK + '/recommended-citation';
 
 // TRACKER SETTINGS
 export const AGE_ADJUST_HIV_DEATHS_US_SETTING =
-  '?mls=1.hiv-3.00&group1=All&dt1=hiv_deaths#age-adjusted-ratios'
+	'?mls=1.hiv-3.00&group1=All&dt1=hiv_deaths#age-adjusted-ratios';
 export const AGE_ADJUST_COVID_DEATHS_US_SETTING =
-  '?mls=1.covid-3.00&group1=All&dt1=covid_deaths#age-adjusted-ratios'
+	'?mls=1.covid-3.00&group1=All&dt1=covid_deaths#age-adjusted-ratios';
 export const AGE_ADJUST_COVID_HOSP_US_SETTING =
-  '?mls=1.covid-3.00&group1=All&dt1=covid_hospitalizations#age-adjusted-ratios'
+	'?mls=1.covid-3.00&group1=All&dt1=covid_hospitalizations#age-adjusted-ratios';
 export const COVID_HOSP_NY_COUNTY_SETTING =
-  '?mls=1.covid_hospitalizations-3.36061'
-export const COVID_VAX_US_SETTING = '?mls=1.covid_vaccinations-3.00'
-export const COPD_US_SETTING = '?mls=1.copd-3.00'
-export const DIABETES_US_SETTING = '?mls=1.diabetes-3.00'
-export const UNINSURANCE_US_SETTING = '?mls=1.health_insurance-3.00'
-export const POVERTY_US_SETTING = '?mls=1.poverty-3.00'
+	'?mls=1.covid_hospitalizations-3.36061';
+export const COVID_VAX_US_SETTING = '?mls=1.covid_vaccinations-3.00';
+export const COPD_US_SETTING = '?mls=1.copd-3.00';
+export const DIABETES_US_SETTING = '?mls=1.diabetes-3.00';
+export const UNINSURANCE_US_SETTING = '?mls=1.health_insurance-3.00';
+export const POVERTY_US_SETTING = '?mls=1.poverty-3.00';
 export const OPIOID_US_SETTING =
-  '?dt1=non_medical_drug_use&mls=1.substance-3.00'
+	'?dt1=non_medical_drug_use&mls=1.substance-3.00';
 
 export const HIV_PREVALANCE_RACE_USA_SETTING =
-  '?mls=1.hiv-3.00&mlp=disparity&dt1=hiv_prevalence'
+	'?mls=1.hiv-3.00&mlp=disparity&dt1=hiv_prevalence';
 export const PHRMA_HIV_ELIGIBILITY_USA_MULTIMAP_SETTING =
-  '?mls=1.medicare_hiv-3.00&group1=All&demo=eligibility&dt1=medicare_hiv&multiple-maps=true'
+	'?mls=1.medicare_hiv-3.00&group1=All&demo=eligibility&dt1=medicare_hiv&multiple-maps=true';
 export const COVID_DEATHS_AGE_FULTON_COUNTY_SETTING =
-  '?mls=1.covid-3.13121&group1=All&group2=All&dt1=covid_deaths&demo=age'
+	'?mls=1.covid-3.13121&group1=All&group2=All&dt1=covid_deaths&demo=age';
 export const PRISON_VS_POVERTY_RACE_GA_SETTING =
-  '?mls=1.incarceration-3.poverty-5.13&mlp=comparevars&dt1=prison'
+	'?mls=1.incarceration-3.poverty-5.13&mlp=comparevars&dt1=prison';
 export const UNINSURANCE_SEX_FL_VS_CA_SETTING =
-  '?mls=1.health_insurance-3.12-5.06&mlp=comparegeos&demo=sex'
+	'?mls=1.health_insurance-3.12-5.06&mlp=comparegeos&demo=sex';
 
 // warm welcome demo setting
 export const WARM_WELCOME_DEMO_SETTING =
-  '?mls=1.covid-3.00&mlp=disparity&dt1=covid_cases&onboard=true'
+	'?mls=1.covid-3.00&mlp=disparity&dt1=covid_cases&onboard=true';
 
 // SECTION IDS
-export const WHAT_DATA_ARE_MISSING_ID = 'definitions-missing-data'
+export const WHAT_DATA_ARE_MISSING_ID = 'definitions-missing-data';
 export const EXPLORE_DATA_PAGE_WHAT_DATA_ARE_MISSING_LINK =
-  EXPLORE_DATA_PAGE_LINK + '#' + WHAT_DATA_ARE_MISSING_ID
-export const WIHE_JOIN_THE_EFFORT_SECTION_ID = 'join'
+	EXPLORE_DATA_PAGE_LINK + '#' + WHAT_DATA_ARE_MISSING_ID;
+export const WIHE_JOIN_THE_EFFORT_SECTION_ID = 'join';
