5a16e12f0da02a1af513305474d99abc6b460627
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-06 14:37:23 -0700

    Updates new methodology with Dr. Purcells edits (#2959)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - closes #2951 
    - lots of formatting
    - refactor mobile and desktop menus to iterate over strongly typed
    routeConfigs to ensure consistent rendering between devices and easier
    to keep up to date as routes change
    - moves "Limitations and Missing Data" to its own top level menu item
    - adds Dr. Purcell's limitations content
    
    
    ## Has this been tested? How?
    
    - [x] manual testing on mobile and desktop views
    - [x] tests passing
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    
    (leave all that apply)
    
    - Bug fix
    - New content or feature
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎
    
    
    Co-authored-by: Donrie Purcell <dpurcell@msm.edu>

--- frontend/src/pages/Methodology/methodologyComponents/MethodologyCardMenuMobile.tsx
+++ frontend/src/pages/Methodology/methodologyComponents/MethodologyCardMenuMobile.tsx
@@ -1,101 +1,48 @@
-import Toolbar from '@mui/material/Toolbar'
-import { Select, FormControl, MenuItem, InputLabel } from '@mui/material'
-import { useHistory } from 'react-router-dom'
-import {
-  NEW_METHODOLOGY_PAGE_LINK,
-  NEW_AGE_ADJUSTMENT_LINK,
-  SOURCES_LINK,
-  TOPIC_CATEGORIES_LINK,
-  BEHAVIORAL_HEALTH_LINK,
-  CHRONIC_DISEASE_LINK,
-  COVID_19_LINK,
-  HIV_LINK,
-  PDOH_LINK,
-  SDOH_LINK,
-  DATA_METHOD_DEFINITIONS_LINK,
-  METRICS_LINK,
-  TOPIC_DEFINITIONS_LINK,
-  RACES_AND_ETHNICITIES_LINK,
-  RECOMMENDED_CITATION_LINK,
-  GLOSSARY_LINK,
-  MEDICATION_UTILIZATION_LINK,
-} from '../../../utils/internalRoutes'
+import Toolbar from '@mui/material/Toolbar';
+import { Select, FormControl, MenuItem, InputLabel } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import { routeConfigs } from '../methodologyContent/routeConfigs';
 
 interface MethodologyCardMenuMobileProps {
-  className?: string
+	className?: string;
 }
 
 export default function MethodologyCardMenuMobile(
-  props: MethodologyCardMenuMobileProps
+	props: MethodologyCardMenuMobileProps,
 ) {
-  const history = useHistory()
+	const history = useHistory();
 
-  const handleSelected = (event: any) => {
-    history.push(event.target.value)
-  }
+	const handleSelected = (event: any) => {
+		history.push(event.target.value);
+	};
 
-  return (
-    <>
-      <div
-        className={`top-0 z-almostTop flex items-center rounded-sm bg-white p-1 sm:items-start sm:justify-start md:justify-center ${
-          props.className ?? ''
-        }`}
-      >
-        <Toolbar className='w-full'>
-          <FormControl sx={{ minWidth: '90vw' }} size='medium'>
-            <InputLabel id='methodology-select-label'>
-              Methodology Pages
-            </InputLabel>
-            <Select
-              labelId='methodology-select-label'
-              value={window.location.pathname}
-              onChange={handleSelected}
-              label='Methodology Pages'
-            >
-              <MenuItem value={NEW_METHODOLOGY_PAGE_LINK}>
-                Introduction
-              </MenuItem>
-              <MenuItem value={NEW_AGE_ADJUSTMENT_LINK}>
-                Age-Adjustment
-              </MenuItem>
-              <MenuItem value={SOURCES_LINK}>Data Sources</MenuItem>
-              <MenuItem value={TOPIC_CATEGORIES_LINK}>
-                Topic Categories & Limitations
-              </MenuItem>
-              <MenuItem value={BEHAVIORAL_HEALTH_LINK}>
-                Behavioral Health
-              </MenuItem>
-              <MenuItem value={CHRONIC_DISEASE_LINK}>Chronic Diseases</MenuItem>
-              <MenuItem value={COVID_19_LINK}>COVID-19</MenuItem>
-              <MenuItem value={HIV_LINK}>HIV</MenuItem>
-              <MenuItem value={PDOH_LINK}>
-                Political Determinants of Health (PDOH)
-              </MenuItem>
-              <MenuItem value={SDOH_LINK}>
-                Social Determinants of Health (SDOH)
-              </MenuItem>
-              <MenuItem value={MEDICATION_UTILIZATION_LINK}>
-                Medication Utilization
-              </MenuItem>
-              <MenuItem value={DATA_METHOD_DEFINITIONS_LINK}>
-                Data Method Definitions
-              </MenuItem>
-              <MenuItem value={METRICS_LINK}>Metrics</MenuItem>
-              <MenuItem value={TOPIC_DEFINITIONS_LINK}>
-                Topic Definitions
-              </MenuItem>
-
-              <MenuItem value={RACES_AND_ETHNICITIES_LINK}>
-                Races and Ethnicities
-              </MenuItem>
-              <MenuItem value={RECOMMENDED_CITATION_LINK}>
-                Recommended Citation
-              </MenuItem>
-              <MenuItem value={GLOSSARY_LINK}>Glossary</MenuItem>
-            </Select>
-          </FormControl>
-        </Toolbar>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div
+				className={`top-0 z-almostTop flex items-center rounded-sm bg-white p-1 sm:items-start sm:justify-start md:justify-center ${
+					props.className ?? ''
+				}`}
+			>
+				<Toolbar className='w-full'>
+					<FormControl sx={{ minWidth: '90vw' }} size='medium'>
+						<InputLabel id='methodology-select-label'>
+							Methodology Pages
+						</InputLabel>
+						<Select
+							labelId='methodology-select-label'
+							value={window.location.pathname}
+							onChange={handleSelected}
+							label='Methodology Pages'
+						>
+							{routeConfigs.map((config) => (
+								<MenuItem key={config.path} value={config.path}>
+									{config.label}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Toolbar>
+			</div>
+		</>
+	);
 }
