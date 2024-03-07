4fb46969edc07a53d5573c066c0f47667c11339e
JC Coles <jc.coles@curri.com>
2024-03-06 22:22:30 -0800

    Improve multimap footer (#2945)
    # Description and Motivation
    - closes #2725
    - Add margin-left and fix Icon Button Background to circle from oval
    
    ## Has this been tested? How?
    - Not yet, just cosmetic fixes
    
    ## Screenshots (if appropriate)
    <img width="783" alt="Screenshot 2024-02-27 at 7 58 18 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/7937257/4fdedfb2-97c9-4e0a-b8a2-9b14937f7400">
    
    
    ## Types of changes
    
    - Bug fix
    - Clean up
    
    ## New frontend preview link is below in the Netlify comment 😎
    
    ---------
    
    Co-authored-by: Ben Hammond <benjamin.hammond@gmail.com>

--- frontend/playwright-tests/incarceration.ci.spec.ts
+++ frontend/playwright-tests/incarceration.ci.spec.ts
@@ -1,29 +1,46 @@
-import { test } from '@playwright/test';
+import { test } from '@playwright/test'
 
 test('Prison by Race', async ({ page }) => {
-  await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All');
-  await page.getByLabel('Launch multiple maps view with side-by-side maps of each race and ethnicity group').click();
-  await page.getByLabel('close multiple maps modal').click();
-  await page.getByLabel('open the topic info modal').click();
-  await page.getByRole('heading', { name: 'People in prison' }).click();
-  await page.getByLabel('close topic info modal').click();
-  await page.locator('#rate-chart').getByText('3,232 children confined in adult facilities in the United States. Learn more.').click();
-  await page.locator('#unknown-demographic-map').getByRole('note').click();
-  await page.getByRole('heading', { name: 'Breakdown summary for people in prison in the United States' }).click();
-  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click();
-  await page.getByRole('button', { name: 'Prison', exact: true }).click();
-  await page.getByRole('button', { name: 'Jail' }).click();
+  await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All')
+  await page
+    .getByLabel(
+      'Launch multiple maps view with side-by-side maps of each race and ethnicity group'
+    )
+    .click()
+  await page.getByRole('button', { name: 'Close' }).click()
+  await page.getByLabel('open the topic info modal').click()
+  await page.getByRole('heading', { name: 'People in prison' }).click()
+  await page.getByLabel('close topic info modal').click()
+  await page
+    .locator('#rate-chart')
+    .getByText(
+      '3,232 children confined in adult facilities in the United States. Learn more.'
+    )
+    .click()
+  await page.locator('#unknown-demographic-map').getByRole('note').click()
+  await page
+    .getByRole('heading', {
+      name: 'Breakdown summary for people in prison in the United States',
+    })
+    .click()
+  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click()
+  await page.getByRole('button', { name: 'Prison', exact: true }).click()
+  await page.getByRole('button', { name: 'Jail' }).click()
 })
 test('Jail by Age', async ({ page }) => {
-  await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All&dt1=jail');
-  await page.getByRole('combobox', { name: 'Demographic Race/ethnicity' }).click();
-  await page.getByRole('option', { name: 'Age' }).click();
-  await page.getByText('Age:').click();
-  await page.getByRole('button', { name: '18+' }).click();
-  await page.getByRole('heading', { name: 'Ages 18+' }).click();
-  await page.getByLabel('Click for more info on people in jail').click();
-  await page.getByRole('heading', { name: 'People in jail' }).click();
-  await page.getByLabel('close topic info modal').click();
-  await page.getByRole('columnheader', { name: 'Age' }).click();
-  await page.getByRole('columnheader', { name: 'People in jail per 100k' }).click();
-});
+  await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All&dt1=jail')
+  await page
+    .getByRole('combobox', { name: 'Demographic Race/ethnicity' })
+    .click()
+  await page.getByRole('option', { name: 'Age' }).click()
+  await page.getByText('Age:').click()
+  await page.getByRole('button', { name: '18+' }).click()
+  await page.getByRole('heading', { name: 'Ages 18+' }).click()
+  await page.getByLabel('Click for more info on people in jail').click()
+  await page.getByRole('heading', { name: 'People in jail' }).click()
+  await page.getByLabel('close topic info modal').click()
+  await page.getByRole('columnheader', { name: 'Age' }).click()
+  await page
+    .getByRole('columnheader', { name: 'People in jail per 100k' })
+    .click()
+})
