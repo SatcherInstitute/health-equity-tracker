f11861a059c31c871eeafdcc49712c452857b0f7
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-11 12:35:19 -0600

    rv

--- config/data_sources/graphql_ahr_data.tf
+++
@@ -1,7 +0,0 @@
-# Resources and routines for AHR Data ingestion.
-
-# Create a BigQuery dataset for GraphQL AHR data.
-resource "google_bigquery_dataset" "graphql_ahr" {
-  dataset_id = "graphql_ahr_data"
-  location   = "US"
-}
