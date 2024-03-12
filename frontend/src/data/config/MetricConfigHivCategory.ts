e3fd63b079632bd309c5121779fdc3eb43f829bf
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-12 09:50:12 -0600

    Render "N" counts for HIV (#2987)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    HIV for #2986 
    
    - disables a lint rule (for later enabling)
    - adds count metric ids for HIV
    - sets metric config for HIV to specify numerators and denominators for
    most rates, and to set subpopulation (for the population breadcrumb in
    the map card)
    
    ## Has this been tested? How?
    
    tests passing
    
    ## Screenshots (if appropriate)
    
    <img width="1355" alt="Screenshot 2024-03-12 at 8 56 59 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/18d00139-6790-4c39-a2c3-690a84c25aa7">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 57 25 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/7dd023d2-8763-40c7-b18f-3358b8125dd3">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 57 41 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/54da19de-f382-47ea-909d-a776f3de0010">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 57 56 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/8274688e-e787-459d-a10f-4919bf8d87e9">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 58 15 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/4f10531b-4266-4e78-9c99-0e8006eb0b3d">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 58 41 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/e7b4ac4d-f72d-4348-bde0-aded214a2f53">
    <img width="1355" alt="Screenshot 2024-03-12 at 8 58 54 AM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/b8505b44-889d-4082-af14-bd4247156b74">
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - New content or feature
    
    
    ## New frontend preview link is below in the Netlify comment 😎

--- frontend/src/data/config/MetricConfigHivCategory.ts
+++ frontend/src/data/config/MetricConfigHivCategory.ts
@@ -24,35 +24,47 @@ export type HivCategoryDataTypeId =
 
 export type HivCategoryMetricId =
   | 'black_women_population_pct'
+  | 'black_women_population_count'
   | 'hiv_care_linkage'
   | 'hiv_care_pct_relative_inequity'
   | 'hiv_care_pct_share'
   | 'hiv_care_population_pct'
+  | 'hiv_care_population'
+  | 'hiv_care'
   | 'hiv_deaths_black_women_pct_relative_inequity'
   | 'hiv_deaths_black_women_pct_share'
   | 'hiv_deaths_black_women_per_100k'
+  | 'hiv_deaths_black_women'
   | 'hiv_deaths_pct_relative_inequity'
   | 'hiv_deaths_pct_share'
   | 'hiv_deaths_per_100k'
   | 'hiv_deaths_ratio_age_adjusted'
+  | 'hiv_deaths'
   | 'hiv_diagnoses_black_women_pct_relative_inequity'
   | 'hiv_diagnoses_black_women_pct_share'
   | 'hiv_diagnoses_black_women_per_100k'
+  | 'hiv_diagnoses_black_women'
   | 'hiv_diagnoses_pct_relative_inequity'
   | 'hiv_diagnoses_pct_share'
   | 'hiv_diagnoses_per_100k'
+  | 'hiv_diagnoses'
   | 'hiv_population_pct'
+  | 'hiv_population'
   | 'hiv_prep_coverage'
   | 'hiv_prep_pct_relative_inequity'
   | 'hiv_prep_pct_share'
   | 'hiv_prep_population_pct'
+  | 'hiv_prep_population'
+  | 'hiv_prep'
   | 'hiv_prevalence_black_women_pct_relative_inequity'
   | 'hiv_prevalence_black_women_pct_share'
   | 'hiv_prevalence_black_women_per_100k'
+  | 'hiv_prevalence_black_women'
   | 'hiv_prevalence_pct_relative_inequity'
   | 'hiv_prevalence_pct_share'
   | 'hiv_prevalence_per_100k'
   | 'hiv_prevalence_ratio_age_adjusted'
+  | 'hiv_prevalence'
   | 'hiv_stigma_index'
   | 'hiv_stigma_pct_share'
   | 'hiv_care_total_additional_gender'
@@ -77,13 +89,19 @@ export const HIV_CARE_METRICS: DataTypeConfig[] = [
     fullDisplayName: 'Linkage to HIV care',
     fullDisplayNameInline: 'linkage to HIV care',
     definition: {
-      text: `Individuals ages 13+ with linkage to HIV care in a particular year.`,
+      text: `Individuals ages 13+ with linkage to HIV care in a particular year. The per 100k rate is the number with linkage to HIV care per 100,000 HIV diagnoses in a particular year.`,
     },
     description: {
       text: 'Access to quality HIV care is essential for ensuring that people living with HIV can live long and healthy lives. However, not everyone with HIV has access to quality care. Studying HIV care in regard to health equity can help us to understand why these disparities exist and how to improve access to quality care for all people living with HIV.',
     },
     dataTableTitle: 'Breakdown summary for linkage to HIV care',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'hiv_care_population',
+        shortLabel: 'Total Population (Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total linkage to HIV care',
         metricId: 'hiv_care_pct_share',
@@ -108,6 +126,18 @@ export const HIV_CARE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'Linkage to HIV care',
         shortLabel: '% linkage',
         type: 'pct_rate',
+        rateNumeratorMetric: {
+            metricId: 'hiv_care',
+            shortLabel: 'Individuals with linkage to HIV care',
+            chartTitle: '',
+            type: 'count',
+          },
+          rateDenominatorMetric: {
+            metricId: 'hiv_care_population',
+            shortLabel: 'Total HIV diagnoses',
+            chartTitle: '',
+            type: 'count',
+          },
       },
       pct_relative_inequity: {
         chartTitle: 'Historical relative inequity in linkage to HIV care',
@@ -134,6 +164,12 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV prevalence',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'hiv_population',
+        shortLabel: 'Total Population (Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total HIV prevalence',
         metricId: 'hiv_prevalence_pct_share',
@@ -156,6 +192,18 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'HIV prevalence per 100k people',
         shortLabel: 'HIV prevalence per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_prevalence',
+          shortLabel: 'Individuals living with HIV',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'hiv_population',
+          shortLabel: 'Total population',
+          chartTitle: '',
+          type: 'count',
+        },
       },
       pct_relative_inequity: {
         chartTitle: 'Historical relative inequity for HIV prevalence',
@@ -180,6 +228,12 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV diagnoses',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'hiv_population',
+        shortLabel: 'Total Population (Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total HIV diagnoses',
         metricId: 'hiv_diagnoses_pct_share',
@@ -202,6 +256,18 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'HIV diagnoses per 100k people',
         shortLabel: 'HIV diagnoses per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_diagnoses',
+          shortLabel: 'HIV diagnoses',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'hiv_population',
+          shortLabel: 'Total population',
+          chartTitle: '',
+          type: 'count',
+        }
       },
       pct_relative_inequity: {
         chartTitle: 'Historical relative inequity for new HIV diagnoses',
@@ -225,6 +291,12 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV deaths',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'hiv_population',
+        shortLabel: 'Total Population (Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total HIV deaths',
         metricId: 'hiv_deaths_pct_share',
@@ -248,6 +320,19 @@ export const HIV_DISEASE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'HIV deaths per 100k people',
         shortLabel: 'deaths per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_deaths',
+          shortLabel: 'HIV deaths',
+          chartTitle: '',
+          type: 'count',
+        }
+        ,
+        rateDenominatorMetric: {
+          metricId: 'hiv_population',
+          shortLabel: 'Total population',
+          chartTitle: '',
+          type: 'count',
+        }
       },
       pct_relative_inequity: {
         chartTitle: 'Historical relative inequity for HIV deaths',
@@ -281,6 +366,7 @@ export const HIV_STIGMA_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV stigma',
     metrics: {
+
       index: {
         metricId: 'hiv_stigma_index',
         chartTitle: 'HIV stigma',
@@ -321,6 +407,12 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV prevalence for Black (NH) women',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'black_women_population_count',
+        shortLabel: 'Total Population (Black Women, Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total HIV prevalence for Black (NH) women',
         metricId: 'hiv_prevalence_black_women_pct_share',
@@ -346,6 +438,18 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
           'HIV prevalence for Black (NH) women per 100k people',
         shortLabel: 'prevalence per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_prevalence_black_women',
+          shortLabel: 'Black women living with HIV',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'black_women_population_count',
+          shortLabel: 'Total Black women',
+          chartTitle: '',
+          type: 'count',
+        },
       },
       pct_relative_inequity: {
         chartTitle:
@@ -370,6 +474,12 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
     dataTableTitle:
       'Breakdown summary for new HIV diagnoses for Black (NH) women',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'black_women_population_count',
+        shortLabel: 'Total Population (Black Women, Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total new HIV diagnoses for Black (NH) women',
         metricId: 'hiv_diagnoses_black_women_pct_share',
@@ -396,6 +506,18 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'New HIV diagnoses for Black (NH) women per 100k',
         shortLabel: 'diagnoses per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_diagnoses_black_women',
+          shortLabel: 'HIV diagnoses for Black women',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'black_women_population_count',
+          shortLabel: 'Total Black women',
+          chartTitle: '',
+          type: 'count',
+        },
       },
       pct_relative_inequity: {
         chartTitle:
@@ -419,6 +541,12 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for HIV deaths for Black (NH) women',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'black_women_population_count',
+        shortLabel: 'Total Population (Black Women, Ages 13+)',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total HIV deaths for Black (NH) Women',
         metricId: 'hiv_deaths_black_women_pct_share',
@@ -444,6 +572,18 @@ export const HIV_BW_DISEASE_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'HIV deaths for Black (NH) women per 100k people',
         shortLabel: 'deaths per 100k',
         type: 'per100k',
+        rateNumeratorMetric: {
+          metricId: 'hiv_deaths_black_women',
+          shortLabel: 'HIV deaths for Black women',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'black_women_population_count',
+          shortLabel: 'Total Black women',
+          chartTitle: '',
+          type: 'count',
+        },
       },
       pct_relative_inequity: {
         chartTitle:
@@ -471,6 +611,12 @@ export const HIV_PREP_METRICS: DataTypeConfig[] = [
     },
     dataTableTitle: 'Breakdown summary for PrEP coverage',
     metrics: {
+            sub_population_count: {
+        chartTitle: '',
+        metricId: 'hiv_prep_population',
+        shortLabel: 'Total PrEP-Eligible Population',
+        type: 'count',
+      },
       pct_share: {
         chartTitle: 'Share of total PrEP prescriptions',
         metricId: 'hiv_prep_pct_share',
@@ -495,6 +641,18 @@ export const HIV_PREP_METRICS: DataTypeConfig[] = [
         columnTitleHeader: 'PrEP coverage',
         shortLabel: '% PrEP coverage',
         type: 'pct_rate',
+        rateNumeratorMetric: {
+          metricId: 'hiv_prep',
+          shortLabel: 'PrEP prescriptions',
+          chartTitle: '',
+          type: 'count',
+        },
+        rateDenominatorMetric: {
+          metricId: 'hiv_prep_population',
+          shortLabel: 'PrEP-eligible population',
+          chartTitle: '',
+          type: 'count',
+        },
       },
       pct_relative_inequity: {
         chartTitle: 'Historical relative inequity for PrEP coverage',
