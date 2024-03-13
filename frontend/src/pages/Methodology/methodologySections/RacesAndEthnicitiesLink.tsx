d639168cc3df0410fac24b2c6c30e971511ffb93
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-13 17:12:02 -0600

    Updates Race/Ethnicity Page of New Methodology (#3002)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    closes #2874 
    
    - splits out the actual definitions array
    - splits out the sub component used to render the lists
    - updates the race definitions for accuracy and relevancy
    - uses the definitions array to dynamically generate the "on this page"
    sublinks. we should repeat this pattern across all the submenus when we
    can, so that updates to the data arrays for the page content will
    automatically stay in sync with the "on this page" content / order
    
    ## Has this been tested? How?
    
    - tests passing
    
    ## Screenshots (if appropriate)
    
    <img width="1520" alt="Screenshot 2024-03-13 at 4 56 52 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/d575ec61-d8a9-40b7-b07e-f49510451c1c">
    <img width="1520" alt="Screenshot 2024-03-13 at 4 57 08 PM"
    src="https://github.com/SatcherInstitute/health-equity-tracker/assets/41567007/04247ff0-9daa-4def-bf66-82c69ec18ca2">
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - New content or feature
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

--- frontend/src/pages/Methodology/methodologySections/RacesAndEthnicitiesLink.tsx
+++ frontend/src/pages/Methodology/methodologySections/RacesAndEthnicitiesLink.tsx
@@ -1,236 +1,12 @@
 import { Helmet } from 'react-helmet-async'
-import { GLOSSARY_LINK } from '../../../utils/internalRoutes'
 import HetNotice from '../../../styles/HetComponents/HetNotice'
+import HetTerm from '../../../styles/HetComponents/HetTerm'
+import { urlMap } from '../../../utils/externalUrls'
+import { ethnicityDefinitions, raceDefinitions, moreNonStandardDefinitions } from '../methodologyContent/RacesAndEthnicitiesDefinitions'
+import RaceEthnicityList from './RacesEthnicitiesList'
 
-interface DataItem {
-  topic: string
-  definitions: Array<{
-    key: string
-    path?: string
-    description: string
-    resource?: string
-    considerations?: Array<{
-      title: string
-      points: string[]
-    }>
-  }>
-}
 
-const raceDefinitions: DataItem[] = [
-  {
-    topic: 'Races and Ethnicities',
-    definitions: [
-      {
-        key: 'All',
-        path: '#all',
-        description: 'Any race or ethnicity, including unknown race/ethnicity.',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'Some states and datasets use unique race/ethnicity groupings that do not correspond directly with the categories used by the U.S. Census.',
-              'There are no required or standardized race and ethnicity categories for data collection across state and local jurisdictions.',
-              'Neither disability nor mental health status is collected with most data sources.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'American Indian and Alaska Native (NH)',
-        path: '#aian_nh',
-        description:
-          'A person having origins in any of the original peoples of North and South America (including Central America), who maintains tribal affiliation or community attachment, and who is not Hispanic/Latino.',
-        resource: GLOSSARY_LINK + '#aian-resources',
-        considerations: [
-          {
-            title: 'Political Determinants of Health',
-            points: [
-              'The composite race group for women in legislative office includes American Indian & Alaska Native.',
-            ],
-          },
-          {
-            title: 'General Considerations',
-            points: [
-              'For the state level, the ACS 2019 estimation is used for the American Indian and Alaska Native demographic.',
-              'Many states do not record data for the American Indian and Alaska Native racial categories, often grouping these individuals into other categories.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'Asian (NH)',
-        path: '#api_nh',
-        description:
-          'A person having origins in any of the original peoples of the Far East, Southeast Asia, or the Indian subcontinent including, for example, Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand, and Vietnam, and who is not Hispanic/Latino.',
-        resource: GLOSSARY_LINK + '#api-resources',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'For the state level, the population counts for the Asian demographic at the state level are provided by the Kaiser Family Foundation.',
-              'The Kaiser Family Foundation only collects population data for the Asian demographic, limiting their per 100k metrics at the state level.',
-            ],
-          },
-          {
-            title: 'Chronic Diseases',
-            points: [
-              'The Asian category includes cases previously classified as "Asian/Pacific Islander" under the pre-1997 Office of Management and Budget (OMB) race/ethnicity classification system when querying HIV prevalence',
-            ],
-          },
-          {
-            title: 'Political Determinants of Health',
-            points: [
-              'The composite race group for women in legislative office includes Asian & Pacific Islander.',
-              'The Center for American Women in Politics (CAWP) dataset classifies Asian American & Pacific Islander (Women) differently from U.S. Census categories.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'Black or African American (NH)',
-        path: '#black_nh',
-        description:
-          'A person having origins in any of the Black racial groups of Africa, and who is not Hispanic/Latino.',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'The Kaiser Family Foundation provides population estimates for the Black demographic at the state level.',
-            ],
-          },
-          {
-            title: 'COVID-19',
-            points: [
-              `‘Percent of vaccinated’ metrics for Black women are sourced from the ACS 5-year estimates.`,
-            ],
-          },
-          {
-            title: 'Political Determinants of Health',
-            points: [
-              'Historical intersectional representation data, such as the percent of U.S. Congress members identified as black women since 1915, is sourced from the open-source @unitedstates project.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'Hispanic/Latino',
-        path: '#hispanic',
-        description: 'Any race(s), Hispanic/Latino.',
-        resource: GLOSSARY_LINK + '#hisp-resources',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'Generally, all race/ethnicities on the tracker include Hispanic/Latino unless otherwise specified.',
-              'Individuals who identify as Hispanic/Latino may not be recorded in their respective race category in certain datasets.',
-            ],
-          },
-          {
-            title: 'HIV',
-            points: [
-              'The race and ethnicity of individuals prescribed PrEP includes the Hispanic/Latino category.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'Middle Eastern / North African (MENA)',
-        path: '#mena',
-        description:
-          'Race/ethnicity grouping collected by CAWP but not currently collected by the U.S. Census.',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'The U.S. Census currently does not collect population data for the MENA demographic.',
-              'MENA individuals are counted by the ACS as White.',
-            ],
-          },
-          {
-            title: 'Political Determinants of Health',
-            points: [
-              'The Center for American Women in Politics (CAWP) dataset classifies Middle Eastern & North African (Women) differently from U.S. Census categories.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'Native Hawaiian or Other Pacific Islander (NH)',
-        path: '#nhpi_nh',
-        description:
-          'A person having origins in any of the original peoples of Hawaii, Guam, Samoa, or other Pacific Islands and who is not Hispanic/Latino.',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'For the state level, the ACS 2019 estimation is used for the Native Hawaiian and Pacific Islander demographic.',
-              `‘Percent of vaccinated’ metrics for Native Hawaiian and Pacific Islander are sourced from the ACS 5-year estimates.`,
-              'Many states do not record data for the Native Hawaiian and Pacific Islander racial categories, often grouping these individuals into other categories.',
-            ],
-          },
-        ],
-      },
-      {
-        key: 'NH',
-        path: '#nh',
-        description: `Not Hispanic/Latino. To promote inclusion, we replace the source data labels ‘Multiracial’ with ‘Two or more races’, and ‘Some other’ with ‘Unrepresented’.`,
-      },
-      {
-        key: 'Unrepresented race (NH)',
-        path: '#other_nonstandard_nh',
-        description:
-          'A single race not tabulated by the CDC, not of Hispanic/Latino ethnicity. Individuals not identifying as one of the distinct races listed in the source data, or multiracial individuals, are grouped together as “Some other race”. This is a problem as it obscures racial identity for many individuals. In our effort to take transformative action towards achieving health equity the Satcher Health Leadership Institute has decided to rename this category to highlight it as a health equity issue. For PrEP coverage, Unrepresented race (NH) is used to recognize individuals who do not identify as part of the Black, White, or Hispanic ethnic or racial groups.',
-      },
-      {
-        key: 'Two or more races (NH)',
-        path: '#multi_or_other_standard',
-        description:
-          'Combinations of two or more of the following race categories: "White," "Black or African American," American Indian or Alaska Native," "Asian," Native Hawaiian or Other Pacific Islander," or "Some Other Race", and who are not Hispanic/Latino.',
-      },
-      {
-        key: 'Two or more races & Unrepresented race (NH)',
-        path: '#multi_or_other_standard_nh',
-        description:
-          'People who are either multiple races or a single race not represented by the data source’s categorization, and who are not Hispanic/Latino.',
-        considerations: [
-          {
-            title: 'General Considerations',
-            points: [
-              'The presence of "Other" within a dataset can change the classification of certain individuals.',
-              `Missing data: There isn't enough data to accurately calculate subpopulation rates by age, sex, and race/ethnicity for the Census Island Areas (US territories other than Puerto Rico).`,
-            ],
-          },
-          {
-            title: 'HIV',
-            points: [
-              `The race and ethnicity of individuals prescribed PrEP includes the "Other" category.`,
-            ],
-          },
-        ],
-      },
-      {
-        key: 'White (NH)',
-        path: '#white',
-        description:
-          'A person having origins in any of the original peoples of Europe, the Middle East, or North Africa, and who is not Hispanic/Latino.',
-        considerations: [
-          {
-            title: 'Chronic Diseases',
-            points: [
-              'For the state level, the population counts for the Asian demographic at the state level are provided by the Kaiser Family Foundation.',
-              'The Kaiser Family Foundation only collects population data for the Asian demographic, limiting their per 100k metrics at the state level.',
-              'The Asian category includes cases previously classified as "Asian/Pacific Islander" under the pre-1997 Office of Management and Budget (OMB) race/ethnicity classification system when querying HIV prevalence.',
-              'The composite race group for women in legislative office includes Asian & Pacific Islander.',
-              'The Center for American Women in Politics (CAWP) dataset classifies Asian American & Pacific Islander (Women) differently from U.S. Census categories.',
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]
-
-const RacesAndEthnicitiesLink = () => {
+export default function RacesAndEthnicitiesLink() {
   return (
     <section id='#races-and-ethnicities'>
       <article>
@@ -259,84 +35,38 @@ const RacesAndEthnicitiesLink = () => {
           Our ongoing efforts aim to bridge these data gaps, ensuring more
           informed and effective health policies across the United States.
         </p>
-        <p>
-          The combined race/ethnicity groups shown on the tracker can be hard to
-          understand, partially due to non-standard race/ethnicity breakdowns
-          across data sources.
-        </p>
+
         <HetNotice
           className='mt-10'
           title='Dataset Definitions and Contextual Variances'
         >
+          <p>All reports on the site reflect the <HetTerm>race</HetTerm> and <HetTerm>ethnicity</HetTerm> categorization used by their respective data sources. In some cases we have relabelled or combined groups to promote more inclusive language and allow comparisons. The majority of these sources adhere to the U.S. Census groupings, which record race as one of five options, and ethnicity as either having or not having Hispanic origin. Due to the inherent complexity of visualizing these related but distinct demographic dimensions, the sources ultimately combine the race and ethnicity groups in one of two ways:</p>
+          <ol>
+            <li>Each <HetTerm>race</HetTerm> group explicitly excludes people of Hispanic origin, and another group <HetTerm>Hispanic or Latino</HetTerm> is added that combines all people of Hispanic origin, regardless of their race.</li>
+            <li>Each <HetTerm>race</HetTerm> group is based solely on the reported race, regardless of Hispanic origin. In these cases, the additional <HetTerm>Hispanic or Latino</HetTerm> group is still added combining all people of Hispanic origin, regardless of their race, however the groups are no longer mutually exclusive and some people (e.g. those identifying as both Black and Hispanic) will be represented in multiple groups. This also means percentages of the total population will no longer sum to 100%, so provide alerting to the user that care should be used in analysis.  </li>
+          </ol>
+
           <p>
-            Understanding race and ethnicity classifications within our tracker
-            requires a nuanced approach. Generally, we include Hispanic/Latino
-            in all race/ethnicity categories, unless stated otherwise.
-          </p>
-          <p>
-            However, it's crucial to recognize that the precise definition of a
+            It's crucial to recognize that the precise definition of a
             race or ethnicity is{' '}
             <strong>
               intrinsically tied to the context of the specific dataset
             </strong>{' '}
-            from which it originates.
+            from which it originates. For instance, the inclusion of an <HetTerm>Other</HetTerm> category can influence how
+            individuals are categorized, potentially affecting distinctions like
+            {' '}<HetTerm>Asian</HetTerm> vs. <HetTerm>Other</HetTerm>.
           </p>
           <p>
-            For instance, the inclusion of an "Other" category can influence how
-            individuals are categorized, potentially affecting distinctions like
-            "Asian" vs. "Other".
+            Per the <a href={urlMap.censusRaceEthnicity}>U.S. Census FAQ</a>: “The race and ethnicity categories generally reflect social definitions in the U.S. and are not an attempt to define race and ethnicity biologically, anthropologically, or genetically. We recognize that the race and ethnicity categories include racial, ethnic, and national origins and sociocultural groups.”
           </p>
         </HetNotice>
-        {raceDefinitions.map((item) => {
-          return (
-            <div id={item.topic} key={item.topic}>
-              {item.definitions.map((def) => {
-                return (
-                  <div
-                    className='mt-0 flex flex-col items-start'
-                    id={def.path}
-                    key={def.key}
-                  >
-                    <h3 className='mt-12 font-sansTitle text-title font-medium'>
-                      {def.key}
-                    </h3>
-                    <article className='mt-0 flex flex-col items-start gap-3 pl-6'>
-                      <p className='text-smallest font-semibold'>Definition</p>
-                      <p className='m-0 italic text-altBlack'>
-                        {def.description}
-                      </p>
-                      {def.considerations && def.considerations.length > 0 && (
-                        <div>
-                          {def.considerations.map((consideration) => (
-                            <div key={consideration.title}>
-                              <p className='text-smallest font-semibold'>
-                                {consideration.title}
-                              </p>
-                              {consideration.points.map((point, idx) => (
-                                <p key={idx}>{point}</p>
-                              ))}
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                      {def.resource && (
-                        <a
-                          href={def.resource}
-                          className='font-sansTitle  font-medium leading-lhNormal text-altGreen no-underline'
-                        >
-                          Explore {def.key} resources →
-                        </a>
-                      )}
-                    </article>
-                  </div>
-                )
-              })}
-            </div>
-          )
-        })}
+
+        <RaceEthnicityList dataItem={ethnicityDefinitions} />
+        <RaceEthnicityList dataItem={raceDefinitions} />
+        <RaceEthnicityList dataItem={moreNonStandardDefinitions} />
+
+
       </article>
     </section>
   )
 }
-
-export default RacesAndEthnicitiesLink
---
+++ frontend/src/pages/Methodology/methodologySections/RacesEthnicitiesList.tsx
@@ -0,0 +1,66 @@
+import HetDivider from "../../../styles/HetComponents/HetDivider"
+import type { DataItem } from "../methodologyContent/RacesAndEthnicitiesDefinitions"
+
+
+interface RaceEthnicityListProps {
+  dataItem: DataItem
+}
+export default function RaceEthnicityList(props: RaceEthnicityListProps) {
+  return (
+    <>
+      <h3 className='mt-16 mb-0 font-sansTitle text-center text-exploreButton font-semibold'>
+        {props.dataItem.topic}
+      </h3>
+
+
+      <div id={props.dataItem.topic} key={props.dataItem.topic}>
+        {props.dataItem.definitions.map((def) => {
+          return (
+            <div
+              className='mt-0 mb-4 flex flex-col items-start'
+              id={def.path}
+              key={def.key}
+            >
+              <h4 className='mt-8 mb-4 font-sansTitle text-text font-medium'>
+                {def.key}
+              </h4>
+              <article className='mt-0 flex flex-col items-start pl-6'>
+                <p className='text-smallest font-semibold mb-1'>Definition</p>
+                <p className='m-0 italic text-altBlack text-small'>
+                  {def.description}
+                </p>
+                {def.considerations && def.considerations.length > 0 && (
+                  <div>
+                    {def.considerations.map((consideration) => (
+                      <div key={consideration.title}>
+                        <p className='text-smallest font-semibold mb-1'>
+                          {consideration.title}
+                        </p>
+                        {consideration.points.map((point, idx) => (
+                          <p className='text-small my-2' key={idx}>{point}</p>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                )}
+                {def.resource && (
+                  <a
+                    href={def.resource}
+                    className='font-sansTitle mt-3 font-medium leading-lhNormal text-altGreen no-underline'
+                  >
+                    Explore {def.key} resources →
+                  </a>
+                )}
+              </article>
+            </div>
+          )
+        })}
+      </div>
+
+    </>
+  )
+}
+
+
+
+
