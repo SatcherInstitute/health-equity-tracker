9904604802f05bee229d0c71c11cc3be51472b6f
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-14 17:33:34 -0600

    Conditionally create composer resource within terraform (#3012)

--- config/composer.tf
+++ config/composer.tf
@@ -1,4 +1,9 @@
+variable "include_composer" {
+  default = true
+}
+
 resource "google_composer_environment" "composer-env" {
+  count  = var.include_composer ? 1 : 0
   name   = "data-ingestion-environment"
   region = var.compute_region
 }
