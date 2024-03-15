b9804d8b193544206e1305fb6de11fee15e50382
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-15 14:21:44 -0600

    keep consistent bucket for composer

--- config/composer.tf
+++ config/composer.tf
@@ -5,4 +5,8 @@ resource "google_composer_environment" "composer-env" {
   count  = var.include_composer ? 1 : 0
   name   = "data-ingestion-environment"
   region = var.compute_region
+
+  storage_config {
+    bucket = "us-central1-data-ingestion-bucket"
+  }
 }
