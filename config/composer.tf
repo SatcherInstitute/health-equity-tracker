73e1d777ed3531fe9edde9441edca6f5e131bd07
Ben Hammond <benjamin.hammond@gmail.com>
2024-03-14 18:25:49 -0600

    Terraform logic fix (#3013)

--- config/composer.tf
+++ config/composer.tf
@@ -1,7 +1,6 @@
 variable "include_composer" {
   default = true
 }
-
 resource "google_composer_environment" "composer-env" {
   count  = var.include_composer ? 1 : 0
   name   = "data-ingestion-environment"
