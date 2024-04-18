855c934e17226de5b1bf7a684d2b62648bc3f5cf
Ben Hammond <benjamin.hammond@gmail.com>
2024-04-18 16:46:16 -0600

    handles loading processing of national data

--- python/tests/datasources/test_maternal_mortality.py
+++ python/tests/datasources/test_maternal_mortality.py
@@ -9,15 +9,17 @@ GOLDEN_DIR = os.path.join(TEST_DIR, 'golden_data')
 
 
 # RUN THIS TO LOAD FAKE TEST DATA INSTEAD OF THE REAL /data
-def get_test_data_as_df():
-    df = pd.read_csv(os.path.join(TEST_DIR, 'IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV'))
+def get_test_data_as_df(*args):
+    print("Reading test input data rather than actual /data dir")
+    print(args[1])
+    df = pd.read_csv(os.path.join(TEST_DIR, args[1]))
     return df
 
 
 # READ IN FAKE TEST DATA INSTEAD OF REAL /data
 @mock.patch(
     'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
-    return_value=get_test_data_as_df(),
+    side_effect=get_test_data_as_df,
 )
 @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
 def testWriteToBq(
@@ -34,5 +36,12 @@ def testWriteToBq(
 
     datasource.write_to_bq('dataset', 'gcs_bucket', **kwargs)
 
-    # ASSERT THAT THE MOCKED READ CSV FUNCTION WAS CALLED ONCE
-    assert mock_csv.call_count == 1
+    # ASSERT THAT THE MOCKED READ CSV FUNCTION WAS CALLED FOR NATIONAL AND STATE DATA
+    assert mock_csv.call_count == 2
+
+    for call in mock_bq.call_args_list:
+        print("**")
+        # print(call[0])
+        df, _, table_name = call[0]
+        print(table_name)
+        print(df)
