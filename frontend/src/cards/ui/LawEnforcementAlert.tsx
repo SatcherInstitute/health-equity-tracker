ce18bab2e9867495c79f89ea68524bc36ffafa1d
eriwarr <eric.m.warren1@gmail.com>
2024-04-18 16:46:16 -0400

    making suggested chagnes

--- frontend/src/cards/ui/LawEnforcementAlert.tsx
+++ frontend/src/cards/ui/LawEnforcementAlert.tsx
@@ -14,36 +14,40 @@ interface LawEnforcementAlertProps {
 
 function LawEnforcementAlert(props: LawEnforcementAlertProps) {
     const dataAlls: Row[] = props.queryResponse.data.filter(
-        (row) => row[props.demographicType] === ALL
+        (row) => row[props.demographicType] === ALL || row[props.demographicType] === 'All'
     )
 
+    console.log({dataAlls})
+    
     const dataBlack: Row[] = props.queryResponse.data.filter(
         (row) => row[props.demographicType] === BLACK_NH
     )
 
     const totalCountId: MetricId = 'gun_violence_legal_intervention_estimated_total'
 
-    const totalCount: number | undefined = dataAlls.length > 0 ? dataAlls[0][totalCountId] : undefined
-    const blackCount: number | undefined = dataBlack.length > 0 ? dataBlack[0][totalCountId] : undefined
+    const totalCount: number | undefined = dataAlls[0]?.[totalCountId]
+    const blackCount: number | undefined = dataBlack[0]?.[totalCountId]
 
     const locationName = props.fips.getSentenceDisplayName()
 
     if (totalCount === undefined) {
         return null
     }
+    
     return (
         <div>
             <HetNotice title="Law Enforcement Impact" kind="health-crisis" className="m-2 border border-reportAlert">
                 <p>
                     In {locationName}, law enforcement actions resulted in <strong>{totalCount}</strong> fatalities last year.
                     {blackCount !== undefined && (
-                        <span> With Black individuals disproportionately affected, accounting for <strong>{blackCount}</strong> of these lives.</span>
+                        <> Black individuals, who are disproportionately affected nationally, accounted for <strong>{blackCount}</strong> of these legal intervention gun fatalities.</>
                     )}
-                    This issue highlights the need for comprehensive reforms and accountability to ensure a justice system that equitably protects every community.
+                    {" "}This issue highlights the need for comprehensive reforms and accountability to ensure a justice system that equitably protects every community.
                 </p>
             </HetNotice>
         </div>
-    )
+    );
+    
 }
 
 export default LawEnforcementAlert
