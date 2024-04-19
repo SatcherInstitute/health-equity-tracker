a291a677d26b17025f2b56858de829ae6e0aa3d2
Ben Hammond <benjamin.hammond@gmail.com>
2024-04-19 15:49:59 -0600

    cleanup; load national rates from within source table

--- python/datasources/maternal_mortality.py
+++ python/datasources/maternal_mortality.py
@@ -2,7 +2,8 @@ from ingestion import gcs_to_bq_util
 from datasources.data_source import DataSource
 import ingestion.standardized_columns as std_col
 from ingestion.merge_utils import merge_state_ids, merge_pop_numbers
-from ingestion.constants import NATIONAL_LEVEL, STATE_LEVEL
+from ingestion.constants import NATIONAL_LEVEL, STATE_LEVEL, US_NAME
+import pandas as pd
 
 RACE_GROUPS_TO_STANDARD = {
     'Non-Hispanic American Indian and Alaska Native': std_col.Race.AIAN_NH.value,
@@ -16,8 +17,12 @@ RACE_GROUPS_TO_STANDARD = {
 # ZIP FILE CONTAINING STATE-LEVEL CSV FOR /data
 # https://ghdx.healthdata.org/record/ihme-data/united-states-maternal-mortality-by-state-race-ethnicity-1999-2019
 
-# IMAGE CONTAINING NATIONAL-LEVEL TABLE FOR /data (manually created csv from this image)
-# https://cdn.jamanetwork.com/ama/content_public/journal/jama/939176/joi230063t1_1687532958.71302.png?Expires=1716416583&Signature=VACaqnLwid41ua7XRHx~imHeYBiobUtXwsEG~A1z4pWqI7J-wsRbo73BmqIs41H4GOGRHyWjTnmdwtEFdHe5zPkY6Er3XKzciV7jTc0m5kX5PXduJvKQ~-KOVArVbP1IhTvtWz3mK6P0thi6hEtEjx2iht0nMM95hDPCIU3VsOrhwmbx-mLt4465DFsMnE2lj3JsFEs3L~cKRx7Gd5SjYCktNP~GPRlNbOAGiEW~jgRv2Sz2MyEhm513owc0HMgODyks3OWA2YfZLGJ7E4ZFqag3Vnup4GN77lAEkF1-~h5oKIVvCImqpU3XxM9g6Mxjf8sTkgeILIh7mGGQ7-Ps8Q__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA
+COLS_TO_STANDARD = {
+    'val': std_col.MM_PER_100K,
+    'race_group': std_col.RACE_CATEGORY_ID_COL,
+    'location_name': std_col.STATE_NAME_COL,
+    'year_id': std_col.TIME_PERIOD_COL,
+}
 
 
 class MaternalMortalityData(DataSource):
@@ -34,28 +39,21 @@ class MaternalMortalityData(DataSource):
 
     def write_to_bq(self, dataset, gcs_bucket, **attrs):
 
+        # load source data once
+        source_df = preprocess_source_data()
+
         for geo_level in [STATE_LEVEL, NATIONAL_LEVEL]:
 
-            df = load_source_data(geo_level)
-
-            df = df.rename(
-                columns={
-                    'val': 'maternal_mortality_per_100k',
-                    'race_group': std_col.RACE_CATEGORY_ID_COL,
-                    'location_name': std_col.STATE_NAME_COL,
-                    'year_id': std_col.TIME_PERIOD_COL,
-                }
+            # filter source data rows for states or national
+            df = source_df.copy()
+            df = (
+                df[df[std_col.STATE_NAME_COL] != US_NAME]
+                if geo_level == STATE_LEVEL
+                else df[df[std_col.STATE_NAME_COL] == US_NAME]
             )
 
-            # round to whole numbers
-            df['maternal_mortality_per_100k'] = df['maternal_mortality_per_100k'].round(0)
-
-            df = df.replace(RACE_GROUPS_TO_STANDARD)
-            std_col.add_race_columns_from_category_id(df)
             df = merge_state_ids(df)
-            df = merge_pop_numbers(df, "race", geo_level)
-
-            df[std_col.TIME_PERIOD_COL] = df[std_col.TIME_PERIOD_COL].astype(str)
+            df = merge_pop_numbers(df, std_col.RACE_COL, geo_level)
 
             keep_string_cols = [
                 std_col.TIME_PERIOD_COL,
@@ -66,12 +64,9 @@ class MaternalMortalityData(DataSource):
             ]
 
             keep_number_cols = [
-                'maternal_mortality_per_100k',
+                std_col.MM_PER_100K,
                 std_col.POPULATION_PCT_COL,
             ]
-
-            if geo_level == NATIONAL_LEVEL:
-                keep_number_cols.extend(['maternal_deaths_estimated_total', 'live_births_estimated_total'])
 
             df = df[keep_string_cols + keep_number_cols]
             # get list of all columns expected to contain numbers
@@ -80,17 +75,24 @@ class MaternalMortalityData(DataSource):
             gcs_to_bq_util.add_df_to_bq(df, dataset, table_name, column_types=col_types)
 
 
-def load_source_data(geo_level):
-
-    file_name = (
-        'IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV'
-        if geo_level == STATE_LEVEL
-        else 'manual_national_table.csv'
-    )
-
-    df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
+def preprocess_source_data() -> pd.DataFrame:
+    """Load and preprocess source data.
+    Returns:
+        pandas.DataFrame: preprocessed source data including state and national rows
+    """
+    source_df = gcs_to_bq_util.load_csv_as_df_from_data_dir(
         'maternal_mortality',
-        file_name,
+        'IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV',
+        dtype={'year_id': str},
+        usecols=COLS_TO_STANDARD.keys(),
     )
 
-    return df
+    source_df = source_df.rename(columns=COLS_TO_STANDARD)
+    source_df = source_df.replace({'National': US_NAME})
+    source_df = source_df.replace(RACE_GROUPS_TO_STANDARD)
+    std_col.add_race_columns_from_category_id(source_df)
+
+    # round rate to whole numbers
+    source_df[std_col.MM_PER_100K] = source_df[std_col.MM_PER_100K].round(0)
+
+    return source_df
