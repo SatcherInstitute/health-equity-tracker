04a8f74175d16e3998b740989f8f5abf3c477407
Ben Hammond <benjamin.hammond@gmail.com>
2024-04-24 14:20:40 -0600

    update dag for age

--- airflow/dags/cdc_wisqars_black_men.py
+++ airflow/dags/cdc_wisqars_black_men.py
@@ -42,6 +42,32 @@ cdc_wisqars_black_men_bq_operator_urbanicity_state = util.create_bq_ingest_opera
     data_ingestion_dag,
 )
 
+# AGE NATIONAL
+cdc_wisqars_black_men_bq_payload_age_national = util.generate_bq_payload(
+    _CDC_WISQARS_BLACK_MEN_WORKFLOW_ID,
+    _CDC_WISQARS_BLACK_MEN_DATASET_NAME,
+    demographic='age',
+    geographic='national',
+)
+cdc_wisqars_black_men_bq_operator_age_national = util.create_bq_ingest_operator(
+    'cdc_wisqars_black_men_to_bq_age_national',
+    cdc_wisqars_black_men_bq_payload_age_national,
+    data_ingestion_dag,
+)
+
+# AGE STATE
+cdc_wisqars_black_men_bq_payload_age_state = util.generate_bq_payload(
+    _CDC_WISQARS_BLACK_MEN_WORKFLOW_ID,
+    _CDC_WISQARS_BLACK_MEN_DATASET_NAME,
+    demographic='age',
+    geographic='state',
+)
+cdc_wisqars_black_men_bq_operator_age_state = util.create_bq_ingest_operator(
+    'cdc_wisqars_black_men_to_bq_age_state',
+    cdc_wisqars_black_men_bq_payload_age_state,
+    data_ingestion_dag,
+)
+
 # Exporters
 payload_urbanicity = {
     'dataset_name': _CDC_WISQARS_BLACK_MEN_DATASET_NAME,
@@ -51,9 +77,20 @@ cdc_wisqars_black_men_exporter_operator_urbanicity = util.create_exporter_operat
     'cdc_wisqars_black_men_exporter_urbanicity', payload_urbanicity, data_ingestion_dag
 )
 
+payload_age = {
+    'dataset_name': _CDC_WISQARS_BLACK_MEN_DATASET_NAME,
+    'demographic': "age",
+}
+cdc_wisqars_black_men_exporter_operator_age = util.create_exporter_operator(
+    'cdc_wisqars_black_men_exporter_age', payload_age, data_ingestion_dag
+)
+
 # Ingestion DAG
 (
     cdc_wisqars_black_men_bq_operator_urbanicity_national
     >> cdc_wisqars_black_men_bq_operator_urbanicity_state
+    >> cdc_wisqars_black_men_bq_operator_age_national
+    >> cdc_wisqars_black_men_bq_operator_age_state
     >> cdc_wisqars_black_men_exporter_operator_urbanicity
+    >> cdc_wisqars_black_men_exporter_operator_age
 )
