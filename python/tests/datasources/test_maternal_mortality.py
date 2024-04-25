d1b0b089708d622c2037c6637cfe92789b63a5bd
Ali <anix1@kent.edu>
2024-04-25 16:36:08 -0600

    cleans up test file

--- python/tests/datasources/test_maternal_mortality.py
+++ python/tests/datasources/test_maternal_mortality.py
@@ -24,19 +24,13 @@ GOLDEN_DATA_RACE_NATIONAL_CURRENT = os.path.join(
     GOLDEN_DIR, 'maternal_mortality_output_race_and_ethnicity_national_current.csv'
 )
 
-# NATIONAL, STATE, CURRENT, HISTORICAL
 
-
-# RUN THIS TO LOAD FAKE TEST DATA INSTEAD OF THE REAL /data
 def get_test_data_as_df(*args, **kwargs):
-    print("Reading test input data rather than actual /data dir")
-    print(args[1])
     print(kwargs)
     df = pd.read_csv(os.path.join(TEST_DIR, args[1]))
     return df
 
 
-# READ IN FAKE TEST DATA INSTEAD OF REAL /data
 @mock.patch(
     'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
     side_effect=get_test_data_as_df,
@@ -56,45 +50,28 @@ def testWriteToBq(
 
     datasource.write_to_bq('dataset', 'gcs_bucket', **kwargs)
 
-    # ASSERT THAT THE MOCKED READ CSV FUNCTION WAS CALLED FOR THE SOURCE RATES AND THE SOURCE COUNTS TABLES
     assert mock_csv.call_count == 2
 
-    # STATE + HISTORICAL
     df_state_historical, _, table_name = mock_bq.call_args_list[0][0]
     assert table_name == 'by_race_state_historical'
-    print(df_state_historical.dtypes)
-    # TODO: assert that the df_state is the same as the golden data file loaded via pd.read_csv
 
     expected_state_historical_df = pd.read_csv(GOLDEN_DATA_RACE_STATE_HISTORICAL, dtype={'state_fips': str})
-    print(expected_state_historical_df.dtypes)
     assert_frame_equal(df_state_historical, expected_state_historical_df, check_like=True, check_dtype=False)
 
-    # # STATE + CURRENT
     df_state_current, _, table_name = mock_bq.call_args_list[1][0]
     assert table_name == 'by_race_state_current'
-    print(df_state_current.dtypes)
-    # # TODO: assert that the df_state is the same as the golden data file loaded via pd.read_csv
 
     expected_state_current_df = pd.read_csv(GOLDEN_DATA_RACE_STATE_CURRENT, dtype={'state_fips': str})
-    print(expected_state_current_df.dtypes)
     assert_frame_equal(df_state_current, expected_state_current_df, check_like=True, check_dtype=False)
 
-    # # NATIONAL + HISTORICAL
     df_national_historical, _, table_name = mock_bq.call_args_list[2][0]
     assert table_name == 'by_race_national_historical'
-    print(df_national_historical.dtypes)
-    # # TODO: assert that the df_national is the same as the golden data file loaded via pd.read_csv
 
     expected_national_historical_df = pd.read_csv(GOLDEN_DATA_RACE_NATIONAL_HISTORICAL, dtype={'state_fips': str})
-    print(expected_national_historical_df.dtypes)
     assert_frame_equal(df_national_historical, expected_national_historical_df, check_like=True, check_dtype=False)
 
-    # # NATIONAL + CURRENT
     df_national_current, _, table_name = mock_bq.call_args_list[3][0]
     assert table_name == 'by_race_national_current'
-    print(df_national_current.dtypes)
-    # # TODO: assert that the df_national is the same as the golden data file loaded via pd.read_csv
 
     expected_national_current_df = pd.read_csv(GOLDEN_DATA_RACE_NATIONAL_CURRENT, dtype={'state_fips': str})
-    print(expected_national_current_df.dtypes)
     assert_frame_equal(df_national_current, expected_national_current_df, check_like=True, check_dtype=False)
