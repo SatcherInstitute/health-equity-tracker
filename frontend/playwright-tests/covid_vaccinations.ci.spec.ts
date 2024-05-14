b073d2c9029cca324212f90e75da90be6ff1de54
Ben Hammond <benjamin.hammond@gmail.com>
2024-05-14 10:00:24 -0600

    Removes `react-cookie`; streamlines E2E tests (#3289)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - closes #3285 
    - removes some duplicated e2e testing
    - moves some less essential tests to "nightly" testing only
    
    ## Has this been tested? How?
    
    e2e tests passing
    
    ## Types of changes
    
    (leave all that apply)
    
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

--- frontend/playwright-tests/covid_vaccinations.ci.spec.ts
+++ frontend/playwright-tests/covid_vaccinations.ci.spec.ts
@@ -5,23 +5,6 @@ test.describe.configure({ mode: 'parallel' });
 
 test.describe('Home to COVID Vax by Age', () => {
 
-    test('Home to Tracker', async ({ page }) => {
-
-        // Landing Page Loads
-        await page.goto('/', { waitUntil: "commit" });
-        await expect(page.locator('#main')).toContainText('Advancing Health Justice');
-
-        const accessibilityScanResults = await new AxeBuilder({ page })
-            .exclude('iframe') // YouTube embed is not fully accessible
-            .analyze();
-        expect(accessibilityScanResults.violations).toEqual([]);
-
-        // Clicking large CTA button takes us to the tracker
-        const exploreButton = page.locator('#landingPageCTA')
-        exploreButton.click();
-        await expect(page).toHaveURL(/.*exploredata/);
-
-    })
 
     test('Tracker Default to Covid Vax', async ({ page }) => {
 
@@ -48,22 +31,6 @@ test.describe('Home to COVID Vax by Age', () => {
         expect(accessibilityScanResults.violations).toEqual([]);
 
     })
-
-    test('Covid Vax Toggle Age', async ({ page }) => {
-
-        // Starting with COVID VAX
-        await page.goto(`/exploredata?mls=1.covid_vaccinations-3.00`, { waitUntil: "commit" });
-
-        // Changing demographic dropdown setting to AGE should change URL
-        await page.getByRole('combobox', { name: 'Demographic Race/ethnicity' }).click();
-        await page.getByRole('option', { name: 'Age' }).click();
-        await expect(page).toHaveURL(/.*demo=age/);
-
-        // back button works properly for demographic toggle changes
-        await page.goBack()
-        await expect(page).not.toHaveURL(/.*demo=age/);
-
-    });
 
 
 });
