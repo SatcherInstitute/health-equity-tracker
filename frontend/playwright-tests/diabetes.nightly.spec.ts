b026be82b772be2ce6178469bc6b812cf38a0fd5
Ben Hammond <benjamin.hammond@gmail.com>
2024-05-28 11:53:21 -0600

    e2e

--- frontend/playwright-tests/diabetes.nightly.spec.ts
+++ frontend/playwright-tests/diabetes.nightly.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test'
 
-test('Diabetes', async ({ page }) => {
+test('Diabetes USA', async ({ page }) => {
   await page.goto('/exploredata?mls=1.diabetes-3.00&group1=All')
   await page.getByText('Race and Ethnicity:').click()
   await page.locator('.MuiBackdrop-root').click()
@@ -31,3 +31,20 @@ test('Diabetes', async ({ page }) => {
     .click()
   await page.getByText('Do you have information that').click()
 })
+
+test('Diabetes County', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.diabetes-3.08031&group1=All');
+  await page.locator('#rate-map').getByRole('heading', { name: 'Diabetes in Denver County,' }).click();
+  await page.locator('#rate-map').getByRole('heading', { name: 'Ages 18+' }).click();
+  await page.getByLabel('Legend for rate map').getByRole('img').click();
+  await page.locator('li').filter({ hasText: 'Denver County' }).click();
+  await page.getByRole('heading', { name: 'Graph unavailable: Data in' }).click();
+  await page.locator('#rate-chart').getByRole('heading', { name: 'Diabetes in Denver County,' }).click();
+  await page.locator('#rate-chart').getByRole('heading', { name: 'Ages 18+' }).click();
+  await page.locator('#rate-chart').getByText('Sources: County Health').click();
+  await page.getByRole('combobox', { name: 'Demographic Race/ethnicity' }).click();
+  await page.locator('#menu- div').first().click();
+  await page.getByRole('combobox', { name: 'Demographic Race/ethnicity' }).click();
+  await page.getByLabel('Age unavailable at the county').click();
+  await page.getByLabel('Sex unavailable at the county').click();
+});
