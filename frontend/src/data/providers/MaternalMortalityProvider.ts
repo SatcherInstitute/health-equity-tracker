171469e7c9a41c342fe1fc1defb49ba21fa44ec9
Ali <anix1@kent.edu>
2024-06-07 14:16:42 -0600

    removed DataFrame import from MaternamMortalityProvider.ts

--- frontend/src/data/providers/MaternalMortalityProvider.ts
+++ frontend/src/data/providers/MaternalMortalityProvider.ts
@@ -4,63 +4,62 @@ import { type MetricQuery, MetricQueryResponse } from '../query/MetricQuery';
 import VariableProvider from './VariableProvider';
 import { appendFipsIfNeeded } from '../utils/datasetutils';
 import { type DatasetId } from '../config/DatasetMetadata';
-import DataFrame from 'dataframe-js';
 import { DataTypeId } from '../config/MetricConfig';
 
 class MaternalMortalityProvider extends VariableProvider {
- constructor() {
-  super('maternal_mortality_provider', [
-   'maternal_mortality_per_100k',
-   'maternal_mortality_pct_share',
-   'maternal_mortality_population_pct',
-   'maternal_deaths_estimated_total',
-   'live_births_estimated_total',
-  ]);
- }
+    constructor() {
+        super('maternal_mortality_provider', [
+            'maternal_mortality_per_100k',
+            'maternal_mortality_pct_share',
+            'maternal_mortality_population_pct',
+            'maternal_deaths_estimated_total',
+            'live_births_estimated_total',
+        ]);
+    }
 
 
- getDatasetId(breakdowns: Breakdowns,
-  dataTypeId?: DataTypeId,
-  timeView?: TimeView): DatasetId | undefined {
-  if (breakdowns.hasOnlyRace()) {
-   if (timeView === 'current') {
-    if (breakdowns.geography === 'state') return "maternal_mortality_data-by_race_state_current";
-    if (breakdowns.geography === 'national') return 'maternal_mortality_data-by_race_national_current';
-   }
-   if (timeView === 'historical') {
-    if (breakdowns.geography === 'state') return "maternal_mortality_data-by_race_state_historical";
-    if (breakdowns.geography === 'national') return 'maternal_mortality_data-by_race_national_historical';
-   }
-  }
- }
+    getDatasetId(breakdowns: Breakdowns,
+        dataTypeId?: DataTypeId,
+        timeView?: TimeView): DatasetId | undefined {
+        if (breakdowns.hasOnlyRace()) {
+            if (timeView === 'current') {
+                if (breakdowns.geography === 'state') return "maternal_mortality_data-by_race_state_current";
+                if (breakdowns.geography === 'national') return 'maternal_mortality_data-by_race_national_current';
+            }
+            if (timeView === 'historical') {
+                if (breakdowns.geography === 'state') return "maternal_mortality_data-by_race_state_historical";
+                if (breakdowns.geography === 'national') return 'maternal_mortality_data-by_race_national_historical';
+            }
+        }
+    }
 
- async getDataInternal(metricQuery: MetricQuery): Promise<MetricQueryResponse> {
-  const breakdowns = metricQuery.breakdowns;
-  const datasetId = this.getDatasetId(breakdowns, undefined, metricQuery.timeView);
-  if (!datasetId) throw Error('DatasetId undefined');
-  const specificDatasetId = appendFipsIfNeeded(datasetId, breakdowns);
-  const maternalMortalityDataset = await getDataManager().loadDataset(specificDatasetId);
-  const consumedDatasetIds = [datasetId];
-  let df = maternalMortalityDataset.toDataFrame();
+    async getDataInternal(metricQuery: MetricQuery): Promise<MetricQueryResponse> {
+        const breakdowns = metricQuery.breakdowns;
+        const datasetId = this.getDatasetId(breakdowns, undefined, metricQuery.timeView);
+        if (!datasetId) throw Error('DatasetId undefined');
+        const specificDatasetId = appendFipsIfNeeded(datasetId, breakdowns);
+        const maternalMortalityDataset = await getDataManager().loadDataset(specificDatasetId);
+        const consumedDatasetIds = [datasetId];
+        let df = maternalMortalityDataset.toDataFrame();
 
-  // Filter by geography
-  df = this.filterByGeo(df, breakdowns);
+        // Filter by geography
+        df = this.filterByGeo(df, breakdowns);
 
-  if (df.toArray().length === 0) {
-   return new MetricQueryResponse([], consumedDatasetIds);
-  }
-  df = this.renameGeoColumns(df, breakdowns);
+        if (df.toArray().length === 0) {
+            return new MetricQueryResponse([], consumedDatasetIds);
+        }
+        df = this.renameGeoColumns(df, breakdowns);
 
-  // Apply demographic breakdown filters
-  df = this.applyDemographicBreakdownFilters(df, breakdowns);
-  df = this.removeUnrequestedColumns(df, metricQuery);
+        // Apply demographic breakdown filters
+        df = this.applyDemographicBreakdownFilters(df, breakdowns);
+        df = this.removeUnrequestedColumns(df, metricQuery);
 
-  return new MetricQueryResponse(df.toArray(), consumedDatasetIds);
- }
+        return new MetricQueryResponse(df.toArray(), consumedDatasetIds);
+    }
 
- allowsBreakdowns(breakdowns: Breakdowns): boolean {
-  return breakdowns.hasExactlyOneDemographic();
- }
+    allowsBreakdowns(breakdowns: Breakdowns): boolean {
+        return breakdowns.hasExactlyOneDemographic();
+    }
 }
 
 
