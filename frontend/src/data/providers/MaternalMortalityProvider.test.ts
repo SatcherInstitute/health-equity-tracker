973c7020ce75f564cfa36ab035c13f5345431aba
Ali <anix1@kent.edu>
2024-06-07 16:30:39 -0600

    adds unit tests to maternal mortality test file

---
+++ frontend/src/data/providers/MaternalMortalityProvider.test.ts
@@ -0,0 +1,100 @@
+import MaternalMortalityProvider from "./MaternalMortalityProvider";
+import { Breakdowns, DemographicType, TimeView } from "../query/Breakdowns";
+import { MetricQuery, MetricQueryResponse } from "../query/MetricQuery";
+import { Fips } from "../utils/Fips";
+import { DatasetId, DatasetMetadataMap } from "../config/DatasetMetadata";
+import {
+  autoInitGlobals,
+  getDataFetcher,
+  resetCacheDebug,
+} from "../../utils/globals";
+import FakeDataFetcher from "../../testing/FakeDataFetcher";
+import { RACE } from "../utils/Constants";
+import { MetricId } from "../config/MetricConfig";
+import { appendFipsIfNeeded } from "../utils/datasetutils";
+
+export async function ensureCorrectDatasetsDownloaded(
+  MaternalMortalityDatasetId: DatasetId,
+  baseBreakdown: Breakdowns,
+  demographicType: DemographicType,
+  timeView: TimeView,
+  acsDatasetIds?: DatasetId[],
+  metricIds?: MetricId[]
+) {
+  // if these aren't sent as args, default to []
+  metricIds = metricIds || [];
+  acsDatasetIds = acsDatasetIds || [];
+
+  const maternalMortalityProvider = new MaternalMortalityProvider();
+  const specificDatasetId = appendFipsIfNeeded(
+    MaternalMortalityDatasetId,
+    baseBreakdown
+  );
+  dataFetcher.setFakeDatasetLoaded(specificDatasetId, []);
+
+  // Evaluate the response with requesting "All" field
+  const responseIncludingAll = await maternalMortalityProvider.getData(
+    new MetricQuery(
+      metricIds,
+      baseBreakdown.addBreakdown(demographicType),
+      undefined,
+      timeView
+    )
+  );
+
+  expect(dataFetcher.getNumLoadDatasetCalls()).toBe(1);
+
+  const consumedDatasetIds = [MaternalMortalityDatasetId];
+  consumedDatasetIds.push(...acsDatasetIds);
+
+  expect(responseIncludingAll).toEqual(
+    new MetricQueryResponse([], consumedDatasetIds)
+  );
+}
+
+autoInitGlobals();
+const dataFetcher = getDataFetcher() as FakeDataFetcher;
+
+describe("MaternalMortalityProvider", () => {
+  beforeEach(() => {
+    resetCacheDebug();
+    dataFetcher.resetState();
+    dataFetcher.setFakeMetadataLoaded(DatasetMetadataMap);
+  });
+
+  test("National Current and Race Breakdown", async () => {
+    await ensureCorrectDatasetsDownloaded(
+      "maternal_mortality_data-by_race_national_current",
+      Breakdowns.forFips(new Fips("00")),
+      RACE,
+      "current"
+    );
+  });
+
+  test("National Historical and Race Breakdown", async () => {
+    await ensureCorrectDatasetsDownloaded(
+      "maternal_mortality_data-by_race_national_historical",
+      Breakdowns.forFips(new Fips("00")),
+      RACE,
+      "historical"
+    );
+  });
+
+  test("State Current and Race Breakdown", async () => {
+    await ensureCorrectDatasetsDownloaded(
+      "maternal_mortality_data-by_race_state_current",
+      Breakdowns.forFips(new Fips("08")),
+      RACE,
+      "current"
+    );
+  });
+
+  test("State Historical and Race Breakdown", async () => {
+    await ensureCorrectDatasetsDownloaded(
+      "maternal_mortality_data-by_race_state_historical",
+      Breakdowns.forFips(new Fips("08")),
+      RACE,
+      "historical"
+    );
+  });
+});
