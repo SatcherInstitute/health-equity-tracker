14ac7b26334ef733455d0600cd6287b8b2e8f099
kccrtv <67879386+kccrtv@users.noreply.github.com>
2024-06-17 14:32:07 -0400

    add new hero and update landing page headers (#3368)
    # Description and Motivation
    kicking off the UI refresh! the previous stock images used, while great
    for displaying diversity, can make it confusing for users on our product
    offerings. the UI updates will keep the app aesthetically appealing,
    more engaging for our end users, and keeps up with modern design
    practices.
    
    - updates hero image and h1 copy
    - changes font for subsequent headers to dm sans from taviraj which
    differentiates the hero
    - adds mockup images to replace smiling family image
    - moves CTA to new hero
    - adds logos of major public health agencies we source our data from
    
    ## Has this been tested? How?
    tests passing
    passes WAVE audit
    
    ## Screenshots (if appropriate)
    ![Screenshot 2024-06-10 at 6 22
    03 PM](https://github.com/SatcherInstitute/health-equity-tracker/assets/67879386/1bc07d39-6fd0-4971-8350-568a4c0bdb01)
    ![Screenshot 2024-06-10 at 6 23
    07 PM](https://github.com/SatcherInstitute/health-equity-tracker/assets/67879386/c996bbad-a0d4-469d-bd76-321db7cb0981)
    
    ## Types of changes
    - New content or feature
    - 
    ## New frontend preview link is below in the Netlify comment 😎
    
    ---------
    
    Co-authored-by: Ben Hammond <benjamin.hammond@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- frontend/src/pages/News/NewsPreviewCard.tsx
+++ frontend/src/pages/News/NewsPreviewCard.tsx
@@ -3,41 +3,49 @@ import AppbarLogo from '../../assets/AppbarLogo.png'
 import { getHtml } from '../../utils/urlutils'
 import { type Article } from './NewsPage'
 import LazyLoad from 'react-lazyload'
-import HetLinkButton from '../../styles/HetComponents/HetLinkButton'
 
 interface NewsPreviewCardProps {
   article: Article
 }
 
-export default function NewsPreviewCard(props: NewsPreviewCardProps) {
+export default function NewsPreviewCard(props: NewsPreviewCardProps): JSX.Element {
   const { article } = props
 
-  return (
-    <HetLinkButton
-      href={`${NEWS_PAGE_LINK}/${article.slug}`}
-      className='m-0 p-2 text-center text-title'
-    >
-      <div className='flex flex-nowrap justify-evenly'>
-        <div className='flex w-9/12 flex-col items-center justify-center'>
-          <LazyLoad once height={100} offset={300}>
-            <img
-              height='100'
-              src={
-                article?._embedded?.['wp:featuredmedia']?.[0]?.media_details
-                  ?.sizes?.medium?.source_url || AppbarLogo
-              }
-              className='w-auto rounded-md'
-              alt=''
-            />
-          </LazyLoad>
+  const getImageSource = (): string => {
+    const imageSource = article?._embedded?.['wp:featuredmedia']?.[0]?.media_details?.sizes?.full?.source_url
+    return imageSource || AppbarLogo
+  }
 
-          <div className='mx-2'>
-            <h3 className='m-0 text-center font-serif text-title font-light leading-lhSomeSpace'>
-              {getHtml(article.title.rendered, true)}
-            </h3>
-          </div>
-        </div>
+  const CategoryNames: React.FC<{ article: Article }> = ({ article }) => {
+    const categoryNames = article?._embedded?.['wp:term']?.[0]?.map(term => term.name) || [];
+
+    return (
+      <div className='flex flex-wrap mt-2'>
+        {categoryNames.map((name, index) => (
+          <span key={index} className="category-span text-[10px] uppercase text-[#282c25] font-sansTitle font-bold bg-ashgray30 rounded-sm py-1 px-2 mr-2 mt-1">
+            {name}
+          </span>
+        ))}
       </div>
-    </HetLinkButton>
+    );
+  };
+
+  return (
+    <a
+      href={`${NEWS_PAGE_LINK}/${article.slug}`}
+      className='h-full text-center text-title no-underline '
+    >
+      <LazyLoad once offset={300}>
+        <div className='mx-8 0 flex flex-col items-left rounded-md hover:scale-105 hover:transition-transform hover:duration-30 '>
+          <div className='news-preview-card-image min-h-40 h-56 w-full bg-no-repeat bg-cover bg-center rounded-sm shadow-raised-tighter mb-4 ' style={{ backgroundImage: `url(${getImageSource()})` }} >
+          </div>
+
+          <CategoryNames article={article} />
+          <h3 className='p-0 text-left font-sansText text-text font-bold text-black leading-lhSomeMoreSpace'>
+            {getHtml(article.title.rendered, true)}
+          </h3>
+        </div>
+      </LazyLoad>
+    </a >
   )
 }
