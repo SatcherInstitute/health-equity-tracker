f5bc850ca7a11128bfa3d43b48be0b4c1cdc3375
Ben Hammond <benjamin.hammond@gmail.com>
2024-06-25 08:28:08 -0600

    Lots more pip migration (#3411)
    - linting
    - migrating lots of packages to `shared_requirements`

--- python/ingestion/pubsub_publisher.py
+++ python/ingestion/pubsub_publisher.py
@@ -1,15 +1,15 @@
 import logging
-from google.cloud import pubsub_v1
+from google.cloud import pubsub_v1  # type: ignore
 
 
 def notify_topic(project_id, topic, **attrs):
     """Publishes a notification on the specified topic using the provided attributes.
-      Attributes should include the id of the source that was ingested and the name of
-      the bucket the data is located in.
+    Attributes should include the id of the source that was ingested and the name of
+    the bucket the data is located in.
 
-      project_id: The id of the project
-      topic: The name of the topic to notify on
-      attrs: The attributes to pass through to the message"""
+    project_id: The id of the project
+    topic: The name of the topic to notify on
+    attrs: The attributes to pass through to the message"""
     publisher = pubsub_v1.PublisherClient()
     # For some reason topic_path doesn't show up as a member, but is used in the
     # official documentation:
@@ -26,5 +26,4 @@ def notify_topic(project_id, topic, **attrs):
     try:
         future.result()
     except Exception as e:
-        logging.warning(
-            'Error publishing message on topic {}: {}'.format(topic, e))
+        logging.warning('Error publishing message on topic %s: %s', topic, e)
