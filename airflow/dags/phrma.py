d7018d3a34da39ba879495e33c13f7ff68c54a49
Ben Hammond <benjamin.hammond@gmail.com>
2024-07-03 15:23:51 -0600

    split up exporter steps phrma

--- airflow/dags/phrma.py
+++ airflow/dags/phrma.py
@@ -1,6 +1,6 @@
 # Ignore the Airflow module, it is installed in both dev and prod
-from airflow import DAG  # type: ignore
-from airflow.utils.dates import days_ago  # type: ignore
+from airflow import DAG  # pylint: disable=no-name-in-module
+from airflow.utils.dates import days_ago  # pylint: disable=no-name-in-module
 
 import util
 
@@ -194,26 +194,18 @@ payload_race = {
     'dataset_name': _PHRMA_DATASET_NAME,
     'demographic': "race_and_ethnicity",
 }
-phrma_exporter_operator_race = util.create_exporter_operator(
-    'phrma_exporter_race', payload_race, data_ingestion_dag
-)
+phrma_exporter_operator_race = util.create_exporter_operator('phrma_exporter_race', payload_race, data_ingestion_dag)
 
 payload_age = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "age"}
-phrma_exporter_operator_age = util.create_exporter_operator(
-    'phrma_exporter_age', payload_age, data_ingestion_dag
-)
+phrma_exporter_operator_age = util.create_exporter_operator('phrma_exporter_age', payload_age, data_ingestion_dag)
 
 
 payload_sex = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "sex"}
-phrma_exporter_operator_sex = util.create_exporter_operator(
-    'phrma_exporter_sex', payload_sex, data_ingestion_dag
-)
+phrma_exporter_operator_sex = util.create_exporter_operator('phrma_exporter_sex', payload_sex, data_ingestion_dag)
 
 
 payload_lis = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "lis"}
-phrma_exporter_operator_lis = util.create_exporter_operator(
-    'phrma_exporter_lis', payload_lis, data_ingestion_dag
-)
+phrma_exporter_operator_lis = util.create_exporter_operator('phrma_exporter_lis', payload_lis, data_ingestion_dag)
 
 
 payload_eligibility = {
@@ -246,11 +238,11 @@ phrma_exporter_operator_eligibility = util.create_exporter_operator(
         phrma_bq_operator_sex_county,
     ]
     >> phrma_bq_operator_age_county
+    >> phrma_exporter_operator_race
     >> [
-        phrma_exporter_operator_race,
-        phrma_exporter_operator_age,
         phrma_exporter_operator_eligibility,
         phrma_exporter_operator_lis,
         phrma_exporter_operator_sex,
     ]
+    >> phrma_exporter_operator_age,
 )
