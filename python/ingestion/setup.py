bc170d1fc6d85b915fa9b82e70711e94261cb626
Eric M Warren II <eric.m.warren1@gmail.com>
2024-07-03 16:36:15 -0500

    Fixes JSON loading error (#3439)
    # Description and Motivation
    <!--- bulleted, high-level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    This PR fixes the issue where the `graphql_ahr_measure_ids.json` file
    was being loaded outside of any function, causing unrelated DAGs to fail
    when they could not find the JSON file. It also ensures that the JSON
    config file is included in the Docker build process by updating the
    `setup.py` file.
    
    ## Has this been tested? How?
    - Verified that unrelated DAGs no longer fail due to the missing JSON
    file on infra test.
    - Tested `get_measure_ids` with both real data and test data.
    
    ## Types of changes
    - Bug fix
    - Refactor/chore

--- python/ingestion/setup.py
+++ python/ingestion/setup.py
@@ -8,6 +8,7 @@ setup(
     package_data={
         'ingestion': [
             'acs_population/*',
+            'ahr_config/*',
             'decia_2010_territory_population/*',
             'decia_2020_territory_population/*',
             'fips_codes/*',
