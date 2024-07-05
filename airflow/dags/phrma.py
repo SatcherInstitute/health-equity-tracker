6815916fa6b5ed6c428f9ce8864249ad2bb16d1d
Ben Hammond <benjamin.hammond@gmail.com>
2024-07-05 09:58:56 -0600

    Fixes `phrma` DAG (#3441)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - split up parallel exporter steps; they may have been overloading the
    allotted resources and causing the unexplained failures
    
    ## Has this been tested? How?
    
    - [x] works now via infra-test
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - Bug fix
    
    ## New frontend preview link is below in the Netlify comment 😎

--- airflow/dags/phrma.py
+++ airflow/dags/phrma.py
@@ -1,7 +1,5 @@
-# Ignore the Airflow module, it is installed in both dev and prod
-from airflow import DAG  # type: ignore
-from airflow.utils.dates import days_ago  # type: ignore
-
+from airflow import DAG  # pylint: disable=no-name-in-module
+from airflow.utils.dates import days_ago  # pylint: disable=no-name-in-module
 import util
 
 _PHRMA_WORKFLOW_ID = 'PHRMA_DATA'
@@ -194,26 +192,18 @@ payload_race = {
     'dataset_name': _PHRMA_DATASET_NAME,
     'demographic': "race_and_ethnicity",
 }
-phrma_exporter_operator_race = util.create_exporter_operator(
-    'phrma_exporter_race', payload_race, data_ingestion_dag
-)
+phrma_exporter_operator_race = util.create_exporter_operator('phrma_exporter_race', payload_race, data_ingestion_dag)
 
 payload_age = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "age"}
-phrma_exporter_operator_age = util.create_exporter_operator(
-    'phrma_exporter_age', payload_age, data_ingestion_dag
-)
+phrma_exporter_operator_age = util.create_exporter_operator('phrma_exporter_age', payload_age, data_ingestion_dag)
 
 
 payload_sex = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "sex"}
-phrma_exporter_operator_sex = util.create_exporter_operator(
-    'phrma_exporter_sex', payload_sex, data_ingestion_dag
-)
+phrma_exporter_operator_sex = util.create_exporter_operator('phrma_exporter_sex', payload_sex, data_ingestion_dag)
 
 
 payload_lis = {'dataset_name': _PHRMA_DATASET_NAME, 'demographic': "lis"}
-phrma_exporter_operator_lis = util.create_exporter_operator(
-    'phrma_exporter_lis', payload_lis, data_ingestion_dag
-)
+phrma_exporter_operator_lis = util.create_exporter_operator('phrma_exporter_lis', payload_lis, data_ingestion_dag)
 
 
 payload_eligibility = {
@@ -246,11 +236,11 @@ phrma_exporter_operator_eligibility = util.create_exporter_operator(
         phrma_bq_operator_sex_county,
     ]
     >> phrma_bq_operator_age_county
+    >> phrma_exporter_operator_race
     >> [
-        phrma_exporter_operator_race,
-        phrma_exporter_operator_age,
         phrma_exporter_operator_eligibility,
         phrma_exporter_operator_lis,
         phrma_exporter_operator_sex,
     ]
+    >> phrma_exporter_operator_age
 )
