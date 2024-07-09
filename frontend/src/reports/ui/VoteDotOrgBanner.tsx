ac7769040a4ec04b568faafaff96278b8741a93d
Ben Hammond <benjamin.hammond@gmail.com>
2024-07-09 12:01:03 -0600

    icon

--- frontend/src/reports/ui/VoteDotOrgBanner.tsx
+++ frontend/src/reports/ui/VoteDotOrgBanner.tsx
@@ -16,7 +16,7 @@ export default function VoteDotOrgBanner() {
   return (
     <HetNotice
       className='mx-2 mt-4 mb-2 lg:mb-4 lg:mt-4 lg:ml-2 border border-secondaryMain text-left text-small'
-      // icon={<HowToVoteIcon color='primary' />}
+      icon={<HowToVoteIcon color='primary' />}
       title='Are You Registered To Vote?'
       kind='helpful-info'
     >
@@ -26,10 +26,8 @@ export default function VoteDotOrgBanner() {
       <HetLinkButton
         onClick={() => setModalIsOpen(true)}
       >
-        Quickly check with Vote.org <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke={het.altGreen} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M17 7L7 17" />
-          <path d="M7 7h10v10" />
-        </svg>
+        Quickly check with Vote.org
+
       </HetLinkButton>
     </HetNotice>
   )
