f7a1ad7e56a106eb24daeeb0cc0c4fd497428e2a
Ben Hammond <benjamin.hammond@gmail.com>
2024-07-18 15:30:38 -0600

    hm

--- frontend/src/styles/HetComponents/HetAppBar.tsx
+++ frontend/src/styles/HetComponents/HetAppBar.tsx
@@ -3,6 +3,7 @@ import HetDesktopToolbar from './HetDesktopToolbar'
 import HetMobileToolbar from './HetMobileToolbar'
 
 export default function HetAppBar() {
+
   return (
     <AppBar position='static' elevation={0}>
       <div className='smMd:hidden'>
