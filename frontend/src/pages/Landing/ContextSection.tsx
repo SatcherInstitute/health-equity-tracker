2280563420094aa4efefece84d059addead3e7eb
kccrtv <kimcincocreative@gmail.com>
2024-07-26 14:50:09 -0600

    adds context section for landing page

---
+++ frontend/src/pages/Landing/ContextSection.tsx
@@ -0,0 +1,42 @@
+export default function ContextSection() {
+	return (
+		<>
+        <section>
+
+			<h4 className='my-3 text-center font-roboto text-smallest font-semibold uppercase text-black'>
+				In Focus
+			</h4>
+			<div className='flex w-full flex-col justify-center items-center md:block sm:block'>
+				<h2
+					className='m-0 font-sansTitle text-bigHeader font-bold leading-lhModalHeading text-altGreen text-center xs:text-header'
+                    >
+					The Scourge of Gun Violence in Atlanta
+				</h2>
+
+				<p className='text-center mt-4 mb-16 text-title'>
+				This pervasive public health challenge harms communities nationwide, disproportionately affecting Black communities. It's crucial to understand its impacts on mental, physical, and social health, often exacerbated by racial motives.
+				</p>
+			</div>
+            <div>
+            <img
+                src='/img/graphics/DrSatcher.png'
+                alt='image of David Satcher, MD, PhD'
+              ></img>
+            <p className='text-center text-smallestHeader my-0'>
+            Our children should be given a personal sense of security. That’s not always there in communities of high poverty. There’s a lot of insecurity. Often children turn to gangs and guns because they feel insecure...we need to take the steps necessary to protect them.
+                </p>
+
+            <p className='text-center text-smallestHeader font-bold my-2'>
+Gun violence is a major public health problem.
+                </p>
+            <p className='text-center text-navBarHeader mt-8'>
+            David Satcher, M.D., Ph.D.
+            <p className="text-center text-text my-0">
+                Founding Director & Senior Advisor
+                </p>
+                </p>
+            </div>
+                    </section>
+		</>
+	)
+}
