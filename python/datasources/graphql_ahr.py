0495240991d1085a57a767ed944a3022903f01b6
eriwarr <eric.m.warren1@gmail.com>
2024-07-26 14:50:39 -0500

    removing unrelaible 2023 data

--- python/datasources/graphql_ahr.py
+++ python/datasources/graphql_ahr.py
@@ -105,6 +105,7 @@ class GraphQlAHRData(DataSource):
             table_name = f"{demographic}_{geo_level}_{time_view}"
             topic_prefixes = [std_col.extract_prefix(rate_col) for rate_col in AHR_BASE_MEASURES_TO_RATES_MAP.values()]
             topic_prefixes.append('ahr')
+            df = df[df['time_period'].astype(int) <= 2022]
             df_for_bq, col_types = get_timeview_df_and_cols(df, time_view, topic_prefixes)
 
             first_two_columns = df_for_bq.columns[:2].tolist()
