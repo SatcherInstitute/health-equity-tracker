3c15974031b1bd386984a4315c3ad958cf2bdcc0
Ben Hammond <benjamin.hammond@gmail.com>
2024-07-25 22:41:41 -0600

    remove preview on multimp card; type cleanup; a11y cleanup

--- frontend/src/pages/ExploreData/DefaultHelperBox.tsx
+++ frontend/src/pages/ExploreData/DefaultHelperBox.tsx
@@ -1,79 +1,82 @@
-import { EXPLORE_DATA_PAGE_LINK } from "../../utils/internalRoutes";
-import HetTextArrowLink from "../../styles/HetComponents/HetTextArrowLink";
-import { HetTags } from "../../styles/HetComponents/HetTags";
-import { reportMapping } from "./DefaultHelperBoxData";
-import TogglePreview from "./TogglePreview";
+import { EXPLORE_DATA_PAGE_LINK } from '../../utils/internalRoutes'
+import HetTextArrowLink from '../../styles/HetComponents/HetTextArrowLink'
+import { HetTags } from '../../styles/HetComponents/HetTags'
+import { reportMappings } from './DefaultHelperBoxData'
+import TogglePreview from './TogglePreview'
 
 export default function DefaultHelperBox() {
-	return (
-		<div
-			className="flex w-full items-center justify-center px-12 pb-0 pt-4 sm:px-20 sm:pt-8"
-			aria-labelledby="main-heading"
-		>
-			<div className="m-0 mb-5 w-full max-w-helperBox content-center items-center justify-evenly justify-items-center rounded-md pb-0">
-				<div className="px-10 py-0 text-left smMd:px-0 md:px-10 xs:px-2">
-					<h1
-						id="main-heading"
-						className="m-0 font-sansTitle text-header font-bold leading-lhModalHeading text-altGreen text-base text-center"
-					>
-						Select a topic above
-					</h1>
-					<p className="text-text text-center">
-						or explore one of the following reports:
-					</p>
-					<ul
-						className="my-0 list-none pl-0 text-left flex flex-wrap"
-						role="list"
-						aria-labelledby="reports-heading"
-					>
-						{reportMapping.map((report, index) => (
-							<li
-								className="my-4 xs:my-2 mx-0 flex flex-col bg-white rounded-md hover:shadow-raised group border border-solid border-altGreen transition-all duration-300 ease-in-out w-full"
-								key={index}
-								role="listitem"
-							>
-								<article
-									className="text-left p-4 text-altGreen grid gap-4 md:items-start place-items-center md:grid-cols-[40%_60%] w-full"
-									aria-labelledby={`report-title-${index}`}
-								>
-									<a
-										href={EXPLORE_DATA_PAGE_LINK + report.setting}
-										className="bg-cover bg-no-repeat ml-6 px-4 w-full h-[18rem] transition-opacity duration-300 ease-in-out hover:opacity-80 hover:shadow-lg"
-										style={{
-											backgroundImage: `url(${report.previewImg})`,
-										}}
-										aria-label={`${report.title} preview image`}
-									></a>
-									<div className="flex flex-col w-full md:items-start px-4 md:px-12">
-										<HetTags tags={report.categories} />
-										<h2
-											role="heading"
-											aria-label={report.title}
-											id={`report-title-${index}`}
-											className="font-medium my-2 md:my-4 text-center md:text-left text-title"
-										>
-											<a
-												href={EXPLORE_DATA_PAGE_LINK + report.setting}
-												className="no-underline"
-											>
-												{report.title} {report.icon && report.icon}
-											</a>
-										</h2>
-										<p className="text-black md:block hidden my-0 md:mb-4">
-											{report.description}
-										</p>
-										<HetTextArrowLink
-											link={EXPLORE_DATA_PAGE_LINK + report.setting}
-											linkText="Explore this report"
-										/>
-									</div>
-								</article>
-								<TogglePreview index={index} report={report} />
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className='flex w-full items-center justify-center px-12 pb-0 pt-4 sm:px-20 sm:pt-8'
+      aria-labelledby='main-heading'
+    >
+      <div className='m-0 mb-5 w-full max-w-helperBox content-center items-center justify-evenly justify-items-center rounded-md pb-0'>
+        <div className='px-10 py-0 text-left smMd:px-0 md:px-10 xs:px-2'>
+          <h1
+            id='main-heading'
+            className='m-0 font-sansTitle text-header font-bold leading-lhModalHeading text-altGreen text-base text-center'
+          >
+            Select a topic above
+          </h1>
+          <p className='text-text text-center'>
+            or explore one of the following reports:
+          </p>
+          <ul
+            className='my-0 list-none pl-0 text-left flex flex-wrap'
+            aria-labelledby='reports-heading'
+          >
+            {reportMappings.map((report, index) => (
+              <li
+                className='my-4 xs:my-2 mx-0 flex flex-col bg-white rounded-md hover:shadow-raised group border border-solid border-altGreen transition-all duration-300 ease-in-out w-full'
+                key={index}
+                role='listitem'
+              >
+                <article
+                  className='text-left p-4 text-altGreen grid gap-4 md:items-start place-items-center md:grid-cols-[40%_60%] w-full'
+                  aria-labelledby={`report-title-${index}`}
+                >
+                  <a
+                    href={EXPLORE_DATA_PAGE_LINK + report.setting}
+                    className='bg-cover bg-no-repeat ml-6 px-4 w-full h-[18rem] transition-opacity duration-300 ease-in-out hover:opacity-80 hover:shadow-lg'
+                    style={{
+                      backgroundImage: `url(${report.previewImg})`,
+                    }}
+                  >
+                    <span className='sr-only'>
+                      {report.title} preview image
+                    </span>
+                  </a>
+                  <div className='flex flex-col w-full md:items-start px-4 md:px-12'>
+                    <HetTags tags={report.categories} />
+                    <h2
+                      aria-label={report.title}
+                      id={`report-title-${index}`}
+                      className='font-medium my-2 md:my-4 text-center md:text-left text-title'
+                    >
+                      <a
+                        href={EXPLORE_DATA_PAGE_LINK + report.setting}
+                        className='no-underline'
+                      >
+                        {report.title} {report.icon && report.icon}
+                      </a>
+                    </h2>
+                    <p className='text-black md:block hidden my-0 md:mb-4'>
+                      {report.description}
+                    </p>
+                    <HetTextArrowLink
+                      link={EXPLORE_DATA_PAGE_LINK + report.setting}
+                      linkText='Explore this report'
+                    />
+                  </div>
+                </article>
+                {report.customCard && (
+                  <TogglePreview index={index} report={report} />
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
--- frontend/src/pages/ExploreData/DefaultHelperBoxData.tsx
+++ frontend/src/pages/ExploreData/DefaultHelperBoxData.tsx
@@ -1,77 +1,93 @@
-import { HIV_PREVALENCE_RACE_USA_SETTING, COVID_DEATHS_AGE_FULTON_COUNTY_SETTING, PRISON_VS_POVERTY_RACE_GA_SETTING, UNINSURANCE_SEX_FL_VS_CA_SETTING, PHRMA_HIV_ELIGIBILITY_USA_MULTIMAP_SETTING } from "../../utils/internalRoutes";
-import Custom100kBarChartCompare from "../../reports/Custom100kBarChartCompare";
-import Custom100kMap from "../../reports/Custom100kMap";
-import CustomDisparityBarChartCompare from "../../reports/CustomDisparityBarChartCompare";
-import CustomRateTrendsLineChart from "../../reports/CustomRateTrendsLineChart";
-import CustomShareTrendsLineChart from "../../reports/CustomShareTrendsLineChart";
+import {
+  HIV_PREVALENCE_RACE_USA_SETTING,
+  COVID_DEATHS_AGE_FULTON_COUNTY_SETTING,
+  PRISON_VS_POVERTY_RACE_GA_SETTING,
+  UNINSURANCE_SEX_FL_VS_CA_SETTING,
+  PHRMA_HIV_ELIGIBILITY_USA_MULTIMAP_SETTING,
+} from '../../utils/internalRoutes'
+import Custom100kBarChartCompare from '../../reports/Custom100kBarChartCompare'
+import CustomDisparityBarChartCompare from '../../reports/CustomDisparityBarChartCompare'
+import CustomRateTrendsLineChart from '../../reports/CustomRateTrendsLineChart'
+import CustomShareTrendsLineChart from '../../reports/CustomShareTrendsLineChart'
 import FiberNewIcon from '@mui/icons-material/FiberNew'
 
-export const reportMapping = [
-	{
-		setting: PHRMA_HIV_ELIGIBILITY_USA_MULTIMAP_SETTING,
-		title: "HIV Disparity Maps by Medicare Eligibility",
-		preview: "Medicare HIV Cases",
-		description:
-			"Visualize HIV disparities among Medicare beneficiaries. These insights are essential for optimizing treatment and reducing health inequities.",
-		categories: [
-			"HIV",
-			"Medication Utilization in the Medicare Population",
-			"Multiple Maps",
-			"National-Level",
-		],
-		icon: <FiberNewIcon />,
-		previewImg: "/img/screenshots/sample-report_medicare.png",
-		customCard: <Custom100kMap openMultiMap={false} />,
-	},
-	{
-		setting: HIV_PREVALENCE_RACE_USA_SETTING,
-		title: "HIV by Race/Ethnicity",
-		preview: "HIV Cases",
-		description:
-			"Uncover disparities in HIV prevalence across different racial and ethnic groups in the U.S. Understanding these patterns is vital for targeted interventions and improved health equity.",
-		categories: ["HIV", "Prevalence", "Race/Ethnicity", "National-Level"],
-		previewImg: "/img/screenshots/sample-report_hiv.png",
-		customCard: <CustomRateTrendsLineChart />,
-	},
-	{
-		setting: COVID_DEATHS_AGE_FULTON_COUNTY_SETTING,
-		title: "COVID-19 Deaths in Fulton County by Age",
-		preview: "COVID-19 Deaths",
-		description:
-			"Analyze COVID-19 mortality in Fulton County, GA, by age. Highlighting vulnerable populations helps to inform public health strategies and resource allocation.",
-		categories: ["COVID-19", "Deaths", "Age", "County-Level"],
-		previewImg: "/img/screenshots/sample-report_covid.png",
-		customCard: <CustomShareTrendsLineChart />,
-	},
-	{
-		setting: PRISON_VS_POVERTY_RACE_GA_SETTING,
-		title: "Prison & Poverty in Georgia by Race",
-		preview: "Prison + Poverty",
-		description:
-			"Explore the intersection of incarceration, poverty, and race in Georgia. Addressing these disparities is key to improving health outcomes and social justice.",
-		categories: [
-			"Social Determinants of Health",
-			"Political Determinants of Health",
-			"Race/Ethnicity",
-			"State-Level",
-			"Compare Topics",
-		],
-		previewImg: "/img/screenshots/sample-report_ga.png",
-		customCard: <Custom100kBarChartCompare />,
-	},
-	{
-		setting: UNINSURANCE_SEX_FL_VS_CA_SETTING,
-		title: "Uninsurance in FL & CA by Sex",
-		preview: "Uninsured",
-		description:
-			"Examine uninsurance rates by sex in Florida and California. Identifying these gaps is crucial for advancing equitable healthcare access.",
-		categories: [
-			"Social Determinants of Health",
-			"State-Level",
-			"Sex",
-			"Compare Places",
-		],
-		previewImg: "/img/screenshots/sample-report_uninsured.png",
-		customCard: <CustomDisparityBarChartCompare />,
-	},
-];
+type ReportMapping = {
+  setting: string
+  title: string
+  preview: string
+  description: string
+  categories: string[]
+  icon?: JSX.Element
+  previewImg: string
+  customCard: JSX.Element | null
+}
+
+export const reportMappings: ReportMapping[] = [
+  {
+    setting: PHRMA_HIV_ELIGIBILITY_USA_MULTIMAP_SETTING,
+    title: 'HIV Disparity Maps by Medicare Eligibility',
+    preview: 'Medicare HIV Cases',
+    description:
+      'Visualize HIV disparities among Medicare beneficiaries. These insights are essential for optimizing treatment and reducing health inequities.',
+    categories: [
+      'HIV',
+      'Medication Utilization in the Medicare Population',
+      'Multiple Maps',
+      'National-Level',
+    ],
+    icon: <FiberNewIcon />,
+    previewImg: '/img/screenshots/sample-report_medicare.png',
+    customCard: null,
+  },
+  {
+    setting: HIV_PREVALENCE_RACE_USA_SETTING,
+    title: 'HIV by Race/Ethnicity',
+    preview: 'HIV Cases',
+    description:
+      'Uncover disparities in HIV prevalence across different racial and ethnic groups in the U.S. Understanding these patterns is vital for targeted interventions and improved health equity.',
+    categories: ['HIV', 'Prevalence', 'Race/Ethnicity', 'National-Level'],
+    previewImg: '/img/screenshots/sample-report_hiv.png',
+    customCard: <CustomRateTrendsLineChart />,
+  },
+  {
+    setting: COVID_DEATHS_AGE_FULTON_COUNTY_SETTING,
+    title: 'COVID-19 Deaths in Fulton County by Age',
+    preview: 'COVID-19 Deaths',
+    description:
+      'Analyze COVID-19 mortality in Fulton County, GA, by age. Highlighting vulnerable populations helps to inform public health strategies and resource allocation.',
+    categories: ['COVID-19', 'Deaths', 'Age', 'County-Level'],
+    previewImg: '/img/screenshots/sample-report_covid.png',
+    customCard: <CustomShareTrendsLineChart />,
+  },
+  {
+    setting: PRISON_VS_POVERTY_RACE_GA_SETTING,
+    title: 'Prison & Poverty in Georgia by Race',
+    preview: 'Prison + Poverty',
+    description:
+      'Explore the intersection of incarceration, poverty, and race in Georgia. Addressing these disparities is key to improving health outcomes and social justice.',
+    categories: [
+      'Social Determinants of Health',
+      'Political Determinants of Health',
+      'Race/Ethnicity',
+      'State-Level',
+      'Compare Topics',
+    ],
+    previewImg: '/img/screenshots/sample-report_ga.png',
+    customCard: <Custom100kBarChartCompare />,
+  },
+  {
+    setting: UNINSURANCE_SEX_FL_VS_CA_SETTING,
+    title: 'Uninsurance in FL & CA by Sex',
+    preview: 'Uninsured',
+    description:
+      'Examine uninsurance rates by sex in Florida and California. Identifying these gaps is crucial for advancing equitable healthcare access.',
+    categories: [
+      'Social Determinants of Health',
+      'State-Level',
+      'Sex',
+      'Compare Places',
+    ],
+    previewImg: '/img/screenshots/sample-report_uninsured.png',
+    customCard: <CustomDisparityBarChartCompare />,
+  },
+]
