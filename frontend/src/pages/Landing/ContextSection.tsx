745da0d6c81c3f3a924834bbb549893500b3fa38
kccrtv <kimcincocreative@gmail.com>
2024-07-29 09:57:29 -0600

    adds context section vertical tabs

--- frontend/src/pages/Landing/ContextSection.tsx
+++ frontend/src/pages/Landing/ContextSection.tsx
@@ -1,42 +1,42 @@
-import type React from 'react';
-import ContextSectionTabs from './ContextSectionTabs';
+import type React from 'react'
+import ContextSectionTabs from './ContextSectionTabs'
 
 export default function ContextSection() {
 	return (
 		<>
 			<section className='px-80 max-w-screen'>
-				<p  className="my-3 text-center font-roboto text-smallest font-semibold uppercase text-black">
+				<p  className='my-3 text-center font-roboto text-smallest font-semibold uppercase text-black'>
 					In Focus
 				</p>
-				<div className="flex w-full flex-col justify-center items-center md:block sm:block">
-					<h2 className="m-0 font-sansTitle text-biggerHeader font-bold leading-lhModalHeading text-altGreen text-center xs:text-header">
+				<div className='flex w-full flex-col justify-center items-center md:block sm:block'>
+					<h2 className='m-0 font-sansTitle text-biggerHeader font-bold leading-lhModalHeading text-altGreen text-center xs:text-header'>
 						The Scourge of Gun Violence in Atlanta
 					</h2>
 
-					<p className="text-center my-4 text-title">
+					<p className='text-center my-4 text-title'>
 						This pervasive public health challenge harms communities nationwide,
 						disproportionately affecting Black communities. It's crucial to
 						understand its impacts on mental, physical, and social health, often
 						exacerbated by racial motives.
 					</p>
 					<img
-						src="/img/graphics/DrSatcher.png"
-						alt="image of David Satcher, MD, PhD"
+						src='/img/graphics/DrSatcher.png'
+						alt='image of David Satcher, MD, PhD'
 					></img>
 
-					<p className="text-center text-smallestHeader my-0">
+					<p className='text-center text-smallestHeader my-0'>
 						Our children should be given a personal sense of security. That’s
 						not always there in communities of high poverty. There’s a lot of
 						insecurity. Often children turn to gangs and guns because they feel
 						insecure...we need to take the steps necessary to protect them.
 					</p>
 
-					<p className="text-center text-smallestHeader font-bold my-2">
+					<p className='text-center text-smallestHeader font-bold my-2'>
 						Gun violence is a major public health problem.
 					</p>
-					<p className="text-center text-navBarHeader mt-8">
+					<p className='text-center text-navBarHeader mt-8'>
 						David Satcher, M.D., Ph.D.
-						<p className="text-center text-text my-0">
+						<p className='text-center text-text my-0'>
 							Founding Director & Senior Advisor
 						</p>
 					</p>
@@ -45,5 +45,5 @@ export default function ContextSection() {
 
 			</section>
 		</>
-	);
+	)
 }
--- frontend/src/pages/Landing/ContextSectionTabs.tsx
+++ frontend/src/pages/Landing/ContextSectionTabs.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from "react";
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
-import { useUrlSearchParams } from "../../utils/urlutils";
-import {Link, Redirect, Route, Switch} from 'react-router-dom'
-import { useIsBreakpointAndUp } from "../../utils/hooks/useIsBreakpointAndUp";
-import { OVERVIEW_LINK } from "../../utils/internalRoutes";
-import TabOne from "./TabOne";
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import TabOne from './TabOne'
+import { Box, Typography } from '@mui/material'
+import React from 'react'
 
-interface TabPanelProps{
-    children?: React.ReactNode
-    index: number
-    value: number
+interface TabPanelProps {
+	children?: React.ReactNode;
+	index: number;
+	value: number;
 }
 
 function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props
   
     return (
       <div
-        role="tabpanel"
+        role='tabpanel'
         hidden={value !== index}
         id={`vertical-tabpanel-${index}`}
         aria-labelledby={`vertical-tab-${index}`}
@@ -32,36 +27,36 @@ function TabPanel(props: TabPanelProps) {
           </Box>
         )}
       </div>
-    );
+    )
   }
   
   function a11yProps(index: number) {
     return {
       id: `vertical-tab-${index}`,
       'aria-controls': `vertical-tabpanel-${index}`,
-    };
+    }
   }
   
 
 export default function ContextSectionTabs(){
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0)
 
 const handleChange = (event: React.SyntheticEvent, newValue: number) => {
 setValue(newValue);
-};
+}
     return(
         <Box
         sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex'}}
       >
         <Tabs
-          orientation="vertical"
+          orientation='vertical'
           
           value={value}
           onChange={handleChange}
-          aria-label="Vertical tabs example"
+          aria-label='Vertical tabs example'
           sx={{ borderRight: 1, borderColor: 'divider' }}
         >
-          <Tab label="Crisis Overview" {...a11yProps(0)} />
+          <Tab label='Crisis Overview' {...a11yProps(0)} />
 
         </Tabs>
         <TabPanel value={value} index={0}>
