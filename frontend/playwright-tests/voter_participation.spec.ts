bf6ba361ba47e867b77fb2a87f87aa144f1988e8
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-01 11:56:39 -0600

    Adds Voter registration tool (#3440)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - Adds new announcement banner to Explore the Data
    - Button opens new modal showing the Vote.org iframe
    - e2e test rewritten to be faster and to handle modal behavior
    
    ## todo
    
    - [x] can banner title be the link? Keep it as small as possible? If
    not; more distinct text between title and link
    - [x] add quote from rep. Lewis re: voting ?
    - [x] clean up vertical modal around widget 
    - [x] move X to close modal up to top right 
    
    
    ## Has this been tested? How?
    
    - passing
    
    ## Types of changes
    
    (leave all that apply)
    
    - New content or feature
    
    ## New frontend preview link is below in the Netlify comment 😎

---
+++ frontend/playwright-tests/voter_participation.spec.ts
@@ -0,0 +1,44 @@
+import { test } from '@playwright/test'
+
+test('Voter Participation Flow', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.voter_participation-3.00&group1=All')
+  await page.getByText('“The vote is precious. It is').click()
+  await page
+    .getByRole('button', { name: 'Check your voter registration' })
+    .click()
+  await page
+    .frameLocator('iframe[title="Vote\\.org Registration Checker"]')
+    .getByRole('heading', { name: 'Are You Registered to Vote?' })
+    .click()
+  await page.getByLabel('close modal').click()
+  await page
+    .locator('#rate-map')
+    .getByRole('heading', { name: 'Voter participation in the' })
+    .click()
+  await page
+    .locator('#rate-map')
+    .getByRole('heading', { name: 'U.S. citizens, Ages 18+' })
+    .click()
+  await page
+    .locator('#rate-chart')
+    .getByRole('heading', { name: 'U.S. citizens, Ages 18+' })
+    .click()
+  await page.getByLabel('Bar Chart showing Voter').getByRole('img').click()
+  await page.getByRole('heading', { name: 'Share of all voter' }).click()
+  await page.getByText('No unknown values for race').click()
+  await page
+    .getByRole('heading', { name: 'Graph unavailable: Population' })
+    .click()
+  await page
+    .getByRole('heading', { name: 'Breakdown summary for voter' })
+    .click()
+  await page
+    .getByRole('figure', { name: 'Breakdown summary for voter' })
+    .locator('h4')
+    .click()
+  await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click()
+  await page.getByRole('cell', { name: 'Asian (NH)' }).click()
+  await page
+    .getByRole('cell', { name: 'Black or African American (NH)' })
+    .click()
+})
