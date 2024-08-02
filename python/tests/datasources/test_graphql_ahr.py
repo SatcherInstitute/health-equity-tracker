144c11f6fd23787cb90a3f5e4e0376dcd632432a
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-02 10:06:30 -0600

    updates golden data; splits state level response inputs by category

--- python/tests/datasources/test_graphql_ahr.py
+++ python/tests/datasources/test_graphql_ahr.py
@@ -2,8 +2,7 @@ import json
 import os
 import pandas as pd
 from datasources.graphql_ahr import GraphQlAHRData
-from ingestion.constants import AGE, CURRENT, NATIONAL_LEVEL, SEX, STATE_LEVEL
-from ingestion.standardized_columns import RACE_OR_HISPANIC_COL, STATE_FIPS_COL
+from ingestion.standardized_columns import STATE_FIPS_COL
 from pandas._testing import assert_frame_equal
 from unittest import mock
 
@@ -39,86 +38,109 @@ def _fetch_ahr_data_from_graphql(demographic: str, geo_level: str, category: str
     return data
 
 
-# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
-# @mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
-# def testWriteToBqBehavioralHealthAgeNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
-#     datasource = GraphQlAHRData()
-#     datasource.write_to_bq(
-#         'dataset', 'gcs_bucket', demographic=AGE, geographic=NATIONAL_LEVEL, category='behavioral_health'
-#     )
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
+def testWriteToBqBehavioralHealthAgeNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
+    datasource = GraphQlAHRData()
+    datasource.write_to_bq(
+        'dataset', 'gcs_bucket', demographic='age', geographic='national', category='behavioral_health'
+    )
 
-#     actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
-#     expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
+    actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
+    expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
 
-#     assert table_name == f"behavioral_health_{AGE}_{NATIONAL_LEVEL}_{CURRENT}"
-#     assert mock_add_df_to_bq.call_count == 1
+    assert table_name == "behavioral_health_age_national_current"
+    assert mock_add_df_to_bq.call_count == 1
 
-#     actual_df.to_csv(table_name, index=False)
+    # actual_df.to_csv(table_name, index=False)
 
-#     print(actual_df.to_string())
+    # print(actual_df.to_string())
 
-#     assert_frame_equal(actual_df, expected_df, check_like=True)
-
-
-# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
-# @mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
-# def testWriteToBqNonBehavioralHealthAgeNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
-#     datasource = GraphQlAHRData()
-#     datasource.write_to_bq(
-#         'dataset', 'gcs_bucket', demographic=AGE, geographic=NATIONAL_LEVEL, category='non-behavioral_health'
-#     )
-
-#     actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
-#     expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
-
-#     assert table_name == f"non-behavioral_health_{AGE}_{NATIONAL_LEVEL}_{CURRENT}"
-#     assert mock_add_df_to_bq.call_count == 1
-
-#     actual_df.to_csv(table_name, index=False)
-
-#     print(actual_df.to_string())
-
-#     assert_frame_equal(actual_df, expected_df, check_like=True)
+    assert_frame_equal(actual_df, expected_df, check_like=True)
 
 
-# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
-# @mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
-# def testWriteToBqRaceState(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
-#     datasource = GraphQlAHRData()
-#     datasource.write_to_bq(
-#         'dataset', 'gcs_bucket', demographic=RACE_OR_HISPANIC_COL, geographic=STATE_LEVEL, category='behavioral_health'
-#     )
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
+def testWriteToBqNonBehavioralHealthAgeNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
+    datasource = GraphQlAHRData()
+    datasource.write_to_bq(
+        'dataset', 'gcs_bucket', demographic='age', geographic='national', category='non-behavioral_health'
+    )
 
-#     actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
-#     expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
+    actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
+    expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
 
-#     assert table_name == f"behavioral_health_{RACE_OR_HISPANIC_COL}_{STATE_LEVEL}_{CURRENT}"
-#     assert mock_add_df_to_bq.call_count == 1
+    assert table_name == "non-behavioral_health_age_national_current"
+    assert mock_add_df_to_bq.call_count == 1
 
-#     # actual_df.to_csv(table_name, index=False)
+    # actual_df.to_csv(table_name, index=False)
 
-#     assert_frame_equal(actual_df, expected_df, check_like=True)
+    # print(actual_df.to_string())
+
+    assert_frame_equal(actual_df, expected_df, check_like=True)
 
 
-# @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
-# @mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
-# def testWriteToBqBehavioralHealthSexNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
-#     datasource = GraphQlAHRData()
-#     datasource.write_to_bq(
-#         'dataset', 'gcs_bucket', demographic=SEX, geographic=NATIONAL_LEVEL, category='behavioral_health'
-#     )
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
+def testWriteToBqBehavioralHealthRaceState(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
+    datasource = GraphQlAHRData()
+    datasource.write_to_bq(
+        'dataset', 'gcs_bucket', demographic='race_and_ethnicity', geographic='state', category='behavioral_health'
+    )
 
-#     actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
-#     expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
+    actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
+    expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
 
-#     assert table_name == f"behavioral_health_{SEX}_{NATIONAL_LEVEL}_{CURRENT}"
-#     assert mock_add_df_to_bq.call_count == 1
+    assert table_name == "behavioral_health_race_and_ethnicity_state_current"
+    assert mock_add_df_to_bq.call_count == 1
 
-#     actual_df.to_csv(table_name, index=False)
+    # actual_df.to_csv(table_name, index=False)
 
-#     print(actual_df.to_string())
+    assert_frame_equal(actual_df, expected_df, check_like=True)
 
-#     assert_frame_equal(actual_df, expected_df, check_like=True)
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
+def testWriteToBqNonBehavioralHealthRaceState(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
+    datasource = GraphQlAHRData()
+    datasource.write_to_bq(
+        'dataset',
+        'gcs_bucket',
+        demographic='race_and_ethnicity',
+        geographic='state',
+        category='non-behavioral_health',
+    )
+
+    actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
+    expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
+
+    assert table_name == "non-behavioral_health_race_and_ethnicity_state_current"
+    assert mock_add_df_to_bq.call_count == 1
+
+    # actual_df.to_csv(table_name, index=False)
+
+    assert_frame_equal(actual_df, expected_df, check_like=True)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch('datasources.graphql_ahr.fetch_ahr_data_from_graphql', side_effect=_fetch_ahr_data_from_graphql)
+def testWriteToBqBehavioralHealthSexNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
+    datasource = GraphQlAHRData()
+    datasource.write_to_bq(
+        'dataset', 'gcs_bucket', demographic='sex', geographic='national', category='behavioral_health'
+    )
+
+    actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
+    expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
+
+    assert table_name == "behavioral_health_sex_national_current"
+    assert mock_add_df_to_bq.call_count == 1
+
+    # actual_df.to_csv(table_name, index=False)
+
+    # print(actual_df.to_string())
+
+    assert_frame_equal(actual_df, expected_df, check_like=True)
 
 
 @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
@@ -126,16 +148,16 @@ def _fetch_ahr_data_from_graphql(demographic: str, geo_level: str, category: str
 def testWriteToBqNonBehavioralHealthSexNational(_mock_fetch: mock.MagicMock, mock_add_df_to_bq: mock.MagicMock):
     datasource = GraphQlAHRData()
     datasource.write_to_bq(
-        'dataset', 'gcs_bucket', demographic=SEX, geographic=NATIONAL_LEVEL, category='non-behavioral_health'
+        'dataset', 'gcs_bucket', demographic='sex', geographic='national', category='non-behavioral_health'
     )
 
     actual_df, _, table_name = mock_add_df_to_bq.call_args_list[0][0]
     expected_df = pd.read_csv(GOLDEN_DATA[table_name], dtype={STATE_FIPS_COL: str})
 
-    assert table_name == f"non-behavioral_health_{SEX}_{NATIONAL_LEVEL}_{CURRENT}"
+    assert table_name == "non-behavioral_health_sex_national_current"
     assert mock_add_df_to_bq.call_count == 1
 
-    actual_df.to_csv(table_name, index=False)
-    print(actual_df.to_string())
+    # actual_df.to_csv(table_name, index=False)
+    # print(actual_df.to_string())
 
     assert_frame_equal(actual_df, expected_df, check_like=True)
