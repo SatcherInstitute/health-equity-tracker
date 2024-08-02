93f4bd011fea18a40e9ef5d7ff0bb345d7d52f37
kccrtv <kimcincocreative@gmail.com>
2024-08-02 11:49:51 -0600

    creates policy components

---
+++ frontend/src/pages/Policy/policySections/DataCollectionTab.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async";
+
+export default function DataCollectionTab() {
+return (
+<>
+            <Helmet>
+				<title>Data Collection - Health Equity Tracker</title>
+			</Helmet>
+				<h2 className='sr-only'>Data Collection</h2>
+</>
+)
+}
---
+++ frontend/src/pages/Policy/policySections/HowToUseTheDataTab.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async";
+
+export default function HowToUseTheDataTab() {
+return (
+<>
+            <Helmet>
+				<title>How To Use The Data - Health Equity Tracker</title>
+			</Helmet>
+				<h2 className='sr-only'>How To Use The Data</h2>
+</>
+)
+}
