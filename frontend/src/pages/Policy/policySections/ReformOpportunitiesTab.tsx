437ff61c8c8a509fefdcd9b5b32ec5a336b3b09b
kccrtv <kimcincocreative@gmail.com>
2024-08-05 09:27:05 -0600

    redo policy route configs

---
+++ frontend/src/pages/Policy/policySections/ReformOpportunitiesTab.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
+export default function ReformOpportunitiesTab() {
+	return (
+		<>
+			<Helmet>
+				<title>Reform Opportunities - Health Equity Tracker</title>
+			</Helmet>
+			<h2 className='sr-only'>Reform Opportunities</h2>
+		</>
+	)
+}
