a779336c4ea0ba0a47a564bd988f6687e381f741
kccrtv <kimcincocreative@gmail.com>
2024-08-06 11:35:08 -0600

    refactors sublink menu component

--- frontend/src/pages/Policy/policyComponents/PolicyPage.tsx
+++ frontend/src/pages/Policy/policyComponents/PolicyPage.tsx
@@ -1,7 +1,8 @@
 import { Helmet } from 'react-helmet-async'
-import { Route, Switch, useLocation } from 'react-router-dom'
+import { Route, Switch, useLocation, useRouteMatch } from 'react-router-dom'
 import { routeConfigs } from '../policyContent/routeConfigs'
 import { GUN_VIOLENCE_POLICY } from '../../../utils/internalRoutes'
+import HetOnThisPageMenu from '../../../styles/HetComponents/HetOnThisPageMenu'
 
 export default function PolicyPage() {
   const location = useLocation()
@@ -23,6 +24,26 @@ export default function PolicyPage() {
 						Gun Violence Section
 					</a>
 
+					<div className='flex flex-wrap grow p-1'>
+						{/* ON THIS PAGE SUB-MENU - MOBILE/TABLET */}
+						<div className='px-12 md:hidden'>
+							{routeConfigs.map((routeConfig) => {
+								const match = useRouteMatch({
+									path: routeConfig.path,
+									exact: true,
+								});
+								const hasSublinks =
+									routeConfig.subLinks && routeConfig.subLinks.length > 0;
+								return match && hasSublinks ? (
+									<HetOnThisPageMenu
+										key={routeConfig.path}
+										links={routeConfig.subLinks}
+									/>
+								) : null;
+							})}
+						</div>
+						</div>
+					
 					<section className='m-[2%] max-w-lgXl flex flex-col grow smMd:flex-row smMd:gap-2 md:gap-12'>
 						<div className='flex flex-wrap grow p-1'>
 							<article className='flex w-full flex-col p-8 text-left md:p-0 '>
@@ -43,6 +64,24 @@ export default function PolicyPage() {
 							</article>
 						</div>
 					</section>
+					{/* ON THIS PAGE SUB-MENU - DESKTOP */}
+					<div className='hidden min-w-fit md:block'>
+						{routeConfigs.map((routeConfig) => {
+							const match = useRouteMatch({
+								path: routeConfig.path,
+								exact: true,
+							});
+							const hasSublinks =
+								routeConfig.subLinks && routeConfig.subLinks.length > 0;
+							return match && hasSublinks ? (
+								<HetOnThisPageMenu
+									key={routeConfig.path}
+									links={routeConfig.subLinks}
+									className='sticky right-0 top-4  z-top h-min'
+								/>
+							) : null;
+						})}
+					</div>
 				</div>
 			</>
 		)
