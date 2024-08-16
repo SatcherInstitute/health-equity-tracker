e727531b644e5a0012b9c848ecda8190076d1124
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-16 12:23:39 -0600

    terraform

--- config/composer.tf
+++ config/composer.tf
@@ -1,11 +1,30 @@
+# variable "include_composer" {
+#   default = true
+# }
+
+# resource "google_composer_environment" "composer-env" {
+#   count  = var.include_composer ? 1 : 0
+#   name   = "data-ingestion-environment"
+#   region = var.compute_region
+
+#   storage_config {
+#     # Generate a stable bucket name based on the project ID
+#     bucket = "us-central1-data-ingestion-bucket-${var.project_id}"
+#   }
+# }
+
 variable "include_composer" {
   default = true
 }
 
-resource "google_composer_environment" "composer-env" {
-  count  = var.include_composer ? 1 : 0
-  name   = "data-ingestion-environment"
-  region = var.compute_region
+resource "google_composer_environment_v3" "composer-env" {
+  count              = var.include_composer ? 1 : 0
+  name               = "data-ingestion-environment"
+  region             = var.compute_region
+
+  environment_config {
+    image_version = "composer-3.0.0-airflow-2.4.4"
+  }
 
   storage_config {
     # Generate a stable bucket name based on the project ID
