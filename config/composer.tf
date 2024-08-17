a92c2abc90a714ba65124d0ff9973ef42c213954
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-16 19:57:37 -0600

    try composer 3 in github action and terraform config

--- config/composer.tf
+++ config/composer.tf
@@ -2,13 +2,21 @@ variable "include_composer" {
   default = true
 }
 
-resource "google_composer_environment" "composer-env" {
+resource "google_composer_environment_v3" "composer-env" {
   count  = var.include_composer ? 1 : 0
   name   = "data-ingestion-environment"
   region = var.compute_region
 
-  storage_config {
-    # Generate a stable bucket name based on the project ID
-    bucket = "us-central1-data-ingestion-bucket-${var.project_id}"
+  environment_config {
+    software_config {
+      image_version = "composer-3-airflow-2.9.1"
+    }
+    storage_config {
+      # Generate a stable bucket name based on the project ID
+      bucket = "us-central1-data-ingestion-bucket-${var.project_id}"
+    }
+    node_config {
+      service_account = "test-auto-deployer@het-infra-test-05.iam.gserviceaccount.com"
+    }
   }
 }
