7418b20009ba96992be3bd441124003a43ba8343
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-16 20:56:31 -0600

    3

--- config/composer.tf
+++ config/composer.tf
@@ -1,22 +1,18 @@
 variable "include_composer" {
   default = true
 }
-
-resource "google_composer_environment_v3" "composer-env" {
-  count  = var.include_composer ? 1 : 0
+resource "google_composer_environment" "composer_env" {
   name   = "data-ingestion-environment"
-  region = var.compute_region
+  region = "us-central1"
 
-  environment_config {
+  config {
     software_config {
       image_version = "composer-3-airflow-2.9.1"
     }
-    storage_config {
-      # Generate a stable bucket name based on the project ID
-      bucket = "us-central1-data-ingestion-bucket-${var.project_id}"
-    }
+
     node_config {
       service_account = "test-auto-deployer@het-infra-test-05.iam.gserviceaccount.com"
     }
+
   }
 }
