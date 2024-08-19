0dc8b4654004f503609267281bb0957ac69c2138
kccrtv <kimcincocreative@gmail.com>
2024-08-19 15:00:04 -0600

    current efforts tab and reform opprotunities tab content

--- frontend/src/pages/Policy/policySections/ReformOpportunitiesTab.tsx
+++ frontend/src/pages/Policy/policySections/ReformOpportunitiesTab.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import ResourceItem from '../policyComponents/ResourceItem'
+<<<<<<< HEAD
 import {
   effortsAndInitiatives,
   legislativeActions,
@@ -64,3 +65,64 @@ export default function ReformOpportunitiesTab() {
     </>
   )
 }
+=======
+import { effortsAndInitiatives, legislativeActions } from '../policyContent/ReformOpportunitiesContent'
+import { OpenInNew } from '@mui/icons-material'
+
+export default function ReformOpportunitiesTab() {
+	return (
+		<>
+			<Helmet>
+				<title>Reform Opportunities - Health Equity Tracker</title>
+			</Helmet>
+			<h2 className='sr-only'>Reform Opportunities</h2>
+			<p>
+				Our data points to several reform opportunities, particularly in
+				enhancing community-based interventions, improving data collection on
+				race and ethnicity for non-fatal injuries, and addressing the root
+				causes of violence through equity-focused policies.
+			</p>
+			<section id='#city-and-county-level-reform-opportunities'>
+				<h3 className='my-0 text-title font-medium text-altGreen'>
+					Reform Opportunities at the County and City Levels
+				</h3>
+				<p>
+					<ul className='list-none'>
+						{effortsAndInitiatives.map((effortsAndInitiative, index) => (
+							<ResourceItem
+								key={index}
+								title={effortsAndInitiative.title}
+								description={effortsAndInitiative.description}
+								link={effortsAndInitiative.link}
+							/>
+						))}
+					</ul>
+				</p>
+			</section>
+			<section id='#call-to-action-for-policy-changes'>
+				<h3 className='my-0 text-title font-medium text-altGreen'>
+					Call to Action for Policy Changes
+				</h3>
+				<p>
+					<ul className='list-none'>
+						{legislativeActions.map((legislativeAction, index) => (
+							<ResourceItem
+								key={index}
+								title={legislativeAction.title}
+								description={legislativeAction.description}
+								link={legislativeAction.link}
+							/>
+						))}
+					</ul>
+				</p>
+				<div className='rounded-md border border-solid border-methodologyGreen md:mx-2 m-2 duration-300 ease-in-out hover:shadow-raised shadow-raised-tighter group bg-hoverAltGreen hover:bg-whiteSmoke80 flex flex-col align-center text-exploreButton p-4 group no-underline hover:scale-105 hover:transition-transform hover:duration-30'>
+					<p className='text-smallest font-semibold tracking-normal'>
+						Find and contact your elected officials
+						<OpenInNew className='text-text' />
+					</p>
+				</div>
+			</section>
+		</>
+	)
+}
+>>>>>>> 72d2bc72 (current efforts tab and reform opprotunities tab content)
