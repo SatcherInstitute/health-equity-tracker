1ee298c64a24dfa999d144b13a942236be9c78d3
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-20 08:43:31 -0600

    Backend: Adds Phrma cancer screening pipeline (#3567)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - part of #3547 
    - more shared phrma utils
    - generates all breakdowns without crashing: age / race / education /
    income / insurance status X national / state
    
    ## Has this been tested? How?
    
    - initial tests scaffolded out and each breakdown runs (and properly
    created .csv files for checks and for later conversion into golden_data)
    
    ## Screenshots
    
    pandas printout of tests running and printing the df per breakdown
    <img width="1454" alt="Screenshot 2024-08-20 at 5 19 53 AM"
    src="https://github.com/user-attachments/assets/8e15177e-ad2e-4460-abb9-c05d742bab52">
    
    
    ## Types of changes
    
    (leave all that apply)
    
    - New content or feature
    
    ## New frontend preview link is below in the Netlify comment 😎

--- python/tests/datasources/test_phrma_brfss.py
+++ python/tests/datasources/test_phrma_brfss.py
@@ -1,5 +1,6 @@
 from unittest import mock
-from datasources.phrma_brfss import PhrmaBrfssData, PHRMA_DIR
+from datasources.phrma_brfss import PhrmaBrfssData
+from ingestion.phrma_utils import PHRMA_DIR
 import os
 from test_utils import _load_csv_as_df_from_real_data_dir
 
@@ -13,7 +14,7 @@ GOLDEN_DATA = {
 }
 
 
-# # # # BREAKDOWN TESTS
+# # # BREAKDOWN TESTS
 
 
 @mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
@@ -32,6 +33,195 @@ def testBreakdownRaceNational(
 
     (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
     assert table_name == 'race_and_ethnicity_national'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownRaceState(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="race_and_ethnicity", geographic="state")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'race_and_ethnicity_state'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownAgeNational(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="age", geographic="national")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'age_national'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownAgeState(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="age", geographic="state")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'age_state'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownInsuranceNational(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="insurance_status", geographic="national")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'insurance_status_national'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownInsuranceState(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="insurance_status", geographic="state")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'insurance_status_state'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownEducationNational(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="education", geographic="national")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'education_national'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownEducationState(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="education", geographic="state")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'education_state'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownIncomeNational(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="income", geographic="national")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'income_national'
+    print("\ntable_name:", table_name)
+    print(breakdown_df)
+    # breakdown_df.to_csv(table_name, index=False)
+
+
+@mock.patch('ingestion.gcs_to_bq_util.add_df_to_bq', return_value=None)
+@mock.patch(
+    'ingestion.gcs_to_bq_util.load_csv_as_df_from_data_dir',
+    side_effect=_load_csv_as_df_from_real_data_dir,
+)
+def testBreakdownIncomeState(
+    mock_data_dir: mock.MagicMock,
+    mock_bq_write: mock.MagicMock,
+):
+    datasource = PhrmaBrfssData()
+    datasource.write_to_bq("dataset", "gcs_bucket", demographic="income", geographic="state")
+
+    assert mock_data_dir.called
+
+    (breakdown_df, _dataset, table_name), _dtypes = mock_bq_write.call_args
+    assert table_name == 'income_state'
     print("\ntable_name:", table_name)
     print(breakdown_df)
     # breakdown_df.to_csv(table_name, index=False)
