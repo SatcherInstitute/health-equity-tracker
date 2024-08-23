29bef76772c37054b873cb32079d74540dce1801
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-22 21:23:28 -0600

    adds pct_shares

--- python/datasources/phrma_brfss.py
+++ python/datasources/phrma_brfss.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from datasources.data_source import DataSource
-from ingestion.constants import NATIONAL_LEVEL, ALL_VALUE, US_NAME
-
+from ingestion.constants import NATIONAL_LEVEL, ALL_VALUE, US_NAME, UNKNOWN
 from ingestion import gcs_to_bq_util, standardized_columns as std_col
 from ingestion.merge_utils import merge_state_ids
+from ingestion.dataset_utils import (
+    generate_pct_share_col_without_unknowns,
+    generate_pct_share_col_with_unknowns,
+    build_bq_col_types,
+)
 from ingestion.het_types import (
     GEO_TYPE,
     PHRMA_BREAKDOWN_TYPE,
@@ -48,9 +52,8 @@ class PhrmaBrfssData(DataSource):
 
         df = self.generate_breakdown_df(demo_type, geo_level)
 
-        float_cols = []
-        col_types = gcs_to_bq_util.get_bq_column_types(df, float_cols)
-        gcs_to_bq_util.add_df_to_bq(df, dataset, table_name, column_types=col_types)
+        bq_col_types = build_bq_col_types(df)
+        gcs_to_bq_util.add_df_to_bq(df, dataset, table_name, column_types=bq_col_types)
 
     def generate_breakdown_df(
         self,
@@ -88,17 +91,36 @@ class PhrmaBrfssData(DataSource):
 
         # rename count cols
         rename_col_map = {}
+        count_to_pct_share_map = {}
         for condition in PHRMA_CANCER_PCT_CONDITIONS:
             cancer_type = condition.lower()
             rate_numerator = f'{condition}_{COUNT_YES_LOWER}'
             rate_denominator = f'{condition}_{COUNT_TOTAL_LOWER}'
             rename_col_map[rate_numerator] = f'{cancer_type}_{SCREENED}_{std_col.RAW_SUFFIX}'
+            count_to_pct_share_map[
+                f'{cancer_type}_{SCREENED}_{std_col.RAW_SUFFIX}'
+            ] = f'{cancer_type}_{SCREENED}_{std_col.PCT_SHARE_SUFFIX}'
             rename_col_map[rate_denominator] = f'{cancer_type}_{SCREENING_ELIGIBLE}_{std_col.RAW_SUFFIX}'
-
         df = df.rename(columns=rename_col_map)
 
         if demo_breakdown == std_col.RACE_OR_HISPANIC_COL:
             std_col.add_race_columns_from_category_id(df)
+
+        if demo_breakdown in [std_col.RACE_OR_HISPANIC_COL, std_col.AGE_COL]:
+            df = generate_pct_share_col_without_unknowns(
+                df,
+                count_to_pct_share_map,
+                demo_breakdown,
+                ALL_VALUE,
+            )
+        else:
+            df = generate_pct_share_col_with_unknowns(
+                df,
+                count_to_pct_share_map,
+                demo_breakdown,
+                ALL_VALUE,
+                UNKNOWN,
+            )
 
         df = df.sort_values(by=[std_col.STATE_FIPS_COL, demo_col]).reset_index(drop=True)
 
