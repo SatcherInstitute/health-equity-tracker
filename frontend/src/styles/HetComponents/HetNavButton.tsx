b07d2d721648344ec19cd403dccbe5cca46cb0b1
kccrtv <kimcincocreative@gmail.com>
2024-08-26 17:28:36 -0600

    create hetnavbutton, start adding menu folders

---
+++ frontend/src/styles/HetComponents/HetNavButton.tsx
@@ -0,0 +1,31 @@
+import type React from 'react'
+import { Button } from '@mui/material'
+import {ExpandMore} from '@mui/icons-material'
+
+interface HetNavButtonProps {
+  label: string
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  className?: string
+  isExpanded?: boolean
+}
+
+const HetNavButton: React.FC<HetNavButtonProps> = ({
+  label,
+  onClick,
+  className = '',
+  isExpanded = false,
+}) => {
+  return (
+    <Button
+      className={`font-sansTitle text-small font-medium text-navlinkColor ${className}`}
+      onClick={onClick}
+      endIcon={<ExpandMore />}
+      aria-haspopup='true'
+      aria-expanded={isExpanded}
+    >
+      {label}
+    </Button>
+  )
+}
+
+export default HetNavButton
