054bddcafe47903fd56bdbfbd6d953b4a6fb6123
James Demlow <jdemlow@gmail.com>
2024-08-28 11:27:20 -0600

    Frontend: Sets up MetricConfig for Maternal Mortality (#3534)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    Closes #3306 
    
    ## Has this been tested? How?
    
    Using integrated terminal and browser
    
    ## Screenshots (if appropriate)
    
    ![Screenshot
    (133)](https://github.com/user-attachments/assets/8887d8b7-5a4c-455b-b53a-d85bfb790056)
    
    
    ## Types of changes
    
    (leave all that apply)
    
    
    - New content or feature
    
    
    ## New frontend preview link is below in the Netlify comment 😎
    
    ---------
    
    Co-authored-by: Ben Hammond <benjamin.hammond@gmail.com>

--- frontend/src/utils/hooks/useScrollToAnchor.tsx
+++ frontend/src/utils/hooks/useScrollToAnchor.tsx
@@ -1,19 +1,17 @@
 import { useCallback } from 'react'
 
 export function useScrollToAnchor() {
-		const scrollToAnchor = useCallback((id: string, offset: number = -70) => {
-			const element = document.getElementById(id)
+  const scrollToAnchor = useCallback((id: string, offset: number = -70) => {
+    const element = document.getElementById(id)
 
-			if (element) {
-				const yOffset = offset
-				const y =
-					element.getBoundingClientRect().top + window.pageYOffset + yOffset
+    if (element) {
+      const yOffset = offset
+      const y =
+        element.getBoundingClientRect().top + window.pageYOffset + yOffset
 
-				window.scrollTo({ top: y, behavior: 'smooth' })
-			} else {
-				console.warn(`Element with id ${id} not found.`)
-			}
-		}, [])
+      window.scrollTo({ top: y, behavior: 'smooth' })
+    }
+  }, [])
 
-		return scrollToAnchor
-	}
+  return scrollToAnchor
+}
