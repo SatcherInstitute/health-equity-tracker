eddb29ed5ba8d2c024377b7a3abd9d28df9e9033
Ben Hammond <benjamin.hammond@gmail.com>
2024-08-29 10:12:08 -0600

    class

--- python/tests/datasources/test_data_source.py
+++ python/tests/datasources/test_data_source.py
@@ -3,15 +3,19 @@ import pandas as pd
 from datasources.data_source import DataSource
 
 
+class TestDataSource(DataSource):
+    def get_id(self):
+        return "test_id"
+
+    def get_table_name(self):
+        return "test_table"
+
+
 def testCleanFrameColumnNames():
-    ds = DataSource()
+    ds = TestDataSource()
     df = pd.DataFrame(
         # /* cSpell:disable */
-        {'Upp3rcase': [],
-         'Special!char': [],
-         'this=that': [],
-         '%count': [],
-         'with SPACES': []})
+        {'Upp3rcase': [], 'Special!char': [], 'this=that': [], '%count': [], 'with SPACES': []}
+    )
     ds.clean_frame_column_names(df)
-    assert set(df.columns) == set(['upp3rcase', 'special_char', 'thiseqthat',
-                                   'pctcount', 'with_spaces'])
+    assert set(df.columns) == set(['upp3rcase', 'special_char', 'thiseqthat', 'pctcount', 'with_spaces'])
