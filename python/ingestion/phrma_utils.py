8f746ad89e828d0d959dc3a92b8d19189753fa7a
Ben Hammond <benjamin.hammond@gmail.com>
2024-09-02 17:31:19 -0600

    typing

--- python/ingestion/phrma_utils.py
+++ python/ingestion/phrma_utils.py
@@ -1,16 +1,16 @@
-from ingestion.het_types import GEO_TYPE, PHRMA_BREAKDOWN_TYPE_OR_ALL, SEX_RACE_ETH_AGE_TYPE
+from ingestion.het_types import GEO_TYPE, PHRMA_BREAKDOWN_TYPE_OR_ALL, SEX_RACE_ETH_AGE_TYPE, PHRMA_DATASET_TYPE
 from ingestion import gcs_to_bq_util, dataset_utils
 import ingestion.standardized_columns as std_col
 from ingestion.constants import STATE_LEVEL, COUNTY_LEVEL, NATIONAL_LEVEL, US_FIPS
 import pandas as pd
 import numpy as np
-from typing import Dict, Literal, cast, List
+from typing import Dict, cast, List
 from ingestion.merge_utils import merge_dfs_list
 
-PHRMA_BRFSS = 'brfss'
-PHRMA_MEDICARE = 'medicare'
+PHRMA_BRFSS: PHRMA_DATASET_TYPE = 'brfss'
+PHRMA_MEDICARE: PHRMA_DATASET_TYPE = 'medicare'
 
-TMP_ALL = 'all'
+TMP_ALL: PHRMA_BREAKDOWN_TYPE_OR_ALL = 'all'
 PHRMA_DIR = 'phrma'
 
 ADHERENCE = 'adherence'
@@ -254,14 +254,14 @@ DTYPE = {'COUNTY_FIPS': str, 'STATE_FIPS': str}
 def load_phrma_df_from_data_dir(
     geo_level: GEO_TYPE,
     breakdown: PHRMA_BREAKDOWN_TYPE_OR_ALL,
-    data_type: Literal[PHRMA_MEDICARE, PHRMA_BRFSS],
+    dataset_type: PHRMA_DATASET_TYPE,
     conditions: List[str],
 ) -> pd.DataFrame:
     """Generates Phrma data by breakdown and geo_level
     geo_level: string equal to `county`, `national`, or `state`
     breakdown: string equal to `age`, `race_and_ethnicity`, `sex`, `lis`, `eligibility`,
      `insurance_status`, `education`, `income`, or `all`
-    data_type: string equal to PHRMA_MEDICARE or PHRMA_BRFSS to determine which data to process
+    dataset_type: string equal to PHRMA_MEDICARE or PHRMA_BRFSS to determine which data to process
     return: a single data frame of data by demographic breakdown and
         geo_level with data columns loaded from multiple Phrma source tables"""
 
@@ -279,9 +279,9 @@ def load_phrma_df_from_data_dir(
     fips_col = std_col.COUNTY_FIPS_COL if geo_level == COUNTY_LEVEL else std_col.STATE_FIPS_COL
 
     breakdown_het_to_source_type = {
-        "age": AGE_GROUP if data_type == PHRMA_MEDICARE else AGE_GROUP_LOWER,
-        "race_and_ethnicity": RACE_NAME if data_type == PHRMA_MEDICARE else RACE_NAME_LOWER,
-        "sex": SEX_NAME if data_type == PHRMA_MEDICARE else SEX_NAME_LOWER,
+        "age": AGE_GROUP if dataset_type == PHRMA_MEDICARE else AGE_GROUP_LOWER,
+        "race_and_ethnicity": RACE_NAME if dataset_type == PHRMA_MEDICARE else RACE_NAME_LOWER,
+        "sex": SEX_NAME if dataset_type == PHRMA_MEDICARE else SEX_NAME_LOWER,
         "lis": LIS,
         "eligibility": ENTLMT_RSN_CURR,
         "income": INCOME_GROUP_LOWER,
@@ -301,7 +301,7 @@ def load_phrma_df_from_data_dir(
         keep_cols.append(COUNTY_FIPS)
     if geo_level == STATE_LEVEL:
         fips_length = 2
-        keep_cols.append(STATE_FIPS if data_type == PHRMA_MEDICARE else STATE_FIPS_LOWER)
+        keep_cols.append(STATE_FIPS if dataset_type == PHRMA_MEDICARE else STATE_FIPS_LOWER)
     if geo_level == NATIONAL_LEVEL:
         fips_length = 2
 
@@ -309,7 +309,7 @@ def load_phrma_df_from_data_dir(
     condition_keep_cols = []
 
     for condition in conditions:
-        if data_type == PHRMA_MEDICARE:
+        if dataset_type == PHRMA_MEDICARE:
             if condition in PHRMA_PCT_CONDITIONS:
                 condition_keep_cols = [*keep_cols, COUNT_YES, COUNT_TOTAL, ADHERENCE_RATE]
             elif condition in PHRMA_100K_CONDITIONS:
@@ -332,7 +332,7 @@ def load_phrma_df_from_data_dir(
             if breakdown == std_col.RACE_OR_HISPANIC_COL:
                 condition_keep_cols.append(AGE_ADJ_RATE_LOWER)
 
-        if data_type == PHRMA_MEDICARE:
+        if dataset_type == PHRMA_MEDICARE:
             file_name = f'{condition}-{sheet_name}.csv'
             subdirectory = condition
         else:  # cancer
