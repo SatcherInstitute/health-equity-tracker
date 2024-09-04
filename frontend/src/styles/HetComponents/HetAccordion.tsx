5bea1d41a882a0476e42bb759c2b2e339f5bfbd9
kccrtv <kimcincocreative@gmail.com>
2024-09-04 16:41:32 -0600

    create HetAccordion component

---
+++ frontend/src/styles/HetComponents/HetAccordion.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+interface AccordionData {
+  question: string
+  answer: React.ReactNode
+}
+
+interface HetAccordionProps {
+  accordionData: AccordionData[] 
+  divClassName?: string
+  accordionClassName?: string
+  summaryClassName?: string
+detailsClassName?: string
+}
+
+const HetAccordion: React.FC<HetAccordionProps> = ({ accordionData, divClassName, accordionClassName, summaryClassName, detailsClassName }) => {
+  const [expanded, setExpanded] = useState<string | false>(false)
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false)
+    }
+
+  return (
+    <article className='grid'>
+      <div className={`${divClassName ?? 'py-4'}`}>
+        {accordionData.map((data, index) => (
+          <Accordion
+            key={data.question}
+            expanded={expanded === `panel${index}`}
+            onChange={handleChange(`panel${index}`)}
+            sx={{
+              border: 'none',
+              boxShadow: 'none',
+              '&::before': {
+                content: 'none',
+              },
+            }}
+            className={`list-none rounded-md border border-solid border-methodologyGreen ${accordionClassName ?? 'mb-8'}`}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel${index + 1}-content`}
+              id={`panel${index + 1}-header`}
+              className={`${
+                expanded === `panel${index}` ? 'bg-hoverAltGreen rounded-t-md' : 'hover:bg-whiteSmoke80'
+              }`}
+            >
+              <div className={`my-0 ${summaryClassName ?? 'py-4 text-title font-medium text-altBlack'}`}>
+                {data.question}
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className={`text-left text-text font-normal text-altBlack ${detailsClassName ?? 'my-0 px-6 py-4'}`}>
+                {data.answer}
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </article>
+  )
+}
+
+export default HetAccordion
