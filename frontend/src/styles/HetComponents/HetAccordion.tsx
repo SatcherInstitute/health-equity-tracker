b8e06f2627969d812f88ef79491d473d6a1176a1
kccrtv <kimcincocreative@gmail.com>
2024-09-04 16:41:32 -0600

    create HetAccordion component

--- frontend/src/styles/HetComponents/HetAccordion.tsx
+++ frontend/src/styles/HetComponents/HetAccordion.tsx
@@ -27,7 +27,11 @@ const HetAccordion: React.FC<HetAccordionProps> = ({ accordionData, divClassName
     }
 
   return (
+<<<<<<< HEAD
     <article className='grid mx-4'>
+=======
+    <article className='grid'>
+>>>>>>> 5bea1d41 (create HetAccordion component)
       <div className={`${divClassName ?? 'py-4'}`}>
         {accordionData.map((data, index) => (
           <Accordion
@@ -51,7 +55,11 @@ const HetAccordion: React.FC<HetAccordionProps> = ({ accordionData, divClassName
                 expanded === `panel${index}` ? 'bg-hoverAltGreen rounded-t-md' : 'hover:bg-whiteSmoke80'
               }`}
             >
+<<<<<<< HEAD
               <div className={`my-0 ${summaryClassName ?? 'py-4 text-title font-medium text-altBlack leading-lhSomeSpace'}`}>
+=======
+              <div className={`my-0 ${summaryClassName ?? 'py-4 text-title font-medium text-altBlack'}`}>
+>>>>>>> 5bea1d41 (create HetAccordion component)
                 {data.question}
               </div>
             </AccordionSummary>
