5a8792b2f950cee48bff90320af8c580027f6741
kccrtv <kimcincocreative@gmail.com>
2024-09-06 10:14:33 -0600

    updated hettermunderline

--- frontend/src/pages/Policy/policyContent/CrisisOverviewContent.tsx
+++ frontend/src/pages/Policy/policyContent/CrisisOverviewContent.tsx
@@ -4,8 +4,7 @@ export const gvaFacts = [
 	{
 		content: (
 			<>
-				In 2023, Atlanta experienced {' '}
-				<HetTermUnderline>no fewer than six mass shootings,</HetTermUnderline> with each
+				In 2023, Atlanta experienced <HetTermUnderline>no fewer than six mass shootings</HetTermUnderline>, with each
 				event tragically claiming six lives and injuring 22 others, marking a
 				series of deliberate, targeted attacks that shook the community.
 			</>
@@ -27,14 +26,14 @@ export const rocketFoundationFacts = [
 	{
 		content: (
 			<>
-				Gun violence in the U.S. is a growing crisis, claiming over 47,000 lives in 2021 alone, with {' '}<HetTermUnderline>more than half being homicides</HetTermUnderline>. This epidemic disproportionately affects people of color, women, and children, making guns the leading cause of death among American youth.
+				Gun violence in the U.S. is a growing crisis, claiming over 47,000 lives in 2021 alone, with <HetTermUnderline>more than half being homicides</HetTermUnderline>. This epidemic disproportionately affects people of color, women, and children, making guns the leading cause of death among American youth.
 			</>
 		),
 	},
 	{
 		content: (
 			<>
-				While the U.S. has an alarmingly high rate of gun-related homicides—22 times higher than in the European Union—<HetTermUnderline>this violence is concentrated in specific neighborhoods</HetTermUnderline>. By focusing our efforts on these areas, we can begin to solve the crisis one city at a time, starting with {' '}<HetTermUnderline>places like Atlanta</HetTermUnderline>.
+				While the U.S. has an alarmingly high rate of gun-related homicides—22 times higher than in the European Union—<HetTermUnderline>this violence is concentrated in specific neighborhoods</HetTermUnderline>. By focusing our efforts on these areas, we can begin to solve the crisis one city at a time, starting with <HetTermUnderline>places like Atlanta</HetTermUnderline>.
 			</>
 		),
 	},
