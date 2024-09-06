f88ef3cb9392badaca9178ab3f7afd469f8e6182
kccrtv <kimcincocreative@gmail.com>
2024-09-06 11:00:43 -0600

    restore simplehorizontalbarchart

--- frontend/src/charts/SimpleHorizontalBarChart.tsx
+++ frontend/src/charts/SimpleHorizontalBarChart.tsx
@@ -15,12 +15,19 @@ import {
   addMetricDisplayColumn,
   PADDING_FOR_ACTIONS_MENU,
   LABEL_HEIGHT,
+  CORNER_RADIUS,
 } from './utils'
 import { createBarLabel } from './mapHelperFunctions'
 import { het, ThemeZIndexValues } from '../styles/DesignTokens'
+import { sortForVegaByIncome } from '../data/sorting/IncomeSorterStrategy'
 
 // determine where (out of 100) to flip labels inside/outside the bar
 const LABEL_SWAP_CUTOFF_PERCENT = 66
+const MEASURE_GROUP_COLOR = het.altGreen
+const MEASURE_ALL_COLOR = het.timeYellow
+const BAR_HEIGHT = 60
+const BAR_PADDING = 0.2
+const DATASET = 'DATASET'
 
 function getSpec(
   altText: string,
@@ -38,10 +45,6 @@ function getSpec(
   barLabelBreakpoint: number,
   usePercentSuffix: boolean,
 ): any {
-  const MEASURE_COLOR = het.altGreen
-  const BAR_HEIGHT = 60
-  const BAR_PADDING = 0.2
-  const DATASET = 'DATASET'
   const chartIsSmall = width < 400
 
   const createAxisTitle = () => {
@@ -92,7 +95,7 @@ function getSpec(
       },
       {
         name: 'height',
-        update: "bandspace(domain('y').length, 0.1, 0.05) * y_step",
+        update: "bandspace(domain('y').length, 0.1, 0.05) * y_step + 10",
       },
     ],
     marks: [
@@ -112,10 +115,25 @@ function getSpec(
             },
           },
           update: {
-            fill: { value: MEASURE_COLOR },
+            cornerRadiusTopRight: {
+              value: CORNER_RADIUS,
+            },
+            cornerRadiusBottomRight: {
+              value: CORNER_RADIUS,
+            },
+            fill: {
+              signal: `datum.${demographicType} === 'All' ? '${MEASURE_ALL_COLOR}' : '${MEASURE_GROUP_COLOR}'`,
+            },
             x: { scale: 'x', field: measure },
             x2: { scale: 'x', value: 0 },
-            y: { scale: 'y', field: demographicType },
+            y: {
+              scale: 'y',
+              field: demographicType,
+              // band: 1,
+              offset: {
+                signal: `datum.${demographicType} === 'All' ? 0 : 10`,
+              },
+            },
             height: { scale: 'y', band: 1 },
           },
         },
@@ -140,12 +158,13 @@ function getSpec(
           },
         },
       },
+      // Labels on Bars
       {
         name: 'measure_text_labels',
         type: 'text',
         style: ['text'],
         from: { data: DATASET },
-        aria: false, // this data accessible in alt_text_labels
+        aria: false, // this data already accessible in alt_text_labels above
         encode: {
           enter: {
             tooltip: {
@@ -169,10 +188,17 @@ function getSpec(
               signal: chartIsSmall ? -15 : 0,
             },
             fill: {
-              signal: `if(datum.${measure} > ${barLabelBreakpoint}, "white", "black")`,
+              signal: `if(datum.${measure} > ${barLabelBreakpoint}  && datum.${demographicType} !== 'All', '${het.white}', '${het.black}')`,
             },
             x: { scale: 'x', field: measure },
-            y: { scale: 'y', field: demographicType, band: 0.8 },
+            y: {
+              scale: 'y',
+              field: demographicType,
+              band: 0.8,
+              offset: {
+                signal: `datum.${demographicType} === 'All' ? 0 : 10`,
+              },
+            },
             limit: { signal: 'width / 3' },
             text: {
               signal: barLabel,
@@ -203,13 +229,15 @@ function getSpec(
           field: demographicType,
         },
         range: { step: { signal: 'y_step' } },
+        paddingOuter: 0.1,
         paddingInner: BAR_PADDING,
       },
+
       {
         name: 'variables',
         type: 'ordinal',
         domain: [measureDisplayName],
-        range: [MEASURE_COLOR],
+        range: [MEASURE_GROUP_COLOR, MEASURE_ALL_COLOR],
       },
     ],
     axes: [
@@ -254,7 +282,9 @@ function getSpec(
               baseline: { value: 'bottom' },
               // Limit at which line is truncated with an ellipsis
               limit: { value: 100 },
-              dy: { signal: AXIS_LABEL_Y_DELTA },
+              dy: {
+                signal: `datum.demographicType !== 'All' ? 5 : ${AXIS_LABEL_Y_DELTA}`, // Adjust based on AXIS_LABEL_Y_DELTA
+              },
               lineHeight: { signal: LABEL_HEIGHT },
             },
           },
@@ -284,18 +314,22 @@ export function SimpleHorizontalBarChart(props: SimpleHorizontalBarChartProps) {
   const [dataWithDisplayCol, barMetricDisplayColumnName] =
     addMetricDisplayColumn(props.metric, dataWithLineBreakDelimiter)
   // Omit the % symbol for the tooltip because it's included in shortLabel.
-  const [data, tooltipMetricDisplayColumnName] = addMetricDisplayColumn(
+  let [data, tooltipMetricDisplayColumnName] = addMetricDisplayColumn(
     props.metric,
     dataWithDisplayCol,
     /* omitPctSymbol= */ true,
   )
+
+  if (props.demographicType === 'income') {
+    data = sortForVegaByIncome(data)
+  }
 
   const barLabelBreakpoint =
     Math.max(...props.data.map((row) => row[props.metric.metricId])) *
     (LABEL_SWAP_CUTOFF_PERCENT / 100)
 
   return (
-    <div className='w-full' ref={ref}>
+    <div ref={ref}>
       <Vega
         renderer='svg'
         downloadFileName={`${
