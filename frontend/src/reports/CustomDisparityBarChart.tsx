33cd22ca22988f16293bf30db24700975a87d803
kccrtv <67879386+kccrtv@users.noreply.github.com>
2024-09-09 12:03:42 -0600

    Adds `className` and refactors various chart and table cards (#3632)
    # Description and Motivation
    - Added `className` prop support to the following components:
      - `CardWrapper`
      - `DisparityBarChartCard`
      - `MapCard`
      - `RateTrendsChartCard`
      - `ShareTrendsChartCard`
      - Various BarChart cards
      - `TableCard`
    - Updated:
      - `AltTableView`
      - `BarChart` components
      - `Breakdown Summary`
      - `Custom 100k BarChart`
      - `CustomAltTable`
      - `Custom Choropleth Map`
      - `Disparity Bar Chart`
      - `RateTrendsChartCard`
      - `ShareTrendsChartCard`
      - `SimpleBarChartCard`
      - `SimpleHorizontalBarChart`
      - `TableCard`
      - `Unknown Map Title`
    - General linting and code quality improvements on `AltTableView` and
    `CustomAltTable`
    
    ## Has this been tested? How?
    tests passing
    
    ## Screenshots (if appropriate)
    
    ## Types of changes
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎
    
    ---------
    
    Co-authored-by: Ben Hammond <benjamin.hammond@gmail.com>

--- frontend/src/reports/CustomDisparityBarChart.tsx
+++ frontend/src/reports/CustomDisparityBarChart.tsx
@@ -9,6 +9,7 @@ interface CustomDisparityBarChartProps {
   dataTypeConfig?: DataTypeConfig
   demographicType?: DemographicType
   reportTitle?: string
+  className?: string
 }
 
 const CustomDisparityBarChart: React.FC<CustomDisparityBarChartProps> = ({
@@ -16,16 +17,16 @@ const CustomDisparityBarChart: React.FC<CustomDisparityBarChartProps> = ({
   dataTypeConfig = METRIC_CONFIG['health_insurance'][0],
   demographicType = 'sex',
   reportTitle = 'Uninsurance in Florida by Sex',
+  className,
 }) => {
   return (
-    <div>
-      <DisparityBarChartCard
-        dataTypeConfig={dataTypeConfig}
-        demographicType={demographicType}
-        fips={fips}
-        reportTitle={reportTitle}
-      />
-    </div>
+    <DisparityBarChartCard
+      dataTypeConfig={dataTypeConfig}
+      demographicType={demographicType}
+      fips={fips}
+      reportTitle={reportTitle}
+      className={className}
+    />
   )
 }
 
