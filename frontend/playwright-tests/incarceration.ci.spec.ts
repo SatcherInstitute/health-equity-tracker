4aef75cb6f90eaeed8f6c23955e9d01ae4b7caf0
Ben Hammond <benjamin.hammond@gmail.com>
2024-09-16 16:43:19 -0600

    fix up test

--- frontend/playwright-tests/incarceration.ci.spec.ts
+++ frontend/playwright-tests/incarceration.ci.spec.ts
@@ -3,21 +3,27 @@ import { test } from '@playwright/test'
 test('Prison by Race', async ({ page }) => {
   await page.goto('/exploredata?mls=1.incarceration-3.00&group1=All')
   await page
-    .getByLabel(
-      'Launch multiple maps view with side-by-side maps of each race and ethnicity group'
-    )
+    .locator('#rate-map')
+    .getByRole('heading', { name: 'Prison incarceration in the' })
     .click()
-  await page.getByRole('button', { name: 'Close' }).click()
-  await page.getByLabel('open the topic info modal').click()
-  await page.getByRole('heading', { name: 'People in prison' }).click()
+  await page.getByLabel('Click for more info on people').click()
   await page.getByLabel('close topic info modal').click()
+  await page.locator('li').filter({ hasText: 'United States' }).click()
+  await page
+    .getByRole('heading', { name: 'Graph unavailable: Rates of' })
+    .click()
+  await page.getByText('Our data sources do not have').click()
   await page
     .locator('#rate-chart')
-    .getByText(
-      '3,232 children confined in adult facilities in the United States. Learn more.'
-    )
+    .getByRole('heading', { name: 'Prison incarceration in the' })
     .click()
-
+  await page.locator('#rate-chart').getByText('children').click()
+  await page.getByRole('button', { name: 'Unknown demographic map' }).click()
+  await page
+    .locator('#unknown-demographic-map')
+    .getByText('0.4% of prison pop. reported')
+    .click()
+  await page.getByRole('button', { name: 'Data table' }).click()
   await page.getByRole('columnheader', { name: 'Race and Ethnicity' }).click()
   await page.getByRole('button', { name: 'Prison', exact: true }).click()
   await page.getByRole('button', { name: 'Jail' }).click()
@@ -34,6 +40,8 @@ test('Jail by Age', async ({ page }) => {
   await page.getByLabel('Click for more info on people in jail').click()
   await page.getByRole('heading', { name: 'People in jail' }).click()
   await page.getByLabel('close topic info modal').click()
+  await page.getByRole('button', { name: 'Data table' }).click()
+
   await page.getByRole('columnheader', { name: 'Age' }).click()
   await page
     .getByRole('columnheader', { name: 'People in jail per 100k' })
