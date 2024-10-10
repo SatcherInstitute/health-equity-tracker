abdee16661620c378941a3e832c2a608e74ca2fe
Ben Hammond <benjamin.hammond@gmail.com>
2024-10-09 22:11:34 -0600

    tweaks intersectional all

--- frontend/src/charts/RateBarChart.tsx
+++ frontend/src/charts/RateBarChart.tsx
@@ -1,7 +1,7 @@
 import { max, scaleBand, scaleLinear } from 'd3'
 import { useMemo } from 'react'
 import type { MetricConfig } from '../data/config/MetricConfigTypes'
-import { isPctType } from '../data/config/MetricConfigUtils'
+import { isPctType, isRateType } from '../data/config/MetricConfigUtils'
 import {
   DEMOGRAPHIC_DISPLAY_TYPES_LOWER_CASE,
   type DemographicType,
@@ -9,17 +9,19 @@ import {
 import { sortForVegaByIncome } from '../data/sorting/IncomeSorterStrategy'
 import type { HetRow } from '../data/utils/DatasetTypes'
 import type { Fips } from '../data/utils/Fips'
+import { useIsBreakpointAndUp } from '../utils/hooks/useIsBreakpointAndUp'
 import { useResponsiveWidth } from '../utils/hooks/useResponsiveWidth'
 import { addLineBreakDelimitersToField, addMetricDisplayColumn } from './utils'
 
 // Constants
 const BAR_PADDING = 0.2
 const LABEL_SWAP_CUTOFF_PERCENT = 66
-const MAX_LABEL_WIDTH = 100
+const MAX_LABEL_WIDTH_BIG = 100
+const MAX_LABEL_WIDTH_SMALL = 50
 const CORNER_RADIUS = 4
-const MARGIN = { top: 20, right: 20, bottom: 50, left: MAX_LABEL_WIDTH + 40 }
-const BAR_HEIGHT = 60 // Base height for single line
-const EXTRA_SPACE_AFTER_ALL = 10 // Adjust this value to change the gap after "All"
+const MARGIN = { top: 20, right: 20, bottom: 50, left: 0 }
+const BAR_HEIGHT = 70
+const EXTRA_SPACE_AFTER_ALL = 10
 
 interface RateBarChartProps {
   data: HetRow[]
@@ -35,10 +37,26 @@ interface RateBarChartProps {
 
 export function RateBarChart({
   data,
+  fips,
   metricConfig,
   demographicType,
+  useIntersectionalComparisonAlls,
+  comparisonAllSubGroup,
 }: RateBarChartProps) {
+  const smallerDemographicLabelTypes: DemographicType[] = [
+    'sex',
+    'age',
+    'insurance_status',
+  ]
+
   const [containerRef, width] = useResponsiveWidth()
+
+  const numTicks = getNumTicks(width)
+
+  const maxLabelWidth = smallerDemographicLabelTypes.includes(demographicType)
+    ? MAX_LABEL_WIDTH_SMALL
+    : MAX_LABEL_WIDTH_BIG
+  MARGIN.left = maxLabelWidth + 40
 
   // Data preprocessing with spacing calculation
   const processedData: HetRow[] = useMemo(() => {
@@ -68,17 +86,13 @@ export function RateBarChart({
   const wrappedLabels = useMemo(() => {
     return processedData.map((d) => ({
       original: d[demographicType],
-      lines: wrapLabel(d[demographicType], MAX_LABEL_WIDTH),
+      lines: wrapLabel(d[demographicType], maxLabelWidth),
     }))
   }, [processedData, demographicType])
 
-  // Calculate dimensions
-  const maxLines = Math.max(...wrappedLabels.map((label) => label.lines.length))
-  const adjustedBarHeight = Math.max(BAR_HEIGHT, maxLines * 16)
   const allIndex = processedData.findIndex((d) => d[demographicType] === 'All')
   const totalExtraSpace = allIndex !== -1 ? EXTRA_SPACE_AFTER_ALL : 0
-  const height =
-    processedData.length * (adjustedBarHeight + 10) + totalExtraSpace
+  const height = processedData.length * (BAR_HEIGHT + 10) + totalExtraSpace
   const innerWidth = width - MARGIN.left - MARGIN.right
   const innerHeight = height - MARGIN.top - MARGIN.bottom
 
@@ -115,7 +129,7 @@ export function RateBarChart({
         <g transform={`translate(${MARGIN.left},${MARGIN.top})`}>
           {/* Vertical Gridlines */}
           <g className='gridlines'>
-            {xScale.ticks(20).map((tick) => (
+            {xScale.ticks(numTicks).map((tick) => (
               <line
                 key={`gridline-${tick}`}
                 x1={xScale(tick)}
@@ -126,7 +140,7 @@ export function RateBarChart({
               />
             ))}
           </g>
-          {/* Y-axis label */}
+          {/* Y-axis DemographicType label */}
           <text
             transform={`translate(${-MARGIN.left + 20},${innerHeight / 2}) rotate(-90)`}
             textAnchor='middle'
@@ -137,6 +151,12 @@ export function RateBarChart({
           {/* Y Axis */}
           <g className='y-axis'>
             {wrappedLabels.map((label, index) => {
+              if (label.original === 'All' && useIntersectionalComparisonAlls) {
+                label.lines = getComparisonAllSubGroupLines(
+                  fips,
+                  comparisonAllSubGroup,
+                )
+              }
               const yPosition = getYPosition(index, label.original)
               return (
                 <g key={label.original} transform={`translate(0,${yPosition})`}>
@@ -147,7 +167,7 @@ export function RateBarChart({
                       y={
                         yScale.bandwidth() / 2 -
                         (label.lines.length - 1) * 8 +
-                        lineIndex * 16
+                        lineIndex * 12
                       }
                       dy='.32em'
                       textAnchor='end'
@@ -167,6 +187,11 @@ export function RateBarChart({
             const shouldLabelBeInside =
               d[metricConfig.metricId] > barLabelBreakpoint
             const yPosition = getYPosition(index, d[demographicType])
+
+            const barLabelColor =
+              shouldLabelBeInside && d[demographicType] !== 'All'
+                ? 'fill-white'
+                : 'fill-current'
 
             return (
               <g
@@ -189,14 +214,13 @@ export function RateBarChart({
                       : 'fill-altGreen'
                   }
                 />
+                {/* Bar Label */}
                 <text
                   x={shouldLabelBeInside ? barWidth - 5 : barWidth + 5}
                   y={yScale.bandwidth() / 2}
-                  dy='.32em'
+                  dy='1.3em'
                   textAnchor={shouldLabelBeInside ? 'end' : 'start'}
-                  className={`text-smallest ${
-                    shouldLabelBeInside ? 'fill-white' : 'fill-current'
-                  }`}
+                  className={`text-smallest ${barLabelColor}`}
                 >
                   {formatValue(d[metricConfig.metricId], metricConfig)}
                 </text>
@@ -259,19 +283,58 @@ function wrapLabel(text: string, width: number): string[] {
 }
 
 function formatValue(value: number, metricConfig: MetricConfig): string {
+  // set maxFractionDigits to:
+  // 0 if number isRateType() and over 10
+  // 1 if number isRateType() and under 10 and over 1
+  // 2 if number isRateType() and under 1
+  // 1 if number isPctType()
+
+  let maxFractionDigits = 1
+  if (isRateType(metricConfig.type)) {
+    if (value > 10) maxFractionDigits = 0
+    else if (value > 1) maxFractionDigits = 1
+    else if (value > 0.1) maxFractionDigits = 2
+  }
+
   if (metricConfig.type === 'per100k')
     return (
       Math.round(value).toLocaleString('en-US', {
-        maximumFractionDigits: 1,
+        maximumFractionDigits: maxFractionDigits,
       }) + ' per 100k'
     )
 
   if (isPctType(metricConfig.type))
     return (
       value.toLocaleString('en-US', {
-        maximumFractionDigits: 2,
-      }) + ' %'
+        maximumFractionDigits: maxFractionDigits,
+      }) + '%'
     )
 
   return value.toLocaleString('en-US')
+}
+
+function getNumTicks(width: number): number {
+  const isSmMd = useIsBreakpointAndUp('smMd')
+  const isCompareMode = window.location.href.includes('compare')
+  let numTicks = Math.floor(width / 40)
+  if (isCompareMode || !isSmMd) {
+    numTicks = Math.max(Math.floor(numTicks / 1.5), 5)
+  }
+  return numTicks
+}
+
+function getComparisonAllSubGroupLines(
+  fips: Fips,
+  comparisonAllSubGroup?: string,
+) {
+  const lines: string[] = [
+    fips.getUppercaseFipsTypeDisplayName() || '',
+    'Average',
+    'All People',
+  ]
+
+  if (comparisonAllSubGroup) {
+    lines.push(comparisonAllSubGroup)
+  }
+  return lines
 }
