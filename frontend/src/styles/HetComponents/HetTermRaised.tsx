5b7e38a39c5d928c92319f3064c203df46e4306d
kccrtv <67879386+kccrtv@users.noreply.github.com>
2024-10-16 09:38:59 -0600

    adds het term raised cards for health equity, pdoh, and sdoh definitions (#3723)
    # Description and Motivation
    Ongoing UI updates to match brand refresh. 
    
    ## Has this been tested? How?
    tests passing
    
    ## Screenshots 
    > ### Old
    > ![Screenshot 2024-10-15 at 3 01
    35 PM](https://github.com/user-attachments/assets/dd2acf5f-d17c-4a31-89a0-b02cc44085a1)
    > ### Old Mobile
    > ![Screenshot 2024-10-15 at 3 01
    02 PM](https://github.com/user-attachments/assets/215a2da4-3273-434a-b8cd-d8a9d300252b)
    
    <hr/>
    
    > ### New
    > ![Screenshot 2024-10-15 at 3 00
    13 PM](https://github.com/user-attachments/assets/26c01fee-c6d0-424c-babb-aecbdd13080f)
    > ### New Mobile
    > ![Screenshot 2024-10-15 at 3 00
    48 PM](https://github.com/user-attachments/assets/e9f00484-c766-4060-92e7-ff05e7825d82)
    
    
    ## Types of changes
    - Refactor / chore
    
    ## New frontend preview link is below in the Netlify comment 😎

---
+++ frontend/src/styles/HetComponents/HetTermRaised.tsx
@@ -0,0 +1,84 @@
+import HetTerm from './HetTerm'
+import HetTermUnderline from './HetTermUnderline'
+
+interface HetTermRaisedProps {
+  className?: string
+  term: string
+  termType?: string
+  emphasizedText?: string
+  description: string
+  emphasizedTextPosition?: 'start' | 'middle' | 'end'
+  source?: string
+}
+
+export const HetTermRaised: React.FC<HetTermRaisedProps> = ({
+  className = '',
+  term,
+  termType,
+  emphasizedText,
+  description,
+  emphasizedTextPosition = 'start',
+  source,
+}) => {
+  const getFormattedDescription = () => {
+    if (!emphasizedText) return description
+
+    switch (emphasizedTextPosition) {
+      case 'start':
+        return (
+          <>
+            <HetTermUnderline className='text-black'>
+              {emphasizedText}
+            </HetTermUnderline>{' '}
+            {description.replace(emphasizedText, '').trim()}
+          </>
+        )
+      case 'middle': {
+        const middleIndex = description.indexOf(emphasizedText)
+        if (middleIndex !== -1) {
+          const beforeText = description.substring(0, middleIndex).trim()
+          const afterText = description
+            .substring(middleIndex + emphasizedText.length)
+            .trim()
+          return (
+            <>
+              {beforeText}{' '}
+              <HetTermUnderline className='text-black'>
+                {emphasizedText}
+              </HetTermUnderline>{' '}
+              {afterText}
+            </>
+          )
+        }
+        return description
+      }
+      case 'end':
+        return (
+          <>
+            {description.replace(emphasizedText, '').trim()}{' '}
+            <HetTermUnderline className='text-black'>
+              {emphasizedText}
+            </HetTermUnderline>
+          </>
+        )
+      default:
+        return description
+    }
+  }
+
+  return (
+    <article
+      className={`rounded-md border border-solid border-methodologyGreen shadow-raised-tighter bg-white p-4 group my-0 fade-in-up-blur ${className}`}
+    >
+      {source && (
+        <span className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1'>
+          {source}
+        </span>
+      )}
+      <p>
+        <HetTerm>{term}</HetTerm> <em>({termType})</em>:{' '}
+        {getFormattedDescription()}
+      </p>
+    </article>
+  )
+}
