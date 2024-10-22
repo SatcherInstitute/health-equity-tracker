67864816a8c582ef907697029e210eb3ca6e6739
kccrtv <67879386+kccrtv@users.noreply.github.com>
2024-10-22 13:08:38 -0600

    Refactor WIHE Page with Carousel and Updated Navigation (#3725)

--- frontend/playwright-tests/wihe.ci.spec.ts
+++ frontend/playwright-tests/wihe.ci.spec.ts
@@ -1,14 +1,38 @@
 import { test, expect } from '@playwright/test'
 import AxeBuilder from '@axe-core/playwright'
 
-test('WIHE Page Loads', async ({ page }) => {
+test('WIHE Page loads', async ({ page }) => {
   await page.goto('/whatishealthequity', { waitUntil: 'commit' })
-  await page
-    .getByRole('heading', { name: 'What is Health Equity?', level: 1 })
-    .click()
 
+  await expect(page).toHaveTitle(
+    'What is Health Equity? - Health Equity Tracker',
+  )
+
+  const heading = await page.getByRole('heading', {
+    name: 'What is Health Equity?',
+    level: 1,
+  })
+  await expect(heading).toBeVisible()
+})
+
+test('Default Guides tab is loaded on /whatishealthequity', async ({
+  page,
+}) => {
+  await page.goto('/whatishealthequity', { waitUntil: 'commit' })
+
+  const guidesTab = await page.getByRole('button', {
+    name: 'Data Visualization Guides',
+  })
+  await expect(guidesTab).toHaveText('Data Visualization Guides')
+})
+
+test('WIHE Page Loads and Accessibility Scan', async ({ page }) => {
+  await page.goto('/whatishealthequity', { waitUntil: 'commit' })
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .exclude('iframe') // YouTube embed is not fully accessible
+    .disableRules(['landmark-one-main', 'page-has-heading-one'])
+    .exclude('.text-tinyTag')
+    .exclude('.shadow-raised-tighter')
     .analyze()
+
   expect(accessibilityScanResults.violations).toEqual([])
 })
