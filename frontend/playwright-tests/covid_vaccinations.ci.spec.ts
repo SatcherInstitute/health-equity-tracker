3e20e91468b54eb349bede705f1f967d7f54b124
Eric M Warren II <eric.m.warren1@gmail.com>
2024-10-30 09:59:56 -0400

    Revert "fix: E2E COVID tracker test (#3737)"
    This reverts commit 14aee0d1ec92790862f090422fc315ef3db0d715.

--- frontend/playwright-tests/covid_vaccinations.ci.spec.ts
+++ frontend/playwright-tests/covid_vaccinations.ci.spec.ts
@@ -1,34 +1,36 @@
-import AxeBuilder from '@axe-core/playwright'
-import { expect, test } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
-test.describe.configure({ mode: 'parallel' })
+test.describe.configure({ mode: 'parallel' });
 
 test.describe('Home to COVID Vax by Age', () => {
-  test('Tracker Default to Covid Vax', async ({ page }) => {
-    // Load Tracker Default helper view
-    await page.goto(`/exploredata`, { waitUntil: 'commit' })
 
-    // Stop the pulsing button so we can target it
-    await page.emulateMedia({ reducedMotion: 'reduce' })
 
-    // Choose VAXX from the no topic screen
-    const madLibTopic = await page.waitForSelector(
-      'button:visible:has-text("select a topic")',
-    )
-    await madLibTopic.click()
-    const covidVaxOption = await page.waitForSelector(
-      'span:has-text("COVID-19 Vaccinations")',
-    )
-    await covidVaxOption.click()
-    await expect(page).toHaveURL(/.*mls=1.covid_vaccinations-3.00/)
+    test('Tracker Default to Covid Vax', async ({ page }) => {
 
-    // Back button works properly for madlib condition changes
-    await page.goBack()
-    await expect(page).not.toHaveURL(/.*mls=1.covid_vaccinations-3.00/)
+        // Load Tracker Default helper view
+        await page.goto(`/exploredata`, { waitUntil: "commit" });
 
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .exclude('iframe')
-      .analyze()
-    expect(accessibilityScanResults.violations).toEqual([])
-  })
-})
+        // stop the pulsing button so we can target it
+        await page.emulateMedia({ reducedMotion: "reduce" });
+
+        // choose VAXX from the no topic screen
+        const madLibTopic = page.locator('button:visible:has-text("select a topic")')
+        madLibTopic.click();
+        const covidVaxOption = page.locator('span:has-text("COVID-19 Vaccinations")')
+        covidVaxOption.click();
+        await expect(page).toHaveURL(/.*mls=1.covid_vaccinations-3.00/);
+
+        // back button works properly for madlib condition changes
+        await page.goBack()
+        await expect(page).not.toHaveURL(/.*mls=1.covid_vaccinations-3.00/);
+
+        const accessibilityScanResults = await new AxeBuilder({ page })
+            .exclude('iframe') // YouTube embed is not fully accessible
+            .analyze();
+        expect(accessibilityScanResults.violations).toEqual([]);
+
+    })
+
+
+});
