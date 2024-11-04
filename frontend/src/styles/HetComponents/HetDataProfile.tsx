0f067e133d48d2301bd7f081dbfffc6888b9f4d9
kccrtv <kimcincocreative@gmail.com>
2024-11-04 10:03:31 -0700

    responsive

--- frontend/src/styles/HetComponents/HetDataProfile.tsx
+++ frontend/src/styles/HetComponents/HetDataProfile.tsx
@@ -19,6 +19,7 @@ import type { DatasetIdWithStateFIPSCode } from '../../data/config/DatasetMetada
 import { ListItem } from '@mui/material'
 import downloadDataset from '../../pages/DataCatalog/downloadDataset'
 import HetLinkButton from './HetLinkButton'
+import { SaveAlt, SaveAltRounded } from '@mui/icons-material'
 
 type LoadStatus = 'loading' | 'unloaded' | 'error' | 'loaded'
 
@@ -108,68 +109,83 @@ export function HetDataProfile({
   const [dialogIsOpen, setDialogIsOpen] = useState(false)
 
   return (
-    <article className='rounded-md border border-solid border-altGreen shadow-raised-tighter bg-white px-20 pb-10 pt-14 group my-8 text-left hover:shadow-raised group transition-all duration-300 ease-in-out'>
-      <h2 className='my-0 text-smallestHeader font-medium leading-lhSomeMoreSpace'>
+    <article className='rounded-md border border-solid border-altGreen shadow-raised-tighter bg-white p-12 md:px-20 md:pb-10 md:pt-14 group my-8 text-left hover:shadow-raised group transition-all duration-300 ease-in-out'>
+      <h2 className='my-0 text-title md:text-smallestHeader font-bold md:font-medium leading-lhSomeMoreSpace'>
         {name}
       </h2>
 
-      <div className='flex md:flex-col my-8'>
+      <div className='flex flex-col my-4 md:my-8 md:gap-1'>
         {timePeriodRange && (
-          <div className='flex gap-4'>
-            <p className='flex my-0 text-altGreen font-semibold min-w-1/3 w-1/3'>
+          <div className='flex md:flex-row flex-col justify-start items-center gap-1 md:gap-4 mb-2 text-small'>
+            <p className='my-0 text-altGreen font-semibold w-full md:min-w-1/3 md:w-1/3 leading-lhNormal'>
               Time-Series Range
             </p>
-            <p className='flex my-0 w-full max-w-2/3'>{timePeriodRange}</p>
+            <p className='pl-2 my-0 ml-auto w-full md:w-7/12 md:max-w-2/3 leading-lhNormal'>
+              {timePeriodRange}
+            </p>
           </div>
         )}
 
-        <div className='flex gap-4'>
-          <p className='flex my-0 text-altGreen font-semibold min-w-1/3 w-1/3'>
+        <div className='flex md:flex-row flex-col justify-start items-center gap-1 md:gap-4 mb-2 text-small'>
+          <p className='my-0 text-altGreen font-semibold w-full md:min-w-1/3 md:w-1/3 leading-lhNormal'>
             Geographic Level
           </p>
-          <p className='flex my-0 w-full max-w-2/3'>{geographicLevel}</p>
+          <p className='pl-2 my-0 ml-auto w-full md:w-7/12 md:max-w-2/3 leading-lhNormal'>
+            {geographicLevel}
+          </p>
         </div>
-        <div className='flex gap-4'>
-          <p className='flex my-0 text-altGreen font-semibold min-w-1/3 w-1/3'>
+        <div className='flex md:flex-row flex-col justify-start items-center gap-1 md:gap-4 mb-2 text-small'>
+          <p className='my-0 text-altGreen font-semibold w-full md:min-w-1/3 md:w-1/3 leading-lhNormal'>
             Demographic Granularity
           </p>
-          <p className='flex my-0 w-full max-w-2/3'>{demographicGranularity}</p>
+          <p className='pl-2 my-0 ml-auto w-full md:w-7/12 md:max-w-2/3 leading-lhNormal'>
+            {demographicGranularity}
+          </p>
         </div>
-        <div className='flex gap-4'>
-          <p className='flex my-0 text-altGreen font-semibold min-w-1/3 w-1/3'>
+        <div className='flex md:flex-row flex-col justify-start items-center gap-1 md:gap-4 mb-2 text-small'>
+          <p className='my-0 text-altGreen font-semibold w-full md:min-w-1/3 md:w-1/3 leading-lhNormal'>
             Update Frequency
           </p>
-          <p className='flex my-0 w-full max-w-2/3'>{updateFrequency}</p>
+          <p className='pl-2 my-0 ml-auto w-full md:w-7/12 md:max-w-2/3 leading-lhNormal'>
+            {updateFrequency}
+          </p>
         </div>
-        <div className='flex gap-4'>
-          <p className='flex my-0 text-altGreen font-semibold min-w-1/3 w-1/3'>
+        <div className='flex md:flex-row flex-col justify-start items-center gap-1 md:gap-4 mb-2 text-small'>
+          <p className='my-0 text-altGreen font-semibold w-full md:min-w-1/3 md:w-1/3 leading-lhNormal'>
             Source Website
           </p>
           <a
             href={prettySiteName}
             aria-label={`Link to ${name}`}
-            className='flex my-0 no-underline w-full max-w-2/3'
+            className='pl-2 my-0 ml-auto w-full md:w-7/12 md:max-w-2/3 leading-lhNormal no-underline'
           >
             {prettySiteName}
           </a>
         </div>
       </div>
-      <p className='leading-lhSomeSpace my-4'>{description}</p>
-      {downloadable && (
-        <div className='mt-8 text-left'>
+      <p className='leading-lhSomeSpace my-0 md:my-4 text-small md:text-text'>
+        {description}
+      </p>
+      <div className='flex lg:flex-row lg:justify-start lg:items-center mt-8 lg:gap-4 flex-col-reverse gap-2'>
+        {downloadableDataDictionary && (
+          <HetLinkButton
+            href='/data_dictionaries/medicare_population.csv'
+            className='font-bold py-0 pl-0 pr-0'
+            buttonClassName='w-auto md:w-1/2 mx-auto lg:w-auto lg:mr-2 lg:ml-0'
+          >
+            <SaveAlt className='pr-2' />
+            Download data dictionary
+          </HetLinkButton>
+        )}
+        {downloadable && (
           <HetButtonSecondary
+            className='lg:mr-auto lg:ml-0 mx-auto'
             text='View downloadable tables'
             onClick={() => setDialogIsOpen(true)}
             href={''}
           />
-        </div>
-      )}
-      {downloadableDataDictionary && (
-        <HetLinkButton href='/data_dictionaries/medicare_population.csv'>
-          Download data dictionary
-        </HetLinkButton>
-      )}
-
+        )}
+      </div>
       <Dialog onClose={() => setDialogIsOpen(false)} open={dialogIsOpen}>
         <DialogTitle className='flex justify-between'>
           <header className='flex w-8/12 sm:w-10/12'>
