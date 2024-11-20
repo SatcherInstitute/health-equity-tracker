f19ef648dfcc401e0393ea5a000a496be4575ead
Eric M Warren II <eric.m.warren1@gmail.com>
2024-11-20 17:18:14 -0500

    Small refactor to InsightDisplay (#3816)
    Small refactor to InsightDisplay (#3816)

--- frontend/src/cards/ui/InsightDisplay.tsx
+++ frontend/src/cards/ui/InsightDisplay.tsx
@@ -2,20 +2,59 @@ import { DeleteForever, TipsAndUpdatesOutlined } from '@mui/icons-material'
 import CircularProgress from '@mui/material/CircularProgress'
 import IconButton from '@mui/material/IconButton'
 import type React from 'react'
+import { useState } from 'react'
+import type {
+  MetricConfig,
+  MetricId,
+} from '../../data/config/MetricConfigTypes'
+import type { DemographicType } from '../../data/query/Breakdowns'
+import type { MetricQueryResponse } from '../../data/query/MetricQuery'
+import { splitIntoKnownsAndUnknowns } from '../../data/utils/datasetutils'
+import { generateInsight } from '../generateInsights'
+
+export const SHOW_INSIGHT_GENERATION = import.meta.env
+  .VITE_SHOW_INSIGHT_GENERATION
+
+export interface ChartData {
+  knownData: Readonly<Record<string, any>>[]
+  metricIds: MetricId[]
+}
 
 type InsightDisplayProps = {
-  insight: string
-  handleGenerateInsight: () => void
-  handleClearInsight: () => void
-  isGeneratingInsight: boolean
+  demographicType: DemographicType
+  metricIds: MetricId[]
+  queryResponses: MetricQueryResponse[]
+  shareConfig: MetricConfig
 }
 
 const InsightDisplay: React.FC<InsightDisplayProps> = ({
-  insight,
-  handleGenerateInsight,
-  handleClearInsight,
-  isGeneratingInsight,
+  queryResponses,
+  shareConfig,
+  demographicType,
+  metricIds,
 }) => {
+  const [insight, setInsight] = useState<string>('')
+  const [isGeneratingInsight, setIsGeneratingInsight] = useState<boolean>(false)
+
+  const queryResponse = queryResponses[0]
+  const validData = queryResponse.getValidRowsForField(shareConfig.metricId)
+  const [knownData] = splitIntoKnownsAndUnknowns(validData, demographicType)
+
+  const handleGenerateInsight = async () => {
+    if (!SHOW_INSIGHT_GENERATION || !knownData.length || !metricIds.length)
+      return
+
+    setIsGeneratingInsight(true)
+    try {
+      const newInsight = await generateInsight({ knownData, metricIds })
+      setInsight(newInsight)
+    } finally {
+      setIsGeneratingInsight(false)
+    }
+  }
+
+  const handleClearInsight = () => setInsight('')
+
   return (
     <>
       <IconButton
