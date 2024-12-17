089a6fe9c6829e5c7fd00dc5d4c546fc1bc8b885
kccrtv <kimcincocreative@gmail.com>
2024-12-17 12:29:32 -0600

    revert hettags

--- frontend/src/styles/HetComponents/HetTags.tsx
+++ frontend/src/styles/HetComponents/HetTags.tsx
@@ -4,21 +4,26 @@ interface Tag {
 }
 
 interface HetTagsProps {
-  tags: Tag[]
+  tags: string[] | Tag[]
 }
 
 export const HetTags: React.FC<HetTagsProps> = ({ tags }) => {
   return (
     <div className='md:flex md:flex-wrap mt-2'>
-      {tags.map(({ name }) => (
-        <span
-          key={name}
-          aria-label={name}
-          className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1 no-underline'
-        >
-          {name}
-        </span>
-      ))}
+      {tags.map((tag) => {
+        // Ensure backward compatibility: If `tag` is a string, treat it as a name.
+        const tagName = typeof tag === 'string' ? tag : tag.name
+
+        return (
+          <span
+            key={tagName}
+            aria-label={tagName}
+            className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1 no-underline hover:bg-hoverTinyTagGray'
+          >
+            {tagName}
+          </span>
+        )
+      })}
     </div>
   )
 }
