2457eb72046536b6256ba607155afe58c7cf1073
kccrtv <kimcincocreative@gmail.com>
2024-12-17 10:45:36 -0600

    rm het tag linking, adds hover state, updates nvm to resolve biome module not found error

--- frontend/src/styles/HetComponents/HetTags.tsx
+++ frontend/src/styles/HetComponents/HetTags.tsx
@@ -1,46 +1,23 @@
-import type React from 'react'
-import { Link } from 'react-router-dom'
-
 interface Tag {
   name: string
   link?: string
 }
 
 interface HetTagsProps {
-  tags: Tag[] | string[]
+  tags: string[] | Tag[]
 }
 
-interface HetTagsProps {
-  tags: Tag[] | string[]
-  containerClassName?: string
-  linkClassName?: string
-}
-
-export const HetTags: React.FC<HetTagsProps> = ({
-  tags,
-  containerClassName,
-  linkClassName,
-}) => {
+export const HetTags: React.FC<HetTagsProps> = ({ tags }) => {
   return (
     <div className='md:flex md:flex-wrap mt-2'>
       {tags.map((tag) => {
         const tagName = typeof tag === 'string' ? tag : tag.name
-        const tagLink = typeof tag === 'string' ? undefined : tag.link
 
-        return tagLink ? (
-          <Link
-            to={tagLink}
-            key={tagName}
-            aria-label={tagName}
-            className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1 no-underline'
-          >
-            {tagName}
-          </Link>
-        ) : (
+        return (
           <span
             key={tagName}
             aria-label={tagName}
-            className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1 no-underline'
+            className='text-tinyTag uppercase text-black font-sansTitle font-bold bg-tinyTagGray rounded-sm py-1 px-2 mr-2 mt-1 no-underline hover:bg-hoverTinyTagGray'
           >
             {tagName}
           </span>
