74203ac42ae716a6c50f323ce4cb04f9bb66f4d9
Ben Hammond <benjamin.hammond@gmail.com>
2025-01-09 10:33:20 -0700

    fail console log

--- frontend/src/pages/WhatIsHealthEquity/wiheSections/GuidesTab.tsx
+++ frontend/src/pages/WhatIsHealthEquity/wiheSections/GuidesTab.tsx
@@ -2,6 +2,8 @@ import HetCarousel from '../../../styles/HetComponents/HetCarousel'
 import { HetCarouselCard } from '../../../styles/HetComponents/HetCarouselCard'
 import { guideMappings } from '../wiheContent/GuidesTabData'
 
+console.log('should fail')
+
 export default function GuidesTab() {
   return (
     <>
