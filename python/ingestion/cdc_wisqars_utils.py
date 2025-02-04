6a3ba0e9a2cde65ef9f91c692a59e7c26d179fc5
Ben Hammond 🎛️ <benjamin.hammond@gmail.com>
2025-02-03 17:15:01 -0700

    Pipeline: Update data handling for low case counts from WISQARS (#3946)
    # Description and Motivation
    <!--- bulleted, high level items. use keywords (eg "closes #144" or
    "fixes #4323") -->
    
    - allows source data that's marked as unstable (count less than 20 but
    at least 10 and resulting rates per 100k); previously this was being
    cast as `null` / No Data
    - allows source data that's marked as unstable but is 0 cases;
    previously came through as `null` / No Data
    - allows `null` values to be treated as zero when summing across
    conditions, but only where there is at least one non-null value. if all
    summed values are `null`, it will NOT result in `0` but in `null`
    instead
    - updates all golden data to expect this new data
    - adjusts `clean_numeric()` and unit tests
    - NOTE: we need to update methodology with this change for transparency
    
    
    ## Has this been tested? How?
    
    - tests passing (with new golden data)
    
    ## Types of changes
    
    (leave all that apply)
    
    - New content or feature
    
    ## New frontend preview link is below in the Netlify comment 😎

--- python/ingestion/cdc_wisqars_utils.py
+++ python/ingestion/cdc_wisqars_utils.py
@@ -13,7 +13,6 @@ Features include:
 
 from typing import List
 import pandas as pd
-import numpy as np
 from ingestion import standardized_columns as std_col, gcs_to_bq_util
 from ingestion.dataset_utils import generate_per_100k_col
 from ingestion.het_types import RATE_CALC_COLS_TYPE, WISQARS_VAR_TYPE, GEO_TYPE, WISQARS_DEMO_TYPE
@@ -58,13 +57,13 @@ RACE_NAMES_MAPPING = {
 def clean_numeric(val):
     """
     Function to clean numeric string values by removing commas and converting '**' to NaN.
-    Takes a single parameter 'val' and returns the cleaned value.
+    Takes a single parameter 'val' and returns the cleaned str value.
     """
     if isinstance(val, str):
         if "**" in val:
-            return np.nan
+            val = val.replace("**", "")
         if "," in val:
-            return val.replace(",", "")
+            val = val.replace(",", "")
     return val
 
 
