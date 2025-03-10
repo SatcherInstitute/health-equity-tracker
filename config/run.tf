b3d503d2160b0326460fac2dcb68ee089cab15cf
Ben Hammond <benjamin.hammond@gmail.com>
2025-03-10 16:53:57 -0600

    mem

--- config/run.tf
+++ config/run.tf
@@ -19,7 +19,7 @@ resource "google_cloud_run_service" "ingestion_service" {
 
         resources {
           limits = {
-            memory = "8G"
+            memory = "4G"
           }
         }
       }
