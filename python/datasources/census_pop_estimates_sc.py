7cc0fd9273c101619721e4dbb85557db83e1a92c
Ben Hammond <benjamin.hammond@gmail.com>
2025-03-17 21:13:43 -0600

    more

--- python/datasources/census_pop_estimates_sc.py
+++ python/datasources/census_pop_estimates_sc.py
@@ -58,7 +58,7 @@ class CensusPopEstimatesSC(DataSource):
 
                 demo = breakdown if breakdown != std_col.RACE_CATEGORY_ID_COL else std_col.RACE_OR_HISPANIC_COL
 
-                gcs_to_bq_util.add_df_to_bq(breakdown_df, dataset, f"by_{demo}_age_{geo}", column_types=col_types)
+                gcs_to_bq_util.add_df_to_bq(breakdown_df, dataset, f"multi_{demo}_age_{geo}", column_types=col_types)
 
 
 def generate_pop_data_18plus(df, breakdown, geo):
