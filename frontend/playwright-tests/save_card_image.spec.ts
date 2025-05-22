98aafb580807184c7b0fa3740777ec972356a5db
Ben Hammond <benjamin.hammond@gmail.com>
2025-05-22 14:04:34 -0600

    e2e working

--- frontend/playwright-tests/save_card_image.spec.ts
+++ frontend/playwright-tests/save_card_image.spec.ts
@@ -10,14 +10,28 @@ test('Built in card-option screenshot functionality', async ({ page }) => {
   await page.getByText('Copied Rate map Image to').click()
   await page.getByRole('img', { name: 'Preview of Rate map Image' }).click()
   await page.getByRole('button', { name: 'Close' }).click()
-  await page.getByRole('button', { name: 'Launch multiple maps view' }).click()
+})
+
+test('Built in multimap card-option screenshot functionality', async ({
+  page,
+}) => {
+  await page.goto(
+    '/exploredata?mls=1.hiv-3.12&dt1=hiv_deaths&group1=All&multiple-maps=true',
+  )
   await page.getByRole('button', { name: 'Card export options' }).click()
   await page.getByText('Copy Image To Clipboard').click()
   await page.getByText('Copied Image to clipboard!').click()
   await page.getByRole('img', { name: 'Preview of Image' }).click()
   await page.getByRole('button', { name: 'Close' }).click()
-  await page.getByRole('combobox', { name: 'Compare mode Off' }).click()
-  await page.getByRole('option', { name: 'Topics' }).click()
+})
+
+test('Built in compare mode card-option screenshot functionality', async ({
+  page,
+}) => {
+  await page.goto(
+    '/exploredata?mls=1.hiv-3.poverty-5.00&group1=All&mlp=comparevars',
+  )
+
   await page
     .locator('#rate-map2')
     .getByRole('button', { name: 'Card export options' })
