ab495a3dfcf408b8991a7c5b26e1b86abb749c1a
Ben Hammond <benjamin.hammond@gmail.com>
2025-12-10 11:57:36 -0700

    test(playwright): add gun homicide suppression test case

---
+++ frontend/playwright-tests/gun_homicides.ci.spec.ts
@@ -0,0 +1,7 @@
+import { test } from '@playwright/test'
+
+test('Gun Homicide Test: Data Suppressed', async ({ page }) => {
+  await page.goto('/exploredata?mls=1.gun_violence-3.01&group1=All')
+
+  await page.locator('#rate-map').getByText('Data suppressed').click()
+})
