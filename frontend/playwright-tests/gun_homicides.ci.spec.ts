02410ae666e97f5ea18293d1cc60dfd6077ba20c
Ben Hammond <benjamin.hammond@gmail.com>
2025-12-10 11:58:13 -0700

    test(gun_homicides): update test description for clarity

--- frontend/playwright-tests/gun_homicides.ci.spec.ts
+++ frontend/playwright-tests/gun_homicides.ci.spec.ts
@@ -1,7 +1,6 @@
 import { test } from '@playwright/test'
 
-test('Gun Homicide Test: Data Suppressed', async ({ page }) => {
+test('Gun Homicide Test: County Data Suppressed', async ({ page }) => {
   await page.goto('/exploredata?mls=1.gun_violence-3.01&group1=All')
-
   await page.locator('#rate-map').getByText('Data suppressed').click()
 })
