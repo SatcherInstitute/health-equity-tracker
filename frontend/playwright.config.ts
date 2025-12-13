6e056f17f70eb8d464184e703314261fe64eafa9
Ben Hammond <benjamin.hammond@gmail.com>
2025-12-12 17:31:05 -0700

    ci(frontend): update playwright test workers and timeout configuration

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -15,7 +15,7 @@ const config: PlaywrightTestConfig = {
   timeout: process.env.CI ? 300 * 1000 : 60 * 1000,
   /* Maximum time one "expect" can run for, default was 5 seconds and was too quick */
   expect: {
-    timeout: process.env.CI ? 90 * 1000 : 10 * 1000,
+    timeout: process.env.CI ? 60 * 1000 : 10 * 1000,
   },
   /* run all tests, even those within a shared file, in parallel  */
   fullyParallel: true,
