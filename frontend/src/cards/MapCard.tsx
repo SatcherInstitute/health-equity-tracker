21266f60df6a0f2325c96b8c7307c468682b707f
Ben Hammond <benjamin.hammond@gmail.com>
2026-09-15 12:14:22 -0600

    fix: avoid duplicate rate-map IDs by not setting ID in MapCard

--- frontend/src/cards/MapCard.tsx
+++ frontend/src/cards/MapCard.tsx
@@ -714,7 +714,6 @@ function MapCardWithKey(props: MapCardProps) {
 
                 <div className={mapIsWide ? 'sm:w-8/12 md:w-9/12' : 'w-full'}>
                   <div
-                    id={props.isCompareCard ? 'rate-map-2' : 'rate-map'}
                     className='w-full'
                     style={{ minHeight: preloadHeight * 0.3 }}
                   >
